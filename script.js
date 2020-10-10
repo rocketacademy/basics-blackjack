4ceef069b725d658d22e896c7933ed551be7720b
jeremylim91 <jeremylim_91@live.com>
2020-10-10 12:24:45 +0800

    added deck + shuffled deck

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
+var deck = [];
+
+// make a deck
+var makeDeck = function () {
+  // create the empty deck at the beginning
+
+  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // make a variable of the current suit
+    var currentSuit = suits[suitIndex];
+    console.log('current suit : ' + currentSuit);
+
+    // loop to create all cards in this suit
+    // rank 1-13
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      var cardName = rankCounter;
+
+      // 1, 11, 12 ,13
+      if (cardName == 1) {
+        cardName = 'ace';
+      } else if (cardName == 11) {
+        cardName = 'jack';
+      } else if (cardName == 12) {
+        cardName = 'queen';
+      } else if (cardName == 13) {
+        cardName = 'king';
+      }
+
+      // make a single card object variable
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      console.log('rank : ' + rankCounter);
+
+      // add the card to the deck
+      deck.push(card);
+
+      rankCounter = rankCounter + 1;
+    }
+    suitIndex = suitIndex + 1;
+  }
+
+  return deck;
+};
+
+// generate random Index
+var getRandomIndex = function (maxNumber) {
+  var randomDecimal = Math.random() * maxNumber;
+  var randomIndex = Math.floor(randomDecimal);
+  return randomIndex;
+};
+
+// shuffle the deck
+var shuffleCards = function (deck) {
+  for (var i = 0; i < 52; i++) {
+    var randomIndex = getRandomIndex(deck.length);
+    var currentItem = deck[i];
+    var randomItem = deck[randomIndex];
+    deck[i] = randomItem;
+    deck[randomIndex] = currentItem;
+  }
+  return deck;
+};
 var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
