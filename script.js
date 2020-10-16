a4667bfa6249eade2a614d95e4de81a2d71878b4
your-github-username <github-acct-email@example.com>
2020-10-16 15:13:41 +0800

    I changed auto drawing to player choose and add user name

--- script.js
+++ script.js
@@ -55,15 +55,24 @@ var shuffleCards = function (cards) {
   return cards;
 };
 var computeScore = function (hand) {
-  var totalScore = 0
+  var totalScore = 0;
+  var hasAce = false;
   for (index = 0; index < hand.length; index++) {
 
     // joker, queen and king will be 10 in the score
     if (hand[index].rank >= 11 && hand[index].rank <= 13) {
       totalScore = totalScore + 10;
+      // ace will be 11
+    } else if (hand[index].rank == 1) {
+      totalScore = totalScore + 11;
+      hasAce = true;
     } else {
       totalScore += hand[index].rank;
     }
+  }
+  //  if the player hand has ace and is > 21 change the ace card to 1
+  if (hasAce == true && totalScore > 21) {
+    totalScore = totalScore - 10;
   }
   return totalScore;
 }
@@ -71,8 +80,51 @@ var computerMustTake = function () {
   var newCard = deck.pop();
   return newCard;
 }
+var displayCards = function (hand) {
+  var nameOfCards = '';
+  for (i = 0; i < hand.length; i += 1) {
+    nameOfCards += hand[i].name + " ";
+  }
+  console.log(nameOfCards);
+  return nameOfCards;
+}
+
 var deck = shuffleCards(makeDeck());
-var gameState = '';
+// var deck = makeDeck();
+// var card1 = {
+//   rank: 9,
+//   suit: "heart",
+//   name: "jack",
+// };
+// var card2 = {
+//   rank: 9,
+//   suit: "heart",
+//   name: "jack",
+// };
+
+// var card3 = {
+//   rank: 9,
+//   suit: "heart",
+//   name: "jack",
+// };
+
+// var card4 = {
+//   rank: 9,
+//   suit: "heart",
+//   name: "jack",
+// };
+// var card5 = {
+//   rank: 1,
+//   suit: "heart",
+//   name: "ace",
+// };
+// deck.push(card5);
+// deck.push(card4);
+// deck.push(card3);
+// deck.push(card2);
+// deck.push(card1);
+var gameState = 'userName';
+var userName = '';
 var playerHand = [];
 var computerHand = [];
 var finish = false;
@@ -82,7 +134,11 @@ var playerScore = 0;
 var main = function (input) {
   var myOutputValue = '';
   //computer gets one face up and one face down cards
-  if (gameState == '') {
+  if (gameState == 'userName') {
+    userName = input;
+    gameState = 'bj'
+    myOutputValue = `Hello ${userName}. Please press submit to play`
+  } else if (gameState == 'bj') {
     var computerCardFaceDown = deck.pop();
     var computerCardFaceUp = deck.pop();
     //player gets two cards
@@ -105,54 +161,53 @@ var main = function (input) {
     console.log('current computer score', computerScore);
     console.log('player score', playerScore);
     //show cards to the player
-    myOutputValue = `Player drew ${playerCard1.name} & ${playerCard2.name}. <br>Computer drew a face down card and face up card ${computerCardFaceUp.name}. `
+    myOutputValue = `${userName} drew ${playerCard1.name} & ${playerCard2.name}. <br>Computer drew a face down card and face up card ${computerCardFaceUp.name}. `
     gameState = 'playerTurn';
   }
   if (gameState == 'playerTurn') {
     if (playerScore == 21 && playerHand.length == 2) {
       finish = true;
-      myOutputValue += 'player wins! player has bj';
-      myOutputValue += '<br>' + playerHand[0].name + ' of ' + playerHand[0].suit;
-      myOutputValue += '<br>' + playerHand[1].name + ' of ' + playerHand[1].suit;
+      myOutputValue += `<br><br>${userName} wins! ${userName} has bj`;
+      myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.`;
+      myOutputValue += `<br>${userName} has cards: ${displayCards(playerHand)}.`;
 
-    } else if (playerScore <= 21) {
-      while (playerScore < 17) {
-        var playerCard3 = deck.pop();
-        playerHand.push(playerCard3);
-        myOutputValue += `<br><br>You just drew another card: ${playerCard3.name}.`
-        playerScore = computeScore(playerHand);
-      }
+    } else if (playerScore < 17 && playerScore <= 21) {
+      myOutputValue += `<br>${userName}'s current score is ${playerScore}. <br>Please press 'h' for hit and 's' for stand!!`;
+      gameState = 'hitOrStand';
+    }
 
-      console.log('player score', playerScore);
+    console.log('player score', playerScore);
 
-      if (playerScore > 17 && playerScore <= 21) {
-        myOutputValue += `<br>Your current score is ${playerScore}. <br>Please press 'h' for hit and 's' for stand!!`;
-        gameState = 'hitOrStand';
-      } else {
-        myOutputValue += `<br>You bust as your score ${playerScore} is more than 21.`
-        gameState = 'computerTurn';
-      }
+    if (playerScore > 17 && playerScore <= 21) {
+      myOutputValue += `<br>${userName}'s current score is ${playerScore}. <br>Please press 'h' for hit and 's' for stand!!`;
+      gameState = 'hitOrStand';
+    } else if (playerScore > 21) {
+      myOutputValue += `<br>${userName} busts as your score ${playerScore} is more than 21.`;
+      myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.`;
+      myOutputValue += `<br>${userName} has cards: ${displayCards(playerHand)}.`;
+      gameState = 'computerTurn';
     }
   } else if (gameState == 'hitOrStand') {
     if (input == 'h') {
-      console.log('here');
+      console.log('hit');
       var playerCard4 = deck.pop();
       playerHand.push(playerCard4);
+      console.log('player card 4', playerCard4);
       playerScore = computeScore(playerHand);
-      myOutputValue = `You drew ${playerCard4.name}. <br> Your score is now: ${playerScore}.`
+      myOutputValue = `${userName} choose to hit! ${userName} drew ${playerCard4.name}. <br> ${userName}'s score is now: ${playerScore}.`;
 
       if (playerScore < 21) {
-        myOutputValue += `<br> Please choose to hit or stand.`
+        myOutputValue += `<br> Please choose to hit or stand.`;
       } else if (playerScore > 21) {
-        myOutputValue += `<br> You bust!`
+        myOutputValue += `<br> ${userName} busts! ${userName}'s score is now ${playerScore}`;
         gameState = 'computerTurn';
       } else {
-        myOutputValue += `<br>You should choose to stand as you have reached the best possible score of 21.`
+        myOutputValue += `<br>${userName}should choose to stand as you have reached the best possible score of 21.`;
       }
     }
     else if (input == 's') {
       playerScore = computeScore(playerHand);
-      myOutputValue = `You chose to stand and your score is now ${playerScore}. <br> Press submit for computer's turn.`
+      myOutputValue = ` ${userName} chooses to stand and ${userName}'s score is now ${playerScore}. <br> Press submit for computer's turn.`;
       gameState = 'computerTurn';
     } else {
       myOutputValue = `Please enter either 'h' or 's'.`
@@ -160,14 +215,14 @@ var main = function (input) {
     //tell player he cannot draw any more cards because he is bust.
   } else if (gameState == 'computerTurn') {
     computerScore = computeScore(computerHand);
-    myOutputValue = `Computer's current score is ${computerScore}. `
+    myOutputValue = `Computer's current score is ${computerScore}. `;
     console.log('current computer score', computerScore);
 
     while (computerScore < 17) {
       newCard = computerMustTake();
       computerHand.push(newCard);
       computerScore = computeScore(computerHand);
-      myOutputValue += `<br><br> Computer just drew another card ${newCard.name} and current score is ${computerScore}.`
+      myOutputValue += `<br><br> Computer just drew another card ${newCard.name} and current score is ${computerScore}.`;
     }
 
     if (computerScore <= 21) {
@@ -177,17 +232,12 @@ var main = function (input) {
       // the ComputerScore will be > 16
       // Check the computer's hand to see if it busts
     } else if (computerScore > 21) {
-      myOutputValue += '<br><br>player wins! computer busts';
-      myOutputValue += '<br>player:' + playerHand[0].name + ' of ' + playerHand[0].suit;
-      myOutputValue += '<br>' + playerHand[1].name + ' of ' + playerHand[1].suit;
-      myOutputValue += '<br>computer:' + computerHand[0].name + ' of ' + computerHand[0].suit;
-      myOutputValue += '<br>' + computerHand[1].name + ' of ' + computerHand[1].suit;
+      myOutputValue += `<br>${userName} wins! computer busts`;
+      myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.  ${userName} has cards: ${displayCards(playerHand)}.`;
       finish = true;
     } else if (computerScore > 17) {
-      myOutputValue += '<br>player:' + playerHand[0].name + ' of ' + playerHand[0].suit;
-      myOutputValue += '<br>' + playerHand[1].name + ' of ' + playerHand[1].suit;
-      myOutputValue += '<br>computer:' + computerHand[0].name + ' of ' + computerHand[0].suit;
-      myOutputValue += '<br>' + computerHand[1].name + ' of ' + computerHand[1].suit;
+      myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.`;
+      myOutputValue += `<br>${userName} has cards: ${displayCards(playerHand)}.`;
       gameState = 'evaluateGameResults';
     }
 
@@ -196,39 +246,41 @@ var main = function (input) {
   } else if (gameState == 'evaluateGameResults') {
     if (computerScore > 21) {
       if (playerScore <= 21) {//playerWins
-        myOutputValue += `player wins!!`;
+        myOutputValue += `${userName} wins!!`;
+        myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.`;
+        myOutputValue += `<br>${userName} has cards: ${displayCards(playerHand)}.`;
       }
       else if (playerScore > 21) {
         //It is a draw
         myOutputValue += `Its a draw`;
+        myOutputValue += `<br>computer has cards: ${displayCards(computerHand)}.`;
+        myOutputValue += `<br>${userName} has cards: ${displayCards(playerHand)}.`;
       }
     } else if (computerScore <= 21) {
       if (playerScore > 21) {
         //player loses
-        myOutputValue = 'player loses!';
+        myOutputValue = `${userName} loses!`;
+        myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.`;
+        myOutputValue += `<br>${userName} has cards: ${displayCards(playerHand)}.`;
         //below surmises the scenarios where user scores <=21 
       } else if (playerScore < computerScore) {
         //computerWins
         myOutputValue = 'computer wins!';
+        myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.`;
+        myOutputValue += `<br>${userName} has cards: ${displayCards(playerHand)}.`;
       } else if (playerScore > computerScore) {
         // player wins
-        myOutputValue = 'player wins!';
+        myOutputValue = `${userName} wins!`;
+        myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.`;
+        myOutputValue += `<br>${userName}has cards: ${displayCards(playerHand)}.`;
       } else {
         myOutputValue += 'its tie!';
-        myOutputValue += '<br>player:' + playerHand[0].name + ' of ' + playerHand[0].suit;
-        myOutputValue += '<br>' + playerHand[1].name + ' of ' + playerHand[1].suit;
+        myOutputValue += `<br>Computer has cards: ${displayCards(computerHand)}.`;
+        myOutputValue += `<br>${userName} has cards: ${displayCards(playerHand)}.`;
       }
       return myOutputValue;
     }
 
   }
   return myOutputValue;
-}
-var displayCards = function (hand) {
-  var nameOfCards = '';
-  for (i = 0; i < hand.length; i += 1) {
-    nameOfCards += hand[i].name + " ";
-  }
-  console.log(nameOfCards);
-  return nameOfCards;
 }
