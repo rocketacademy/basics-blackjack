821ad11f9407f9cde5e99019d1cdb74c4d9619a3
jeremylim91 <jeremylim_91@live.com>
2020-10-17 13:33:44 +0800

    completed version with split

--- script.js
+++ script.js
@@ -1,68 +1,3 @@
-/*
-if (gameMode==DEAL_STARTING_HAND
-* make a deck, shuffle it
-*deal 2 cards to each player
-* gameMode==ANALYSE_HAND
-if (gameMode== ANALYSE_HAND)
-* if player has 21, win; if computer has 21, win.
-* Else /if either player != 21{
-  * let player decide to take any amt of cards by clicking submit. (i.e. if input=='', deal card to player)
-  * if input == 'stand', {gamemode= final}
-    * return}
-  * else if player cards >21
-      * gameMode=final
-  *   * return Busted!
-*if (gameMode==final)
-  * compare ranks of all cards;
-  * winning condition:
-    * if player1 >21, player 2 wins
-    * if player2 >21, player 1 wins
-    * if player 1  sum of rank is (<=21) AND (>player 2 sum of rank): player 1 wins
-    * if player 2  sum of rank is (<=21) AND (>player 1 sum of rank): player 2 wins
-    for (var i = 0; i < playerHand.length; i++) {
-  console.log(`value of card at iteration ${i} is `);
-  console.log(playerHand[i].blckJckValue);
-  playerHandSumValue = playerHand[i].blckJckValue + playerHandSumValue;
-}
-console.log('sum value of cards in player 1 hand is');
-console.log(playerHandSumValue);
-for (var i = 0; i < computerHand.length; i++) {
-  console.log(`value of card at iteration ${i} is `);
-  console.log(computerHand[i].blckJckValue);
-  computerHandSumValue = computerHand[i].blckJckValue + computerHandSumValue;
-}
-console.log('sum value of cards in computer\'s hand is');
-console.log(computerHandSumValue); */
-/*
-player lose conditions
-1. exceed 21;
-2. computer is larger than player, and still under 21.
-3. computer gets backjack */
-
-/*
-// Function title: Push cards from deck to  computer hand, then update respective sumValue
-var dealCardToComputerAndUpdateSumValue = function (chosenHand, lengthOfChosenHand) {
-  // pop card from deck and put in player/computer's hand
-  chosenHand.push(shuffledDeck.pop());
-  var newCard = chosenHand[lengthOfChosenHand].name + ' of' + chosenHand[lengthOfChosenHand].suit;
-  console.log(`card drawn was ${newCard}`);
-  // if the drawn card is an Ace, its value will be reduced to 1 if computerHandSumValue>21
-  if ((chosenHand[lengthOfChosenHand].name == 'ace') && (computerHandSumValue + chosenHand[lengthOfChosenHand].blckJckValue > 21)) {
-    chosenHand[lengthOfChosenHand].blckJckValue = 1;
-  }
-  // else if card drawn is not an ace, but its value will cause player to exceed 21, then previously drawn aces will be counted as 1.
-  if ((computerHandSumValue + chosenHand[lengthOfChosenHand].blckJckValue) > 21) {
-    for (var i = 0; i < lengthOfChosenHand; i++) {
-      if ((chosenHand[i].name == 'ace') && (computerHandSumValue > 21)) {
-        chosenHand[i].blckJckValue = 1;
-        computerHandSumValue = computerHandSumValue - 10;// this updates the computerHandSumValue since the value in the hand is now reduced by 10 (i.e. 11-1)
-      }
-    }
-  }// calculate sum value of computer's hand
-  computerHandSumValue = chosenHand[lengthOfChosenHand].blckJckValue + computerHandSumValue;
-  return computerHandSumValue;
-}; */
-
 /*= =============================================================
 =========Global variables=======================================
 ================================================================ */
@@ -77,16 +12,19 @@ var computerHand = [];
 var shuffledDeck = [];
 
 var DEAL_STARTING_HAND = ' deal the starting hand to players';
-var CHECK_FOR_SPLIT = '';
+var CHECK_FOR_SPLIT = ' check for split';
 var ANALYSE_HAND = 'analyse hand';
 var ANALYSE_HAND2 = 'analyse hand 2';
+
+var subMode = '';
+var analyseHand1Completed = ' completed analysing hand 1';
+var analyseHand2 = 'start analysing hand 2';
+subMode = analyseHand1Completed;
 
 var REVEAL_CARDS = 'reveal cards';
 var gameMode = DEAL_STARTING_HAND;
 var split = 'split hands';
-var completedSplit = 'completed splitting';
 var subGameMode = '';
-/*----------------------------------------------------------------*/
 
 /*= =============================================================
 =========Helper functions=======================================
@@ -175,48 +113,22 @@ var getSumValue = function (chosenHand) {
       console.log('temp chosen hand sum value is more than 21');
       if (chosenHand[j].name == 'ace') {
         chosenHand[j].blckJckValue = 1;
-        for (var i = 0; i < chosenHand.length; i++) {
-          tempChosenHandSumValue = chosenHand[i].blckJckValue + tempChosenHandSumValue;
-        }
       } else {
         return tempChosenHandSumValue;
+      }
+      for (var i = 0; i < chosenHand.length; i++) {
+        tempChosenHandSumValue = chosenHand[i].blckJckValue + tempChosenHandSumValue;
       }
     }
   }
   return tempChosenHandSumValue;
 };
-/*
-// [Function title]: Push cards from deck to appropriate player hand, then update  sumValue
-var dealCardAndUpdateSumValue = function (chosenHand, chosenHandSumValue) {
-  // pop card from deck and put in player/computer's hand
-  chosenHand.push(shuffledDeck.pop());
-  var newCard = chosenHand[chosenHand.length - 1].name + ' of' + chosenHand[chosenHand.length - 1].suit;
-  console.log(`card drawn was ${newCard}`);
-
-  // if the drawn card is an Ace, its value will be reduced to 1 if playerHandSumValue>21
-  if ((chosenHand[chosenHand.length - 1].name == 'ace') && (chosenHandSumValue + chosenHand[chosenHand.length - 1].blckJckValue > 21)) {
-    chosenHand[chosenHand.length - 1].blckJckValue = 1;
-  }
-  // else if card drawn is not an ace, but its value will cause player to exceed 21, then previously drawn aces will be counted as 1.
-  if ((chosenHandSumValue + chosenHand[chosenHand.length - 1].blckJckValue) > 21) {
-    for (var i = 0; i < chosenHand.length; i++) {
-      if ((chosenHand[i].name == 'ace') && (chosenHandSumValue > 21)) {
-        chosenHand[i].blckJckValue = 1;
-      }
-    }
-  }// re-calculate chosenHandSumValue]
-  var updatedChosenHandSumValue = 0;
-  for (var i = 0; i < chosenHand.length; i++) {
-    updatedChosenHandSumValue = chosenHand[i].blckJckValue + updatedChosenHandSumValue;
-  }
-  return updatedChosenHandSumValue;
-}; */
-/*--------------------------------------------------------------------------------------------------*/
 
 // [Function title]: pop from playerHand to playerHand2, then recount sumValue for both hands
 var executeSplitDeck = function (firstHand, secondHand) {
   secondHand.push(firstHand.pop());
 };
+/*----------------------------------------------------------------------------------------------*/
 
 // [Function title]:Shuffle the deck
 var shuffleCards = function (deck, lengthOfDeck) {
@@ -232,35 +144,7 @@ var shuffleCards = function (deck, lengthOfDeck) {
 /*----------------------------------------------------------------------------------------------*/
 
 // [Function title]:Analyse player hand
-/* var analysePlayerHand = function (splitHand, splitHandSumValue, userInput) {
-  var myOutputValue = '';
-  var message = '';
-  if (userInput == '') {
-    console.log('player has chosen to hit');
-    tempSplitHandSumValue = dealCardAndUpdateSumValue(splitHand, splitHandSumValue);
-  }
-  if (userInput == 'stay') {
-    console.log('user has decided to stay');
-    if (subGameMode == split) {
-      message = `for hand 1; <br>your second hand contains: ${playerHand2[0].name} of ${playerHand2[0].suit} and ${playerHand2[1].name} of ${playerHand2[1].suit} <br> click 'submit' to hit, else 'stay' to stick with these cards`;
-    } gameMode = REVEAL_CARDS;
-    myOutputValue = 'Player, you\'ve chosen to stay with your current cards' + message;
-    // if player clicks inputs '', draw a card for him and update his array
-  }
-  // Manage game-ending conditions (player exceeds 21)
-  if (splitHandSumValue > 21) {
-    console.log('user\'s input exceeded 21');
-    gameMode = DEAL_STARTING_HAND;
-    myOutputValue = `player, you lose!  You drew a ${splitHand[splitHand.length - 1].name} of ${splitHand[splitHand.length - 1].suit}, making your hand value ${splitHandSumValue}. <br> Click 'submit' to restart the game. `;
-  }
-  if (splitHandSumValue <= 21) {
-    console.log('splitHandSumValue is still below 21');
-    var cardDrawn = `${splitHand[splitHand.length - 1].name} of ${splitHand[splitHand.length - 1].suit}`;
-    myOutputValue = `You drew a ${cardDrawn}. Your hand's value is ${splitHandSumValue}. Click 'submit' to hit, else type 'stay' to stick with your current number`;
-  }
-  return myOutputValue;
-};
-*/
+
 var analysePlayerHand = function (hand, userInput) {
   var tempSumValue = '';
   var message = '';
@@ -269,23 +153,26 @@ var analysePlayerHand = function (hand, userInput) {
     dealCard(hand);
     tempSumValue = getSumValue(hand);
     console.log(`temp sum value is ${tempSumValue}`);
+
     if (tempSumValue > 21) {
       console.log('user\'s input exceeded 21');
-      gameMode = DEAL_STARTING_HAND;
-      message = `player, you lose!  You drew a ${playerHand[playerHand.length - 1].name} of ${playerHand[playerHand.length - 1].suit}, making your hand value ${tempSumValue}. <br> Click 'submit' to restart the game. `;
+      message = `pPlayer, this hand is busted!  You drew a ${hand[hand.length - 1].name} of ${hand[hand.length - 1].suit}, making your hand value ${tempSumValue}. <br> Click 'submit' to restart the game. `;
     } else if (tempSumValue <= 21) {
       console.log('tempSumValue is still below 21');
-      var cardDrawn = `${playerHand[playerHand.length - 1].name} of ${playerHand[playerHand.length - 1].suit}`;
+      var cardDrawn = `${hand[hand.length - 1].name} of ${hand[hand.length - 1].suit}`;
       message = `You drew a ${cardDrawn}. Your hand's value is ${tempSumValue}. Click 'submit' to hit, else type 'stay' to stick with your current number`;
     }
   } else if (userInput == 'stay') {
     console.log('user has decided to stay');
-    gameMode = REVEAL_CARDS;
-    return 'Player, you\'ve chosen to stay with your current cards. click submit to continue';
-  // if player clicks inputs '', draw a card for him and update his array
+    message = 'Player, you\'ve chosen to stay with your current cards. click submit to continue';
   }
-  // Manage game-ending conditions (player exceeds 21)
 
+  if (subGameMode == split) {
+    gameMode = ANALYSE_HAND2;
+  }
+  if (subGameMode !== split) {
+    gameMode = REVEAL_CARDS;
+  }
   return message;
 };
 
@@ -300,6 +187,8 @@ var main = function (input) {
     // initialise all arrays and necssary values to 0;
     playerHand = [];
     playerHandSumValue = 0;
+    playerHand2 = [];
+    playerHand2SumValue = 0;
     computerHand = [];
     computerHandSumValue = 0;
     // shuffle cards
@@ -315,69 +204,77 @@ var main = function (input) {
       dealCard(computerHand);
       computerHandSumValue = getSumValue(computerHand);
     }
+
     gameMode = CHECK_FOR_SPLIT;
+    console.log(`moving game mode into ${CHECK_FOR_SPLIT}`);
   }
 
   if (gameMode == CHECK_FOR_SPLIT) {
-    if (playerHand[0].rank == playerHand[1].rank) {
-      if (input == 'split') {
-        executeSplitDeck(playerHand, playerHand2);
-        console.log('splitDeck has been executed');
+    console.log(`game mode is ${gameMode}`);
+    if ((playerHand[0].rank == playerHand[1].rank) && (input == 'split')) {
+      console.log('playerhand [0]] is equls to playerHand [1]');
 
-        // deal one new card to each of players hands
-        dealCard(playerHand);
-        playerHandSumValue = getSumValue(playerHand);
-        console.log('added new card to playerHand; it now contains: ');
-        console.log(playerHand);
+      console.log('attempting to execute split deck');
+      executeSplitDeck(playerHand, playerHand2);
+      console.log('splitDeck has been executed');
 
-        dealCard(playerHand2);
-        playerHand2SumValue = getSumValue(playerHand2);
-        console.log('added new card to playerHand; it now contains: ');
-        console.log(playerHand2);
+      // deal one new card to each of players hands
+      console.log('dealing a card to player\'s first hand and second hand');
+      dealCard(playerHand);
+      playerHandSumValue = getSumValue(playerHand);
+      console.log('added new card to playerHand; it now contains: ');
+      console.log(playerHand);
 
-        subGameMode = split;
-        gameMode = ANALYSE_HAND;
+      dealCard(playerHand2);
+      playerHand2SumValue = getSumValue(playerHand2);
+      console.log('added new card to playerHand; it now contains: ');
+      console.log(playerHand2);
 
-        return `Player, your first hand contains: ${playerHand[0].name} of ${playerHand[0].suit} and ${playerHand[1].name} of ${playerHand[1].suit}.`
-          + `your second hand contains: ${playerHand2[0].name} of ${playerHand2[0].suit} and ${playerHand2[1].name} of ${playerHand2[1].suit}`
-          + 'For your first hand, enter \'hit\', else \'stay\'; ';
-      } if (input == 'no') {
-        gameMode = ANALYSE_HAND;
-        message = '<br>You have chosen not to split your hand. Click \'submit\' to hit, else type \'stay\' to stick with the current cards';
-      } else {
-        message = 'Would you like to split your hand? If yes, enter \'split\'; else, enter \'no\'';
-      }
-    } else {
-      message = 'click \'submit\' to hit, else type \'stay\' to stick with the current cards';
+      subGameMode = split;
+      gameMode = ANALYSE_HAND;
+
+      return `Player, your first hand contains: ${playerHand[0].name} of ${playerHand[0].suit} and ${playerHand[1].name} of ${playerHand[1].suit}.`
+          + ` Your second hand contains: ${playerHand2[0].name} of ${playerHand2[0].suit} and ${playerHand2[1].name} of ${playerHand2[1].suit}`
+          + '<br> For your first hand, click \'submit\' to hit, else type \'stay\'; ';
     }
-
+    if (input == 'no') {
+      gameMode = ANALYSE_HAND;
+      return 'You have chosen not to split your hand. Click \'submit\' to hit, else type \'stay\' to stick with the current cards';
+    }
+    message = 'Would you like to split your hand? If yes, enter \'split\'; else, enter \'no\'';
     // update the playerhandSumValue
     console.log('player hand is ');
     console.log(playerHand);
     console.log('computer hand is ');
     console.log(computerHand);
-    myOutputValue = `Player, your hand contains a ${playerHand[0].name} of ${playerHand[0].suit} and  ${playerHand[1].name} of ${playerHand[1].suit}. <br> Your hand's value is ${playerHandSumValue}.` + `<br>${message}`;
-    gameMode = ANALYSE_HAND;
+    myOutputValue = `Player, your hand contains a ${playerHand[0].name} of ${playerHand[0].suit} and  ${playerHand[1].name} of ${playerHand[1].suit}. <br> The value is ${playerHandSumValue}.<br>${message}`;
     return myOutputValue;
   }
 
   if (gameMode == ANALYSE_HAND) {
     console.log(`game mode is ${gameMode}`);
     // compare sum value of cards in player and computer's hand
-    console.log(`playerHandSumValue is  ${playerHandSumValue}`);
-    console.log(`computerHandSumValue is  ${computerHandSumValue}`);
-    // Manage player's actions (i.e. stay vs hit)
-    // if player inputs 'stay', change game mode to final round
-
     myOutputValue = analysePlayerHand(playerHand, input);
     playerHandSumValue = getSumValue(playerHand);
+    if (subGameMode == split) {
+      gameMode = ANALYSE_HAND2;
+    }
+    return myOutputValue;
   }
 
-  if ((gameMode == ANALYSE_HAND2) && (subGameMode == ANALYSE_HAND2)) {
-    console.log(`game mode is ${gameMode}`);
-    // compare sum value of cards in player and computer's hand
-    myOutputValue = analysePlayerHand(playerHand2, input);
-    playerHandSumValue = getSumValue(playerHand2);
+  if (gameMode == ANALYSE_HAND2) {
+    if (subMode == analyseHand1Completed) {
+      subMode = analyseHand2;
+      console.log(`submode is now ${subMode}`);
+      return `It's time to examine your second hand. Your cards are ${playerHand2[0].name} of ${playerHand2[0].suit} and ${playerHand2[1].name} of ${playerHand2[1].suit}<br> click 'submit' to hit, else type 'stay';`;
+    }
+    if (subMode == analyseHand2) {
+      console.log(`submode is ${subMode}`);
+      myOutputValue = analysePlayerHand(playerHand2, input);
+      playerHand2 = getSumValue(playerHand2);
+      gameMode = REVEAL_CARDS;
+    }
+    return myOutputValue;
   }
 
   if (gameMode == REVEAL_CARDS) {
@@ -411,6 +308,8 @@ var main = function (input) {
       myOutputValue = `Computer wins! <br>Value of your playing hand: ${playerHandSumValue} <br> Value of computer's playing hand: ${computerHandSumValue}`;
       if (subGameMode == split) {
         myOutputValue = `Computer wins! <br>Value of your first hand: ${playerHandSumValue} <br>Value of your second  hand: ${playerHand2SumValue};<br> Value of computer's playing hand: ${computerHandSumValue}`;
+      } else if ((playerHandSumValue > 21) && (playerHand2SumValue > 21)) {
+        myOutputValue = 'player, you lose! both your hands exceeded 21!';
       }
     } else if (playerHandSumValue == computerHandSumValue) {
       myOutputValue = `it's a tie! <br>Value of your playing hand: ${playerHandSumValue} <br> Value of computer's playing hand ${computerHandSumValue}`;
