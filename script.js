9c7b65d373b3fc1b7e40ecfc44331cab534feb67
aseemkthakur <aseemkthakur@gmail.com>
2021-01-05 19:32:44 +0800

    updated the game logic

--- script.js
+++ script.js
@@ -73,16 +73,13 @@ var cardScore = function (card) {
   return BlackjackCardScore;
 };
 
-var shuffledCards = shuffleDeck(makeDeck());
+var shuffledCards = [];
 var playerHand = [];
 var computerHand = [];
-var gameMode;
+var gameMode = 'start';
+var myOutputValue;
 
-var dealCard = function (deck) {
-  deck.push(shuffledCards.pop());
-  return deck;
-};
-
+// caluclate the score of a card hand irrespective of the number of cards
 var handScore = function (handDeck) {
   var counter = 0;
   var handScoreTotal = 0;
@@ -93,12 +90,123 @@ var handScore = function (handDeck) {
   return handScoreTotal;
 };
 
+// the dealCard function takes a card from fromDeck array to toHand array
+var dealCard = function (fromDeck, toHand) {
+  var dealtCard = fromDeck.pop();
+  toHand.push(dealtCard);
+  return toHand;
+};
+
+// the function lists down all the cards part of the deck and total score of deck
+var showHandInfo = function (deck) {
+  var counter = 0;
+  var handInfo = '<br> The hand contains ' + deck.length + ' cards, which are: ';
+  while (counter < deck.length) {
+    handInfo = handInfo + deck[counter].name + ' of ' + deck[counter].suit + ' wih the card score of ' + cardScore(deck[counter]) + ', ';
+    counter += 1;
+  }
+  handInfo = handInfo + ' The total value of hand is: ' + handScore(deck);
+  return handInfo;
+};
+
+// function to check if the player hand or computer hand
+// has hit abover 21 or equal to 21
+var checkWinningCondition = function (playerHandExample, computerHandExample) {
+  if (gameMode == 'playerTurn' || gameMode == 'start') {
+    if (handScore(playerHandExample) > 21) {
+      myOutputValue = myOutputValue + '. <br> <br> Since Player hand is greater than 21. Player is bust. Player has lost. <br>';
+      gameMode = 'start';
+    }
+    else if (handScore(playerHandExample) == 21) {
+      myOutputValue = myOutputValue + '. <br> <br> Since Player hand score is 21. Player has won the round! <br>';
+      gameMode = 'start';
+    }
+    else if (handScore(playerHand) < 21) {
+      myOutputValue = myOutputValue + '<br> <br> Enter hit if you want another card or enter stay if you do not want another card <br>';
+      gameMode = 'playerTurn';
+    }
+  }
+  else if (gameMode == 'computerTurn') {
+    if (handScore(computerHandExample) > 21) {
+      myOutputValue = myOutputValue + '. <br> <br> Since Computer hand is greater than 21. Dealer is bust. Dealer has lost. <br>';
+      gameMode = 'start';
+    }
+  }
+
+  return myOutputValue;
+};
+
+// function to compare both player hand and computer hand to recognise the winner
+var checkWinner = function (playerHandSpecimen, computerHandSpecimen) {
+  if (handScore(playerHandSpecimen) > handScore(computerHandSpecimen)) {
+    myOutputValue = myOutputValue + '<br> <br> Player wins because player hand rank is: ' + handScore(playerHandSpecimen) + ' and the computer hand score is: ' + handScore(computerHandSpecimen);
+    gameMode = 'start';
+  }
+  else {
+    myOutputValue = myOutputValue + '<br> <br> Computer wins because player hand rank is: ' + handScore(playerHandSpecimen) + ' and the computer hand score is: ' + handScore(computerHandSpecimen);
+    gameMode = 'start';
+  }
+  return myOutputValue;
+};
+
 // On submit button being pressed, a card is drawn by computer and player to be compared
 var main = function (input) {
-  // player dealt two cards
-  dealCard(playerHand);
-  dealCard(playerHand);
-  console.log(playerHand);
-  console.log(handScore(playerHand));
-  console.log(computerHand);
+  if (gameMode == 'start') {
+    // cards created using makeDeck function and then shuffled using shuffleDeck function
+    playerHand = [];
+    computerHand = [];
+    shuffledCards = shuffleDeck(makeDeck());
+
+    // player dealt two cards from shuffledCards and added to the array playerHand
+    // var playerCard1 = shuffledCards.pop();
+    // playerHand.push(playerCard1);
+    // var playerCard2 = shuffledCards.pop();
+    // playerHand.push(playerCard2);
+    console.log(shuffledCards.length);
+    dealCard(shuffledCards, playerHand);
+    dealCard(shuffledCards, playerHand);
+    console.log(showHandInfo(playerHand));
+
+    // computer is dealt two cards from shuffledCards array to computerHand array
+    dealCard(shuffledCards, computerHand);
+    dealCard(shuffledCards, computerHand);
+
+    // shows only one card for the dealer
+    myOutputValue = 'Player Hand: ' + showHandInfo(playerHand) + '<br> <br> Computer up card: ' + showHandInfo(computerHand.slice(0, 1));
+
+    checkWinningCondition(playerHand, computerHand);
+  }
+  if (gameMode == 'playerTurn') {
+    if (input == 'hit') {
+      dealCard(shuffledCards, playerHand);
+      myOutputValue = myOutputValue + showHandInfo(playerHand);
+      checkWinningCondition(playerHand, computerHand);
+    }
+    else if (input == 'stay') {
+      myOutputValue = myOutputValue + '<br> Player chose stay. Click submit to see dealer turn.<br>';
+      gameMode = 'computerTurn';
+      return myOutputValue;
+    }
+  }
+  if (gameMode == 'computerTurn') {
+    if (handScore(computerHand) > 16) {
+      myOutputValue = myOutputValue + '<br> Computer hand: ' + showHandInfo(computerHand) + '<br> Computer chose stay. <br>';
+      checkWinningCondition(playerHand, computerHand);
+      if (gameMode != 'start') {
+        checkWinner(playerHand, computerHand);
+      }
+    }
+    else if (handScore(computerHand) < 17) {
+      myOutputValue = myOutputValue + '<br> Computer hand: ' + showHandInfo(computerHand) + '<br> Computer chose hit. <br>';
+      dealCard(shuffledCards, computerHand);
+      myOutputValue = myOutputValue + showHandInfo(computerHand);
+      // check winning condition
+      checkWinningCondition(playerHand, computerHand);
+      // check winner
+      if (gameMode != 'start') {
+        checkWinner(playerHand, computerHand);
+      }
+    }
+  }
+  return myOutputValue;
 };
