6589d8d4e40c432d72f6ad32cdf70a6165d61f00
Karen-Heng <shanglikaren@gmail.com>
2021-01-07 13:34:13 +0800

    Latest Version

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
+/* eslint-disable max-len */
 // Global Variables Here
 var gameMode = 'default';
-var dealerCard1;
-var dealerCard2;
-var dealerCard3;
-var playerCard1;
-var playerCard2;
-var playerCard3;
-var playerCard4;
 var playerScore;
 var dealerScore;
-var playerScoreDeck1;
-var playerScoreDeck2;
+var playerArray = [];
+var dealerArray = [];
 
-// Helper function #1 : Create a random index
+// Helper function : Create a random index
 var getRandomIndex = function (size) {
   var randomNum = Math.floor(Math.random() * size);
   return randomNum;
 };
 
-// Helper function #2 : Shuffle the deck
+// Helper function : Shuffle the deck
 var shuffleCards = function (cards) {
   var index = 0;
   while (index < cards.length) {
@@ -31,39 +25,48 @@ var shuffleCards = function (cards) {
   } return cards;
 };
 
-// Helper function #3 : Determine the winner
+// Helper function : Add the sum of values in the array
+var getTotalCardSum = function (cardsArray) {
+  var i = 0;
+  var sum = 0;
+  while (i < cardsArray.length) {
+    var currentCard = cardsArray[i];
+    sum = sum + currentCard.value;
+    i = i + 1;
+  }
+  return sum;
+};
+
+// Helper function : Determine the winner
 var decideWinner = function (playerScore, dealerScore) {
   if (dealerScore > playerScore && dealerScore <= 21) {
-    return 'Player Score: ' + playerScore + '<br>Dealer Score: ' + dealerScore + '<br><br>Dealer wins!';
+    return '<br><br><b>DEALER WINS!</b>';
   }
-  if (dealerScore > playerScore && dealerScore >= 21) {
-    return 'Player Score: ' + playerScore + '<br>Dealer Score: ' + dealerScore + '<br><br>Player wins!';
+  if ((dealerScore > playerScore && dealerScore >= 21)) {
+    return '<br><br><b>PLAYER WINS!</b>';
   }
   if (dealerScore < playerScore && playerScore <= 21) {
-    return 'Player Score: ' + playerScore + '<br>Dealer Score: ' + dealerScore + '<br><br>Player wins!';
+    return '<br><br><b>PLAYER WINS!</b>!';
   }
   if (dealerScore < playerScore && playerScore >= 21) {
-    return 'Player Score: ' + playerScore + '<br>Dealer Score: ' + dealerScore + '<br><br>Dealer wins!';
+    return '<br><br><b>DEALER WINS!</b>';
   }
   if (dealerScore == playerScore) {
-    return 'Player Score: ' + playerScore + '<br>Dealer Score: ' + dealerScore + '<br><br>It is a draw.';
+    return '<br><br>IT IS A DRAW. Refresh to restart game.';
+  }
+  if (dealerScore == 21) {
+    return 'BlackJack! Dealer Wins!';
+  }
+  if (playerScore == 21) {
+    return 'BlackJack! Player Wins!';
   }
 };
 
-// Helper function #4 : When dealer hits
-var whenDealerHits = function (playerScore, dealerScore) {
-  if (dealerScore >= 17 && dealerScore < 21) {
-    return decideWinner(playerScore, dealerScore);
-  }
-  if (dealerScore > 21) {
-    return 'Player Score: ' + playerScore + '<br>Dealer Score: ' + dealerScore + '<br><br>Dealer is busted! <br>Player Wins!';
-  }
-};
-
+// GAME STARTS HERE //
 var main = function (input) {
   var makeDeck = function () {
     var deck = [];
-    var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    var suits = ['♥️', '♦️', '♠️', '♣️'];
 
     var suitIndex = 0;
     while (suitIndex < suits.length) {
@@ -79,16 +82,16 @@ var main = function (input) {
 
         // 1, 11, 12 ,13
         if (cardName == 1) {
-          cardName = 'ace';
+          cardName = 'A';
           cardValue = 11;
         } else if (cardName == 11) {
-          cardName = 'jack';
+          cardName = 'J';
           cardValue = 10;
         } else if (cardName == 12) {
-          cardName = 'queen';
+          cardName = 'Q';
           cardValue = 10;
         } else if (cardName == 13) {
-          cardName = 'king';
+          cardName = 'K';
           cardValue = 10;
         }
 
@@ -112,125 +115,118 @@ var main = function (input) {
 
   var deck = shuffleCards(makeDeck());
 
-  var playerArray = [];
-  var dealerArray = [];
-
-  var getTotalCardSum = function(cardsArray){
-    counter = 0;
-    while(counter < cardsArray.length){
-      sum += cardsArray[counter];
-      counter = counter + 1;
-    }
-    return sum;
-  }
+  // Output Messages
+  var twoCardsDealtEach;
+  var threeCardsEach;
+  var threeCardsPlayerTwoCardsDealer;
+  var twoCardsPlayerThreeCardsDealer;
 
   if (gameMode == 'default') {
     // On Click, 2 cards are shown for player and 1 for dealer.
-
     playerArray.push(deck.pop());
     dealerArray.push(deck.pop());
     playerArray.push(deck.pop());
     dealerArray.push(deck.pop());
-    
 
     // Calculate what is the score for player and dealer's hand
-    getTotalSum(playerArray);
-    getTotalSum(dealerArray);
+    playerScore = getTotalCardSum(playerArray);
+    dealerScore = getTotalCardSum(dealerArray);
 
-//     // When the player has 2 identical cardValues, he splits it into 2 decks.
-//     if (playerCard1.value == playerCard2.value)
-//     // Player will automatically receive 1 new card for each deck.
-    
+    twoCardsDealtEach = '<u><b>PLAYER:</b></u><br> ' + playerArray[0].name + playerArray[0].suit + '<br>' + playerArray[1].name + playerArray[1].suit + '<br>Player Score: ' + playerScore + '<br><br><u><b>DEALER:</b></u><br>' + dealerArray[0].name + dealerArray[0].suit + '<br>' + dealerArray[1].name + dealerArray[1].suit + '<br>Dealer Score: ' + dealerScore;
 
-//     console.log(playerCard1.value);
-//     console.log(playerCard2.value);
-//     console.log(playerCard3.value);
-//     console.log(playerCard4.value);
+    // If player score is <=11, check if any card is an ace add 10 to change value of ace to 11.
+    if (playerScore <= 11 && (playerArray[0].name == 'ace' || playerArray[1].name == 'ace')) {
+      playerScore += 10;
 
-//     // Check if its blackjack for either hand.
-//     if (playerScoreDeck1 < 11 && (playerCard1.name == 'ace' || playerCard3.name == 'ace')) {
-//       playerScoreDeck1 += 10;
+      if (playerScore == 21) {
+        return twoCardsDealtEach + decideWinner(playerScore, dealerScore);
+      }
+      if (playerArray[0].name == 'aces' && playerArray[1].name == 'aces') {
+        playerScore = 21;
+        return twoCardsDealtEach + decideWinner(playerScore, dealerScore);
+      }
+    }
+    // If both of the players cards are aces, minus 10 from playersScore.
+    if (playerArray[0].name == 'ace' || playerArray[1].name == 'ace') {
+      playerScore -= 10;
+      return twoCardsDealtEach + decideWinner(playerScore, dealerScore);
+    }
 
-//       if (playerScoreDeck1 == 21) {
-//         return 'BlackJack! Player Wins!';
-//       }
-//     }
+    if (playerScore == 21) {
+      return twoCardsDealtEach + decideWinner(playerScore, dealerScore);
+    }
 
-//     else if (playerScoreDeck1 == 21) {
-//       return 'BlackJack! Player Wins!';
-//     }
-//     gameMode = 'User Hit or Stand';
-//     return 'PlayerScore: ' + playerScoreDeck1 + '<br>DealerScore: ' + dealerScore + '<br><br>User, do you want to hit or stand?';
-//   }
-//     if (playerScoreDeck2 < 11 && (playerCard1.name == 'ace' || playerCard3.name == 'ace')) {
-//       playerScoreDeck1 += 10;
+    if (dealerScore == 21) {
+      return twoCardsDealtEach + decideWinner(playerScore, dealerScore);
+    }
 
-//       if (playerScoreDeck1 == 21) {
-//         return 'BlackJack! Player Wins!';
-//       }
-//     }
+    // Else, prompt player if they want to hit or stand.
+    gameMode = 'User Hit or Stand';
+    return twoCardsDealtEach + '<br><br>Player, do you want to hit or stand?';
+  }
 
-//     else if (playerScoreDeck1 == 21) {
-//       return 'BlackJack! Player Wins!';
-//     }
-//     gameMode = 'User Hit or Stand';
-//     return 'PlayerScore: ' + playerScoreDeck2 + '<br>DealerScore: ' + dealerScore + '<br><br>User, do you want to hit or stand?';
-//   }
+  if (gameMode == 'User Hit or Stand') {
+    // Player to input 'hit' to add 1 more card.
+    if (input == '') {
+      return 'Please input "hit" or "stand" to continue';
+    }
+    if (input == 'hit') {
+      gameMode = 'Hit Mode';
+    }
+    if (input == 'stand') {
+      gameMode = 'Stand Mode';
+    }
+    if (gameMode == 'Hit Mode') {
+      playerArray.push(deck.pop());
+      playerScore = getTotalCardSum(playerArray);
 
-//   // If player score is <11, check if any card is an ace add 10 to change value of ace to 1
-//   if (playerScore < 11 && (playerCard1.name == 'ace' || playerCard2.name == 'ace')) {
-//     playerScore += 10;
+      threeCardsPlayerTwoCardsDealer = '<u><b>PLAYER:</b></u><br> ' + playerArray[0].name + playerArray[0].suit + '<br>' + playerArray[1].name + playerArray[1].suit + '<br>' + playerArray[2].name + playerArray[2].suit + '<br>Player Score: ' + playerScore + '<br><br><u><b>DEALER:</b></u><br>' + dealerArray[0].name + dealerArray[0].suit + '<br>' + dealerArray[1].name + dealerArray[1].suit + '<br>Dealer Score: ' + dealerScore;
 
-//     if (playerScore == 21) {
-//       return 'BlackJack! Player Wins!';
-//     }
-//     // Else, prompt player if they want to hit or stand.
-//     gameMode = 'User Hit or Stand';
-//     return 'PlayerScore: ' + playerScore + '<br>DealerScore: ' + dealerScore + '<br><br>User, do you want to hit or stand?';
-//   }
+      if (playerScore == 21 || playerScore > 21) {
+        return threeCardsPlayerTwoCardsDealer + decideWinner(playerScore, dealerScore);
+      }
 
-//   if (gameMode == 'User Hit or Stand') {
-//     // Player to input 'hit' to add 1 more card.
-//     if (input == 'hit') {
-//       playerCard4 = deck.pop();
-//       playerScore += playerCard4.value;
-//       console.log(playerScore, 'player 1+2+3');
+      // If dealer score is <= 16, draw the 3rd card.
+      if (dealerScore <= 16) {
+        dealerArray.push(deck.pop());
+        dealerScore = getTotalCardSum(dealerArray);
 
-//       if (playerScore > 21) {
-//         return 'Player Score: ' + playerScore + '<br>Dealer Score: ' + dealerScore + '<br><br>Player is busted! <br>Dealer Wins!';
-//       }
+        threeCardsEach = '<u><b>PLAYER:</b></u><br> ' + playerArray[0].name + playerArray[0].suit + '<br>' + playerArray[1].name + playerArray[1].suit + '<br>' + playerArray[2].name + playerArray[2].suit + '<br>Player Score: ' + playerScore + '<br><br><u><b>DEALER:</b></u><br>' + dealerArray[0].name + dealerArray[0].suit + '<br>' + dealerArray[1].name + dealerArray[1].suit + '<br>' + dealerArray[1].name + dealerArray[1].suit + '<br>Dealer Score: ' + dealerScore;
 
-//       // If dealer score is <= 16, draw the 3rd card.
-//       if (dealerScore <= 16) {
-//         dealerCard3 = deck.pop();
-//         dealerScore += dealerCard3.value;
-//         console.log(dealerScore, 'dealer 1+2+3');
-//         // Use helper function #4 to determine winner if dealer auto hits.
-//         return whenDealerHits(playerScore, dealerScore);
-//       }
-//       // Use helper function #3 to determine final winner if dealer auto hits.
-//       return decideWinner(playerScore, dealerScore);
-//     }
+        // Use helper function to determine winner if dealer auto hits.
+        return threeCardsEach + decideWinner(playerScore, dealerScore); // 3 cards each
+      }
+      // Use helper function to determine winner
+      // eslint-disable-next-line max-len
+      return threeCardsPlayerTwoCardsDealer + decideWinner(playerScore, dealerScore); // 3 cards player, 2 cards dealer.
+    }
 
-//     if (input == 'stand') {
-//       // If dealer score is <11, check if any card is an ace add 10 to change value of ace to 11.
-//       if (dealerScore < 11 && (dealerCard1.name == 'ace' || dealerCard2.name == 'ace')) {
-//         dealerScore += 10;
-//       }
-//       if (dealerScore > 21) {
-//         return 'Player Score: ' + playerScore + '<br>Dealer Score: ' + dealerScore + '<br><br>Dealer is busted! <br>Player Wins!';
-//       }
-//     }
-//     // If dealer score is <= 16, dealer hits (draw the 3rd card).
-//     if (dealerScore <= 16) {
-//       // Recalculate score for dealer's hand.
-//       dealerCard3 = deck.pop();
-//       dealerScore += dealerCard3.value;
-//       console.log(dealerScore, 'dealer 1+2+3');
-//       // Use helper function #4 to determine winner if dealer auto hits.
-//       return whenDealerHits(playerScore, dealerScore);
-//     }
-//     // Use helper function #3 to determine final winner if dealer decides to stand.
-//     return decideWinner(playerScore, dealerScore);
-//   }
-// };
+    if (gameMode == 'Stand Mode') {
+    // If dealer score is <11, check if any card is an ace add 10 to change value of ace to 11.
+      if (dealerScore <= 11 && (dealerArray[0].name == 'ace' || dealerArray[1].name == 'ace')) {
+        dealerScore += 10;
+
+        if (dealerScore == 21) {
+          // Use helper function to determine winner
+          // eslint-disable-next-line max-len
+          return twoCardsDealtEach + decideWinner(playerScore, dealerScore); // player 2 cards, dealer 2 cards.
+        }
+      }
+
+      // If dealer score is <= 16, dealer hits (draw the 3rd card).
+      if (dealerScore <= 16) {
+      // Recalculate score for dealer's hand.
+        dealerArray.push(deck.pop());
+        dealerScore = getTotalCardSum(dealerArray);
+
+        twoCardsPlayerThreeCardsDealer = '<u><b>PLAYER:</b></u><br> ' + playerArray[0].name + playerArray[0].suit + '<br>' + playerArray[1].name + playerArray[1].suit + '<br>Player Score: ' + playerScore + '<br><br><u><b>DEALER:</b></u><br>' + dealerArray[0].name + dealerArray[0].suit + '<br>' + dealerArray[1].name + dealerArray[1].suit + '<br>' + dealerArray[2].name + dealerArray[2].suit + '<br>Dealer Score: ' + dealerScore;
+
+        // eslint-disable-next-line max-len
+        // Use helper function to determine winner if dealer auto hits.
+        return twoCardsPlayerThreeCardsDealer + decideWinner(playerScore, dealerScore); // player 2 cards, dealer 3 cards.
+      }
+      // Use helper function to determine winner if dealer auto hits.
+      return decideWinner(playerScore, dealerScore);
+    }
+  }
+};
