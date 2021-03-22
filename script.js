6ca89491592c5ebc0fe4edf44883ad95709be510
emily-zhangg <emily@officecleanz.com>
2021-03-22 16:31:23 +0800

    blackjack update

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+//function to generate deck
+var cards =[];
+var cardDeck = function (){
+  var cardSuits =['hearts', 'spades','clubs','diamonds'];
+  var cardSuitsIndex =0;
+  while(cardSuitsIndex<4){
+    var currentsuit = cardSuits[cardSuitsIndex];
+    var rankNumber=1;
+    while (rankNumber<14){
+      var cardName=rankNumber;
+      if(rankNumber == 1){
+        cardName='Ace'
+      }
+      else if(rankNumber == 11){
+        cardName='Joker'
+      }
+      else if(rankNumber == 12){
+        cardName='Queen'
+      }
+      else if(rankNumber == 13){
+        cardName='King'
+      }
+      cards.push({
+        name:  cardName,
+        suit: currentsuit,
+        rank: rankNumber,
+      });
+      rankNumber = rankNumber + 1;
+    }
+    cardSuitsIndex = cardSuitsIndex + 1;
+  }
+}
+//function to shuffle deck
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  cardDeck();
+  console.log(cards);
 };
