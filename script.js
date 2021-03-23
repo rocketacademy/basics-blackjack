53f636d22cc188229076fad6b0b101b63954b945
jokatty <jyotikattani@gmail.com>
2021-03-23 14:43:32 +0800

    replace hard coded deck of cards with deck generated using loops

--- script.js
+++ script.js
@@ -1,265 +1,39 @@
-var deck = [
-  {
-    name: 'ace',
-    suit: 'hearts',
-    rank: 1,
-  },
-  {
-    name: '2',
-    suit: 'hearts',
-    rank: 2,
-  },
-  {
-    name: '3',
-    suit: 'hearts',
-    rank: 3,
-  },
-  {
-    name: '4',
-    suit: 'hearts',
-    rank: 4,
-  },
-  {
-    name: '5',
-    suit: 'hearts',
-    rank: 5,
-  },
-  {
-    name: '6',
-    suit: 'hearts',
-    rank: 6,
-  },
-  {
-    name: '7',
-    suit: 'hearts',
-    rank: 7,
-  },
-  {
-    name: '8',
-    suit: 'hearts',
-    rank: 8,
-  },
-  {
-    name: '9',
-    suit: 'hearts',
-    rank: 9,
-  },
-  {
-    name: '10',
-    suit: 'hearts',
-    rank: 10,
-  },
-  {
-    name: 'jack',
-    suit: 'hearts',
-    rank: 11,
-  },
-  {
-    name: 'queen',
-    suit: 'hearts',
-    rank: 12,
-  },
-  {
-    name: 'king',
-    suit: 'hearts',
-    rank: 13,
-  },
-  {
-    name: 'ace',
-    suit: 'diamonds',
-    rank: 1,
-  },
-  {
-    name: '2',
-    suit: 'diamonds',
-    rank: 2,
-  },
-  {
-    name: '3',
-    suit: 'diamonds',
-    rank: 3,
-  },
-  {
-    name: '4',
-    suit: 'diamonds',
-    rank: 4,
-  },
-  {
-    name: '5',
-    suit: 'diamonds',
-    rank: 5,
-  },
-  {
-    name: '6',
-    suit: 'diamonds',
-    rank: 6,
-  },
-  {
-    name: '7',
-    suit: 'diamonds',
-    rank: 7,
-  },
-  {
-    name: '8',
-    suit: 'diamonds',
-    rank: 8,
-  },
-  {
-    name: '9',
-    suit: 'diamonds',
-    rank: 9,
-  },
-  {
-    name: '10',
-    suit: 'diamonds',
-    rank: 10,
-  },
-  {
-    name: 'jack',
-    suit: 'diamonds',
-    rank: 11,
-  },
-  {
-    name: 'queen',
-    suit: 'diamonds',
-    rank: 12,
-  },
-  {
-    name: 'king',
-    suit: 'diamonds',
-    rank: 13,
-  },
-  {
-    name: 'ace',
-    suit: 'clubs',
-    rank: 1,
-  },
-  {
-    name: '2',
-    suit: 'clubs',
-    rank: 2,
-  },
-  {
-    name: '3',
-    suit: 'clubs',
-    rank: 3,
-  },
-  {
-    name: '4',
-    suit: 'clubs',
-    rank: 4,
-  },
-  {
-    name: '5',
-    suit: 'clubs',
-    rank: 5,
-  },
-  {
-    name: '6',
-    suit: 'clubs',
-    rank: 6,
-  },
-  {
-    name: '7',
-    suit: 'clubs',
-    rank: 7,
-  },
-  {
-    name: '8',
-    suit: 'clubs',
-    rank: 8,
-  },
-  {
-    name: '9',
-    suit: 'clubs',
-    rank: 9,
-  },
-  {
-    name: '10',
-    suit: 'clubs',
-    rank: 10,
-  },
-  {
-    name: 'jack',
-    suit: 'clubs',
-    rank: 11,
-  },
-  {
-    name: 'queen',
-    suit: 'clubs',
-    rank: 12,
-  },
-  {
-    name: 'king',
-    suit: 'clubs',
-    rank: 13,
-  },
-  {
-    name: 'ace',
-    suit: 'spades',
-    rank: 1,
-  },
-  {
-    name: '2',
-    suit: 'spades',
-    rank: 2,
-  },
-  {
-    name: '3',
-    suit: 'spades',
-    rank: 3,
-  },
-  {
-    name: '4',
-    suit: 'spades',
-    rank: 4,
-  },
-  {
-    name: '5',
-    suit: 'spades',
-    rank: 5,
-  },
-  {
-    name: '6',
-    suit: 'spades',
-    rank: 6,
-  },
-  {
-    name: '7',
-    suit: 'spades',
-    rank: 7,
-  },
-  {
-    name: '8',
-    suit: 'spades',
-    rank: 8,
-  },
-  {
-    name: '9',
-    suit: 'spades',
-    rank: 9,
-  },
-  {
-    name: '10',
-    suit: 'spades',
-    rank: 10,
-  },
-  {
-    name: 'jack',
-    suit: 'spades',
-    rank: 11,
-  },
-  {
-    name: 'queen',
-    suit: 'spades',
-    rank: 12,
-  },
-  {
-    name: 'king',
-    suit: 'spades',
-    rank: 13,
-  },
-];
+function createDeck() {
+  let suits = ['hearts', 'dimonds', 'clubs', 'spades'];
+  let suitsLength = suits.length;
+  let cardRank;
+  let cardName;
+  let cardDeck = [];
+  for (let i = 0; i < suitsLength; i++) {
+    let currentSuit = suits[i];
+    console.log('current suit is: ' + suits[i]);
+    for (let j = 1; j <= 13; j++) {
+      cardRank = j;
+      cardName = cardRank;
+      if (cardRank == 1) {
+        cardName = 'ace';
+      }
+      else if (cardRank == 11) {
+        cardName = 'jack';
+      }
+      else if (cardRank == 12) {
+        cardName = 'queen';
+      }
+      else if (cardRank == 13) {
+        cardName = 'king';
+      }
+      let deck = {
+        name: cardName,
+        suit: currentSuit,
+        rank: cardRank,
+      };
+      cardDeck.push(deck);
+    }
+  }
+  return cardDeck;
+}
+let deck = createDeck();
+
 function genrateRandomCards(max) {
   return Math.floor(Math.random(max));
 }
