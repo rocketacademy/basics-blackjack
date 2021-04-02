eedc8fa41a5b45ea4e01a1885d79ad3837b6b3de
isaac-prog <panda_mm88@yahoo.com>
2021-04-02 23:48:01 +0800

    1667$ update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// username
+var name = '';
+
 // create deck
 var makeDeck = function () {
   // Initialise an empty deck array
@@ -65,27 +68,61 @@ var shuffleCards = function (cardDeck) {
   return cardDeck;
 };
 
-const deck = makeDeck();
-const shuffledDeck = shuffleCards(deck);
+var deck = makeDeck();
+var shuffledDeck = shuffleCards(deck);
 
 // ai array
 var dealerCards = [];
 var dealerPoints = [];
+var dealerCardsInDetail = [];
 var sumOfDealerPoints = 0;
 // player array
 var playerCards = [];
 var playerPoints = [];
+var playerCardsInDetail = [];
 var sumOfPlayerPoints = 0;
+var bet = '';
+var playerMoney = 100;
+var moneyBet = 0;
 
 // ai draw a card (unknown card)
 // player draws 2 caeds
 var main = function (input) {
+  if (!name) {
+    // If the user did not input anything, prompt them to enter something
+    // as their user name.
+    if (!input) {
+      return 'Please input something :(';
+    }
+    name = input;
+
+    return 'Welcome ' + name + '! Enter the amount of money you wish to bet. (Integers Only!)';
+  }
+
+  // $$$ setup
+  if (!bet) {
+    if (isNaN(input) == true && input != 'start' && input != 'restart' && input != 'hit' && input != 'stand') {
+      return 'enter integers only!';
+    }
+    if (playerMoney < input) {
+      return 'you only have $' + playerMoney;
+    }
+
+    if (playerMoney > input) { moneyBet = input;
+      playerMoney -= moneyBet;
+      return 'you bet: $' + moneyBet + '<br> You have $' + playerMoney + ' left' + "<br> Type 'start' to begin BlackJack!";
+    }
+  }
+
+  // finally the game starts
   if (input == 'start') {
+    console.log(shuffledDeck);
     const dealerDraw = shuffledDeck.pop();
     const dealerPoints1 = dealerDraw.points;
     const dealerName = dealerDraw.name;
     dealerCards.push(dealerName);
     dealerPoints.push(dealerPoints1);
+    dealerCardsInDetail.push(dealerDraw);
     console.log(dealerDraw);
     console.log(dealerPoints1);
     console.log(dealerName);
@@ -95,6 +132,7 @@ var main = function (input) {
     const playerrName1 = playerDraw1.name;
     playerCards.push(playerrName1);
     playerPoints.push(playerrPoints1);
+    playerCardsInDetail.push(playerDraw1);
     console.log(playerDraw1);
     console.log(playerrPoints1);
     console.log(playerrName1);
@@ -104,63 +142,100 @@ var main = function (input) {
     const playerName2 = playerDraw2.name;
     playerCards.push(playerName2);
     playerPoints.push(playerPoints2);
+    playerCardsInDetail.push(playerDraw2);
     console.log(playerDraw2);
     console.log(playerPoints2);
     console.log(playerName2);
+
     // summation of points
     sumOfPlayerPoints = playerPoints.reduce((a, b) => a + b, 0);
+
   	if (sumOfPlayerPoints == 21) {
-      return 'BlackJack! You win!' + '<br> Your cards:  ' + playerCards + '<br> your score: ' + sumOfPlayerPoints;
+      playerMoney = playerMoney + 2 * moneyBet;
+      return 'BlackJack! You win!' + '<br> Your cards:  ' + playerCards + '<br> your score: ' + sumOfPlayerPoints + '<br> You currently have $' + playerMoney + "<br> Type 'restart' if you wish to play again! :) ";
     }
   	return 'dealer cards: Unknown card & ' + dealerCards + '<br> Your Cards: ' + playerCards + "<br> Do you want to 'hit' or 'stand'? " + sumOfPlayerPoints;
   }
 
   // hit = draw, stand = vs
   if (input == 'hit') {
+    console.log(shuffledDeck);
     const playerDrawHit = shuffledDeck.pop();
     const playerrPointsHit = playerDrawHit.points;
     const playerrNameHit = playerDrawHit.name;
     playerCards.push(playerrNameHit);
     playerPoints.push(playerrPointsHit);
+    playerCardsInDetail.push(playerDrawHit);
     console.log(playerDrawHit);
     console.log(playerrPointsHit);
     console.log(playerrNameHit);
     // summation of points
     sumOfPlayerPoints = playerPoints.reduce((a, b) => a + b, 0);
 
-  	if (sumOfPlayerPoints > 21 && playerCards.includes('ace')) {
-    	sumOfPlayerPoints = sumOfPlayerPoints - 10;
+    if (sumOfPlayerPoints > 21 && playerCards.includes('ace')) {
+    	  sumOfPlayerPoints = sumOfPlayerPoints - 10;
 
       return 'dealer cards: Unknown card & ' + dealerCards + '<br> Your Cards: ' + playerCards + "<br> Do you want to 'hit' or 'stand'? " + sumOfPlayerPoints; }
 
-    if (sumOfPlayerPoints > 21 && playerCards.includes('ace') == 'false') {
-      return 'bust! You lose!' + '<br> Your cards:  ' + playerCards + '<br> your score: ' + sumOfPlayerPoints;
+    if (sumOfPlayerPoints > 21 && playerCards.includes('ace') == false) {
+      return 'bust! You lose!' + '<br> Your cards:  ' + playerCards + '<br> your score: ' + sumOfPlayerPoints + '<br> you currently have $' + playerMoney + "<br> Type 'restart' if you wish to play again! :) ";
     }
     if (sumOfPlayerPoints == 21) {
-      return 'BlackJack! You win!' + '<br> Your cards:  ' + playerCards + '<br> your score: ' + sumOfPlayerPoints;
+      playerMoney = playerMoney + 2 * moneyBet;
+      return 'BlackJack! You win!' + '<br> Your cards:  ' + playerCards + '<br> your score: ' + sumOfPlayerPoints + '<br> you currently have: $' + playerMoney + "<br> Type 'restart' if you wish to play again! :) ";
     }
+
   	return 'dealer cards: Unknown card & ' + dealerCards + '<br> Your Cards: ' + playerCards + "<br> Do you want to 'hit' or 'stand'? " + sumOfPlayerPoints;
   }
 
   // player choose to hit or stand
   // if stand, reveal unknwon card
   if (input == 'stand') {
-    const dealerDrawStand = shuffledDeck.pop();
-    const dealerPointsStand = dealerDrawStand.points;
-    const dealerNameStand = dealerDrawStand.name;
-    dealerCards.push(dealerNameStand);
-    dealerPoints.push(dealerPointsStand);
-    console.log(dealerDrawStand);
-    console.log(dealerPointsStand);
-    console.log(dealerNameStand);
-    // summation of points
-    sumOfDealerPoints = dealerPoints.reduce((a, b) => a + b, 0);
-  	if (sumOfPlayerPoints > sumOfDealerPoints) {
-      return 'You WIN!!! ' + '<br> Your Score: ' + sumOfPlayerPoints + '<br> Your cards:  ' + playerCards + '<br> Dealer Score: ' + sumOfDealerPoints + '<br> Dealer cards:  ' + dealerCards;
+    console.log(shuffledDeck);
+    while (sumOfDealerPoints < 16) {
+      const dealerDrawStand = shuffledDeck.pop();
+      const dealerPointsStand = dealerDrawStand.points;
+      const dealerNameStand = dealerDrawStand.name;
+      dealerCards.push(dealerNameStand);
+      dealerPoints.push(dealerPointsStand);
+      dealerCardsInDetail.push(dealerDrawStand);
+      console.log(dealerDrawStand);
+      console.log(dealerPointsStand);
+      console.log(dealerNameStand);
+
+      // summation of points
+      sumOfDealerPoints = dealerPoints.reduce((a, b) => a + b, 0);
+    }
+
+  	if (sumOfPlayerPoints > sumOfDealerPoints || sumOfDealerPoints > 21) {
+      playerMoney = playerMoney + 2 * moneyBet;
+      return 'You WIN!!! ' + '<br> Your Score: ' + sumOfPlayerPoints + '<br> Your cards:  ' + playerCards + '<br> Dealer Score: ' + sumOfDealerPoints + '<br> Dealer cards:  ' + dealerCards + '<br> you currently have: $' + playerMoney + "<br> Type 'restart' if you wish to play again! :) ";
     }
 
     if (sumOfPlayerPoints < sumOfDealerPoints) {
-      return 'You LOSE!!! ' + '<br> Your Score: ' + sumOfPlayerPoints + '<br> Your cards:  ' + playerCards + '<br> Dealer Score: ' + sumOfDealerPoints + '<br> Dealer cards:  ' + dealerCards;
+      return 'You LOSE!!! ' + '<br> Your Score: ' + sumOfPlayerPoints + '<br> Your cards:  ' + playerCards + '<br> Dealer Score: ' + sumOfDealerPoints + '<br> Dealer cards:  ' + dealerCards + '<br> you currently have: $' + playerMoney + "<br> Type 'restart' if you wish to play again! :) ";
     }
+  }
+  // resets hand cards and deck
+  if (input == 'restart') {
+    // reset and reshuffle the deck
+    shuffledDeck = shuffleCards(deck);
+    shuffledDeck = shuffledDeck.concat(dealerCardsInDetail);
+    shuffledDeck = shuffledDeck.concat(playerCardsInDetail);
+
+    console.log(shuffledDeck);
+
+    // resets hand cards
+    dealerCards = [];
+    dealerPoints = [];
+    dealerCardsInDetail = [];
+    sumOfDealerPoints = 0;
+    // player array
+    playerCards = [];
+    playerPoints = [];
+    playerCardsInDetail = [];
+    sumOfPlayerPoints = 0;
+
+    return 'enter the amount you wish to bet if you wish to play again';
   }
 };
