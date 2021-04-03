2b32c719b3f69237a34b2c20d2f55562d3a8ce64
jetljt <lijingtaocharles@gmail.com>
2021-04-03 11:58:39 +0800

    project3 updated

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // initializations
-var playerScoreNormal = 0;
-var playerScoreSpecial = 0;
-var dealerScoreNormal = 0;
-var dealerScoreSpecial = 0;
-var mode = 'player';
+var playerScore = 0;
+var dealerScore = 0;
 var output = '';
+var playerHand = [];
+var dealerHand = [];
+var isAceInside = false;
+var aceCount = 0;
+var isDealerMode = false;
 
 // helper functions
-
 // (1) shuffledDeck
 var makeDeck = function () {
   var cardDeck = [];
@@ -59,46 +60,26 @@ var shuffleCards = function (cardDeck) {
 };
 var shuffledDeck = shuffleCards(makeDeck());
 
-var playerDeck = [];
-var dealerDeck = [];
-
-// (2) points calculator
-var rankCal = function (y) {
-  if (y.rank == 11
-    || y.rank == 12
-    || y.rank == 13) {
-    y.rank = 10;
-  }
-  return y.rank;
-};
-var total = function (x) {
-  var m = 0;
+// points calculator & check for Ace
+var handValue = function (hand) {
+  var i = 0;
   var sum = 0;
-  while (m < x.length) {
-    rankRes = rankCal(x[m]);
-    sum += rankRes;
-    m += 1;
+  while (i < hand.length) {
+    if (hand[i].rank == 11
+    || hand[i].rank == 12
+    || hand[i].rank == 13) {
+      sum += 10;
+    } else if (hand[i].rank == 1) {
+      sum += 11;
+      aceCount += 1;
+      isAceInside = true;
+    } else {
+      sum += hand[i].rank;
+    }
+    i += 1;
   }
   return sum;
 };
-
-// check if an Ace card is inside the array of
-// card objects held by the player.
-
-var checkAce = function (x) {
-  var n = 0;
-  while (n < x.length) {
-    if (x[n].rank == 1) {
-      var rankRes = true;
-      console.log(rankRes);
-      return rankRes;
-    }
-    console.log(rankRes);
-    n += 1;
-  }
-};
-
-var isAce = checkAce(playerDeck);
 
 var main = function (input) {
 // default mode: mode 1: player & computer both draw their first
@@ -106,65 +87,86 @@ var main = function (input) {
   // he wants to hit or stand. if he stands, go to mode 2.
 
   // parallel 1
-  if (mode == 'player') {
-    if (!input) {
-      output = 'welcome to blackjack, player! enter "hit" to draw your cards';
-      dealerDeck.push(shuffledDeck.pop());
-      dealerDeck.push(shuffledDeck.pop());
-      dealerScoreNormal = total(dealerDeck);
-      return output;
-    }
-    // parallel 2
+  if (!input) {
+    output = 'welcome to blackjack, player! enter "hit" to draw your cards';
+    dealerHand.push(shuffledDeck.pop());
+    dealerHand.push(shuffledDeck.pop());
+    dealerScore = handValue(dealerHand);
+    return output;
+  }
+  // parallel
+  if (!isDealerMode) {
     if (input == 'hit' || input == 'hit ') {
-      playerDeck.push(shuffledDeck.pop());
+      playerHand.push(shuffledDeck.pop());
       var i = 0;
       output = 'Hey player! your current deck of cards are: <br>';
-      while (i < playerDeck.length) {
-        output += playerDeck[i].name + ' of ' + playerDeck[i].suit + ',<br>';
-        playerScoreNormal = total(playerDeck);
+      while (i < playerHand.length) {
+        output += playerHand[i].name + ' of ' + playerHand[i].suit + ',<br>';
+        playerScore = handValue(playerHand);
         i += 1;
       }
-      if (isAce == 1) {
-        output += '<br>player, do you want to count your Ace as "1" or "11"? key in either. <br><br> your total score now is: ' + playerScoreNormal + '.';
+
+      // conditionals for the players for winning/losing decision
+      if (isAceInside == true && playerScore > 21) {
+        var i = 0;
+        while (i < aceCount) {
+          playerScore -= 10;
+          i += 1;
+        }
+        output += ' <br><br> your current score now is: ' + playerScore + '.';
+      } else if (playerScore == 21) {
+        output += '<br<br><br> you got 21 points you damn lucky you won!';
+        return output;
+      } else if (isAceInside == false && playerScore > 21) {
+        output += ' <br><br> you are busted!';
+        return output;
+      } else { output += '<br><br> player, your current score is: ' + playerScore + '.';
       }
 
-      if (input == '11') {
-        output += '<br> player, your total score now is: ' + playerScoreNormal + '.';
-      }
-      else if (input == '1') {
-        playerScoreSpecial = playerScoreNormal - 10;
-        output += '<br> player, your total score now is: ' + playerScoreSpecial + '.';
-      } else {
-        output += '<br><br> player, your total score now is: ' + playerScoreNormal + '.';
-      }
-
-      if (playerScoreNormal == 21) {
-        output += '<br<br> omfg you damn lucky you won!';
-        return output;
-      } if (playerScoreNormal > 21) {
-        output += '<br><br> you are busted! pray that the dealer will go bust as well.key in "dealer" to see the deck of cards it will have drawn.';
-        return output;
-      }
-      output += '<br><br> dealer‘s current deck of cards are: <br>' + dealerDeck[0].name + ' of ' + dealerDeck[0].suit + ',<br> x of xxxx(a face-down card)' + ',<br><br> do you want to "hit" or "stand"? key in either and click the submit button.';
+      output += '<br><br> dealer‘s current deck of cards are: <br>' + dealerHand[0].name + ' of ' + dealerHand[0].suit + ',<br> hand of xxxx(a face-down card)' + ',<br><br> do you want to "hit" or "stand"? key in either and click the submit button.';
       return output;
     }
-    // parallel 3
     if (input == 'stand' || input == 'stand ') {
-    // decide if the dealer needs to draw cards
-      if (dealerScoreNormal < 16) {
-        dealerDeck.push(shuffledDeck.pop());
-        dealerScoreNormal = total(dealerDeck);
-        output = '<br><br> dealer‘s current deck of cards are: <br>' + dealerDeck[0].name + ' of ' + dealerDeck[0].suit + ',<br> ' + dealerDeck[1].name + ' of ' + dealerDeck[1].suit + ',<br> ' + dealerDeck[2].name + ' of ' + dealerDeck[2].suit + ',<br> the dealer‘s total score is: ' + dealerScoreNormal + '<br><br> the player‘s total score is: ' + playerScoreNormal;
-        return output;
-      }
+      isDealerMode = true;
     }
-  } else if (mode == 'dealer') {
+  }
 
+  // parallel 3: dealer's turn + compare + decide winner
+  if (isDealerMode == true) {
+    // reset aceCount & isAceInside
+    aceCount = 0;
+    isAceInside = false;
+    // re-invoke handValue function to update the value for aceCount & isAceInside for the dealer
+    dealerScore = handValue(dealerHand);
+    // decide if the dealer needs to draw cards
+    if (dealerScore < 16) {
+      dealerHand.push(shuffledDeck.pop());
+      dealerScore = handValue(dealerHand);
+      output = '<br><br> the dealer’s score was less than 16 so the dealer has to draw a 3rd card. The dealer‘s current deck of cards are: <br>' + dealerHand[0].name + ' of ' + dealerHand[0].suit + ',<br> ' + dealerHand[1].name + ' of ' + dealerHand[1].suit + ',<br> ' + dealerHand[2].name + ' of ' + dealerHand[2].suit + ',<br> the dealer‘s current score is: ' + dealerScore + '<br><br> the player‘s current score is: ' + playerScore;
+    } else {
+      output = '<br><br> the dealer’s score was less than 16 so the dealer has to draw a 3rd card. <br><br> The dealer‘s current deck of cards are: <br>' + dealerHand[0].name + ' of ' + dealerHand[0].suit + ',<br> ' + dealerHand[1].name + ' of ' + dealerHand[1].suit + ',<br> the dealer‘s current score is: ' + dealerScore + '<br><br> the player‘s current score is: ' + playerScore;
+    }
+
+    if (dealerScore == 21) {
+      output += '<br>the dealer got 21 points! <br> the dealer won!';
+    } else if (dealerScore > 21 && isAceInside == true) {
+      var i = 0;
+      while (i < aceCount) {
+        dealerScore -= 10;
+        i += 1;
+      }
+      if (dealerScore > playerScore) {
+        output += '<br><br> the dealer won!';
+      } else {
+        output += '<br><br> the player won!';
+      }
+    } else if (dealerScore > 21 && isAceInside == false) {
+      output += '<br><br> the dealer is busted! the player won!';
+    } else if (dealerScore > playerScore) {
+      output += '<br><br> the dealer won!';
+    } else {
+      output += '<br><br> the player won!';
+    }
+    return output;
   }
 };
-
-// mode 2: computer's turn to hit or stand
-// if stand, go to mode 3
-
-// mode 3: compare totals of computerDeck and that of playerDeck to
-// decide the winner
