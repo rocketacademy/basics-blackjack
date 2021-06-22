83c0537088ae43621f0c486481f81b695f46affc
Hsiu Ping Gay <hsiups@Hsius-MacBook-Pro.local>
2021-06-22 07:27:22 +0700

    added mode for drawing third card

--- script.js
+++ script.js
@@ -1,4 +1,330 @@
+var deck = [
+  {
+    name: "ace",
+    suit: "hearts",
+    rank: 1,
+  },
+  {
+    name: "2",
+    suit: "hearts",
+    rank: 2,
+  },
+  {
+    name: "3",
+    suit: "hearts",
+    rank: 3,
+  },
+  {
+    name: "4",
+    suit: "hearts",
+    rank: 4,
+  },
+  {
+    name: "5",
+    suit: "hearts",
+    rank: 5,
+  },
+  {
+    name: "6",
+    suit: "hearts",
+    rank: 6,
+  },
+  {
+    name: "7",
+    suit: "hearts",
+    rank: 7,
+  },
+  {
+    name: "8",
+    suit: "hearts",
+    rank: 8,
+  },
+  {
+    name: "9",
+    suit: "hearts",
+    rank: 9,
+  },
+  {
+    name: "10",
+    suit: "hearts",
+    rank: 10,
+  },
+  {
+    name: "jack",
+    suit: "hearts",
+    rank: 11,
+  },
+  {
+    name: "queen",
+    suit: "hearts",
+    rank: 12,
+  },
+  {
+    name: "king",
+    suit: "hearts",
+    rank: 13,
+  },
+  {
+    name: "ace",
+    suit: "diamonds",
+    rank: 1,
+  },
+  {
+    name: "2",
+    suit: "diamonds",
+    rank: 2,
+  },
+  {
+    name: "3",
+    suit: "diamonds",
+    rank: 3,
+  },
+  {
+    name: "4",
+    suit: "diamonds",
+    rank: 4,
+  },
+  {
+    name: "5",
+    suit: "diamonds",
+    rank: 5,
+  },
+  {
+    name: "6",
+    suit: "diamonds",
+    rank: 6,
+  },
+  {
+    name: "7",
+    suit: "diamonds",
+    rank: 7,
+  },
+  {
+    name: "8",
+    suit: "diamonds",
+    rank: 8,
+  },
+  {
+    name: "9",
+    suit: "diamonds",
+    rank: 9,
+  },
+  {
+    name: "10",
+    suit: "diamonds",
+    rank: 10,
+  },
+  {
+    name: "jack",
+    suit: "diamonds",
+    rank: 11,
+  },
+  {
+    name: "queen",
+    suit: "diamonds",
+    rank: 12,
+  },
+  {
+    name: "king",
+    suit: "diamonds",
+    rank: 13,
+  },
+  {
+    name: "ace",
+    suit: "clubs",
+    rank: 1,
+  },
+  {
+    name: "2",
+    suit: "clubs",
+    rank: 2,
+  },
+  {
+    name: "3",
+    suit: "clubs",
+    rank: 3,
+  },
+  {
+    name: "4",
+    suit: "clubs",
+    rank: 4,
+  },
+  {
+    name: "5",
+    suit: "clubs",
+    rank: 5,
+  },
+  {
+    name: "6",
+    suit: "clubs",
+    rank: 6,
+  },
+  {
+    name: "7",
+    suit: "clubs",
+    rank: 7,
+  },
+  {
+    name: "8",
+    suit: "clubs",
+    rank: 8,
+  },
+  {
+    name: "9",
+    suit: "clubs",
+    rank: 9,
+  },
+  {
+    name: "10",
+    suit: "clubs",
+    rank: 10,
+  },
+  {
+    name: "jack",
+    suit: "clubs",
+    rank: 11,
+  },
+  {
+    name: "queen",
+    suit: "clubs",
+    rank: 12,
+  },
+  {
+    name: "king",
+    suit: "clubs",
+    rank: 13,
+  },
+  {
+    name: "ace",
+    suit: "spades",
+    rank: 1,
+  },
+  {
+    name: "2",
+    suit: "spades",
+    rank: 2,
+  },
+  {
+    name: "3",
+    suit: "spades",
+    rank: 3,
+  },
+  {
+    name: "4",
+    suit: "spades",
+    rank: 4,
+  },
+  {
+    name: "5",
+    suit: "spades",
+    rank: 5,
+  },
+  {
+    name: "6",
+    suit: "spades",
+    rank: 6,
+  },
+  {
+    name: "7",
+    suit: "spades",
+    rank: 7,
+  },
+  {
+    name: "8",
+    suit: "spades",
+    rank: 8,
+  },
+  {
+    name: "9",
+    suit: "spades",
+    rank: 9,
+  },
+  {
+    name: "10",
+    suit: "spades",
+    rank: 10,
+  },
+  {
+    name: "jack",
+    suit: "spades",
+    rank: 11,
+  },
+  {
+    name: "queen",
+    suit: "spades",
+    rank: 12,
+  },
+  {
+    name: "king",
+    suit: "spades",
+    rank: 13,
+  },
+];
+
+var shuffledDeck = shuffleCards(deck);
+var doYouWantTo = "draw third card";
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var computerCard1 = shuffledDeck.pop();
+  var computerCard2 = shuffledDeck.pop();
+  var playerCard1 = shuffledDeck.pop();
+  var playerCard2 = shuffledDeck.pop();
+
+  var output = `Your cards are: <br>${playerCard1} <br>${playerCard2} <br>Would you like to draw another card? <br>Please type "yes" or "no" and click submit.`;
+
+  if ((doYouWantTo = "draw third card")) {
+    if (input == "yes") {
+      var playerCard3 = shuffledDeck.pop();
+      doYouWantTo = "draw fourth card";
+      output = `You've drawn ${playerCard3}. <br>Your previous two cards are <br>${playerCard1} <br>${playerCard2} <br>Would you like to draw another card?<br>Please type "yes" or "no" and click submit`;
+    } else if (input == "no") {
+      doYouWantTo = "end player's draw";
+    }
+  }
+
+  if (doYouWantTo == "end player's draw") {
+    var playerPoints = Number(playerCard1.rank) + Number(playerCard2.rank);
+    var computerPoints = Number(computerCard1) + Number(computerCard2);
+    if ((playerCard1.rank == 1 || playerCard2.rank == 1) && playerPoints < 11) {
+      playerPoints = Number(playerPoints) + 10;
+    }
+    if (playerPoints < computerPoints && computerPoints < 22) {
+      output == "computer wins";
+    }
+  }
+
+  return output;
 };
+
+// Deal cards
+function dealCards() {
+  var computerCard1 = shuffledDeck.pop();
+  var computerCard2 = shuffledDeck.pop();
+  var playerCard1 = shuffledDeck.pop();
+  var playerCard2 = shuffledDeck.pop();
+}
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Shuffle the elements in the cardDeck array
+function shuffleCards(cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+}
