51e34c0c9e42459e73a750afbc4ab5d4c548b714
makck <joel.mak32@gmail.com>
2021-06-29 04:25:07 +0800

    More Comfortable: Multi-Player

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Global variables
 var gameMode = "get player name";
 var shuffledDeck = [];
-var playerCard = [];
 var computerCard = [];
+var allPlayersHand = []; // Nested Array. Outer array represents players, inner array holds cards which are objects
 var playerName = [];
 var playerBets = [];
-var playerScore = [100, 100];
+var playerTurn = 0; // variable to keep track of which player's turn
+var playerScore = [];
 var winner = "";
 var winnerDetermined = false;
 
@@ -74,72 +75,117 @@ var shuffleCards = function (cardDeck) {
   return cardDeck;
 };
 
+// Function to give 100 points to each player when the game initialise.
+var assignBankRoll = function () {
+  for (var i = 0; i < playerName.length; i += 1) {
+    playerScore.push(Number(100));
+  }
+};
+
 // Function to obtain names of players and store in an array.
 var getName = function (input) {
   if (input == "end") {
     gameMode = "get player bets";
+    assignBankRoll();
     return `Welcome to the game ${playerName.join(
       ", "
-    )}. <br> The game is starting. <br><br> You start off with 100 points. Please input your bet amount.`;
+    )}. <br> It's time to place your bets. <br><br> You start off with 100 points. Please input your bet amount.`;
   }
+
+  if (input.length < 1) {
+    return `Your name cannot be empty!`;
+  }
+
   playerName.push(input);
   return `Welcome to the game ${playerName.join(
     ", "
-  )}. <br> Type the next name to register more players. Otherwise, type "end".`;
+  )}. <br><br> Type the next name to register more players. Otherwise, type "end".`;
 };
 
+// Helper global variable for this function to track how many players have placed their bets.
+var numPlayersPlacedBet = 0;
 // Function to get players' bets. Bets are stored in an array.
 var getBets = function (input) {
-  playerBets.push(input);
-  return `You are waging ${input} points.`;
+  var index = 0;
+  var output = "";
+  if (numPlayersPlacedBet < playerName.length - 1) {
+    playerBets.push(input);
+    output = `${
+      playerName[numPlayersPlacedBet]
+    } is waging ${input} points. <br><br> ${
+      playerName[numPlayersPlacedBet + 1]
+    } please input your bet.`;
+    numPlayersPlacedBet += 1;
+    return output;
+  } else {
+    playerBets.push(input);
+    output = `${playerName[numPlayersPlacedBet]} is waging ${input} points. <br><br> All players have placed their bets. <br> Let the games begin!`;
+    gameMode = "shuffle";
+    return output;
+  }
 };
 
-// Function to update the players' current scores given the outcome of the game. The function takes in a single parameter which is the index of the winning player.
-// In the current 2 player game, index 0 = player, index 1 = computer.
-var updateScores = function (winningPlayer) {
-  if (winningPlayer == 0) {
-    playerScore[0] += Number(playerBets[0]);
-    playerScore[playerScore.length - 1] -= Number(playerBets[0]);
-  } else if (winningPlayer == 1) {
-    playerScore[0] -= Number(playerBets[0]);
-    playerScore[playerScore.length - 1] += Number(playerBets[0]);
+// Function to update the players' current scores given the outcome of the game. First parameter is the index of the player, second parameter is boolean, if true, player wins the bet.
+var updateScores = function (playerIndex, playerWon) {
+  if (playerWon) {
+    playerScore[playerIndex] += Number(playerBets[playerIndex]);
+  } else {
+    playerScore[playerIndex] -= Number(playerBets[playerIndex]);
   }
 };
 
 // Function to print the current points on all players.
 var showLeaderboard = function () {
-  return `<br><br> ======LEADERBOARD====== <br> Player: ${playerScore[0]} <br> Computer: ${playerScore[1]}. <br><br> Please input your bet amount for the next round.`;
+  var output = "";
+  for (var i = 0; i < playerName.length; i += 1) {
+    output += `${playerName[i]}: ${playerScore[i]} <br>`;
+  }
+  return `<br><br> ======LEADERBOARD====== <br> ${output} <br><br> To continue to the next round, ${playerName[0]} please input your bet amount for the next round.`;
 };
 
-// Function to deal the initial 2 cards to players. Input is the shuffledDeck array, as well as the player or computer card array.
-var dealCards = function (shuffledDeck, inputHand) {
+// Function to deal the initial 2 cards to all players. Function takes in the number of players as an integer.
+var dealCards = function (numPlayers) {
   var index = 0;
-  while (inputHand.length != 2) {
-    inputHand.push(shuffledDeck[index]);
-    shuffledDeck.splice(index, 1);
+  var inputArray = [];
+  while (index < numPlayers) {
+    inputArray.push([shuffledDeck[0], shuffledDeck[1]]);
+    shuffledDeck.splice(0, 2);
+    index += 1;
   }
-  index = 0;
-  return inputHand;
+  return inputArray;
 };
 
 // Function to total points of the player's hand. Takes in array of cards on player's hand.
 var calculateHand = function (playerHand) {
-  var index = 0;
+  var outerIndex = 0;
+  var innerIndex = 0;
   var totalScore = 0;
-  while (index < playerHand.length) {
-    totalScore += playerHand[index].rank;
-    index += 1;
-  }
-  if (totalScore > 21) {
-    totalScore = 0;
-    for (var i = 0; i < playerHand.length; i += 1) {
-      if (playerHand[i].name == "Ace") {
-        playerHand[i].rank = 1;
+  var numAce = 0;
+  var allPlayerPoints = [];
+  while (outerIndex < playerHand.length) {
+    while (innerIndex < playerHand[outerIndex].length) {
+      totalScore += playerHand[outerIndex][innerIndex].rank;
+      if (playerHand[outerIndex][innerIndex].name == "Ace") {
+        numAce += 1;
       }
-      totalScore += playerHand[i].rank;
+      innerIndex += 1;
     }
+    if (totalScore > 21 && numAce > 0) {
+      totalScore = 0;
+      for (var i = 0; i < playerHand[outerIndex].length; i += 1) {
+        if (playerHand[outerIndex][i].name == "Ace") {
+          playerHand[outerIndex][i].rank = 1;
+        }
+        totalScore += playerHand[outerIndex][i].rank;
+      }
+    }
+    allPlayerPoints.push(totalScore);
+    totalScore = 0;
+    innerIndex = 0;
+    outerIndex += 1;
   }
-  return totalScore;
+
+  return allPlayerPoints;
 };
 
 // Function to print the hands of each player. Input is the array of object for each player.
@@ -153,62 +199,60 @@ var printHand = function (playerHand) {
   return outputValue;
 };
 
-// Compare initial hands
-var compareInitialHands = function (playerCard, computerCard) {
-  var playerCurrentTotal = calculateHand(playerCard);
-  var computerCurrentTotal = calculateHand(computerCard);
+// Function to compare scores of all players against the dealer.
+var compareHands = function (inputPlayerCard, inputComputerCard) {
+  var playerCurrentTotal = calculateHand(inputPlayerCard);
+  var computerCurrentTotal = calculateHand(inputComputerCard);
+  var i = 0;
+  var output = [];
 
-  if (playerCurrentTotal == 21 && computerCurrentTotal != 21) {
-    winnerDetermined = true;
-    updateScores(0);
-    return `Player ${playerName[0]} Blackjack! Player wins! <br><br> Player ${
-      playerName[0]
-    } cards: <br> ${printHand(
-      playerCard
-    )} <br> Computer cards: <br> ${printHand(computerCard)}`;
-  } else if (computerCurrentTotal == 21) {
-    winnerDetermined = true;
-    updateScores(1);
-    return `Computer Blackjack! Computer wins! <br><br> Player cards: <br> ${printHand(
-      playerCard
-    )} <br> Computer cards: <br> ${printHand(computerCard)}`;
-  } else {
-    return `Player ${playerName[0]} cards: <br> ${printHand(
-      playerCard
-    )} <br> Computer cards: <br> ${printHand(computerCard)} <br><br> Player ${
-      playerName[0]
-    } to choose "stand" or "hit".`;
+  while (i < playerCurrentTotal.length) {
+    var playerWon = false;
+    // if computer = player, tie. OR If computer bust and player bust, tie.
+    if (
+      playerCurrentTotal[i] == computerCurrentTotal[0] ||
+      (playerCurrentTotal[i] > 21 && computerCurrentTotal[0] > 21)
+    ) {
+      output.push(
+        `This is a draw! Hand Total is ${
+          playerCurrentTotal[i]
+        } points. <br><br> Player ${playerName[i]} cards: <br> ${printHand(
+          inputPlayerCard[i]
+        )}<br><br>`
+      );
+    }
+
+    // if computer < player, player wins. OR If computer bust and player did not, player win.
+    else if (
+      (playerCurrentTotal[i] > computerCurrentTotal[0] &&
+        playerCurrentTotal[i] <= 21) ||
+      (computerCurrentTotal[0] > 21 && playerCurrentTotal[i] <= 21)
+    ) {
+      output.push(
+        `Player ${playerName[i]} won the dealer! Hand Total is ${
+          playerCurrentTotal[i]
+        } points. <br><br> ${playerName[i]} cards: <br> ${printHand(
+          inputPlayerCard[i]
+        )} <br><br>`
+      );
+      playerWon = true;
+      updateScores(i, playerWon);
+    }
+
+    // if computer > player, computer wins. OR player bust and computer did not.
+    else {
+      output.push(
+        `Player ${playerName[i]} lost to the dealer! Hand Total is ${
+          playerCurrentTotal[i]
+        } points. <br><br> ${playerName[i]} cards: <br> ${printHand(
+          inputPlayerCard[i]
+        )}<br><br>`
+      );
+      updateScores(i, playerWon);
+    }
+    i += 1;
   }
-};
-
-// Function to compare scores after initial comparison ie continuation of game after 2 cards have been dealt with no blackjack.
-var compareHands = function (playerCard, computerCard) {
-  var playerCurrentTotal = calculateHand(playerCard);
-  var computerCurrentTotal = calculateHand(computerCard);
-
-  if (playerCurrentTotal > computerCurrentTotal) {
-    updateScores(0);
-    return `Player ${
-      playerName[0]
-    } wins! Hand Total is ${playerCurrentTotal} points. <br><br> Player ${
-      playerName[0]
-    } cards: <br> ${printHand(
-      playerCard
-    )} <br> Computer cards: <br> ${printHand(computerCard)}`;
-  } else if (playerCurrentTotal == computerCurrentTotal) {
-    return `This is a draw! <br><br> Player ${
-      playerName[0]
-    } cards: <br> ${printHand(
-      playerCard
-    )} <br> Computer cards: <br> ${printHand(computerCard)}`;
-  } else {
-    updateScores(1);
-    return `Computer wins! Hand Total is ${computerCurrentTotal} points. <br><br> Player ${
-      playerName[0]
-    } cards: <br> ${printHand(
-      playerCard
-    )} <br> Computer cards: <br> ${printHand(computerCard)}`;
-  }
+  return output;
 };
 
 // Function to reset all global variables for the next round.
@@ -219,39 +263,14 @@ var resetGame = function () {
   gameMode = "get player bets";
   winnerDetermined = false;
   playerBets = [];
+  numPlayersPlacedBet = 0;
+  playerTurn = 0;
 };
 
 // Function to draw additional card and add to hand. Function reads the specific playerCard array
 var drawAdditionalCard = function (inputPlayerHand) {
   inputPlayerHand.push(shuffledDeck[0]);
   shuffledDeck.splice(0, 1);
-};
-
-// Function to check player hand if it has gone bust
-var checkPlayerHand = function (playerHand) {
-  var playerBusted = false;
-  var playerCurrentTotal = calculateHand(playerHand);
-  if (playerCurrentTotal > 21) {
-    playerBusted = true;
-    updateScores(1);
-    return [
-      playerBusted,
-      `Player ${playerName[0]} BUSTED!! <br><br> Player cards: <br> ${printHand(
-        playerCard
-      )} <br> Computer cards: <br> ${printHand(
-        computerCard
-      )} <br><br> Click "submit" to begin new round.`,
-    ];
-  } else {
-    return [
-      playerBusted,
-      `Player ${playerName[0]} cards: <br> ${printHand(
-        playerCard
-      )} <br> Computer cards: <br> ${printHand(
-        computerCard
-      )} <br><br> Player to choose "stand" or "hit".`,
-    ];
-  }
 };
 
 // Function for computer to hit or stand. Computer has to hit when total hand is below 17.
@@ -261,79 +280,131 @@ var computerDecisions = function (inputComputerCard) {
   var outputValue = "";
   var computerCurrentTotal = calculateHand(inputComputerCard);
   if (computerCurrentTotal > 17) {
-    return `Computer has decided not to hit. <br><br> Click "submit" to compare hands.`;
+    return `The dealer has decided not to hit. <br><br> Click "submit" to compare all players' hands.`;
   }
 
   while (computerCurrentTotal < 17) {
-    drawAdditionalCard(inputComputerCard);
+    drawAdditionalCard(inputComputerCard[0]);
     computerCurrentTotal = calculateHand(inputComputerCard);
-    outputValue += `${
-      inputComputerCard[inputComputerCard.length - 1].name
-    } of ${inputComputerCard[inputComputerCard.length - 1].suit} <br>`;
     index += 1;
   }
-  computerCurrentTotal = calculateHand(inputComputerCard);
-  if (computerCurrentTotal > 21) {
-    updateScores(0);
-    var tempOutput = `Computer BUSTED! The computer has drawn ${index} card(s): <br> ${outputValue}<br><br> Computer has:<br> ${printHand(
-      inputComputerCard
-    )}`;
-    resetGame();
-    return tempOutput;
-  }
-  return `The computer has drawn ${index} card(s): <br> ${outputValue} <br> Click "submit" to compare your hands.`;
+  return `The dealer has drawn ${index} card(s). <br><br> Click "submit" to compare all players' hands.`;
 };
 
+//  Main function to run the game
 var main = function (input) {
   var outputValue = "";
   var playerState = [];
   if (gameMode == "get player name") {
     return getName(input);
-  } else if (gameMode == "get player bets") {
-    gameMode = "shuffle";
+  }
+
+  if (gameMode == "get player bets") {
     return getBets(input);
-  } else if (gameMode == "shuffle") {
+  }
+
+  if (gameMode == "shuffle") {
     shuffledDeck = shuffleCards(deckGeneration());
     gameMode = "deal";
-    return `The deck has been shuffled. <br> Click "Submit" to deal cards.`;
-  } else if (gameMode == "deal") {
-    playerCard = dealCards(shuffledDeck, playerCard);
-    computerCard = dealCards(shuffledDeck, computerCard);
-    gameMode = "compare initial hands";
-    return `Click "submit" to compare your current hands and show your cards.`;
-  } else if (gameMode == "compare initial hands") {
-    outputValue = compareInitialHands(playerCard, computerCard);
-    if (winnerDetermined) {
-      resetGame();
-      return outputValue + showLeaderboard();
-    } else {
-      gameMode = "player choice";
-      return outputValue;
-    }
-  } else if (gameMode == "player choice") {
+    return `The deck has been shuffled. <br><br> Click "Submit" to deal cards.`;
+  }
+
+  if (gameMode == "deal") {
+    allPlayersHand = dealCards(playerName.length);
+    computerCard = dealCards(1);
+    gameMode = "player turn";
+    return `Click "submit" for each player to start looking at their own hand.`;
+  }
+
+  if (gameMode == "player turn" && playerTurn < playerName.length) {
+    var playerCurrentTotal = calculateHand(allPlayersHand);
+    var computerCurrentTotal = calculateHand(computerCard);
+    gameMode = "player choice";
+    return `${playerName[playerTurn]}, you drew: <br> ${printHand(
+      allPlayersHand[playerTurn]
+    )} <br><br> Please input "hit" or "stand". `;
+  }
+
+  if (gameMode == "player choice") {
     if (!(input == "hit" || input == "stand")) {
       return `Please input only "hit" or "stand".`;
     }
 
+    var output = "";
+    var lastPlayerDecided = false;
+    if (playerTurn == playerName.length - 1) {
+      lastPlayerDecided = true;
+      var lastStatement =
+        "All players have made their moves. <br><br> The dealer will make his move now.";
+    } else {
+      lastStatement = `Click "submit" for ${
+        playerName[playerTurn + 1]
+      }'s turn.`;
+    }
+
     if (input == "hit") {
-      drawAdditionalCard(playerCard);
-      playerState = checkPlayerHand(playerCard);
-      if (playerState[0]) {
-        outputValue = playerState[1];
-        resetGame();
-        return outputValue + showLeaderboard();
+      drawAdditionalCard(allPlayersHand[playerTurn]);
+      if (
+        calculateHand(allPlayersHand)[playerTurn] > 21 &&
+        lastPlayerDecided == false
+      ) {
+        gameMode = "player turn";
+        output = `On your hand: <br> ${printHand(
+          allPlayersHand[playerTurn]
+        )} <br><br> Your total has exceeded 21. You can only stand. <br><br> ${lastStatement}`;
+        playerTurn += 1;
+        return output;
+      } else if (
+        calculateHand(allPlayersHand)[playerTurn] > 21 &&
+        lastPlayerDecided
+      ) {
+        gameMode = "computer choice";
+        output = `On your hand: <br> ${printHand(
+          allPlayersHand[playerTurn]
+        )} <br><br> Your total has exceeded 21. You can only stand. <br><br> ${lastStatement}`;
+        playerTurn += 1;
+        return output;
       } else {
-        return playerState[1];
+        return `On your hand: <br> ${printHand(
+          allPlayersHand[playerTurn]
+        )} <br><br> Please input "hit" or "stand".`;
       }
     }
 
-    if (input == "stand") {
+    if (input == "stand" && !lastPlayerDecided) {
+      gameMode = "player turn";
+      playerTurn += 1;
+      return `${lastStatement}`;
+    } else if (input == "stand" && lastPlayerDecided) {
       gameMode = "computer choice";
-      return `${computerDecisions(computerCard)}`;
+      playerTurn += 1;
+      return `${lastStatement}`;
     }
-  } else if (gameMode == "computer choice") {
-    outputValue = compareHands(playerCard, computerCard);
-    resetGame();
-    return outputValue + showLeaderboard();
   }
+
+  if (gameMode == "computer choice") {
+    gameMode = "compare hands";
+    return computerDecisions(computerCard);
+  }
+
+  if (gameMode == "compare hands") {
+    var dealerOutput = `Dealer hand total is ${
+      calculateHand(computerCard)[0]
+    } points. <br><br> Dealer cards: <br>${printHand(computerCard[0])}`;
+    var finalOutput = compareHands(allPlayersHand, computerCard).join("");
+    resetGame();
+    return `${finalOutput} ${dealerOutput} <br><br> ${showLeaderboard()}`;
+  }
+
+  // } else if (gameMode == "computer choice") {
+  //   outputValue = compareHands(playerCard, computerCard);
+  //   resetGame();
+  //   return outputValue + showLeaderboard();
+  // }
 };
+
+// Stopped at stage where player decides to hit or stand
+// All cards comparison will only be done after all players have decided to hit or stand.
+// If the player bust after drawing a card, he will only be presented the "stand" option.
+// Even if a player has bust, it will only be revealed in the final score comparison.
+// Points will only be updated at the end.
