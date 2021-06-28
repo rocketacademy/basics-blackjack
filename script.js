b8932cc806fb12b50526755867901cfe76337a44
gcmillora <gcmillora@up.edu.ph>
2021-06-29 00:57:39 +0800

    added some logic, no winning condition yet

--- script.js
+++ script.js
@@ -1,4 +1,117 @@
+//Global Variables
+var blackjackLimit = 21;
+var userStand = false;
+var gameState = false;
+
+var deck;
+var userHand = [];
+var botHand = [];
+
+var getCurrentSumHand = function (hand) {
+  var total = 0;
+  var counter = 0;
+  var countAce = 0;
+  while (counter < hand.length) {
+    if (hand[counter].rank <= 10 && hand[counter].rank >= 2) {
+      total += hand[counter].rank;
+    } else if (hand[counter].rank >= 11) total += 10;
+    else {
+      countAce += 1;
+      total += 11;
+    }
+    counter++;
+  }
+
+  if (total > blackjackLimit) {
+    if (countAce > 0) {
+      var counter = 0;
+      while (counter < countAce && total > blackjackLimit) {
+        total -= 10;
+        counter++;
+      }
+    }
+  }
+  return total;
+};
+
+var makeDeck = function () {
+  var cardDeck = [];
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  var suitsIcon = ["♥", "♦", "♣", "♠"];
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    var currentSuit = suits[suitIndex];
+    var currentSuitIcon = suitsIcon[suitIndex];
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      var cardName = rankCounter;
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+        icon: currentSuitIcon,
+      };
+      cardDeck.push(card);
+      rankCounter += 1;
+    }
+    suitIndex += 1;
+  }
+  return cardDeck;
+};
+
+var shuffleCards = function (cardDeck) {
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    var randomIndex = getRandomIndex(cardDeck.length);
+    var randomCard = cardDeck[randomIndex];
+    var currentCard = cardDeck[currentIndex];
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    currentIndex = currentIndex + 1;
+  }
+  return cardDeck;
+};
+
+var drawOneCard = function () {
+  var cardDrawn = deck.pop();
+  return cardDrawn;
+};
+
+var isBlackJack = function (hand) {
+  var sum = getCurrentSumHand(hand);
+  if (sum === blackjackLimit) {
+    return true;
+  }
+};
+
+deck = shuffleCards(makeDeck());
+var playBlackJack = function (input) {
+  if (!userHand.length) {
+    userHand.push(drawOneCard());
+    userHand.push(drawOneCard());
+    botHand.push(drawOneCard());
+    botHand.push(drawOneCard());
+    //Check if any hand wins.
+    if (isBlackJack(userHand)) {
+      return `User won!`;
+    }
+    if (isBlackJack(botHand)) {
+      return `Bot won!`;
+    }
+
+    return `Do you want to 'hit' or 'stand'?`;
+  }
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  playBlackJack();
 };
