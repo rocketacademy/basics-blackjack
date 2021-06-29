be70c6c6bee88c1fe0b8b703cb333c70ac04ce98
jeremylim91 <jeremylim_91@live.com>
2021-06-29 20:51:10 +0800

    complete bets and multiplayer

--- script.js
+++ script.js
@@ -2,12 +2,20 @@
 ======GLOBAL VARIABLES========
 =============================*/
 var MODE_QUERY_PLAYER_NAME = 'MODE_QUERY_PLAYER_NAME';
-var MODE_PLAY_GAME = 'MODE_PLAY_GAME';
+var MODE_SCAN_FOR_BLACKJACK = 'MODE_SCAN_FOR_BLACKJACK';
+var MODE_CHOOSE_NUM_PLAYERS = 'MODE_CHOOSE_NUM_PLAYERS';
+var MODE_CHOOSE_NUM_PLAYERS = 'MODE_CHOOSE_NUM_PLAYERS';
+var MODE_HIT_OR_STAND = 'MODE_HIT_OR_STAND';
+var MODE_COLLECT_BET = 'MODE_COLLECT_BET';
+var MODE_DEAL_CARDS = 'MODE_DEAL_CARDS';
 
 // Store player's cards and computer's cards in separate arrays
-var playerHand = [];
+// var playerHand = [];
 var computerHand = [];
-var mode = MODE_QUERY_PLAYER_NAME;
+var mode = MODE_CHOOSE_NUM_PLAYERS;
+
+// set a var that controls the index of the playerProfile that is currently playing the game. Start at 0 since arrays are 0 indexed
+var currPlayerIndex = 0;
 
 // The maximum valid sum in Blackjack is 21.
 var sumLimit = 21;
@@ -17,6 +25,12 @@ var dealerHitThreshold = 16;
 var playerHasChosenToStand = false;
 // Keep track of when the game ends to prevent further moves
 var gameOver = false;
+
+// Track num of players
+var numPlayers = 0;
+
+// Set an array that will contains details about each player (i.e. an array of objects)
+var playerProfiles = [];
 
 /*============================
 ======HELPER FUNCTIONS========
@@ -166,64 +180,290 @@ var convertHandToString = function (hand) {
  * @returns {String}
  */
 var getDefaultOutput = function () {
-  return `Player's hand: <br>
-  ${convertHandToString(playerHand)} <br>
-  Sum: ${getHandSum(playerHand)} 
+  return `${playerProfiles[currPlayerIndex].name}'s hand: <br>
+  ${convertHandToString(playerProfiles[currPlayerIndex].hand)} <br>
+  Sum: ${getHandSum(playerProfiles[currPlayerIndex].hand)} 
   <br><br>
   Computer's hand: ${convertHandToString(computerHand)} <br>
   Sum: ${getHandSum(computerHand)}`;
+};
+
+/** Create profiles for each player
+ */
+
+var createPlayerProfiles = function (playerName) {
+  playerProfiles.push({
+    id: currPlayerIndex + 1,
+    name: playerName,
+    hand: [],
+    bet: 0,
+    points: 100,
+  });
+};
+
+/** Deal cards to player and computer, one
+ */
+var dealCardsToPlayersAndComputer = function () {
+  for (var j = 0; j < 2; j += 1) {
+    // Iterate thru playerProfiles, deal 1 cards to each player's hand
+    for (var i = 0; i < playerProfiles.length; i += 1) {
+      dealCardToHand(playerProfiles[i].hand);
+    }
+    dealCardToHand(computerHand);
+  }
+};
+
+/** Handle bet win
+ * @returns {Array} Array of players who have a black jack
+ */
+var handleBetWin = function (isBlackjack) {
+  // Increase the points by a multiplier
+  // If player wins with a blackjack, multiplier is 1.5 times of betted amt
+  var winAmt = playerProfiles[currPlayerIndex].bet;
+  if (isBlackjack == true) {
+    winAmt = playerProfiles[currPlayerIndex].bet * 1.5;
+    playerProfiles[currPlayerIndex].points += winAmt;
+    return winAmt;
+  }
+  // else player did not win with blackjack, multiplier is 1 times of the betted amt
+  playerProfiles[currPlayerIndex].points += winAmt;
+  return winAmt;
+};
+
+var handleBetLose = function () {
+  // Decrease the points by the amount betted
+  playerProfiles[currPlayerIndex].points -= playerProfiles[currPlayerIndex].bet;
+  // Return the amount of money that the playerProfiles[currPlayerIndex] lost
+  return playerProfiles[currPlayerIndex].bet;
+};
+
+var craftInstructionsForNextSteps = function () {
+  // If the last player is playing, it's the end of the current game. Press refresh to start again
+  if (playerProfiles[currPlayerIndex].id == numPlayers) {
+    return 'Press refresh to play again';
+  }
+  // If the last player is not playing, cue the next person is play
+  return (
+    "It's " +
+    playerProfiles[currPlayerIndex + 1].name +
+    's turn next. Click submit to continue'
+  );
+};
+
+/** End a players turn by resetting relevant global variables and changing to an appropriate mode
+ */
+var endCurrPlayerTurn = function () {
+  // Start a new round-- change mode and reset currPlyerIndex
+  if (playerProfiles[currPlayerIndex].id == numPlayers) {
+    // Change mode to progress the game
+    mode = MODE_SCAN_FOR_BLACKJACK;
+    // reset playerHasChosenToStand
+    playerHasChosenToStand = !playerHasChosenToStand;
+    // Point the current player to first player
+    currPlayerIndex = 0;
+    return;
+  }
+
+  // else if current player is not the last player
+  // Reset the playerHasChosenToStand to false
+  playerHasChosenToStand = false;
+
+  // Change mode to progress the game
+  mode = MODE_SCAN_FOR_BLACKJACK;
+  // Move to the next player by incrementing the currPlayerIndex
+  currPlayerIndex += 1;
 };
 
 /*============================
 =======Main function==========
 =============================*/
 var main = function (input) {
+  //If a gameOver is true, display a message to prompt user to refresh the screen
   if (gameOver) {
     return 'The game is over. Please refresh to play again.';
   }
+
+  // Mode to get user input on no. of players
+  if (mode == MODE_CHOOSE_NUM_PLAYERS) {
+    // validate that user has provided an an integer larger than 0
+    if (isNaN(input) == true || !Number(input) > 0) {
+      return 'Please enter a number larger than 0';
+    }
+    // Convert user input from string to number, and assign it to the global var tracking the number of dice users have chosen to play with
+    numPlayers = Number(input);
+
+    //Progress game to next mode
+    mode = MODE_QUERY_PLAYER_NAME;
+    // Output a msg about the number of players the user has chosen
+    return (
+      'There are ' +
+      numPlayers +
+      ' players in this game. Player 1, please enter your name to begin.'
+    );
+  }
+
+  // Mode to get player names
   if (mode == MODE_QUERY_PLAYER_NAME) {
-    // Prompt user again if input is empty. The trim() method allows removes empty spaces before and after a word. Hence if the user inputs a series of empty spaces, it be trimemed to a single space i.e. ''
+    // For each of the players, get their name and create a player profile based on the name
+
+    // Prompt user again if input field was empty. The trim() method allows removes empty spaces before and after a word. Hence if the user inputs a series of empty spaces, it be trimemed to a single space i.e. ''
     if (input.trim() == '') {
       return 'Please input your name to begin';
     }
-    mode = MODE_PLAY_GAME;
+    // Assign input to a var called playerName to make it more descriptive
     playerName = input;
-    return 'Welcome, ' + playerName + '! Click submit to deal your cards.';
-  }
-  if (mode === MODE_PLAY_GAME) {
-    if (playerHand.length === 0) {
-      // If initial hands have not been dealt, deal initial hands
-      // User clicks submit button to deal cards.
-      // Deal first card for player then computer
-      dealCardToHand(playerHand);
-      dealCardToHand(computerHand);
 
-      // Deal second card for player then computer
-      dealCardToHand(playerHand);
-      dealCardToHand(computerHand);
+    // Create a player profile with this name
+    createPlayerProfiles(playerName);
 
-      // The cards are analyzed for any game winning conditions. (Blackjack)
-      // If computer has Blackjack, computer auto wins because computer is dealer
-      if (isBlackjack(computerHand)) {
-        gameOver = true;
-        // Computer wins, return
-        return `${getDefaultOutput()} <br>
-        Computer has Blackjack and wins. Please refresh to play again.`;
-      }
+    // If the last player has entered his name, prompt players to start entering bets.
+    if (playerProfiles[currPlayerIndex].id == numPlayers) {
+      // change the mode
+      mode = MODE_COLLECT_BET;
 
-      // If player has Blackjack and computer does not, player wins
-      if (isBlackjack(playerHand)) {
-        gameOver = true;
-        // Player wins, return
-        return `${getDefaultOutput()} <br>
-        Player has Blackjack and wins. Please refresh to play again.`;
-      }
+      // reset currPlayer Index;
+      currPlayerIndex = 0;
 
-      // The cards are displayed to the user.
-      return `${getDefaultOutput()} <br>
-      Please enter "hit" or "stand", then press Submit`;
+      // Create an output message
+      return (
+        'Welcome, ' +
+        playerProfiles[playerProfiles.length - 1].name +
+        '. <br><br>' +
+        playerProfiles[0].name +
+        ', you have ' +
+        playerProfiles[0].points +
+        ' points.<br> Please enter a bet.'
+      );
     }
 
+    // Else the last player has not keyed in their name. Prompt them to do so.
+    var myOutputValue =
+      'Welcome, ' +
+      playerProfiles[currPlayerIndex].name +
+      '. Player ' +
+      (playerProfiles[currPlayerIndex].id + 1) +
+      ', please enter your name. ';
+
+    // increase the currentPlayerIndex
+    currPlayerIndex += 1;
+
+    // Output the msg
+    return myOutputValue;
+  }
+
+  // Mode to collect user input on their bet amount
+  if (mode == MODE_COLLECT_BET) {
+    // validate that user has provided an an integer larger than 0
+    if (isNaN(input) == true || !Number(input) > 0) {
+      return 'Please enter a number larger than 0';
+    }
+
+    // validate that the bet amount is not larger than the amount of points this player has
+    if (input > playerProfiles[currPlayerIndex].points) {
+      return (
+        playerProfiles[currPlayerIndex].name +
+        ', you have ' +
+        playerProfiles[currPlayerIndex].points +
+        ' points. Please enter an amount less than or equal to this. '
+      );
+    }
+
+    // Assign the bet to the current player
+    playerProfiles[currPlayerIndex].bet = Number(input);
+
+    //If this is the final player output a msg to cue player 1 to start playing
+    if (playerProfiles[currPlayerIndex].id == numPlayers) {
+      //Progress game to next mode
+      mode = MODE_DEAL_CARDS;
+
+      // Set currPlayerIndex back to 0 so that he/she is the next player
+      currPlayerIndex = 0;
+
+      // Prompt user to click submit
+      return (
+        playerProfiles[playerProfiles.length - 1].name +
+        ", you've chosen to bet " +
+        playerProfiles[playerProfiles.length - 1].bet +
+        ' points. <br><br>' +
+        playerProfiles[currPlayerIndex].name +
+        ", you'll play first. Click submit to deal cards and see your cards"
+      );
+    }
+
+    // Else there are still players who have not entered their bet. Acknowledge the current player's bet and invite the next player to enter their bet.
+    var myOutputValue =
+      playerProfiles[currPlayerIndex].name +
+      ", you've chosen to bet " +
+      playerProfiles[currPlayerIndex].bet +
+      ' points. ' +
+      playerProfiles[currPlayerIndex + 1].name +
+      ', please enter your bet.';
+
+    // Increase the currPlayerIndex
+    currPlayerIndex += 1;
+
+    return myOutputValue;
+  }
+
+  // Mode to deal cards out to players and computer
+  if (mode == MODE_DEAL_CARDS) {
+    // Deal cards to all all players and computer
+    dealCardsToPlayersAndComputer();
+    mode = MODE_SCAN_FOR_BLACKJACK;
+  }
+
+  // Mode to play the game
+  if (mode === MODE_SCAN_FOR_BLACKJACK) {
+    // The cards are analyzed for Blackjack
+
+    // If computer has Blackjack, computer auto wins because computer is dealer
+    if (isBlackjack(computerHand)) {
+      //Deduct player points for losing.
+      var pointsLost = handleBetLose(playerProfiles[currPlayerIndex]);
+
+      // Computer wins, return
+      var myOutputValue = `${getDefaultOutput()} <br>
+        Computer has Blackjack and wins. ${
+          playerProfiles[currPlayerIndex].name
+        }, you lost ${pointsLost} points.<br><br>
+        Your current points: ${playerProfiles[currPlayerIndex].points} <br>
+        ${craftInstructionsForNextSteps()}`;
+
+      // End curr player's turn by incrementing currPlayerIndex
+      endCurrPlayerTurn();
+
+      return myOutputValue;
+    }
+
+    // If player has Blackjack and computer does not, player wins
+    if (isBlackjack(playerProfiles[currPlayerIndex].hand)) {
+      // handle winning the bet
+      var pointsWon = handleBetWin(playerProfiles[currPlayerIndex], true);
+
+      // current player wins, create an output value to inform users
+      var myOutputValue = `${getDefaultOutput()} <br>
+      ${
+        playerProfiles[currPlayerIndex].name
+      } has Blackjack and wins ${pointsWon} points. <br> Your current points: ${
+        playerProfiles[currPlayerIndex].points
+      } <br> ${craftInstructionsForNextSteps()}`;
+
+      // End curr player's turn by incrementing
+      endCurrPlayerTurn();
+
+      return myOutputValue;
+    }
+
+    // Change mode to progress game
+    mode = MODE_HIT_OR_STAND;
+
+    // Change the mode to progress the game
+    // The cards are displayed to the user.
+    return `${getDefaultOutput()} <br>
+      Please enter "hit" or "stand", then press Submit`;
+  }
+
+  if (mode == MODE_HIT_OR_STAND) {
     // Then begins a new action, where the user has to decide something: do they hit or stand.
     if (!playerHasChosenToStand) {
       // If user input is neither "hit" nor "stand" prompt user
@@ -233,17 +473,28 @@ var main = function (input) {
       }
       // Handle 'hit'
       if (input === 'hit') {
-        dealCardToHand(playerHand);
+        dealCardToHand(playerProfiles[currPlayerIndex].hand);
+
         // If bust, show player that she busts
-        if (getHandSum(playerHand) > sumLimit) {
-          gameOver = true;
-          return `${getDefaultOutput()} <br>
-    Player has busted and loses. Please refresh to play again.`;
+        if (getHandSum(playerProfiles[currPlayerIndex].hand) > sumLimit) {
+          // gameOver = true;
+          // Handle losing the bet. The function returns the num of points that were lost
+          var pointsLost = handleBetLose(playerProfiles[currPlayerIndex]);
+          var myOutputValue = `${getDefaultOutput()} <br> 
+          ${
+            playerProfiles[currPlayerIndex].name
+          } has busted and loses ${pointsLost} points.<br> Your current points: ${
+            playerProfiles[currPlayerIndex].points
+          } <br> ${craftInstructionsForNextSteps()}`;
+
+          // End curr player's turn by incrementing
+          endCurrPlayerTurn();
+
+          return myOutputValue;
         }
       }
-
       // Handle 'stand'
-      if (input === 'stand') {
+      if (input == 'stand') {
         playerHasChosenToStand = true;
       }
     }
@@ -252,36 +503,65 @@ var main = function (input) {
     // Computer hits if sum less than or equal to dealer hit threshold
     // Otherwise, computer stays
     var computerHandSum = getHandSum(computerHand);
+
     if (computerHandSum <= dealerHitThreshold) {
       dealCardToHand(computerHand);
       // Update computer hand sum after dealing new card
       computerHandSum = getHandSum(computerHand);
-      // If bust, show computer that she busts
-      if (computerHandSum > sumLimit) {
-        gameOver = true;
-        return `${getDefaultOutput()} <br>
-      Computer has busted and loses. Please refresh to play again.`;
-      }
+    }
+    // If bust, show computer that she busts
+    if (computerHandSum > sumLimit) {
+      // gameOver = true;
+      // Handle bet win
+      var pointsWon = handleBetWin(playerProfiles[currPlayerIndex], false);
+      var myOutputValue = `${getDefaultOutput()} <br>
+            Computer has busted and loses. ${
+              playerProfiles[currPlayerIndex].name
+            }, you won ${pointsWon}. <br> Your points:  ${
+        playerProfiles[currPlayerIndex].points
+      } <br>${craftInstructionsForNextSteps()}`;
+
+      // End curr player's turn by incrementing
+      endCurrPlayerTurn();
+
+      return myOutputValue;
     }
 
     // If player and computer have both not busted and chosen to stand, decide who wins
     if (playerHasChosenToStand && computerHandSum > dealerHitThreshold) {
       // The game is always over after this point
-      gameOver = true;
+      // gameOver = true  ;
+
       // If player hand sum is greater than computer hand sum, player wins!
-      if (getHandSum(playerHand) > computerHandSum) {
-        return `${getDefaultOutput()} <br>
-        Player wins! Please refresh to play again.`;
+      if (getHandSum(playerProfiles[currPlayerIndex].hand) > computerHandSum) {
+        // Handle bet win
+        var pointsWon = handleBetWin(playerProfiles[currPlayerIndex]);
+
+        var myOutputValue = `${getDefaultOutput()} <br>
+        ${
+          playerProfiles[currPlayerIndex].name
+        } wins ${pointsWon} points! Your points: ${
+          playerProfiles[currPlayerIndex].points
+        } <br> ${craftInstructionsForNextSteps()}`;
+        // End curr player's turn by incrementing
+      } else {
+        // Else, computer wins
+        var pointsLost = handleBetLose(playerProfiles[currPlayerIndex]);
+        var myOutputValue = `${getDefaultOutput()} <br>
+        Computer wins! ${
+          playerProfiles[currPlayerIndex].name
+        }, you lost ${pointsLost} points. <br> ${craftInstructionsForNextSteps()}`;
       }
-      // Else, computer wins
-      return `${getDefaultOutput()} <br>
-      Computer wins! Please refresh to play again.`;
+      endCurrPlayerTurn();
+
+      return myOutputValue;
     }
 
-    // If game is not yet over, show current game status
-    return `${getDefaultOutput()} <br>
-    playerHasChosenToStand is ${playerHasChosenToStand} <br>
-    If player has not yet chosen to stand, please enter "hit" or "stand". <br>
-    Else, press Submit to see Computer's next move.`;
+    if (playerHasChosenToStand) {
+      return `${getDefaultOutput()} <br>Click submit to see the computer's next move.`;
+    } else {
+      return `${getDefaultOutput()} <br>
+      Please enter "hit" or "stand". <br>      `;
+    }
   }
 };
