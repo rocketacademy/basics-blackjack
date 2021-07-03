7c8c104213abccd62cc65c5e65ce107efef61d0b
adzdam <adrianrdamian@gmail.com>
2021-07-03 10:33:04 +0800

    more comfortable up to betting

--- script.js
+++ script.js
@@ -1,122 +1,8 @@
-var initialCards = function (shuffledDeck, playerHand, computerHand) {
-  var counter = 0;
-  while (counter < 2) {
-    var playerCard = shuffledDeck.pop();
-    playerHand.push(playerCard);
-
-    var computerCard = shuffledDeck.pop();
-    computerHand.push(computerCard);
-
-    counter = counter + 1;
-  }
-  var computerCardSum = addCards(computerHand);
-  while (computerCardSum < 17) {
-    var computerCard = shuffledDeck.pop();
-    computerHand.push(computerCard);
-    computerCardSum = addCards(computerHand);
-  }
-
-  console.log("Player Cards:");
-  console.log(playerHand);
-  console.log("Computer Cards: ");
-  console.log(computerHand);
-};
-
-var addCards = function (cardsArray) {
-  var currentPlayerTotal = 0;
-  var counter = 0;
-  while (counter < cardsArray.length) {
-    var cardValue = cardsArray[counter].name;
-    if (cardValue == "jack" || cardValue == "queen" || cardValue == "king") {
-      cardValue = 10;
-      //change this later
-    } else if (cardValue == "ace") {
-      cardValue = 1;
-    }
-    currentPlayerTotal = currentPlayerTotal + Number(cardValue);
-
-    counter = counter + 1;
-  }
-  console.log(currentPlayerTotal);
-  return currentPlayerTotal;
-};
-
-var hitCard = function (currentPlayerArray) {
-  var additionalCard = shuffledDeck.pop();
-  currentPlayerArray.push(additionalCard);
-};
-
-var chooseAceValue = function (input, playerHand) {
-  var index = 0;
-  while (index < playerHand.length) {
-    if (playerHand[index].name == "ace") {
-      playerHand[index].name = input;
-    }
-    index = index + 1;
-  }
-};
-
-var compareSum = function (playerSum, computerSum) {
-  if (playerSum == 21) {
-    return `You Got: ${playerSum}<br> You won!`;
-  } else if (computerSum > 21) {
-    return `Computer Got: ${computerSum}! You won`;
-  } else if (computerSum > playerSum) {
-    return `Computer Total: ${computerTotal}<br>Your Total: ${playerSum}<br>You lost`;
-  } else if (computerSum < playerSum) {
-    return `Computer Total: ${computerTotal}<br>Your Total: ${playerSum}<br>You won`;
-  }
-};
-
-var gotAce = function (playerHand) {
-  var index = 0;
-  while (index < playerHand.length) {
-    if (playerHand[index].name == "ace") {
-      return true;
-    }
-    index = index + 1;
-  }
-};
-
-var resetGame = function () {
-  playerTotal = 0;
-  computerTotal = 0;
-  playerHand = [];
-  computerHand = [];
-  gameStart = true;
-  nowPlaying = false;
-  hit = false;
-  haveAce = false;
-};
-
-var showCards = function (playerHand) {
-  var index = 0;
-  var showCards = "Your cards are: ";
-  while (index < playerHand.length) {
-    var cardSuits = playerHand[index].suit;
-    var cardName = playerHand[index].name;
-    showCards = `${showCards}<br> ${cardName} of ${cardSuits}`;
-    index = index + 1;
-  }
-  return showCards;
-};
-
-var outPutMessages = function (playerHand, playerSum, haveAce) {
-  if (haveAce == true) {
-    return `${showCards(
-      playerHand
-    )}<br><br>Current Score: ${playerSum}<br>You got an ACE, Please choose if 1 or 11?`;
-  } else {
-    return `${showCards(
-      playerHand
-    )}<br><br>Current Score: ${playerSum}<br> stand or hit?`;
-  }
-};
 var makeDeck = function () {
   // Initialise an empty deck array
   var cardDeck = [];
   // Initialise an array of the 4 suits in our deck. We will loop over this array.
-  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  var suits = ["hearts ❤️", "diamonds 💎", "clubs ♣️", "spades♠️"];
   // Loop over the suits array
   var suitIndex = 0;
   while (suitIndex < suits.length) {
@@ -160,10 +46,6 @@ var makeDeck = function () {
   // Return the completed card deck
   return cardDeck;
 };
-// var deck = makeDeck()
-// var shuffledDeck = shuffleCards(deck)
-// var playerHand = [];
-// var computerHand = [];
 
 // Get a random index ranging from 0 (inclusive) to max (exclusive).
 var getRandomIndex = function (max) {
@@ -191,11 +73,197 @@ var shuffleCards = function (cardDeck) {
   return cardDeck;
 };
 
-//MAIN FUNCTION
+var initialCards = function (shuffledDeck, playerHand, computerHand) {
+  var counter = 0;
+  while (counter < 2) {
+    var playerCard = shuffledDeck.pop();
+    playerHand.push(playerCard);
+
+    var computerCard = shuffledDeck.pop();
+    computerHand.push(computerCard);
+
+    counter = counter + 1;
+  }
+  console.log("Player Cards:");
+  console.log(playerHand);
+  console.log("Computer Cards: ");
+  console.log(computerHand);
+};
+
+var hitOneCard = function (currentPlayerArray) {
+  var additionalCard = shuffledDeck.pop();
+  currentPlayerArray.push(additionalCard);
+};
+
+var sumOfCards = function (cardsArray) {
+  var currentPlayerTotal = 0;
+  var counter = 0;
+  while (counter < cardsArray.length) {
+    var cardValue = cardsArray[counter].name;
+    if (cardValue == "jack" || cardValue == "queen" || cardValue == "king") {
+      cardValue = 10;
+      //change this later
+    } else if (cardValue == "ace") {
+      cardValue = 1;
+    }
+    currentPlayerTotal = currentPlayerTotal + Number(cardValue);
+
+    counter = counter + 1;
+  }
+  console.log(currentPlayerTotal);
+  return currentPlayerTotal;
+};
+
+var gotAce = function (playerHand) {
+  var index = 0;
+  while (index < playerHand.length) {
+    if (playerHand[index].name == "ace") {
+      return true;
+    }
+    index = index + 1;
+  }
+};
+
+var chooseAceValue = function (input, playerHand) {
+  var index = 0;
+  while (index < playerHand.length) {
+    if (playerHand[index].name == "ace") {
+      playerHand[index].name = input;
+    }
+    index = index + 1;
+  }
+};
+
+var showCards = function (playerHand) {
+  var index = 0;
+  var showCards = "";
+  while (index < playerHand.length) {
+    var cardSuits = playerHand[index].suit;
+    var cardName = playerHand[index].name;
+    showCards = `${showCards}<br> ${cardName} of ${cardSuits}`;
+    index = index + 1;
+  }
+  return showCards;
+};
+
+var checkComputerHand = function (computerHand) {
+  var computerCardSum = sumOfCards(computerHand);
+  while (computerCardSum < 17) {
+    var computerCard = shuffledDeck.pop();
+    computerHand.push(computerCard);
+    computerCardSum = sumOfCards(computerHand);
+  }
+};
+
+var compareSum = function (playerHand, computerHand, playerPoints, playerBet) {
+  var playerSum = sumOfCards(playerHand);
+  var computerSum = sumOfCards(computerHand);
+  if (playerSum == 21) {
+    var playerWinnings = 1.5 * Number(playerBet);
+    playerPoints = Number(playerWinnings) + Number(playerPoints);
+    return `Your final cards: ${showCards(
+      playerHand
+    )}<br>Total cards: ${playerSum}<br> You won!<br> You got ${playerWinnings}<br> Your Total Points: ${playerPoints}`;
+  } else if (computerSum > 21 || computerSum < playerSum) {
+    var playerWinnings = playerBet;
+    playerPoints = Number(playerWinnings) + Number(playerPoints);
+    return `Your final cards: ${showCards(
+      playerHand
+    )}<br>Total cards: ${playerSum}<br><br>Computer Final Cards: ${showCards(
+      computerHand
+    )}<br>Comp Total${computerSum} You won!<br> You got ${playerWinnings}<br> Your Total Points: ${playerPoints}`;
+  } else if (computerSum > playerSum) {
+    playerPoints = Number(playerPoints) - Number(playerBet);
+    return `Your final cards: ${showCards(
+      playerHand
+    )}<br>Total cards: ${playerSum}<br><br>Computer Final Cards: ${showCards(
+      computerHand
+    )}<br>Comp Total: ${computerSum} You lost!<br> Dealer will take ${playerBet} bet <br> Your Total Points: ${playerPoints}`;
+  }
+};
+var outputMessage = function (
+  playerName,
+  playerPoints,
+  playerBet,
+  playerHand,
+  haveAce
+) {
+  playerSum = sumOfCards(playerHand);
+  if (haveAce == true) {
+    var outputMessage = `Player name:${playerName}<br>Total Points Remaining: ${playerPoints}<br><br> Bet for this round: ${playerBet} <br><br> ${showCards(
+      playerHand
+    )}<br><br>Current Card Total: ${playerSum}<br>You got an ACE, Please choose if 1 or 11?`;
+    return outputMessage;
+  } else {
+    var outputMessage = `Player name:${playerName}<br>Total Points Remaining: ${playerPoints}<br><br> Bet for this round: ${playerBet} <br><br> ${showCards(
+      playerHand
+    )}<br><br>Current Card Total: ${playerSum}<br><br>hit or stand?`;
+    return outputMessage;
+  }
+};
+
+var initialGameStage = function (currentGameState, input) {
+  if (currentGameState == "ask player name") {
+    currentGameState = "ask bet";
+    return "Welcome to Black Jack! <br> Please tell me your name.";
+  }
+  if (currentGameState == "ask bet") {
+    currentGameState = "game start";
+    playerName = input;
+    return `Hi ${playerName}! You have, ${playerPoints} points.<br><br>Place your bet! <br><br> Let's Play Black Jack! <br><br><br> Watch this video to know the game more: <br>https://www.youtube.com/watch?v=eyoh-Ku9TCI `;
+  }
+  if (currentGameState == "continue game") {
+    currentGameState = "game start";
+    playerName = input;
+    return `Hi ${playerName}! You have, ${playerPoints} points remaining.<br><br>Place your bet again!`;
+  }
+  if (currentGameState == "game start") {
+    if (isNaN(input) == false) {
+      if (input <= playerPoints) {
+        playerBet = input;
+      } else if (input > playerPoints) {
+        return `Your current point: ${playerPoints} Your bet exceeds your points. Please try again.<br><br> Place your bet!`;
+      }
+      initialCards(shuffledDeck, playerHand, computerHand);
+      playerTotal = sumOfCards(playerHand);
+      gameStart = false;
+      nowPlaying = true;
+      if (gotAce(playerHand) == true) {
+        haveAce = true;
+        return outputMessage(
+          playerName,
+          playerPoints,
+          playerBet,
+          playerHand,
+          haveAce
+        );
+      }
+      return outputMessage(
+        playerName,
+        playerPoints,
+        playerBet,
+        playerHand,
+        haveAce
+      );
+    }
+  }
+};
+
+var resetGame = function () {
+  playerTotal = 0;
+  computerTotal = 0;
+  playerHand = [];
+  computerHand = [];
+  gameStart = true;
+  nowPlaying = false;
+  hit = false;
+  haveAce = false;
+};
+
+//________________________MAIN____________________//
 
 var playerTotal = 0;
 var computerTotal = 0;
-var outGameMessage;
 var deck = makeDeck();
 var shuffledDeck = shuffleCards(deck);
 var playerHand = [];
@@ -204,45 +272,82 @@ var gameStart = true;
 var nowPlaying = false;
 var hit = false;
 var haveAce = false;
+var welcomePlayer = true;
+var gotPlayerName = false;
+var gotBet = false;
+var drawInitialCards = false;
+
+var playerName = "";
+var playerPoints = 100;
+var playerBet = 0;
+var currentGameState = "ask player name";
 
 var main = function (input) {
   if (gameStart == true) {
-    resetGame();
-    initialCards(shuffledDeck, playerHand, computerHand);
-    playerTotal = addCards(playerHand);
-    gameStart = false;
-    nowPlaying = true;
-    if (gotAce(playerHand) == true) {
-      haveAce = true;
-      outGameMessage = outPutMessages(playerHand, playerTotal, haveAce);
-      return outGameMessage;
+    if (welcomePlayer == true) {
+      currentGameState = "ask player name";
+      gotPlayerName = true;
+      welcomePlayer = false;
+      return initialGameStage(currentGameState, input);
+    } else if (gotPlayerName == true) {
+      currentGameState = "ask bet";
+      gotPlayerName = false;
+      drawInitialCards = true;
+      return initialGameStage(currentGameState, input);
+    } else if (drawInitialCards == true) {
+      resetGame();
+      currentGameState = "game start";
+      gotPlayerName = false;
+      return initialGameStage(currentGameState, input);
     }
-    outGameMessage = outPutMessages(playerHand, playerTotal, haveAce);
-    return outGameMessage;
   }
   if (nowPlaying == true && haveAce == true) {
     chooseAceValue(input, playerHand);
     haveAce = false;
-    playerTotal = addCards(playerHand);
-    outGameMessage = outPutMessages(playerHand, playerTotal, haveAce);
-    return outGameMessage;
+    return outputMessage(
+      playerName,
+      playerPoints,
+      playerBet,
+      playerHand,
+      haveAce
+    );
   }
   if (nowPlaying == true && haveAce == false) {
     if (input == "hit") {
-      hitCard(playerHand);
-      playerTotal = addCards(playerHand);
-      if (playerTotal > 21) {
-        return `Your total is ${playerTotal} You Lost`;
+      hitOneCard(playerHand);
+      if (gotAce(playerHand) == true) {
+        haveAce = true;
+        return outputMessage(
+          playerName,
+          playerPoints,
+          playerBet,
+          playerHand,
+          haveAce
+        );
       }
-      outGameMessage = outPutMessages(playerHand, playerTotal, haveAce);
-      return outGameMessage;
+      playerTotal = sumOfCards(playerHand);
+      if (playerTotal > 21) {
+        return `BUST! Your total is ${playerTotal} You Lost`;
+      }
+      return outputMessage(
+        playerName,
+        playerPoints,
+        playerBet,
+        playerHand,
+        haveAce
+      );
     } else if (input == "stand") {
-      playerTotal = addCards(playerHand);
-      computerTotal = addCards(computerHand);
-      outGameMessage = compareSum(playerTotal, computerTotal);
+      checkComputerHand(computerHand);
+      outputGameMessage = compareSum(
+        playerHand,
+        computerHand,
+        playerPoints,
+        playerBet
+      );
       nowPlaying = false;
       gameStart = true;
-      return outGameMessage;
+      drawInitialCards = true;
+      return outputGameMessage;
     }
   }
   return `Ooppsss Something Went Wrong, Try Again`;
