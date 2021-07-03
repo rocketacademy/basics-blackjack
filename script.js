328b43df0c1ef71a31e7063412bda11fde09bdc5
helloeddychan <eddy@newcampus.co>
2021-07-03 12:03:03 +0800

    blackjack working version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Create a standard 52-card deck
+// Create a standard 52-card deck
 var makeDeck = function () {
   // Initialise an empty deck array
   var cardDeck = [];
@@ -52,12 +52,16 @@ var makeDeck = function () {
   return cardDeck;
 };
 
-// Get a random index ranging from 0 (inclusive) to max (exclusive).
+/**
+ * Get a random index ranging from 0 (inclusive) to max (exclusive).
+ */
 var getRandomIndex = function (max) {
   return Math.floor(Math.random() * max);
 };
 
-// Shuffle elements in the cardDeck array. Return the shuffled deck.
+/**
+ * Shuffle elements in the cardDeck array. Return the shuffled deck.
+ */
 var shuffleCards = function (cardDeck) {
   // Loop over the card deck array once
   var currentIndex = 0;
@@ -77,24 +81,26 @@ var shuffleCards = function (cardDeck) {
   // Return the shuffled deck
   return cardDeck;
 };
+//initialise shuffled card deck before the game starts
+var deck = shuffleCards(makeDeck());
 
 //Set up Blackjack rules
 // The maximum valid sum in Blackjack is 21.
 var cardSumLimit = 21;
 // Dealer always hits until she reaches a sum greater than 16.
-var dealerPlayUntil = 16;
+var dealerHitThreshold = 16;
 // If player has chosen to stand, then player can no longer hit until game is over
-var playerHasChosenToStand = false;
+var playerStand = false;
 // Keep track of when the game ends to prevent further moves
 var gameOver = false;
 
 //hand arrays for player and computer
 var playerHand = [];
-var dealerHand = [];
+var computerHand = [];
 
 //deal card function
 var dealCard = function (hand) {
-  hand.push(cardDeck.pop());
+  hand.push(deck.pop());
 };
 
 //calculating value of cards on "hand"
@@ -132,7 +138,7 @@ var cardValue = function (hand) {
 
 //game ends if hand contains blackjack
 var blackJack = function (hand) {
-  return hand.length == 2 && cardValue(hand) == cardSumLimit;
+  return hand.length === 2 && cardValue(hand) === cardSumLimit;
 };
 
 //convert hand array to string
@@ -140,101 +146,108 @@ var handString = function (hand) {
   return `[${hand.map((card) => card.name)}]`;
 };
 
-var defaultOutPut = function () {
-  return `Player has hand ${convertHandToString(
+var getDefaultOutput = function () {
+  return `Player has hand ${handString(playerHand)} with sum ${cardValue(
     playerHand
-  )} with sum ${getHandSum(playerHand)}. <br>
-    Dealer has hand ${convertHandToString(dealerHand)} with sum ${getHandSum(
-    dealerHand
+  )}. <br>
+    Dealer has hand ${handString(computerHand)} with sum ${cardValue(
+    computerHand
   )}.`;
 };
 var currentMode = 0;
 
 var main = function (input) {
-  if (input == "") {
-    currentMode = 1;
-  }
   if (gameOver) {
-    return `The game is over. Please refresh browser to play again.`;
-  }
-  //If hands == 0, deal hand
-  if ((currentMode = 1)) {
-    //first card
-    dealCard(playerHand);
-    dealCard(dealerHand);
-    //second card
-    dealCard(playerHand);
-    dealCard(dealerHand);
-  }
-  //dealer blackjack
-  if (blackJack(dealerHand)) {
-    gameOver = true;
-    return `${defaultOutput()} <br>
-        Dealer has Blackjack and wins. Please refresh to play again.`;
-  }
-  //player blackjack
-  if (blackJack(playerHand)) {
-    gameOver = true;
-    return `${defaultOutput()} <br>
-        Player has Blackjack and wins. Please refresh to play again.`;
+    return "The game is over. Please refresh to play again.";
   }
 
-  // The cards are displayed to the user.
-  return `${defaultOutput()} <br>
-      Please enter "hit" or "stand", then press Submit`;
-};
-// hit or stand
-var dealingTime = function (input) {
-  if (!playerStand) {
-    // Check if input is hit or stand
-    if (input != "hit" && input != "stand") {
-      return "Please enter either hit or stand as possible moves in Blackjack";
+  // If initial hands have not been dealt, deal initial hands
+  if (playerHand.length === 0) {
+    // User clicks submit button to deal cards.
+    // Deal first card for player then computer
+    dealCard(playerHand);
+    dealCard(computerHand);
+
+    // Deal second card for player then computer
+    dealCard(playerHand);
+    dealCard(computerHand);
+
+    // The cards are analyzed for any game winning conditions. (Blackjack)
+    // If computer has Blackjack, computer auto wins because computer is dealer
+    if (blackJack(computerHand)) {
+      gameOver = true;
+      // Computer wins, return
+      return `${getDefaultOutput()} <br>
+        Computer has Blackjack and wins. Please refresh to play again.`;
     }
 
-    if (input == "hit") {
+    // If player has Blackjack and computer does not, player wins
+    if (blackJack(playerHand)) {
+      gameOver = true;
+      // Player wins, return
+      return `${getDefaultOutput()} <br>
+        Player has Blackjack and wins. Please refresh to play again.`;
+    }
+
+    // The cards are displayed to the user.
+    return `${getDefaultOutput()} <br>
+      Please enter "hit" or "stand", then press Submit`;
+  }
+
+  // Then begins a new action, where the user has to decide something: do they hit or stand.
+  if (!playerStand) {
+    // If user input is neither "hit" nor "stand" prompt user
+    if (input !== "hit" && input !== "stand") {
+      return 'Please input either "hit" or "stand" as possible moves in Blackjack';
+    }
+
+    if (input === "hit") {
       dealCard(playerHand);
-      // If bust, show player that he busts
+      // If bust, show player that she busts
       if (cardValue(playerHand) > cardSumLimit) {
         gameOver = true;
-        return `${defaultOutput()} <br>
+        return `${getDefaultOutput()} <br>
           Player has busted and loses. Please refresh to play again.`;
       }
     }
-  }
-  if (input == "stand") {
-    playerStand = true;
+
+    if (input === "stand") {
+      playerStand = true;
+    }
   }
 
-  // Automate for dealer
-  var dealerHandSum = cardValue(dealerHand);
-  if (dealerHandSum <= dealerHitThreshold) {
-    dealCardToHand(computerHand);
+  // The computer also decides to hit or stand.
+  // Computer hits if sum less than or equal to dealer hit threshold
+  // Otherwise, computer stays
+  var computerHandSum = cardValue(computerHand);
+  if (computerHandSum <= dealerHitThreshold) {
+    dealCard(computerHand);
     // Update computer hand sum after dealing new card
-    dealerHandSum = cardValue(dealerHand);
+    computerHandSum = cardValue(computerHand);
     // If bust, show computer that she busts
-    if (dealerHandSum > sumLimit) {
+    if (computerHandSum > cardSumLimit) {
       gameOver = true;
-      return `${defaultOutput()} <br>
-      Dealer has busted and loses. Please refresh to play again.`;
+      return `${getDefaultOutput()} <br>
+      Computer has busted and loses. Please refresh to play again.`;
     }
   }
 
   // If player and computer have both not busted and chosen to stand, decide who wins
-  if (playerStand && dealerHandSum > dealerHitThreshold) {
+  if (playerStand && computerHandSum > dealerHitThreshold) {
     // The game is always over after this point
     gameOver = true;
     // If player hand sum is greater than computer hand sum, player wins!
     if (cardValue(playerHand) > computerHandSum) {
-      return `${defaultOutput()} <br>
+      return `${getDefaultOutput()} <br>
         Player wins! Please refresh to play again.`;
     }
     // Else, computer wins
-    return `${defaultOutput()} <br>
-      Dealer wins! Please refresh to play again.`;
+    return `${getDefaultOutput()} <br>
+      Computer wins! Please refresh to play again.`;
   }
 
   // If game is not yet over, show current game status
-  return `${defaultOutput()} <br>
+  return `${getDefaultOutput()} <br>
     playerStand is ${playerStand} <br>
     If player has not yet chosen to stand, please enter "hit" or "stand". <br>
     Else, press Submit to see Computer's next move.`;
