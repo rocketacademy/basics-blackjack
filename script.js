3b60cb6e76f073e8c69fde5bd2d22775f5477285
simyisheng <simyisheng826@gmail.com>
2021-08-03 23:52:13 +0800

    edit for chips

--- script.js
+++ script.js
@@ -8,194 +8,194 @@
 // The game either ends or continues.
 
 //Game State, default at start
-var gameState ='betState'
+var gameState = "betState";
 
 //Global array for computer and player
+var comHand = [];
+var playerHand = [];
 
-var comHand = []
-var playerHand = []
-var playerScore = 0
-var comScore = 0
-var playerBet = 0
-var playerChips = 100
+//Global variable for player chips and bet amount
+var playerBet = 0;
+var playerChips = 100;
 
-var BLACKJACK_LIMIT = 21
+//Blackjack limit
+var BLACKJACK_LIMIT = 21;
 
 //Initialize deck
-var shuffledDeck = shuffleCards(makeDeck())
+var shuffledDeck = shuffleCards(makeDeck());
 var main = function (input) {
-  
-  
-  
-  
-  //Start of game
-
-
-  if(gameState== 'betState'){
-    playerBet = Number(input)
-    if(isNaN(playerBet)||playerBet>playerChips){
-      return `Error! Please enter a number!`
+  //Take first input at bet amount
+  if (gameState == "betState") {
+    playerBet = Number(input);
+    if (isNaN(playerBet)) {
+      return `Error! Please enter a number!`;
     }
-    gameState ='drawState'
-    return `You chose to bet ${playerBet} amount of chips.`
+    if (playerBet > playerChips) {
+      return `You do not have that much chips! Current chips: ${playerChips}`;
+    }
+    //Move on to next state
+    gameState = "drawState";
+    return `You chose to bet ${playerBet} amount of chips.`;
   }
 
-  if(gameState == 'drawState'){
-    
+  //State to draw initial hand
+  if (gameState == "drawState") {
     //Initialize both computer and player hand
-    initializeHand(shuffledDeck)
-    console.log(`Player Initial Score: ${addScore(playerHand)}`)
-    console.log(`Com Initial Score: ${addScore(comHand)}`)
+    initializeHand(shuffledDeck);
+    console.log(`Player Initial Score: ${addScore(playerHand)}`);
+    console.log(`Com Initial Score: ${addScore(comHand)}`);
     //Check if blackjack is obtained
-    if(addScore(playerHand)==BLACKJACK_LIMIT||addScore(comHand)==BLACKJACK_LIMIT){
-    return `${blackJack(addScore(playerHand),addScore(comHand))}<br>Player drew<br>${cardPrinter(playerHand)}<br>Computer drew<br>${cardPrinter(comHand)}`
-    }
-    else{
+    if (
+      addScore(playerHand) == BLACKJACK_LIMIT ||
+      addScore(comHand) == BLACKJACK_LIMIT
+    ) {
+      resetState();
+      return `${blackJack(
+        addScore(playerHand),
+        addScore(comHand)
+      )}<br>Player drew<br>${cardPrinter(
+        playerHand
+      )}<br>Computer drew<br>${cardPrinter(comHand)}`;
+    } else {
       //Move on to next stage of game
-      gameState ='playState'
+      gameState = "playState";
       //Display player current hand
-      return `Player drew <br>${cardPrinter(playerHand)}<br>Please choose to hit or stand`
+      return `Player drew <br>${cardPrinter(
+        playerHand
+      )}<br>Please choose to hit or stand`;
     }
   }
-  
-  
-  if(gameState== 'playState'){
-    var playerChoice = input.toLowerCase()
-    //Return error if player does not enter one of the two choices 
-    if(playerChoice!='hit'&&playerChoice!='stand'){
-      return `Error! Please choose to hit (draw another card) or stand (keep playing with current hand)`
+
+  if (gameState == "playState") {
+    var playerChoice = input.toLowerCase();
+    //Return error if player does not enter one of the two choices
+    if (playerChoice != "hit" && playerChoice != "stand") {
+      return `Error! Please choose to hit (draw another card) or stand (keep playing with current hand)`;
     }
     //If player choose to hit, draw another card for player
-    if(playerChoice == 'hit'){
-      playerHand[2] = shuffledDeck.pop()
+    if (playerChoice == "hit") {
+      playerHand[2] = shuffledDeck.pop();
     }
-    console.log(`Player Score: ${addScore(playerHand)}`)
+    console.log(`Player Score: ${addScore(playerHand)}`);
     //If player has more than 21, lose immediately
-    if(addScore(playerHand)>BLACKJACK_LIMIT){
-      playerChips-=playerBet
-      var results = `Player Burst! Player drew<br>${cardPrinter(playerHand)}<br>You now have ${playerChips} amount of chips. Press Submit to play again!`
-      resetState()
-      return results
+    if (addScore(playerHand) > BLACKJACK_LIMIT) {
+      playerChips -= playerBet;
+      var results = `Player Burst! Player drew<br>${cardPrinter(
+        playerHand
+      )}<br>You now have ${playerChips} amount of chips. Press Submit to play again!`;
+      resetState();
+      return results;
     }
     //If computer score is lower than 16, draw another card
-    if(addScore(comHand)<=16){
-      comHand[2]= shuffledDeck.pop()
+    if (addScore(comHand) <= 16) {
+      comHand[2] = shuffledDeck.pop();
     }
-    gameState = 'resultState'
-    console.log(comHand)
-    return `Player currently has ${addScore(playerHand)}<br>${cardPrinter(playerHand)}`    
+    gameState = "resultState";
+    console.log(comHand);
+    return `Player currently has ${addScore(playerHand)}<br>${cardPrinter(
+      playerHand
+    )}`;
   }
-  if(gameState == 'resultState'){
-    var results = `${compareScore(addScore(playerHand),addScore(comHand))}<br>You now have ${playerChips} chips. Press submit to play again!`
-    resetState()
-    return results
+  if (gameState == "resultState") {
+    var results = `${compareScore(
+      addScore(playerHand),
+      addScore(comHand)
+    )}<br>You now have ${playerChips} chips. Press submit to play again!`;
+    resetState();
+    return results;
   }
-  
-}
-
+};
 
 //Function to reset game
-function resetState(){
-  playerHand = []
-  comHand = []
-  playerScore =0
-  comScore = 0
-  gameState = 'bet'
-  return `Press Submit to play again!`
+function resetState() {
+  playerHand = [];
+  comHand = [];
+  playerScore = 0;
+  comScore = 0;
+  gameState = "betState";
+  return `Press Submit to play again!`;
 }
-
 
 //Function to print cards drawn
-function cardPrinter(list){
-  message = ''
-  for(i=0;i<list.length;i++){
-    message += `${list[i].name} of ${list[i].suit}<br>`
+function cardPrinter(list) {
+  message = "";
+  for (i = 0; i < list.length; i++) {
+    message += `${list[i].name} of ${list[i].suit}<br>`;
   }
-  return message 
+  return message;
 }
-
 
 //function to add score from value of cards
-function addScore(hand){
-  var score = 0 
-  var numberOfAce = 0
-  
-  
-  for(i=0;i<hand.length;i++){
-    if(hand[i].rank==1){
-      numberOfAce +=1
+function addScore(hand) {
+  var score = 0;
+  var numberOfAce = 0;
+
+  for (i = 0; i < hand.length; i++) {
+    if (hand[i].rank == 1) {
+      numberOfAce += 1;
     }
-    if(hand[i].rank>10){
-      score +=10;
-    }else {
-    score = score + hand[i].rank
-    }
-  }
-  for(i=0;i<numberOfAce;i++){
-    if(score+10<=BLACKJACK_LIMIT){
-      score+=10
+    if (hand[i].rank > 10) {
+      score += 10;
+    } else {
+      score = score + hand[i].rank;
     }
   }
-  return score
+  for (i = 0; i < numberOfAce; i++) {
+    if (score + 10 <= BLACKJACK_LIMIT) {
+      score += 10;
+    }
+  }
+  return score;
 }
-
-
-
 
 //Function to compare score
-function compareScore(player,com){
-  if(player>com){
-    playerChips += playerBet
-    return `Player Wins!🎊<br>`
-  }
-  else if(com>player){
-    playerChips -= playerBet
-    return `Computer Wins!🎊<br>`
-  }
-  else{
-    return `Draw!🃏`
+function compareScore(player, com) {
+  if (player > com) {
+    playerChips += playerBet;
+    return `Player Wins!🎊<br>`;
+  } else if (com > player) {
+    playerChips -= playerBet;
+    return `Computer Wins!🎊<br>`;
+  } else {
+    return `Draw!🃏`;
   }
 }
 
-
-
-
-//Checker for BlackJack 
-function blackJack(player,com){
-  var blackJackMessage = ''
-  if(player == 21 || com==21){
-    if(player == 21 && com==21){
-    blackJackMessage += `Both BlackJack!🤩🤩 WOW!`
-    } 
-    else if(com ==21){
-    blackJackMessage += `Computer BlackJack!🤩`
-    }
-    else{
-    blackJackMessage += `Player BlackJack!🤩`
+//Checker for BlackJack
+function blackJack(player, com) {
+  var blackJackMessage = "";
+  if (player == 21 || com == 21) {
+    if (player == 21 && com == 21) {
+      blackJackMessage += `Both BlackJack!🤩🤩 WOW!`;
+    } else if (com == 21) {
+      playerChips -= 2 * playerBet;
+      blackJackMessage += `Computer BlackJack!🤩`;
+    } else {
+      playerChips += 2 * playerBet;
+      blackJackMessage += `Player BlackJack!🤩`;
     }
   }
-  return blackJackMessage 
+  return `${blackJackMessage} You now have ${playerChips} left.`;
 }
-
 
 //Initialize hands
-function initializeHand(deck){
-  comHand = []
-  playerHand = []
-for(i=0;i<2;i++){
-  comHand[i] = [{name:'',suit:'',rankCounter:0,value:0}]
-  playerHand[i] = [{name:'',suit:'',rankCounter:0,value:0}]
-  comHand[i] = deck.pop()
-  playerHand[i]=deck.pop()
-}
+function initializeHand(deck) {
+  comHand = [];
+  playerHand = [];
+  for (i = 0; i < 2; i++) {
+    comHand[i] = [{ name: "", suit: "", rankCounter: 0, value: 0 }];
+    playerHand[i] = [{ name: "", suit: "", rankCounter: 0, value: 0 }];
+    comHand[i] = deck.pop();
+    playerHand[i] = deck.pop();
+  }
 
-return comHand, playerHand
+  return comHand, playerHand;
 }
 
 //Get random number from range of max
 function getRandomIndex(max) {
   return Math.floor(Math.random() * max);
-};
+}
 
 // Shuffle the elements in the cardDeck array
 function shuffleCards(cardDeck) {
@@ -216,14 +216,14 @@ function shuffleCards(cardDeck) {
   }
   // Return the shuffled deck
   return cardDeck;
-};
+}
 
 //Function to create deck
-function makeDeck(){
+function makeDeck() {
   // Initialise an empty deck array
   var cardDeck = [];
   // Initialise an array of the 4 suits in our deck. We will loop over this array.
-  var suits = [`heart ❤️`, 'diamonds ♦', 'clubs ♣', 'spades ♤'];
+  var suits = [`heart ❤️`, "diamonds ♦", "clubs ♣", "spades ♤"];
   // Loop over the suits array
   var suitIndex = 0;
   while (suitIndex < suits.length) {
@@ -236,26 +236,22 @@ function makeDeck(){
     while (rankCounter <= 13) {
       // By default, the card name is the same as rankCounter
       var cardName = rankCounter;
-      
+
       // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
       if (cardName == 1) {
-        cardName = 'ace';
-        
+        cardName = "ace";
       } else if (cardName == 11) {
-        cardName = 'jack';
-        
+        cardName = "jack";
       } else if (cardName == 12) {
-        cardName = 'queen';
-        
+        cardName = "queen";
       } else if (cardName == 13) {
-        cardName = 'king';
+        cardName = "king";
       }
       // Create a new card with the current name, suit, and rank
       var card = {
         name: cardName,
         suit: currentSuit,
         rank: rankCounter,
-       
       };
       // Add the new card to the deck
       cardDeck.push(card);
