1e84815c668ebf8b1c98659c628d5d26f58089e8
kiidia <kdiandri@gmail.com>
2021-08-09 22:12:10 +0700

    working blackjack game

--- script.js
+++ script.js
@@ -1,265 +1,56 @@
-var deck = [
-  {
-    name: "ace",
-    suit: "hearts",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "hearts",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "hearts",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "hearts",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "hearts",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "hearts",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "hearts",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "hearts",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "hearts",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "hearts",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "hearts",
-    rank: 11,
-  },
-  {
-    name: "queen",
-    suit: "hearts",
-    rank: 12,
-  },
-  {
-    name: "king",
-    suit: "hearts",
-    rank: 13,
-  },
-  {
-    name: "ace",
-    suit: "diamonds",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "diamonds",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "diamonds",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "diamonds",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "diamonds",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "diamonds",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "diamonds",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "diamonds",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "diamonds",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "diamonds",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "diamonds",
-    rank: 11,
-  },
-  {
-    name: "queen",
-    suit: "diamonds",
-    rank: 12,
-  },
-  {
-    name: "king",
-    suit: "diamonds",
-    rank: 13,
-  },
-  {
-    name: "ace",
-    suit: "clubs",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "clubs",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "clubs",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "clubs",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "clubs",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "clubs",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "clubs",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "clubs",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "clubs",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "clubs",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "clubs",
-    rank: 11,
-  },
-  {
-    name: "queen",
-    suit: "clubs",
-    rank: 12,
-  },
-  {
-    name: "king",
-    suit: "clubs",
-    rank: 13,
-  },
-  {
-    name: "ace",
-    suit: "spades",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "spades",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "spades",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "spades",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "spades",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "spades",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "spades",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "spades",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "spades",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "spades",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "spades",
-    rank: 11,
-  },
-  {
-    name: "queen",
-    suit: "spades",
-    rank: 12,
-  },
-  {
-    name: "king",
-    suit: "spades",
-    rank: 13,
-  },
-];
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+    console.log("current suit", currentSuit);
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
 // Get a random index ranging from 0 (inclusive) to max (exclusive).
 var getRandomIndex = function (max) {
   return Math.floor(Math.random() * max);
@@ -287,86 +78,133 @@ var shuffleCards = function (cardDeck) {
 };
 // Shuffle the deck and save it in a new variable shuffledDeck
 // to communicate that we have shuffled the deck.
-var shuffledDeck = shuffleCards(deck);
-// // Initialise index to 0 to start from the beginning of the array
-// var index = 0;
-
-// // Define loop condition to loop until index is the length of cardDeck
-// while (index < cardDeck.length) {
-//   // Access attributes of each card with dot notation.
-//   console.log(cardDeck[index].name);
-//   // Construct a string using attributes of each card object
-//   var cardTitle = cardDeck[index].name + " of " + cardDeck[index].suit;
-//   // Log the string
-//   console.log(cardTitle);
-//   // Increment the card index
-//   index = index + 1;
-// }
+var cardDeck = makeDeck();
+var shuffledDeck = shuffleCards(cardDeck);
 
 var playerArray = [];
 var computerArray = [];
 var playerScore = 0;
 var computerScore = 0;
+var currentGameMode = "deal cards";
+
+var showPlayersDeck = function (playerArray) {
+  var message = `Here is your current deck <br><br>`;
+  var messageCounter = 0;
+  while (messageCounter < playerArray.length) {
+    message =
+      message +
+      `${playerArray[messageCounter].rank} of ${playerArray[messageCounter].suit}, `;
+    messageCounter = messageCounter + 1;
+  }
+  return message;
+};
+
+var calculateScore = function (cardArray) {
+  if (cardArray == playerArray) {
+    var scoreCounterPlayer = 0;
+    while (scoreCounterPlayer < cardArray.length) {
+      playerScore = playerScore + playerArray[scoreCounterPlayer].rank;
+      scoreCounterPlayer = scoreCounterPlayer + 1;
+    }
+  } else if (cardArray == computerArray) {
+    var scoreCounterComputer = 0;
+    while (scoreCounterComputer < computerArray.length) {
+      computerScore = computerScore + computerArray[scoreCounterComputer].rank;
+      scoreCounterComputer = scoreCounterComputer + 1;
+    }
+  }
+};
+
 var main = function (input) {
-  // // Draw 2 cards from the top of the deck
-  // var computerCard = shuffledDeck.pop();
-  // var playerCard = shuffledDeck.pop();
+  var myOutputValue = "";
+  //Deal Cards
+  if (currentGameMode == "deal cards") {
+    var computerCounter = 0;
+    while (computerCounter < 2) {
+      var computerCard = shuffledDeck.pop();
+      computerArray.push(computerCard);
+      computerCounter = computerCounter + 1;
+    }
+    console.log("computer array", computerArray);
+    console.log("player array", playerArray);
 
-  // //push cards into array
-  // playerArray.push(playerCard);
-  // computerArray.push(computerCard);
-  // console.log("player array", playerArray);
-  // console.log("computer array", computerArray);
+    var playerCounter = 0;
+    while (playerCounter < 2) {
+      var playerCard = shuffledDeck.pop();
+      playerArray.push(playerCard);
+      playerCounter = playerCounter + 1;
+    }
 
-  var computerCounter = 0;
-  while (computerCounter < 2) {
-    var computerCard = shuffledDeck.pop();
-    computerArray.push(computerCard);
-    computerCounter = computerCounter + 1;
-  }
-  console.log("computer array", computerArray);
-
-  var playerCounter = 0;
-  while (playerCounter < 2) {
-    var playerCard = shuffledDeck.pop();
-    playerArray.push(playerCard);
-    playerCounter = playerCounter + 1;
-  }
-  console.log("player array", playerArray);
-
-  //Getting Player's Score
-  var playerScoreCounter = 0;
-  while (playerScoreCounter < 2) {
+    //getting player score
+    calculateScore(playerArray);
     console.log("player score", playerScore);
-    playerScore = playerScore + playerArray[playerScoreCounter].rank;
+
+    //getting computer score
+    calculateScore(computerArray);
+    console.log("computer score", computerScore);
+
+    myOutputValue = `${showPlayersDeck(playerArray)} <br><br>
+    Your current score is ${playerScore}.`;
+    currentGameMode = "user wants to hit";
+  }
+
+  //Player wants to hit
+  if (currentGameMode == "user wants to hit") {
+    if (input == "hit") {
+      var extraPlayerCard = shuffledDeck.pop();
+      playerArray.push(extraPlayerCard);
+      playerScore = playerScore + extraPlayerCard.rank;
+      console.log("player hand", playerArray);
+      console.log("player score", playerScore);
+      myOutputValue = `${showPlayersDeck(playerArray)} <br><br>
+    Your current score is ${playerScore}.`;
+    } else if (input == "stand") {
+      currentGameMode = "computer wants to hit";
+    }
+  }
+  //Computer wants to hit
+  if (currentGameMode == "computer wants to hit") {
+    while (computerScore <= 17) {
+      var extraComputerCard = shuffledDeck.pop();
+      computerArray.push(extraComputerCard);
+      computerScore = computerScore + extraComputerCard.rank;
+    }
+    console.log("computer hand", computerArray);
+    console.log("computer score", computerScore);
+    currentGameMode = "comparing scores";
+  }
+
+  if (currentGameMode == "comparing scores") {
+    // Construct an output string to communicate which cards were drawn
+    var myOutputValue = `Computer has ${computerScore} <br><br>
+    Player has ${playerScore} <br><br>`;
+
+    // Compare computer and player cards by rank attribute
+    // If computer card rank is greater than player card rank, computer wins
+    if (
+      (playerScore > computerScore && playerScore < 21) ||
+      computerScore > 21
+    ) {
+      myOutputValue =
+        myOutputValue +
+        "Congratulations! Player wins! <br><br> Click Submit to play again.";
+      // Else if computer card rank is less than player card rank, player wins
+    } else if (
+      (playerScore < computerScore && computerScore < 21) ||
+      playerScore > 21
+    ) {
+      myOutputValue =
+        myOutputValue +
+        "Computer wins! Better Luck Next Time! <br><br> Click Submit to play again.";
+      // Otherwise (i.e. ranks are equal), it's a tie
+    } else {
+      myOutputValue =
+        myOutputValue + "It's a tie. <br><br>Click Submit to play again.";
+    }
     console.log("player score", playerScore);
-    playerScoreCounter = playerScoreCounter + 1;
-  }
-
-  var computerScoreCounter = 0;
-  while (computerScoreCounter < 2) {
     console.log("computer score", computerScore);
-    computerScore = computerScore + computerArray[computerScoreCounter].rank;
-    console.log("computer score", computerScore);
-    computerScoreCounter = computerScoreCounter + 1;
+
+    currentGameMode = "deal cards";
   }
-
-  // // Construct an output string to communicate which cards were drawn
-  var myOutputValue = `Computer had ${computerScore} <br><br>
-    Player had ${playerScore} <br><br>`;
-
-  // Compare computer and player cards by rank attribute
-  // If computer card rank is greater than player card rank, computer wins
-  if (playerScore > computerScore) {
-    myOutputValue = myOutputValue + "Player wins.";
-    // Else if computer card rank is less than player card rank, player wins
-  } else if (playerScore < computerScore) {
-    myOutputValue = myOutputValue + "Computer wins!";
-    // Otherwise (i.e. ranks are equal), it's a tie
-  } else {
-    myOutputValue = myOutputValue + "It's a tie.";
-  }
-
-  // Return the fully-constructed output string
   return myOutputValue;
 };
