15f72592833641d2570b474e4c12635ceffacc85
porkchop4 <hohanfan@gmail.com>
2021-08-09 13:33:59 +0800

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,83 @@
+var deckOfCards = [];
+var suits = ["hearts", "diamonds", "clubs", "spades"];
+var cardCounter = 1;
+var suitCounter = 0;
+var scoreCounter = 1;
+
+var createDeck = function () {
+  while (suitCounter <= suits.length - 1) {
+    var cardName = cardCounter;
+    cardSuit = suits[suitCounter];
+    if (cardCounter == 1) {
+      cardName = "Ace";
+    }
+    if (cardCounter == 11) {
+      cardName = "Jack";
+    }
+    if (cardCounter == 12) {
+      cardName = "Queen";
+    }
+    if (cardCounter == 13) {
+      cardName = "King";
+    }
+    var card = {
+      name: cardName,
+      suit: cardSuit,
+      score: scoreCounter,
+    };
+    deckOfCards.push(card);
+    if (cardCounter == 13) {
+      cardCounter = 1;
+      suitCounter += 1;
+      scoreCounter = 1;
+    } else {
+      cardCounter += 1;
+      scoreCounter += 1;
+    }
+  }
+  return deckOfCards;
+};
+
+//var shuffleCounter = 0;
+var shuffleDeck = function (cards) {
+  var shuffleCounter = 0;
+  // can shuffle any number of times, assume length of deck for now
+  while (shuffleCounter <= cards.length) {
+    var firstIndex = Math.floor(Math.random() * cards.length);
+    var secondIndex = Math.floor(Math.random() * cards.length);
+    var tempCard = cards[firstIndex];
+    cards[firstIndex] = cards[secondIndex];
+    cards[secondIndex] = tempCard;
+    shuffleCounter += 1;
+  }
+  return cards;
+};
+
+var playersCards = [];
+var computersCards = [];
+
+var dealCard = function (cards, deck) {
+  cards.push(deck.pop());
+};
+
+var sumOfHand = function (hand) {
+  handCounter = 0;
+  handSum = 0;
+  while (handCounter <= hand.length) {
+    handSum += hand[handCounter].scoreCounter;
+    handCounter += 1;
+  }
+  return handSum;
+};
+
 var main = function (input) {
   if (playersCards.length == 0) {
-    shuffleDeck(createDeck());
-    dealCard(playersCards);
-    dealCard(computersCards);
-    dealCard(playersCards);
-    dealCard(computersCards);
+    deckOfCards = shuffleDeck(createDeck());
+    console.log(deckOfCards);
+    dealCard(playersCards, deckOfCards);
+    dealCard(computersCards, deckOfCards);
+    dealCard(playersCards, deckOfCards);
+    dealCard(computersCards, deckOfCards);
     if (sumOfHand(playersCards) == 21 && sumOfHand(computersCards) == 21) {
       return "Blackjack! Computer won (dealer)";
     } else if (sumOfHand(playersCards) == 21) {
@@ -63,73 +136,4 @@ var main = function (input) {
       return "you lost cos it was a draw";
     }
   }
-};
-
-var deckOfCards = [];
-var suits = ["hearts", "diamonds", "clubs", "spades"];
-var cardCounter = 1;
-var suitCounter = 0;
-var scoreCounter = 1;
-
-var createDeck = function () {
-  while (suitCounter <= suits.length - 1) {
-    cardName = cardCounter;
-    cardSuit = suits[suitCounter];
-    if (cardCounter == 1) {
-      var cardName = "Ace";
-    }
-    if (cardCounter == 11) {
-      var cardName = "Jack";
-    }
-    if (cardCounter == 12) {
-      var cardName = "Queen";
-    }
-    if (cardCounter == 13) {
-      var cardName = "King";
-    }
-    var card = {
-      name: cardName,
-      suit: cardSuit,
-      score: scoreCounter,
-    };
-    deckOfCards.push(card);
-    if (cardCounter == 13) {
-      cardCounter = 0;
-      suitCounter += 1;
-      scoreCounter = 1;
-    }
-    cardCounter += 1;
-    scoreCounter += 1;
-  }
-  return deckOfCards;
-};
-
-var shuffleCounter = 0;
-var shuffleDeck = function (cards) {
-  while (shuffleCounter <= cards.length) {
-    // index needs to go up to 53
-    var randomNumber = Math.floor(Math.random() * 53 + 1);
-    var shuffleCard1 = cards[shuffleCounter];
-    var shuffleCard2 = cards[randomNumber];
-    cards[randomNumber] = shuffleCard1;
-    cards[shuffleCounter] = shuffleCard2;
-    shuffleCounter += 1;
-  }
-  return cards;
-};
-
-var playersCards = [];
-var computersCards = [];
-
-var dealCard = function (cards) {
-  cards.push(deckOfCards.pop());
-};
-
-var sumOfHand = function (hand) {
-  handCounter = 0;
-  handSum = 0;
-  while (handCounter <= hand.length) {
-    handSum += hand[handCounter].scoreCounter;
-  }
-  return handSum;
 };
