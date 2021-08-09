17366bbd34e5ea30fb3673bf5ce628a74cbd58e6
whyexis <chongyingxiang@gmail.com>
2021-08-09 16:16:26 +0800

    Added aces variable value

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-///////////////
-// Constants //
-///////////////
+///////////////////////////////////////////////////////
+////////////////////// Constants //////////////////////
+///////////////////////////////////////////////////////
 
 const HEARTS = "hearts";
 const DIAMONDS = "diamonds";
 const CLUBS = "clubs";
 const SPADES = "spades";
-const INPUT_PLAYERS = "input players";
-const INPUT_NAMES = "input names";
-const INPUT_BETS = "input bets";
+const INPUT_PLAYERS = "inputPlayers";
+const INPUT_NAMES = "inputNames";
+const INPUT_BETS = "inputBets";
 const BLACKJACK = "blackjack";
+const HIT = "hit";
+const STAND = "stand";
+const SPLIT = "split";
 const COLOUR_1 = "#3700B3";
 const COLOUR_2 = "#018786";
 const COLOUR_3 = "#B00020";
+const defaultNames = [
+  "Alice",
+  "Bob",
+  "Chen Xi",
+  "Dennis",
+  "Ee Ching",
+  "Ferris",
+];
 
-//////////////////////
-// Global Variables //
-//////////////////////
+//////////////////////////////////////////////////////////////
+////////////////////// Global Variables //////////////////////
+//////////////////////////////////////////////////////////////
+
 let gameMode = INPUT_PLAYERS;
 let numOfPlayers = 0;
 const playerBoard = [];
 
-//////////////////////
-// Helper Functions //
-//////////////////////
+//////////////////////////////////////////////////////////////
+////////////////////// Helper Functions //////////////////////
+//////////////////////////////////////////////////////////////
 
 /**
  * ------------------------------------------------------------------------
@@ -36,17 +48,32 @@ const playerBoard = [];
  * @return  {Array}       A deck grouped by suits and ordered from 2 to Ace.
  * ------------------------------------------------------------------------
  */
+
 function createDeck() {
   var cardDeck = [];
   var suits = [HEARTS, DIAMONDS, CLUBS, SPADES];
 
   for (var suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
     var currentSuit = suits[suitIndex];
+    var suitEmoji;
+    switch (currentSuit) {
+      case HEARTS:
+        suitEmoji = "♥";
+        break;
+      case DIAMONDS:
+        suitEmoji = "♦";
+        break;
+      case CLUBS:
+        suitEmoji = "♣";
+        break;
+      case SPADES:
+        suitEmoji = "♠";
+        break;
+    }
 
     for (var rankCounter = 2; rankCounter <= 14; rankCounter += 1) {
       var cardName = rankCounter;
       var cardValue = rankCounter;
-      var suitEmoji;
 
       switch (cardName) {
         case 11:
@@ -64,21 +91,6 @@ function createDeck() {
         case 14:
           cardName = "A";
           cardValue = 11;
-          break;
-      }
-
-      switch (currentSuit) {
-        case HEARTS:
-          suitEmoji = "♥";
-          break;
-        case DIAMONDS:
-          suitEmoji = "♦";
-          break;
-        case CLUBS:
-          suitEmoji = "♣";
-          break;
-        case SPADES:
-          suitEmoji = "♠";
           break;
       }
 
@@ -102,6 +114,7 @@ function createDeck() {
  * @return  {Number}            A random index.
  * ------------------------------------------------------------------------
  */
+
 function getRandomIndex(max) {
   return Math.floor(Math.random() * max);
 }
@@ -113,6 +126,7 @@ function getRandomIndex(max) {
  * @return  {Array}                 A shuffled deck.
  * ------------------------------------------------------------------------
  */
+
 function shuffleCards(cardDeck) {
   for (
     var currentIndex = 0;
@@ -128,18 +142,24 @@ function shuffleCards(cardDeck) {
   return cardDeck;
 }
 
+///////////////////////////////////////////////////////////////////
+////////////////////// Start of Player Class //////////////////////
+///////////////////////////////////////////////////////////////////
+
 /**
  * ------------------------------------------------------------------------
  * Template for player objects.
  * @argument {String} id   The player's id.
  * ------------------------------------------------------------------------
  */
+
 class Player {
   constructor(id) {
     this.id = id;
     this._name = "";
     this.hand = [];
     this.canSplit = false;
+    this._stand = false;
     this.hasBlackjack = false;
     this.hasBust = false;
     this.hasWon = false;
@@ -154,6 +174,7 @@ class Player {
    * @param {String} name
    * ------------------------------------------------------------------------
    */
+
   set name(name) {
     this._name = name;
   }
@@ -168,6 +189,7 @@ class Player {
    * @param {Number} bet
    * ------------------------------------------------------------------------
    */
+
   set bet(bet) {
     this._bet = bet;
   }
@@ -190,8 +212,19 @@ class Player {
    * @param  {Object}  Card object from deck.
    * ------------------------------------------------------------------------
    */
+
   hit(card) {
     this.hand.push(card);
+  }
+
+  /**
+   * ------------------------------------------------------------------------
+   * Set the player's stand status to true.
+   * ------------------------------------------------------------------------
+   */
+
+  set stand() {
+    this._stand = true;
   }
 
   /**
@@ -200,6 +233,7 @@ class Player {
    * @return {Number} handValue
    * ------------------------------------------------------------------------
    */
+
   get handValue() {
     let initialValue = 0;
     let handValue = this.hand.reduce(function (accumulator, currentValue) {
@@ -213,6 +247,7 @@ class Player {
    * Check the player's initial hand for blackjack or splits.
    * ------------------------------------------------------------------------
    */
+
   checkHand() {
     if (this.handValue == 21 && this.hand.length == 2) {
       this.hasBlackjack = true;
@@ -227,9 +262,14 @@ class Player {
   /**
    * ------------------------------------------------------------------------
    * Check if the player has bust.
+   * If the player has Aces and player has bust, change the ace's value to 1.
    * ------------------------------------------------------------------------
    */
+
   checkBust() {
+    if (this.handValue > 21 && this.hand.some(({ name }) => name === "A")) {
+      this.hand.find(({ name }) => name === "A").value = 1;
+    }
     if (this.handValue > 21) {
       this.hasBust = true;
     }
@@ -241,9 +281,12 @@ class Player {
    * @return {String} output
    * ------------------------------------------------------------------------
    */
+
   get showHand() {
     return `${this.name}<br>
-    Hand: ${this.hand.map((e) => e.display).join(" | ")}<br>
+    <span style="border:1px; border-style:solid; border-color:#FFFFFF; padding: 0.25em 0.5em;">${this.hand
+      .map((e) => e.display)
+      .join(" | ")}</span><br>
     Value: ${this.handValue}.<br>`;
   }
 
@@ -252,9 +295,11 @@ class Player {
    * Reset player's hand, bet and booleans for the next round.
    * ------------------------------------------------------------------------
    */
+
   reset() {
     this.hand = [];
     this.canSplit = false;
+    this._stand = false;
     this.hasBlackjack = false;
     this.hasBust = false;
     this.hasWon = false;
@@ -262,15 +307,27 @@ class Player {
     this._bet = 0;
   }
 }
-/////////////////////////
-// End of Player Class //
-/////////////////////////
+/////////////////////////////////////////////////////////////////
+////////////////////// End of Player Class //////////////////////
+/////////////////////////////////////////////////////////////////
+
+/**
+ * ------------------------------------------------------------------------
+ * Create players based on user input.
+ * ------------------------------------------------------------------------
+ */
 
 function createPlayers(numOfPlayers) {
   for (let i = 1; i <= numOfPlayers; i += 1) {
     playerBoard.push(new Player(i));
   }
 }
+
+/**
+ * ------------------------------------------------------------------------
+ * Deal cards to players and dealer.
+ * ------------------------------------------------------------------------
+ */
 
 function dealCards(deck) {
   for (let i = 0; i < 2; i += 1) {
@@ -281,6 +338,12 @@ function dealCards(deck) {
   }
 }
 
+/**
+ * ------------------------------------------------------------------------
+ * Check the initial hands of every player and dealer.
+ * ------------------------------------------------------------------------
+ */
+
 function initialChecks() {
   for (let player of playerBoard) {
     player.checkHand();
@@ -289,6 +352,12 @@ function initialChecks() {
 }
 
 function hitStandSplit() {}
+
+/**
+ * ------------------------------------------------------------------------
+ * Dealer hits if the hand value is belowe 17.
+ * ------------------------------------------------------------------------
+ */
 
 function dealerHitStand(dealer) {
   while (dealer.handValue < 17) {
@@ -348,33 +417,91 @@ function evalHands() {
   return output;
 }
 
-function computeBets() {}
+function endGame() {
+  dealerHitStand(dealer);
+  let evaluation = evalHands();
 
-function resetGame() {
-  // Reset all players' hands, bets and booleans
+  // Compute bets then reset everyone's hands, bets and booleans
   for (let player of playerBoard) {
+    player.calcPoints();
     player.reset();
   }
-
-  // Reset dealer's hand and booleans
   dealer.reset();
 
   // Generate new deck
   cardDeck = shuffleCards(createDeck());
 
-  // Set game mode to input bets
   gameMode = INPUT_BETS;
+
+  return evaluation;
 }
 
-/**
- * ------------------------------------------------------------------------
- * Game Variables
- * ------------------------------------------------------------------------
- */
+////////////////////////////////////////////////////////////
+////////////////////// Game Variables //////////////////////
+////////////////////////////////////////////////////////////
 
 const dealer = new Player("dealer");
 dealer.name = "Dealer";
 let cardDeck = shuffleCards(createDeck());
+
+const gameplay = {
+  inputPlayers: {
+    execute(input) {
+      if (
+        isNaN(Number(input)) ||
+        Number(input) < 1 ||
+        Number(input) > 6 ||
+        input == ""
+      ) {
+        return "Please enter the number of players from 1 to 6.";
+      }
+      numOfPlayers = input;
+      createPlayers(numOfPlayers);
+      gameMode = INPUT_NAMES;
+      return `There are now ${input} player(s). Please enter the name(s) of the player(s) separated by slashes "/".`;
+    },
+  },
+  inputNames: {
+    execute(input) {
+      let names = input.split("/");
+      if (input == "") {
+        names = defaultNames.splice(0, numOfPlayers);
+      } else if (names.length != numOfPlayers) {
+        return `There are ${numOfPlayers} players but only ${names.length} names entered, please check and try again.`;
+      }
+      for (let i = 0; i < names.length; i += 1) {
+        playerBoard[i].name = names[i];
+      }
+      gameMode = INPUT_BETS;
+      return `Now that everything is set, let's start playing Blackjack!<br><br>
+    Bet your points in the same order as the names separated by slashes "/".<br>
+    Every player starts with 100 points.`;
+    },
+  },
+  inputBets: {
+    execute(input) {
+      let bets = input.split("/");
+      if (bets.length != numOfPlayers) {
+        return `There are ${numOfPlayers} players but only ${bets.length} bets were entered, please check and try again.<br>
+        If a player does not wish to bet, please enter "0" for that player.`;
+      }
+      for (let i = 0; i < bets.length; i += 1) {
+        playerBoard[i].bet = bets[i];
+      }
+      gameMode = BLACKJACK;
+      dealCards(cardDeck);
+      initialChecks();
+      let defaultMessage = "Bets are placed, cards are dealt.";
+      if (dealer.hasBlackjack) {
+        return `${defaultMessage}<br><br>${endGame()}`;
+      }
+      return `${defaultMessage} Best of luck to everyone!`;
+    },
+  },
+  blackjack: {
+    execute(input) {},
+  },
+};
 
 /**
  * ------------------------------------------------------------------------
@@ -383,48 +510,7 @@ let cardDeck = shuffleCards(createDeck());
  */
 
 function main(input) {
-  if (gameMode == INPUT_PLAYERS) {
-    if (
-      isNaN(Number(input)) ||
-      Number(input) < 1 ||
-      Number(input) > 6 ||
-      input == ""
-    ) {
-      return "Please enter the number of players from 1 to 6.";
-    }
-    numOfPlayers = input;
-    createPlayers(numOfPlayers);
-    gameMode = INPUT_NAMES;
-    return `There are now ${input} player(s). Please enter the name(s) of the player(s) in order separated by slashes "/".`;
-  }
-
-  if (gameMode == INPUT_NAMES) {
-    const defaultNames = [
-      "Alice",
-      "Bob",
-      "Chen Xi",
-      "Dennis",
-      "Ee Ching",
-      "Ferris",
-    ];
-    let names = input.split("/");
-    if (input == "") {
-      names = defaultNames.splice(0, numOfPlayers);
-    }
-    for (let i = 0; i < names.length; i += 1) {
-      playerBoard[i].name = names[i];
-    }
-    gameMode = BLACKJACK;
-    return `Now that everything is set, let's start playing Blackjack!`;
-  }
-
-  dealCards(cardDeck);
-  initialChecks();
-  dealerHitStand(dealer);
-
-  let evaluation = evalHands();
-  resetGame();
-  return evaluation;
+  return gameplay[gameMode].execute(input);
 }
 
 /* Pseudocode
