274668c5d2badb3ffd137f1c8c9c5dac68b6fe83
Felicia Lim <felicialim@Felicias-MacBook.local>
2021-08-10 20:21:13 +0800

    Set up

--- script.js
+++ script.js
@@ -1,4 +1,117 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+//Game modes:
+//1. Get initial hand
+//2. Computer hit or stand
+//3. Player hit or stand
+//4. Compare values
+ 
+var gameMode = "1. Get initial hand"
+
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = 'ace';
+      } else if (cardName == 11) {
+        cardName = 'jack';
+      } else if (cardName == 12) {
+        cardName = 'queen';
+      } else if (cardName == 13) {
+        cardName = 'king';
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
 };
+
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+var deck = [];
+var shuffledDeck = shuffleCards(deck);
+
+var main = function (input) {
+  //Deal cards to computer and player
+  var computerCardOne = shuffledDeck.pop();
+  var playerCardOne = shuffledDeck.pop();
+  var computerCardTwo = shuffledDeck.pop();
+  var playerCardTwo = shuffledDeck.pop();
+  var additionalCard = shuffledDeck.pop();
+
+  //DO WE NEED TO SET VALUES TO EACH CARD FIRST??
+  //ERROR HERE: HOW TO ADD VALUES OF CARDS TOGETHER???
+  var computerInitial = computerCardOne + computerCardTwo;
+  console.log(computerInitial);
+  var playerInitial = playerCardOne + playerCardTwo;
+  console.log(playerInitial);
+
+  var myOutputValue = "";
+// if computer's initial draw is less than 17, 
+if (computerInitial < 17){
+  while (computerInitial < 17){
+  var computerTotal = computerInitial + additionalCard;
+  console.log(computerTotal)
+  };
+};
+    myOutputValue = "hello <br>" + myOutputValue;
+  
+return myOutputValue;
+};
+
