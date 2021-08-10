63cca578d32956acbeba526103c95ec70a331b81
Luthor9269 <mayank_aswal@live.com>
2021-08-11 00:15:01 +0800

    Base without Ace functionality

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ var shuffledDeck = [];
 var playerCards = [];
 var computerCards = [];
 var gameMode = "drawCards";
-var playerSum = [];
-var computerSum = [];
 
 var main = function (input) {
   var myOutputValue = " ";
@@ -16,14 +14,39 @@ var main = function (input) {
   if (gameMode == "hit" && input == "hit") {
     var playerHand = calPlayerCard(shuffledDeck);
     console.log(playerHand);
-    myOutputValue = playerHand;
+    if (calPlayerSum(playerCards) > 21) {
+      return (
+        playerHand +
+        "<br>" +
+        showPlayerCards(playerCards) +
+        "<br>" +
+        "Your total is " +
+        calPlayerSum(playerCards) +
+        "<br>" +
+        "You bust!, refresh to try again"
+      );
+    }
+    myOutputValue =
+      playerHand +
+      "<br>" +
+      showPlayerCards(playerCards) +
+      "<br>" +
+      "Your total is " +
+      calPlayerSum(playerCards) +
+      "<br>" +
+      " key in 'hit' to draw another card" +
+      "<br>" +
+      "key in 'pass' to stop drawing";
   } else if (gameMode == "hit" && input == "pass") {
     myOutputValue =
       "You passed, here are your cards " +
       "<br>" +
       showPlayerCards(playerCards) +
       "<br>" +
-      calPlayerSum(playerCards);
+      "Your total is " +
+      calPlayerSum(playerCards) +
+      "<br>" +
+      "Press submit to hand over to Computer";
     gameMode = "ComputerTurn";
     return myOutputValue;
   }
@@ -34,13 +57,14 @@ var main = function (input) {
       "<br>" +
       compCards +
       "<br>" +
+      "Computer's total is " +
       calComputerSum(computerCards);
     gameMode = "results";
-    return myOutputValue;
   }
+  ////////////need to make gamewinning function
   if (gameMode == "results") {
-    var win = calWinningHand(playerCards, computerCards);
-    myOutputValue = win;
+    var calWinningFunction = calWinningHand(playerCards, computerCards);
+    myOutputValue = calWinningFunction;
   }
   return myOutputValue;
 };
@@ -116,33 +140,35 @@ var startOfGame = function () {
     var playerHand2 = calPlayerCard(shuffledDeck);
     // logic for if player is > 21 or =21
     if (calPlayerSum(playerCards) == 21) {
-      return showPlayerCards(playerCards) + "<br>" + "BlackJack !You won!";
+      return (
+        showPlayerCards(playerCards) +
+        "<br>" +
+        showComputerCards(computerCards) +
+        "<br>" +
+        "Your total is " +
+        calPlayerSum(playerCards) +
+        "<br>" +
+        "BlackJack! You won! Refresh to try again"
+      );
     } else if (calPlayerSum(playerCards) > 21) {
       return (
-        showPlayerCards(playerCards) + "<br>" + playerSum + "<br>" + "You Bust!"
+        showPlayerCards(playerCards) +
+        "<br>" +
+        "Your total is " +
+        calPlayerSum(playerCards) +
+        "<br>" +
+        "You went over! You bust! Refresh to try again"
       );
-    } else if (calPlayerSum(playerCards) == calComputerSum(computerCards)) {
-      return showPlayerCards(playerCards) + "Draw";
     }
+    //making two cards to add into sum so while loop below can run
     // while runs as long as computerSum is less then 17
     var computerHand1 = calComputerCard(shuffledDeck);
     var computerHand2 = calComputerCard(shuffledDeck);
-    if (calComputerSum(computerCards) == 21) {
-      return (
-        showComputerCards(computerCards) +
-        "<br>" +
-        "Computer got BlackJack !You Lost!"
-      );
-    } else if (calComputerSum(computerCards) > 21) {
-      return showComputerCards(computerCards) + "<br>" + "You Bust!";
-    } else if (calComputerSum(computerCards) == calComputerSum(computerCards)) {
-      return showPlayerCards(playerCards) + "Draw";
-    }
     console.log(computerHand1);
     console.log(computerHand2);
     while (calComputerSum(computerCards) < 17) {
-      console.log(computerHand1);
       var computerNewDraw = calComputerCard(shuffledDeck);
+      console.log(computerNewDraw);
     }
     myOutputValue =
       "You got " +
@@ -151,7 +177,12 @@ var startOfGame = function () {
       "<br>" +
       playerHand2 +
       "<br>" +
-      calPlayerSum(playerCards);
+      "Your total is " +
+      calPlayerSum(playerCards) +
+      "<br>" +
+      " key in 'hit' to draw another card" +
+      "<br>" +
+      "key in 'pass' to stop drawing";
     // var winningHand = calWinningHand(playerCards, computerCards);
     // var myOutputValue = winningHand;
     gameMode = "hit";
@@ -175,7 +206,6 @@ var calPlayerSum = function (playerCards) {
     counter = counter + 1;
     sum += rankOfCurrentCard;
   }
-  playerSum.push(sum);
   return sum;
 };
 var calComputerCard = function (shuffledDeck) {
@@ -194,26 +224,36 @@ var calComputerSum = function (computerCards) {
     counter = counter + 1;
     sum += rankOfCurrentCard;
   }
-  computerSum.push(sum);
   return sum;
 };
 
-/////////output is showing as blank need to fix this
 var calWinningHand = function (playerCards, computerCards) {
   myOutputValue = " ";
-  playerSum = calPlayerSum(playerCards);
+  var playerSum = calPlayerSum(playerCards);
   console.log(playerSum);
-  computerSum = calComputerSum(computerCards);
+  var computerSum = calComputerSum(computerCards);
   console.log(computerSum);
-  if (playerSum < computerSum) {
-    myOutputValue =
-      " You got " +
+
+  if (computerSum > 21) {
+    return (
+      " Your total was  " +
       calPlayerSum(playerCards) +
       "<br>" +
-      "Computer got " +
+      "Computer's total was  " +
       calComputerSum(computerCards) +
       "<br>" +
-      "You Lost!";
+      " The computer bust! You Won! Refresh to try again"
+    );
+  }
+  if (playerSum < computerSum) {
+    myOutputValue =
+      " Your total was  " +
+      calPlayerSum(playerCards) +
+      "<br>" +
+      "Computer's total was  " +
+      calComputerSum(computerCards) +
+      "<br>" +
+      "You Lost! Refresh to try again";
   } else if (playerSum > computerSum) {
     myOutputValue =
       " You got " +
@@ -222,8 +262,8 @@ var calWinningHand = function (playerCards, computerCards) {
       "Computer got " +
       calComputerSum(computerCards) +
       "<br>" +
-      "You Won!";
-  } else if ((playerSum = computerSum)) {
+      "You Won! Refresh to try again";
+  } else if (playerSum == computerSum) {
     myOutputValue =
       " You got " +
       calPlayerSum(playerCards) +
@@ -267,18 +307,7 @@ var showComputerCards = function (computerCards) {
   return myOutputValue;
 };
 
-//////logic to win game at 21 or bust over 21.
-/////// add lgi
-
-var gameLogic = function (computerCards, playerCards) {
-  if (calPlayerSum(playerCards) == 21 && calComputerSum(computerCards) == 21) {
-    myOutputValue = "You both drew 21! what are the chances?! DRAW";
-  }
-};
-///// if player cards 21 and comp cards 21, draw
-///// if player cards 21 and comp cards not 21, player wins immediately
-///// if player cards not 21 and comp is 21 , comp wins
-///// if player or comp more than 21, retart the game
+/////////////need to make an ace function and embed it into calPlayerCards function , give user choice to change ace to 1 or 11
 
 //// if players have an Ace and playerCards.length =2 , ace.rank == 11
 /// if players have an Ace and playerCards.length >3 Ace.rank == 1
