75e7834a7d8c3e203bc5b73ad7364bb468654e67
nadinelk <nadinekurnadi@gmail.com>
2021-08-11 18:48:09 +0700

    did code refactoring

--- script.js
+++ script.js
@@ -87,38 +87,39 @@ var dealCards = function (deck) {
   console.log(dealerChosenCard);
 };
 
-var getWinningMsg = function (playerCardSum, dealerCardSum) {
-  if (playerCardSum == 21) {
-    return "Congrats, you got a blackjack! You win!";
+var doesPlayerWin = function (playerCardSum, dealerCardSum) {
+  console.log("player sum: " + playerCardSum);
+  console.log("dealer sum: " + dealerCardSum);
+  if (
+    (playerCardSum <= 21 && playerCardSum > dealerCardSum) ||
+    (dealerCardSum > 21 && playerCardSum <= 21) ||
+    (playerCardSum == 21 && dealerCardSum !== 21)
+  ) {
+    return 1;
   }
-  if (playerCardSum > dealerCardSum) {
-    return (
-      "Player, you won against computer with the scores of " +
-      playerCardSum +
-      " to " +
-      dealerCardSum
-    );
+  if (
+    (dealerCardSum <= 21 && dealerCardSum > playerCardSum) ||
+    (playerCardSum > 21 && dealerCardSum <= 21) ||
+    (dealerCardSum == 21) & (playerCardSum !== 21)
+  ) {
+    return 2;
   }
-  if (dealerCardSum > 21) {
-    return "Dealer busted! Congrats, you won!";
+  if (
+    playerCardSum == dealerCardSum ||
+    (playerCard > 21 && dealerCardSum > 21)
+  ) {
+    return 3;
   }
 };
 
-var getLosingMsg = function (playerCardSum, dealerCardSum) {
-  if (dealerCardSum == 21) {
-    return "Dealer got a blackjack! Unfortunately, you lose :(";
-  }
-  if (dealerCardSum > playerCardSum) {
-    return (
-      "Player, you lost against computer with the scores of " +
-      playerCardSum +
-      " to " +
-      dealerCardSum +
-      ". <br> Such a shame."
-    );
-  }
-  if (playerCardSum > 21) {
-    return "Whoops, you busted!";
+var getOutputMsg = function (playerWins) {
+  console.log(playerWins);
+  if (playerWins == 1) {
+    return "Congrats player! You win!";
+  } else if (playerWins == 2) {
+    return "Oh no, you lost :(";
+  } else if (playerWins == 3) {
+    return "There's no winner for this round.";
   }
 };
 
@@ -126,6 +127,8 @@ var main = function (input) {
   var cardDeck = createDeck();
 
   var shuffledDeck = shuffleDeck(cardDeck);
+
+  var playerWins = doesPlayerWin(playerCardSum, dealerCardSum);
 
   // deal the cards two times
   var numOfDeals = 0;
@@ -156,7 +159,7 @@ var main = function (input) {
   // create default output value.
   var myOutputValue =
     outputOfPlayersCards +
-    ". <br> Computer's card is " +
+    ". <br> Dealer's card is " +
     dealerCard[0].name +
     " of " +
     dealerCard[0].suit;
@@ -169,13 +172,13 @@ var main = function (input) {
   console.log("dealer card sum is " + dealerCardSum);
 
   // store the winning and losing message
-  var winningMsg = getWinningMsg(playerCardSum, dealerCardSum);
-  var losingMsg = getLosingMsg(playerCardSum, dealerCardSum);
+  var outputMsg = getOutputMsg(playerWins);
 
   // check if player card totals to 21
   // if playerCardSum == 21, player wins. Else, player choose 'hit' or 'stand'
   if (playerCardSum == 21) {
-    myOutputValue = outputOfPlayersCards + " <br> " + winningMsg;
+    myOutputValue =
+      outputOfPlayersCards + ". <br> You got a blackjack. You win!";
   } else {
     myOutputValue =
       myOutputValue + ". <br> Please choose between hit or stand.";
@@ -183,20 +186,17 @@ var main = function (input) {
 
   // if player choose 'stand', open dealer's closed card.
   if (input == STAND) {
+    myOutputValue =
+      "Player, you card sum is " +
+      playerCardSum +
+      ". <br> Dealer's card sum is " +
+      dealerCardSum +
+      ". ";
+
     if (dealerCardSum >= 17) {
       // check winning condition
-      if (dealerCardSum == 21) {
-        return outputOfDealersCards + " <br> " + losingMsg + ".";
-      } else if (dealerCardSum > 21) {
-        return outputOfDealersCards + " <br> " + winningMsg;
-      } else {
-        if (playerCardSum > dealerCardSum) {
-          return outputOfDealersCards + " <br> " + winningMsg + ".";
-        }
-        if (dealerCardSum > playerCardSum) {
-          return outputOfDealersCards + " <br> " + losingMsg + ".";
-        }
-      }
+      doesPlayerWin(playerCardSum, dealerCardSum);
+      return myOutputValue + "<br>" + outputMsg;
     }
     // if dealer card sum is <= 16, dealer draw
     if (dealerCardSum <= 16) {
@@ -204,30 +204,21 @@ var main = function (input) {
 
       dealerCardSum = dealerCardSum + dealerCard[2].rank;
 
-      winningMsg = getWinningMsg(playerCardSum, dealerCardSum);
-      losingMsg = getLosingMsg(playerCardSum, dealerCardSum);
+      playerWins = doesPlayerWin(playerCardSum, dealerCardSum);
+      outputMsg = getOutputMsg(playerWins);
 
-      outputOfDealersCards =
-        outputOfDealersCards +
-        " <br> Dealer just draw a " +
-        dealerCard[2].name +
-        " of " +
-        dealerCard[2].suit;
+      myOutputValue =
+        "Player, you card sum is " +
+        playerCardSum +
+        ". <br> Dealer's card sum is " +
+        dealerCardSum +
+        ". ";
 
       // check winning condition
-      if (dealerCardSum == 21) {
-        return outputOfDealersCards + ". <br> " + losingMsg + "";
-      } else if (dealerCardSum > 21) {
-        return outputOfDealersCards + ". <br> " + winningMsg + ".";
-      } else if (playerCardSum == dealerCardSum) {
-        return "It's a tie!";
-      } else {
-        if (playerCardSum > dealerCardSum) {
-          return outputOfDealersCards + ". <br> " + winningMsg + ".";
-        }
-        if (dealerCardSum > playerCardSum) {
-          return outputOfDealersCards + ". <br> " + losingMsg + ".";
-        }
+      if (doesPlayerWin(playerCardSum, dealerCardSum)) {
+        return myOutputValue + "<br>" + outputMsg;
+      } else if (!doesPlayerWin(playerCardSum, dealerCardSum)) {
+        return myOutputValue + "<br>" + outputMsg;
       }
     }
   }
@@ -247,26 +238,12 @@ var main = function (input) {
         playerCard[0].rank + playerCard[1].rank + playerCard[2].rank;
     }
 
-    // update winning message and losing message
-    winningMsg = getWinningMsg(playerCardSum, dealerCardSum);
-    losingMsg = getLosingMsg(playerCardSum, dealerCardSum);
-
     myOutputValue =
-      "Player, you just draw a " +
-      playerCard[2].name +
-      " of " +
-      playerCard[2].suit +
-      ".";
-
-    outputOfPlayersCards =
-      myOutputValue +
-      " <br> " +
-      outputOfPlayersCards +
-      ", and " +
-      playerCard[2].name +
-      " of " +
-      playerCard[2].suit +
-      ".";
+      "Player, you card sum is " +
+      playerCardSum +
+      ". <br> Dealer's card sum is " +
+      dealerCardSum +
+      ". ";
 
     // check winning condition
     if (dealerCardSum <= 16) {
@@ -275,80 +252,29 @@ var main = function (input) {
       dealerCardSum = dealerCardSum + dealerCard[2].rank;
       console.log(dealerCardSum);
 
-      winningMsg = getWinningMsg(playerCardSum, dealerCardSum);
-      losingMsg = getLosingMsg(playerCardSum, dealerCardSum);
+      playerWins = doesPlayerWin(playerCardSum, dealerCardSum);
+      outputMsg = getOutputMsg(playerWins);
 
-      outputOfDealersCards =
-        outputOfDealersCards +
-        " <br> Dealer just draw a " +
-        dealerCard[2].name +
-        " of " +
-        dealerCard[2].suit;
+      myOutputValue =
+        "Player, your card sum is " +
+        playerCardSum +
+        ". <br> Dealer's card sum is " +
+        dealerCardSum;
 
-      if (playerCardSum == 21) {
-        return myOutputValue + " <br> " + winningMsg;
-      }
-      if (playerCardSum > 21) {
-        return outputOfPlayersCards + "<br> " + losingMsg;
-      }
-      if (playerCardSum == dealerCardSum) {
-        return "It's a tie!";
-      }
-      if (dealerCardSum == 21) {
-        return outputOfDealersCards + ". <br> " + losingMsg;
-      } else if (playerCardSum == dealerCardSum) {
-        return "It's a tie!";
-      }
-      if (dealerCardSum > 21) {
-        return (
-          myOutputValue +
-          " <br> " +
-          outputOfDealersCards +
-          ". <br> " +
-          winningMsg +
-          "."
-        );
-      } else {
-        if (playerCardSum > dealerCardSum) {
-          return (
-            outputOfPlayersCards +
-            " <br> " +
-            outputOfDealersCards +
-            ". <br> " +
-            winningMsg +
-            "."
-          );
-        }
-        if (dealerCardSum > playerCardSum) {
-          return (
-            outputOfPlayersCards +
-            " <br> " +
-            outputOfDealersCards +
-            ". <br> " +
-            losingMsg +
-            "."
-          );
-        }
+      if (doesPlayerWin(playerCardSum, dealerCardSum)) {
+        return myOutputValue + ". <br>" + outputMsg;
+      } else if (!doesPlayerWin(playerCardSum, dealerCardSum)) {
+        return myOutputValue + ". <br>" + outputMsg;
       }
     }
     if (dealerCardSum >= 17) {
-      if (playerCardSum == 21) {
-        return myOutputValue + " <br> " + winningMsg;
-      }
-      if (playerCardSum > 21) {
-        return outputOfPlayersCards + "<br> " + losingMsg;
-      }
-      if (playerCardSum == dealerCardSum) {
-        return "It's a tie!";
-      }
-      if (playerCardSum < 21) {
-        console.log(playerCardSum);
-        if (playerCardSum > dealerCardSum) {
-          return outputOfPlayersCards + "<br>" + winningMsg + ".";
-        }
-        if (playerCardSum < dealerCardSum) {
-          return outputOfPlayersCards + " <br> " + losingMsg + ".";
-        }
+      playerWins = doesPlayerWin(playerCardSum, dealerCardSum);
+      outputMsg = getOutputMsg(playerWins);
+
+      if (doesPlayerWin(playerCardSum, dealerCardSum)) {
+        return myOutputValue + ". <br>" + outputMsg;
+      } else if (!doesPlayerWin(playerCardSum, dealerCardSum)) {
+        return myOutputValue + ". <br>" + outputMsg;
       }
     }
   }
