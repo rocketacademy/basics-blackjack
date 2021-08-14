b5080cb2a713c178e1ccf4c9d7a49bb6e1cee433
melodii8 <melodiipohyi@gmail.com>
2021-08-14 13:20:28 +0800

    minor edit to initial player name

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var aceDecision = "decide Ace value";
 var betMode = "bet mode";
 var gameMode = nameRequest;
 var instructions = "instructions for blackjack";
-var playerName = "MEL!";
+var playerName = "MELODII!";
 
 var playerHand = [];
 var computerHand = [];
