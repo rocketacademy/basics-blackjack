3dff23f2bbfd9d220cf68405aeb901df4ec8b52c
lim-jiahao <lim.jiahaoo@gmail.com>
2021-09-15 14:53:26 +0800

    initial version, generates 2 cards for player / computer

--- script.js
+++ script.js
@@ -1,4 +1,83 @@
+var makeCard = function (suit, emoji, rank) {
+  var specialRanks = {
+    11: "jack",
+    12: "queen",
+    13: "king",
+    1: "ace",
+  };
+  var name = rank.toString();
+  var points = rank;
+
+  if (Object.keys(specialRanks).includes(name)) {
+    name = specialRanks[rank];
+    if (name != "ace") points = 10;
+  }
+
+  return { name, suit, rank, points, emoji };
+};
+
+var makeDeck = function () {
+  var deck = [];
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  var suitEmojis = ["♥️", "♦", "♣️", "♠️"];
+
+  for (var suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
+    for (var rank = 1; rank <= 13; rank += 1) {
+      var card = makeCard(suits[suitIndex], suitEmojis[suitIndex], rank);
+      deck.push(card);
+    }
+  }
+  return deck;
+};
+
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleDeck = function (cardDeck) {
+  for (var i = 0; i < cardDeck.length; i += 1) {
+    var randomIndex = getRandomIndex(cardDeck.length);
+    var randomCard = cardDeck[randomIndex];
+    var currentCard = cardDeck[i];
+
+    cardDeck[i] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+  }
+  return cardDeck;
+};
+
+var deck;
+
+var playerCards = [];
+var computerCards = [];
+
+const STARTING_CARDS = 2;
+
+var resetGame = function () {
+  deck = makeDeck();
+  deck = shuffleDeck(deck);
+  playerCards = [];
+  computerCards = [];
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  resetGame();
+  for (var i = 0; i < STARTING_CARDS; i += 1) {
+    playerCards.push(deck.pop());
+    computerCards.push(deck.pop());
+  }
+
+  var output = "Player's cards:<br>";
+  for (var i = 0; i < STARTING_CARDS; i += 1) {
+    output += `${playerCards[i].name} of ${playerCards[i].emoji}<br>`;
+  }
+
+  output += "<br>Computer's cards:<br>";
+  for (var i = 0; i < STARTING_CARDS; i += 1) {
+    output += `${computerCards[i].name} of ${computerCards[i].emoji}<br>`;
+  }
+
+  return output;
 };
