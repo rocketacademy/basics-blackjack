4aa4bf89f472c26e7ade062f990add9e5cfb8a8c
Simwang94 <simwangg@gmail.com>
2021-09-19 17:22:18 +0800

    complete 1 player blackjack

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
 //Global variables defining the different game modes and setting default to gameModeOne
 var gameModeOne = "deal cards";
 var gameModeTwo = "player turn";
-var gameModeThree = "dealer turn";
-var gameModeFour = "results";
+var gameModeThree = "results";
+
 var gameMode = gameModeOne;
 var playerHand = [];
 var dealerHand = [];
+var cardDeck = [];
 
 var main = function (input) {
   if (gameMode == gameModeOne) {
     //Deck is created and shuffled
-    var cardDeck = shuffleCards(makeDeck());
+    cardDeck = shuffleCards(makeDeck());
     //Players are dealt 2 cards each
     playerHand = dealPlayerCards(cardDeck);
     console.log(playerHand);
     //Dealer is dealt 2 cards
     dealerHand = dealDealerCards(cardDeck);
     console.log(dealerHand);
-
     console.log(cardDeck);
     //GameMode changes to player turn
-    gameMode = gameModeFour;
+    gameMode = gameModeTwo;
 
     var myOutputValue =
-      "Player hand:<br>" +
-      showHand(playerHand) +
-      "<br>To hit: Input H<br>To stand: Input S";
+      "Dealer deals the cards...<br><br><br>Player's turn.<br><br>Hit the submit button to view your hand";
     return myOutputValue;
   }
 
-  if (gameMode == gameModeFour && input == "S") {
+  //Game mode 2 where player views his hand and decides on whether to hit or stand
+  //Check to ensure that player has > 14 points
+  if (gameMode == gameModeTwo) {
+    //In game mode 2, submitting empty input box will show player hand
+    var myOutputValue =
+      "Player hand:<br>" +
+      showHand(playerHand) +
+      "<br>To hit: Input H<br>To stand: Input S<br><br>You need a minimum of 14 points to Stand";
+    //If player enters H, additional card will be drawn
+    if (input == "H") {
+      playerHand = drawCard(playerHand, cardDeck);
+      myOutputValue =
+        "Player hand:<br>" +
+        showHand(playerHand) +
+        "<br>To hit: Input H<br>To stand: Input S<br><br>You need a minimum of 14 points to Stand";
+      return myOutputValue;
+      //If player enters S, dealer will draw cards automatically and game mode will change to game mode 3. In multiplayer mode, this will move on to next player instead until all players have drawn.
+    }
+    if (input == "S" && countHand(playerHand) >= 14) {
+      //Once player chooses to stand, dealer can start drawing cards
+      dealerHand = topupDealerHand(dealerHand, cardDeck);
+      gameMode = gameModeThree;
+      return (myOutputValue =
+        "Dealer's turn.<br><br>Dealer is drawing the necessary cards...<br><br><br>Press submit to view results.");
+    }
+    return myOutputValue;
+  }
+
+  //After dealer is done adding cards, dealer will call for the results
+  if (gameMode == gameModeThree) {
     myOutputValue =
       "Player hand:<br>" +
       showHand(playerHand) +
@@ -41,7 +68,6 @@ var main = function (input) {
     return myOutputValue;
   }
 
-  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
@@ -149,7 +175,7 @@ var showHand = function (hand) {
   var handOutput = " ";
   while (handIndex < hand.length) {
     handOutput =
-      handOutput + hand[handIndex].name + hand[handIndex].suit + ", ";
+      handOutput + hand[handIndex].name + hand[handIndex].suit + "<br>";
 
     console.log(hand[handIndex].name);
     console.log(hand[handIndex].suit);
@@ -163,8 +189,17 @@ var countHand = function (hand) {
   var rankIndex = 0;
   var totalPoints = 0;
   while (rankIndex < hand.length) {
-    //Get the point of each card in hand
-    var currentCardPoints = hand[rankIndex].rank;
+    //Get the rank of each card in hand
+    var currentCardRank = hand[rankIndex].rank;
+    var currentCardPoints = currentCardRank;
+    //Convert rank of picture cards to 10
+    if (
+      currentCardRank == 11 ||
+      currentCardRank == 12 ||
+      currentCardRank == 13
+    ) {
+      currentCardPoints = 10;
+    }
     //Add points into totalPoints
     totalPoints = totalPoints + currentCardPoints;
 
@@ -192,4 +227,27 @@ var determineWinner = function (dealerHand, playerHand) {
     if (dealerPoints < playerPoints) return "Player win";
     else return "Its a draw";
   }
+};
+
+//Funtion to draw an additional card into a specified hand array
+var drawCard = function (hand, deck) {
+  console.log("card drawn");
+  var drawnCard = deck.pop();
+  hand.push(drawnCard);
+  return hand;
+};
+
+//Function to automatically top up dealer's hand
+var topupDealerHand = function (hand, deck) {
+  //Max number of additional cards that can be drawn in blackjack is 3, so loop needs to run 3 rounds
+  var topupIndex = 0;
+  while (topupIndex < 3) {
+    //Check if dealer hand has less than 17 points
+    var dealerPoints = countHand(hand);
+    if (dealerPoints < 17) {
+      drawCard(hand, deck);
+    }
+    topupIndex += 1;
+  }
+  return hand;
 };
