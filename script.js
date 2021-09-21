d2cf7311835a0f423787373f298aedc9f2f2be88
GaryTangYC <tangyewchiang@gmail.com>
2021-09-21 16:51:31 +0800

    basics blackjack working file(excluding ace conversion)

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var cardDeck = [];
 var playerCards = [];
 var playerCardsValue = '';
 var dealerCards = [];
-var playerCardsValue ;
+var dealerCardsValue = '';
 var gameMode = 'Initialize Game';
-var gameTurn = '1stTurn';
+var playerBlackJackCheck = '';
+var dealerBlackJackCheck = '';
 
 
 //function to create deck
@@ -75,37 +76,71 @@ var checkBlackJack = function(cardsInHand){
   }
 }
 
+var outputOfCards = function(cardsInHand){
+var cards = '';
+var handIndex = 0;
+while (handIndex < cardsInHand.length){
+  cards = cards + ' , ' + cardsInHand[handIndex].name +  ' of ' + cardsInHand[handIndex].suit;
+  handIndex += 1;
+}
 
+  return cards;
+};
+
+var cardComparison = function(){
+  if (dealerCardsValue > 21){
+    return `You got ${outputOfCards(playerCards)} with a total value of ${playerCardsValue}. <br> Dealer busted with ${outputOfCards(dealerCards)} and a total value of ${dealerCardsValue}  <br> You Won!`
+  }  
+  else if (playerCardsValue > dealerCardsValue){
+    return `You got ${outputOfCards(playerCards)} with a total value of ${playerCardsValue}. <br> Dealer got ${outputOfCards(dealerCards)} with a total value of ${dealerCardsValue}  <br> You Won!`
+  }
+  else if (playerCardsValue < dealerCardsValue){
+    return `You got ${outputOfCards(playerCards)} with a total value of ${playerCardsValue}. <br> Dealer got ${outputOfCards(dealerCards)} with a total value of ${dealerCardsValue}  <br> Dealer Won =(`
+  }
+  else {
+    return `You got ${outputOfCards(playerCards)} with a total value of ${playerCardsValue}. <br> Dealer got ${outputOfCards(dealerCards)} with a total value of ${dealerCardsValue}  <br> It's a tie!`
+  }
+};
 
 var main = function (input) {
   
   if (gameMode == 'Initialize Game'){
+    //deal 2 cards to player and dealer
     playerCards = cardDeck.splice(0, 2);
     console.log(`playercard1 ${playerCards[0].name}`)
     console.log(`playercard2 ${playerCards[1].name}`)
     dealerCards = cardDeck.splice(0, 2);
+    
+    //sum of all cards value in player & dealer hands
+    playerCardsValue = playerCards.reduce(( sum, playerCards) => {
+      return sum + playerCards.rank
+    }, 0);
+    dealerCardsValue = dealerCards.reduce(( sum, dealerCards) => {
+      return sum + dealerCards.rank
+    }, 0);
 
-  var playerBlackJackCheck = checkBlackJack(playerCards);
-  console.log('blackjackcheck', playerBlackJackCheck)   
+    //check if any hands obtain blackjack
+    playerBlackJackCheck = checkBlackJack(playerCards);
+    dealerBlackJackCheck = checkBlackJack(dealerCards);
+    console.log('playerblackjackcheck', playerBlackJackCheck)
+    console.log('dealerblackjackcheck', dealerBlackJackCheck)    
   
-    if (playerBlackJackCheck == true )
-    {
-      gameMode = 'HitOrStand';
-      return 'You got a BlackJack!!. You Won!';
+    if (playerBlackJackCheck == true && dealerBlackJackCheck != true) {
+      gameMode = 'Initialize Game'
+      return `You got ${outputOfCards(playerCards)}. <br> A BlackJack!!. You Won!`;
+    }
+    else if (playerBlackJackCheck == true && dealerBlackJackCheck == true) {
+      gameMode = 'Initialize Game';
+      return `You got ${outputOfCards(playerCards)}. <br> Dealer got ${outputOfCards(dealerCards)}. <br> It's a tie with both blackjack!!`;
     }
     else {
-      //sum of all cards value in player hands
-      playerCardsValue = playerCards.reduce(( sum, playerCards) => {
-        return sum + playerCards.rank
-      }, 0);
       gameMode = 'HitOrStand';
-      return `You got ${playerCards[0].name} & ${playerCards[1].name} with a total value of ${playerCardsValue}. Please choose hit or stand.`;
+      return `You got ${outputOfCards(playerCards)} with a total value of ${playerCardsValue}. <br> Dealer one-sided card value = ${dealerCards[0].name} <br> Please choose hit or stand.`;
     }
   }
 
   if (gameMode == 'HitOrStand'){
-    if (input == 'hit')
-    {    
+    if (input == 'hit'){    
      // add one card to playerCards
      playerCards.push(cardDeck[0]);
      cardDeck.splice(0, 1);
@@ -114,47 +149,38 @@ var main = function (input) {
      playerCardsValue = playerCards.reduce(( sum, playerCards) => {
       return sum + playerCards.rank
     }, 0)
-     return `You got ${playerCards[0].name}, ${playerCards[1].name}, ${playerCards[2].name} with a total value of ${playerCardsValue}. Please choose hit or stand.`
-     //if value does not exceed 21, return value and if player wants to continue hit or stand
 
+      //if value does not exceed 21, return value and if player wants to continue hit or stand
+      if(playerCardsValue <= 21){
+      return `You got ${outputOfCards(playerCards)} with a total value of ${playerCardsValue}. <br> Dealer one-sided card value = ${dealerCards[0].name}  <br>  Please choose hit or stand.`
+      } else{
+        gameMode = 'Initialize Game'
+        return `You have busted with ${outputOfCards(playerCards)} with a total value of ${playerCardsValue}. <br> Dealer has won!`
+      }
     }
-    // else if {
-    //   //sum of all cards value in player hands
-    //   playerCardsValue = playerCards.reduce(( sum, playerCards) => {
-    //     return sum + playerCards.rank
-    //   }, 0)
 
-    //   return `You got ${playerCards[0].name} & ${playerCards[1].name} with a total value of ${playerCardsValue}. Please choose hit or stand.`
-    } else {
-      return 'Pls enter either hit or stand.'
-    }
-  // }
-
-    };     
-    // //use reduce to sum card value
-    // if playerCards.includes('ace'){
-    //   return 'Blackjack';
-    // }
-    // else {
-    //   return 'Normal';
-    
-    // const sum = playerCards.rank.reduce((accumulator, currentValue) => {
-    //   return accumulator + currentValue;
-    // });
+    else if (input == 'stand'){          
+      // while dealer total value < 17 then add one card to dealer (loop) and update dealer cards value
+      while (dealerCardsValue < 17){
         
-  // }
-  // var myOutputValue = 'hello world';
-  // return myOutputValue;
+        dealerCards.push(cardDeck[0]);
+        cardDeck.splice(0, 1);
 
+       
+        dealerCardsValue = dealerCards.reduce(( sum, dealerCards) => {
+          return sum + dealerCards.rank
+        }, 0);
+      }
+      
+      var finalResults = cardComparison();
+      console.log('final print out', finalResults)
+      gameMode = 'Initialize Game'
+      return finalResults
+      }
 
+      else{      
+      return `Pls enter either hit or stand.<br>You got ${outputOfCards(playerCards)} with a total value of ${playerCardsValue}. <br> Dealer one-sided card value = ${dealerCards[0].name}`
+      } 
+    }
 
-
-
-//blackjack rules: deal 2 cards to player
-
-//if total is more than 21 loses
-// if 2 cards is auto 21, player wins
-
-// else show player cards value and if player choose to hit or stand (if hit add one card) 
-
-//if stand, dealer draw 2 cards, dealer hit if below 17. stand if above or equal to 17.
+};      
