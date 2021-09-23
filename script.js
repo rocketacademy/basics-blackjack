9d7ac465ada8d756f42a99ad8f535b5f0f6bd359
Hedylyt <hedylimyt@gmail.com>
2021-09-23 21:41:59 +0800

    updated card deck and beautified font plus color

--- script.js
+++ script.js
@@ -18,269 +18,41 @@ var drawImage =
   '<img src="https://c.tenor.com/2w0ss8jJzXwAAAAC/lol-friends.gif"/>';
 
 var makeDeck = function () {
-  var newDeck = [
-    {
-      name: "ace",
-      suit: "hearts",
-      rank: 11,
-    },
-    {
-      name: "2",
-      suit: "hearts",
-      rank: 2,
-    },
-    {
-      name: "3",
-      suit: "hearts",
-      rank: 3,
-    },
-    {
-      name: "4",
-      suit: "hearts",
-      rank: 4,
-    },
-    {
-      name: "5",
-      suit: "hearts",
-      rank: 5,
-    },
-    {
-      name: "6",
-      suit: "hearts",
-      rank: 6,
-    },
-    {
-      name: "7",
-      suit: "hearts",
-      rank: 7,
-    },
-    {
-      name: "8",
-      suit: "hearts",
-      rank: 8,
-    },
-    {
-      name: "9",
-      suit: "hearts",
-      rank: 9,
-    },
-    {
-      name: "10",
-      suit: "hearts",
-      rank: 10,
-    },
-    {
-      name: "jack",
-      suit: "hearts",
-      rank: 10,
-    },
-    {
-      name: "queen",
-      suit: "hearts",
-      rank: 10,
-    },
-    {
-      name: "king",
-      suit: "hearts",
-      rank: 10,
-    },
-    {
-      name: "ace",
-      suit: "diamonds",
-      rank: 11,
-    },
-    {
-      name: "2",
-      suit: "diamonds",
-      rank: 2,
-    },
-    {
-      name: "3",
-      suit: "diamonds",
-      rank: 3,
-    },
-    {
-      name: "4",
-      suit: "diamonds",
-      rank: 4,
-    },
-    {
-      name: "5",
-      suit: "diamonds",
-      rank: 5,
-    },
-    {
-      name: "6",
-      suit: "diamonds",
-      rank: 6,
-    },
-    {
-      name: "7",
-      suit: "diamonds",
-      rank: 7,
-    },
-    {
-      name: "8",
-      suit: "diamonds",
-      rank: 8,
-    },
-    {
-      name: "9",
-      suit: "diamonds",
-      rank: 9,
-    },
-    {
-      name: "10",
-      suit: "diamonds",
-      rank: 10,
-    },
-    {
-      name: "jack",
-      suit: "diamonds",
-      rank: 10,
-    },
-    {
-      name: "queen",
-      suit: "diamonds",
-      rank: 10,
-    },
-    {
-      name: "king",
-      suit: "diamonds",
-      rank: 10,
-    },
-    {
-      name: "ace",
-      suit: "clubs",
-      rank: 11,
-    },
-    {
-      name: "2",
-      suit: "clubs",
-      rank: 2,
-    },
-    {
-      name: "3",
-      suit: "clubs",
-      rank: 3,
-    },
-    {
-      name: "4",
-      suit: "clubs",
-      rank: 4,
-    },
-    {
-      name: "5",
-      suit: "clubs",
-      rank: 5,
-    },
-    {
-      name: "6",
-      suit: "clubs",
-      rank: 6,
-    },
-    {
-      name: "7",
-      suit: "clubs",
-      rank: 7,
-    },
-    {
-      name: "8",
-      suit: "clubs",
-      rank: 8,
-    },
-    {
-      name: "9",
-      suit: "clubs",
-      rank: 9,
-    },
-    {
-      name: "10",
-      suit: "clubs",
-      rank: 10,
-    },
-    {
-      name: "jack",
-      suit: "clubs",
-      rank: 10,
-    },
-    {
-      name: "queen",
-      suit: "clubs",
-      rank: 10,
-    },
-    {
-      name: "king",
-      suit: "clubs",
-      rank: 10,
-    },
-    {
-      name: "ace",
-      suit: "spades",
-      rank: 11,
-    },
-    {
-      name: "2",
-      suit: "spades",
-      rank: 2,
-    },
-    {
-      name: "3",
-      suit: "spades",
-      rank: 3,
-    },
-    {
-      name: "4",
-      suit: "spades",
-      rank: 4,
-    },
-    {
-      name: "5",
-      suit: "spades",
-      rank: 5,
-    },
-    {
-      name: "6",
-      suit: "spades",
-      rank: 6,
-    },
-    {
-      name: "7",
-      suit: "spades",
-      rank: 7,
-    },
-    {
-      name: "8",
-      suit: "spades",
-      rank: 8,
-    },
-    {
-      name: "9",
-      suit: "spades",
-      rank: 9,
-    },
-    {
-      name: "10",
-      suit: "spades",
-      rank: 10,
-    },
-    {
-      name: "jack",
-      suit: "spades",
-      rank: 10,
-    },
-    {
-      name: "queen",
-      suit: "spades",
-      rank: 10,
-    },
-    {
-      name: "king",
-      suit: "spades",
-      rank: 10,
-    },
-  ];
-  return newDeck;
+  var cardDeck = [];
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    var currentSuit = suits[suitIndex];
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      var cardName = rankCounter;
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+      if (rankCounter == 11 || rankCounter == 12 || rankCounter == 13) {
+        var cardRank = 10;
+      } else if (rankCounter == 1) {
+        cardRank = 11;
+      } else {
+        cardRank = rankCounter;
+      }
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: cardRank,
+      };
+      cardDeck.push(card);
+      rankCounter += 1;
+    }
+    suitIndex += 1;
+  }
+  return cardDeck;
 };
 
 var getRandomIndex = function (max) {
@@ -358,6 +130,10 @@ var main = function (input) {
         mustStand = true;
       }
     } else if (input == "Stand" || mustStand == true) {
+      // calculate player's current score
+      playerTotalRank = calRank(playerCard);
+      console.log(playerTotalRank);
+
       // calculate computer's current score
       var computerTotalRank = calRank(computerCard);
 
