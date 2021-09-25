b475afeea7b005704ed6242c7e62a24ef89f3ab4
nehtelee <nehtelee@gmail.com>
2021-09-25 10:20:24 +0800

    Fancified Blackjack base with GIFs and emojis

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ var dealersCard1 = 0;
 var dealersCard2 = 0;
 var playersTotal = 0;
 var dealersTotal = 0;
+var gifDeliberation =
+  '<img src ="https://c.tenor.com/RFRPkimHjfcAAAAC/zach-galifianakis-very-bad-trip-meme.gif"/>';
+var gifCelebrate =
+  '<img src ="https://c.tenor.com/gluIOryzqFMAAAAC/confetti.gif"/>';
+var gifTryAgain =
+  '<img src ="https://media3.giphy.com/media/5BLIUJbZfDzIPv0EpL/giphy.gif?cid=ecf05e47qwmahbnl8ujcbnhzt1f4itivkc90zqy2bqnty6pp&rid=giphy.gif&ct=g"/>';
+var gifTie =
+  '<img src ="https://media1.giphy.com/media/xT3i0VNrc6Ny7bxfJm/giphy.gif?cid=ecf05e476bjnp9ffd24k0wppm8aszhyr4z1nzpmo55ccayer&rid=giphy.gif&ct=g"/>';
 
 var main = function (input) {
   if (gameMode == "reset") {
@@ -26,7 +34,7 @@ var main = function (input) {
     dealersPlay();
     if (dealersTotal > 21) {
       gameMode = "reset";
-      return "The dealer busted his hand. You win!";
+      return "The dealer busted his hand. You win! <br><br>" + gifCelebrate;
     }
     gameMode = "reset";
     return compareResults();
@@ -45,7 +53,7 @@ var main = function (input) {
 
 //initialize deck of cards
 var createNShuffleDeck = function () {
-  var suits = ["hearts", "clubs", "spades", "diamonds"];
+  var suits = ["♥️", "♣️", "♠️", "♦️"];
   var suitIndex = 0;
   while (suitIndex < suits.length) {
     var currentSuit = suits[suitIndex];
@@ -54,18 +62,18 @@ var createNShuffleDeck = function () {
       var rankValue = rankCounter;
       var cardName = rankValue;
       if (rankCounter == 1) {
-        cardName = "Ace";
+        cardName = "A";
       }
       if (rankCounter == 11) {
-        cardName = "Jack";
+        cardName = "J";
         rankValue = 10;
       }
       if (rankCounter == 12) {
-        cardName = "Queen";
+        cardName = "Q";
         rankValue = 10;
       }
       if (rankCounter == 13) {
-        cardName = "King";
+        cardName = "K";
         rankValue = 10;
       }
       var card = {
@@ -100,18 +108,17 @@ var dealCards = function (input) {
   playersCard2 = cardDeck.pop();
   playersHand.push(playersCard2);
   console.log("Player's array:", playersHand);
-  // dealersCard1 = cardDeck.pop();
-  dealersCard1 = { name: "Ace", suit: "hearts", rank: 1 };
+  dealersCard1 = cardDeck.pop();
   dealersHand.push(dealersCard1);
   dealersCard2 = cardDeck.pop();
   dealersHand.push(dealersCard2);
   console.log("Dealer's array:", dealersHand);
 
   //first ace is always 11 except for if both cards are aces
-  if (playersCard1.name == "Ace") {
+  if (playersCard1.name == "A") {
     playersCard1.rank = 11;
   }
-  if (playersCard2.name == "Ace") {
+  if (playersCard2.name == "A") {
     playersCard2.rank = 11;
   }
   if (playersCard1.rank + playersCard2.rank > 21) {
@@ -120,11 +127,11 @@ var dealCards = function (input) {
   playersTotal = Number(playersCard1.rank) + Number(playersCard2.rank);
   console.log("Player's Total:", playersTotal);
 
-  if (dealersCard1.name == "Ace") {
+  if (dealersCard1.name == "A") {
     dealersCard1.rank = 11;
     console.log("Dealer's Ace rank: 11");
   }
-  if (dealersCard2.name == "Ace") {
+  if (dealersCard2.name == "A") {
     dealersCard2.rank = 11;
     console.log("Dealer's Ace rank: 11");
   }
@@ -138,38 +145,40 @@ var dealCards = function (input) {
   //check for Blackjack
   var myOutputValue;
   if (
-    (dealersCard1.name == "Ace" && dealersCard2.rank == 10) ||
-    (dealersCard2.name == "Ace" && dealersCard1.rank == 10)
+    (dealersCard1.name == "A" && dealersCard2.rank == 10) ||
+    (dealersCard2.name == "A" && dealersCard1.rank == 10)
   ) {
     gameMode = "reset";
-    myOutputValue = "The dealer got a natural blackjack! You lose.";
+    myOutputValue =
+      "The dealer got a natural blackjack! You lose. <br><br>" + gifTryAgain;
   } else if (
-    (playersCard1.name == "Ace" && playersCard2.rank == 10) ||
-    (playersCard2.name == "Ace" && playersCard1.rank == 10)
+    (playersCard1.name == "A" && playersCard2.rank == 10) ||
+    (playersCard2.name == "A" && playersCard1.rank == 10)
   ) {
     gameMode = "reset";
-    myOutputValue = "You got a natural blackjack! You win.";
+    myOutputValue =
+      "You got a natural blackjack! You win. <br><br>" + gifCelebrate;
   } else if (
-    ((dealersCard1.name == "Ace" && dealersCard2.rank == 10) ||
-      (dealersCard2.name == "Ace" && dealersCard1.rank == 10)) &&
-    ((playersCard1.name == "Ace" && playersCard2.rank == 10) ||
-      (playersCard2.name == "Ace" && playersCard1.rank == 10))
+    ((dealersCard1.name == "A" && dealersCard2.rank == 10) ||
+      (dealersCard2.name == "A" && dealersCard1.rank == 10)) &&
+    ((playersCard1.name == "A" && playersCard2.rank == 10) ||
+      (playersCard2.name == "A" && playersCard1.rank == 10))
   ) {
     myOutputValue =
-      "Both you and the dealer got natural blackjacks! It's a tie.";
+      "Both you and the dealer got natural blackjacks! It's a tie. <br><br>" +
+      gifTie;
   } else {
     myOutputValue =
       "You got " +
       playersCard1.name +
-      " of " +
       playersCard1.suit +
       " and " +
       playersCard2.name +
-      " of " +
       playersCard2.suit +
       ". Your total is " +
       playersTotal +
-      " points. Enter 'draw' to draw another card or 'end' if you're satisfied with your cards.";
+      " points. <br><br> Enter 'draw' to draw another card or 'end' if you're satisfied with your cards. <br><br>" +
+      gifDeliberation;
   }
   return myOutputValue;
 };
@@ -185,7 +194,7 @@ var playersPlay = function () {
     //first ace is always 11 except for if both cards are aces
     if (
       (arrayIndex == "0" || arrayIndex == "1") &&
-      playersHand[arrayIndex].name == "Ace"
+      playersHand[arrayIndex].name == "A"
     ) {
       playersHand[arrayIndex].rank = 11;
       console.log("Array Index:", arrayIndex);
@@ -202,14 +211,14 @@ var playersPlay = function () {
   console.log("Player's total before recount:", playersTotal);
   //if player's total hand exceeds 21, revert ace with a 11 value to ace with a 1 value
   if (playersTotal > 21) {
-    if (playersHand[0].name == "Ace") {
+    if (playersHand[0].name == "A") {
       playersHand[0].rank = 1;
       console.log(
         "Exceeds 21 - player's Ace reverts to 1: index 0",
         playersHand
       );
     }
-    if (playersHand[1].name == "Ace") {
+    if (playersHand[1].name == "A") {
       playersHand[1].rank = 1;
       console.log(
         "Exceeds 21 - player's Ace reverts to 1: index 1",
@@ -226,25 +235,25 @@ var playersPlay = function () {
   console.log("Player's total after recount:", playersTotal);
   if (playersTotal <= 21) {
     return (
-      "You have drawn " +
+      "You drew " +
       playersDrawCard.name +
-      " of " +
       playersDrawCard.suit +
       ". Your total is " +
       playersTotal +
-      " points. Enter 'draw' to draw another card or 'end' if you're satisfied with your cards."
+      " points. <br><br> Enter 'draw' to draw another card or 'end' if you're satisfied with your cards. <br><br>" +
+      gifDeliberation
     );
   }
   if (playersTotal > 21) {
     gameMode = "reset";
     return (
-      "You have drawn " +
+      "You drew " +
       playersDrawCard.name +
-      " of " +
       playersDrawCard.suit +
       ". Your total is " +
       playersTotal +
-      " points. You've busted your hand. Better luck next time!"
+      " points. <br><br> You've busted your hand! You lose. <br><br>" +
+      gifTryAgain
     );
   }
 };
@@ -253,11 +262,11 @@ var playersPlay = function () {
 var dealersPlay = function () {
   dealersTotal = 0;
   //first ace is always 11 except for if both cards are aces
-  if (dealersCard1.name == "Ace") {
+  if (dealersCard1.name == "A") {
     dealersCard1.rank = 11;
     console.log("Dealer's Ace rank: 11");
   }
-  if (dealersCard2.name == "Ace") {
+  if (dealersCard2.name == "A") {
     dealersCard2.rank = 11;
     console.log("Dealer's Ace rank: 11");
   }
@@ -275,14 +284,14 @@ var dealersPlay = function () {
   console.log("Dealer's total before recount:", dealersTotal);
   //if dealer's total hand exceeds 21, revert ace with a 11 value to ace with a 1 value
   if (dealersTotal > 21) {
-    if (dealersCard1.name == "Ace") {
+    if (dealersCard1.name == "A") {
       dealersCard1.rank = 1;
       console.log(
         "Exceeds 21 - dealer's Ace reverts to 1: card 1",
         dealersHand
       );
     }
-    if (dealersCard2.name == "Ace") {
+    if (dealersCard2.name == "A") {
       dealersCard2.rank = 1;
       console.log(
         "Exceeds 21 - dealer's Ace reverts to 1: card 2",
@@ -314,7 +323,8 @@ var compareResults = function () {
       playersTotal +
       " points while the dealer scored " +
       dealersTotal +
-      " points. You lose!"
+      " points. You lose! <br><br>" +
+      gifTryAgain
     );
   }
   if (dealersTotal < playersTotal) {
@@ -323,7 +333,8 @@ var compareResults = function () {
       playersTotal +
       " points while the dealer scored " +
       dealersTotal +
-      " points. You win!"
+      " points. You win! <br><br>" +
+      gifCelebrate
     );
   }
   if (dealersTotal == playersTotal) {
@@ -332,7 +343,8 @@ var compareResults = function () {
       playersTotal +
       " points while the dealer scored " +
       dealersTotal +
-      " points. It's a tie!"
+      " points. It's a tie! <br><br>" +
+      gifTie
     );
   }
 };
