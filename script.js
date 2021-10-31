846b86d32afbb4627206a94ba66539ff659248eb
Marcus4478 <marcuswyneo@gmail.com>
2021-10-31 18:34:31 +0800

    add initialise deck

--- script.js
+++ script.js
@@ -9,273 +9,47 @@ for now - find winner
 */
 
 // GLOBAL VARIABLES
+var deck = [];
 var mode = "init";
-var deck = [
-  {
-    name: "ace",
-    suit: "hearts",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "hearts",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "hearts",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "hearts",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "hearts",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "hearts",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "hearts",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "hearts",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "hearts",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "hearts",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "hearts",
-    rank: 11,
-  },
-  {
-    name: "queen",
-    suit: "hearts",
-    rank: 12,
-  },
-  {
-    name: "king",
-    suit: "hearts",
-    rank: 13,
-  },
-  {
-    name: "ace",
-    suit: "diamonds",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "diamonds",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "diamonds",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "diamonds",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "diamonds",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "diamonds",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "diamonds",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "diamonds",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "diamonds",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "diamonds",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "diamonds",
-    rank: 11,
-  },
-  {
-    name: "queen",
-    suit: "diamonds",
-    rank: 12,
-  },
-  {
-    name: "king",
-    suit: "diamonds",
-    rank: 13,
-  },
-  {
-    name: "ace",
-    suit: "clubs",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "clubs",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "clubs",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "clubs",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "clubs",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "clubs",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "clubs",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "clubs",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "clubs",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "clubs",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "clubs",
-    rank: 11,
-  },
-  {
-    name: "queen",
-    suit: "clubs",
-    rank: 12,
-  },
-  {
-    name: "king",
-    suit: "clubs",
-    rank: 13,
-  },
-  {
-    name: "ace",
-    suit: "spades",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "spades",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "spades",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "spades",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "spades",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "spades",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "spades",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "spades",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "spades",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "spades",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "spades",
-    rank: 11,
-  },
-  {
-    name: "queen",
-    suit: "spades",
-    rank: 12,
-  },
-  {
-    name: "king",
-    suit: "spades",
-    rank: 13,
-  },
-];
-var player = [];
-var computer = [];
 var shuffledDeck = [];
 var myOutputValue = "";
+
+// create a standard 52-card deck
+var initialiseDeck = function () {
+  var names = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+  ];
+  var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+  var ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+
+  // loop thru suits length = 4
+  index = 0;
+  for (var suit = 0; suit < suits.length; suit += 1) {
+    // nested loop thru names length = 13
+    for (var name = 0; name < names.length; name += 1) {
+      deck[index] = {
+        name: names[name],
+        value: values[name],
+        rank: ranks[name],
+        suit: suits[suit],
+      };
+      index += 1;
+    }
+  }
+};
 
 // Get a random index ranging from 0 (inclusive) to max (exclusive).
 var getRandomIndex = function (max) {
@@ -328,14 +102,5 @@ var showHandMessage = function () {
 };
 
 var main = function (input) {
-  // shuffle deck
-  // deal cards to player and computer
-  if ((mode = "init")) {
-    shuffledDeck = shuffleCards(deck);
-    dealStartingHand();
-    showHandMessage();
-    return myOutputValue;
-  }
-
   return myOutputValue;
 };
