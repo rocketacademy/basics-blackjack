b3e43285fab5c37cb6e74b6e7174dac124bd41b1
honeyroastedturkey <wong1046@e.ntu.edu.sg>
2021-11-09 00:53:48 +0800

    base model of blackjack

--- script.js
+++ script.js
@@ -1,4 +1,219 @@
+var currentDeck = [];
+var playerHand = [];
+var computerHand = [];
+
+var mode = "drawPhase";
+var hitOrStand = "";
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (mode == "drawPhase") {
+    //make deck
+    currentDeck = makeDeck();
+
+    //shuffle deck
+    currentDeck = shuffle(currentDeck);
+
+    //dealing cards
+    playerHand.push(currentDeck.pop());
+    computerHand.push(currentDeck.pop());
+    playerHand.push(currentDeck.pop());
+    computerHand.push(currentDeck.pop());
+
+    //determine blackjack
+    mode = isBlackJack(playerHand, computerHand); //changes mode
+  }
+
+  if (mode == "normal") {
+    mode = "playerHit";
+    return `Your cards are:  ${playerHand[0].name} ${playerHand[0].suits} and ${
+      playerHand[1].name
+    } ${playerHand[1].suits} for a total value of: ${determineValue(
+      playerHand
+    )} <br>
+     One of the Computer's card: ${computerHand[0].name} ${
+      computerHand[0].suits
+    } <br>
+     Enter 'hit' or 'stand' to proceed:`;
+  } else if (mode == "playerBlackjack") {
+    return `Congratulations! You've hit blackjack! <br> 
+      Your cards are:          ${playerHand[0].name} ${playerHand[0].suits} and ${playerHand[1].name} ${playerHand[1].suits} <br>
+      Computers's cards are:   ${computerHand[0].name} ${computerHand[0].suits} and  ${computerHand[1].name} ${computerHand[1].suits}<br>
+      Refresh the page to play again!    `;
+  } else if (mode == "computerBlackjack") {
+    return `Oh No! The computer hit blackjack! <br>
+      Your cards were:       ${playerHand[0].name} ${playerHand[0].suits} and ${playerHand[1].name} ${playerHand[1].suits} <br>
+      Computer's card were:  ${computerHand[0].name} ${computerHand[0].suits} and  ${computerHand[1].name} ${computerHand[1].suits} <br>
+      Refresh the page to play again! `;
+  } else if (mode == "bothBlackjack") {
+    return `Errr.... You both hit blackjack...<br>
+      Your hand:              ${playerHand[0].name} ${playerHand[0].suits} and ${playerHand[1].name} ${playerHand[1].suits} <br>
+      The computers's hand:   ${computerHand[0].name} ${computerHand[0].suits} and ${computerHand[1].name} ${computerHand[1].suits}<br>
+      Refresh the page to play again!    `;
+  }
+
+  if (mode == "playerHit") {
+    if (input == "hit") {
+      playerHand.push(currentDeck.pop());
+      if (determineValue(playerHand) > 21) {
+        mode = "playerBust";
+      } else
+        return `Your cards are: ${displayCards(
+          playerHand
+        )} which is a total of ${determineValue(playerHand)} <br>
+         One of the Computer's card: ${computerHand[0].name} ${
+          computerHand[0].suits
+        } <br>
+     Enter 'hit' or 'stand' to proceed:`;
+    }
+    if (input == "stand") {
+      mode = "determineWinner";
+    }
+  }
+
+  if (mode == "playerBust") {
+    return `Player lose`;
+  }
+
+  if ((mode = "determineWinner")) {
+    while (determineValue(computerHand) < 17) {
+      computerHand.push(currentDeck.pop());
+    }
+    if (determineValue(computerHand) > 21) {
+      string = `You Won as the dealer bust with a value of \n
+      ${determineValue(computerHand)}`;
+    } else if (determineValue(computerHand) > determineValue(playerHand)) {
+      string = `I\'m sorry but you lost, as the \n
+      computer's total of ${determineValue(playerHand)} \n
+      beats your total of ${determineValue(computerHand)} `;
+    } else {
+      string = `Congratulations! You Won!\n <br>
+      Your total of ${determineValue(computerHand)} beats\n
+      Computer's total of ${determineValue(playerHand)} \n
+       `;
+    }
+    return `Your cards are: ${displayCards(playerHand)} \n 
+    for a value of ${determineValue(playerHand)}   <br>
+
+    Computer's card are: ${displayCards(computerHand)}  \n
+    for a value of   ${determineValue(computerHand)} <br>
+
+    ${string}`;
+  }
+};
+//returns array of cards{rank:1-13, suits:spades|hearts|clubs|diamonds, name:Ace-10,jack,king,queen }
+var makeDeck = function () {
+  var deck = [];
+  var suites = ["♠️", "♥️", "♣️", "♦️"];
+  for (var i = 0; i < 4; i++) {
+    var currentSuit = suites[i];
+    for (var j = 1; j < 14; j++) {
+      var cardName = j;
+      var value = j;
+      if (j == 1) {
+        cardName = "A";
+      } else if (j == 11) {
+        cardName = "J";
+        value = 10;
+      } else if (j == 12) {
+        cardName = "Q";
+        value = 10;
+      } else if (j == 13) {
+        cardName = "K";
+        value = 10;
+      }
+
+      var card = {
+        rank: j,
+        suits: currentSuit,
+        name: cardName,
+        value: value,
+      };
+      deck.push(card);
+    }
+  }
+  return deck;
+};
+
+//returns shuffled deck
+var shuffle = function (array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
+//determine if a or b or both is blackjack => playerBlackjack, computerBlackjack, bothBlackjack, or normal
+var isBlackJack = function (arr_playerHand, arr_computerHand) {
+  var plrCard1 = {};
+  var plrCard2 = {};
+  var comCard1 = {};
+  var comCard2 = {};
+
+  var status = "normal";
+  [plrCard1, plrCard2] = arr_playerHand;
+  [comCard1, comCard2] = arr_computerHand;
+  if (
+    (plrCard1.value == 1 && plrCard2.value == 10) ||
+    (plrCard2.value == 1 && plrCard1.value == 10)
+  ) {
+    if (
+      (comCard1.value == 1 && comCard2.value == 10) ||
+      (comCard2.value == 1 && comCard1.value == 10)
+    ) {
+      status = "bothBlackjack";
+    } else status = "playerBlackjack";
+  }
+  if (
+    (comCard1.value == 1 && comCard2.value == 10) ||
+    (comCard2.value == 1 && comCard1.value == 10)
+  ) {
+    status = "computerBlackjack";
+  }
+  return status;
+};
+
+//determine value of cards return value
+var determineValue = function (arrayTest) {
+  var sum = 0;
+  var noOfAces = 0;
+  for (i in arrayTest) {
+    if (arrayTest[i].value == 1) {
+      noOfAces += 1;
+      sum += 11;
+    } else {
+      sum += arrayTest[i].value;
+    }
+  }
+  while (noOfAces > 0) {
+    while (sum > 21) {
+      sum -= 10;
+      noOfAces -= 1;
+    }
+    if (sum < 22) {
+      break;
+    }
+  }
+  return sum;
+};
+
+var displayCards = function (stack) {
+  var arr = [];
+  var card = [];
+  for (i in stack) {
+    arr.push(stack[i].name, stack[i].suits);
+  }
+  return arr.join(" ");
 };
