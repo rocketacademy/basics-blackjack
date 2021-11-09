63396ff9135b9aa98bb1e3213e6406167b960a18
Cheoklate <gcheok88@gmail.com>
2021-11-09 19:22:14 +0800

    BasiscsBlackjack 2.0

--- script.js
+++ script.js
@@ -1,195 +1,151 @@
-function buildDeck() {
-  var array = [];
-  for (let i = 1; i <= 52; i++) {
-      array.push(i);
-  }
-  return array;
-}
+// To do: refactor and optimize code by using generic get win lose message
+// implement Aces logic
+// improve presentation
+// shift dealer draw up to "calculating winner" state and list what dealer drew in a while loop
+var state = "BUILD DECK";
+var numOfPlayer1Wins = 0;
+var numOfDealerWins = 0;
+var cardDeck = [];
+var playerCards = [];
+var dealerCards = [];
+var playerValue = 0;
+var dealerValue = 0;
 
-function shuffle(array) {
-  let currentIndex = array.length,
-      randomIndex;
+var cardGame = function (input) {
+  // debugger
+  var makeDeck = function () {
+    var suits = ["hearts", "diamonds", "clubs", "spades"];
+    var suitIndex = 0;
 
-  while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-          array[randomIndex],
-          array[currentIndex],
-      ];
-  }
+    while (suitIndex < suits.length) {
+      var currentSuit = suits[suitIndex];
+      var rankCounter = 1;
 
-  return array;
-}
-
-function getCard(i) {
-  return {
-      value: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"][
-          (i - 1) % 13
-      ],
-      suit: ["spades", "clubs", "hearts", "diamonds"][Math.ceil(i / 13) - 1],
+      while (rankCounter <= 13) {
+        var cardName = rankCounter;
+        var cardValue = rankCounter;
+        if (cardName == 1) {
+          cardName = "ace";
+          cardValue = 11;
+        } else if (cardName == 11) {
+          cardName = "jack";
+          cardValue = 10;
+        } else if (cardName == 12) {
+          cardName = "queen";
+          cardValue = 10;
+        } else if (cardName == 13) {
+          cardName = "king";
+          cardValue = 10;
+        }
+        var card = {
+          name: cardName,
+          suit: currentSuit,
+          value: cardValue,
+        };
+        cardDeck.push(card);
+        rankCounter += 1;
+      }
+      suitIndex += 1;
+    }
   };
-}
 
-function isPortrait(card) {
-  return ["J", "Q", "K"].includes(card.value);
-}
+  var getRandomIndex = function (max) {
+    return Math.floor(Math.random() * max);
+  };
 
-function getHandValue(hand) {
-  const cards = hand.map(getCard);
-  const nonAces = cards.filter((card) => card.value !== "A");
-  const aces = cards.filter((card) => card.value === "A");
+  var shuffleCards = function (cardDeck) {
+    var currentIndex = 0;
+    while (currentIndex < cardDeck.length) {
+      var randomIndex = getRandomIndex(cardDeck.length);
+      var randomCard = cardDeck[randomIndex];
+      var currentCard = cardDeck[currentIndex];
+      cardDeck[currentIndex] = randomCard;
+      cardDeck[randomIndex] = currentCard;
+      currentIndex = currentIndex + 1;
+    }
+  };
 
-  // Blackjack
-  if (aces.length === 1 && nonAces.length === 1 && isPortrait(nonAces[0])) {
-      return 21;
+  var gameOver = function () {
+    var playerHand = playerValue;
+    var dealerHand = dealerValue;
+    var dealerWins = numOfDealerWins;
+    var playerWins = numOfPlayer1Wins;
+    state = "START";
+    playerCards = [];
+    dealerCards = [];
+    playerValue = 0;
+    dealerValue = 0;
+    return `<br>
+    total value of player cards is: ${playerHand}<br>
+    total value of dealer cards is :${dealerHand}<br>
+    Dealer has won ${dealerWins} times<br>
+    Player has won ${playerWins} times<br>
+    click Submit to play again<br>`;
+  };
+
+
+  if (state == "BUILD DECK") {
+    makeDeck();
+    shuffleCards(cardDeck);
+    state = "START";
+    return `Click Submit to deal cards`;
+  } else if (state == "START") {
+    while (playerCards.length < 2 && dealerCards.length < 2) {
+      playerCards.push(cardDeck.pop());
+      dealerCards.push(cardDeck.pop());
+    }
+    state = "OPTION";
+    playerValue += Number(playerCards[0].value) + Number(playerCards[1].value);
+    dealerValue += Number(dealerCards[0].value) + Number(dealerCards[1].value);
+    return `The player drew ${JSON.stringify(playerCards)}<br>
+    the dealer drew ${JSON.stringify(dealerCards)}<br>
+    total value of player cards is: ${playerValue}<br>
+    total value of dealer cards is :${dealerValue}<br>
+    Enter HIT or STAY`;
+  } else if (state == "OPTION") {
+    if (input !== "HIT" && input !== "STAY") {
+      return `Please type "HIT" or "STAY" and click Submit to continue`;
+    } else if (input == "HIT" && playerValue < 21 && dealerValue != 21) {
+      playerCards.push(cardDeck.pop());
+      playerValue += Number(playerCards[playerCards.length - 1].value);
+      state = "OPTION";
+      return `you drew ${JSON.stringify(
+        playerCards[playerCards.length - 1]
+      )}<br>
+      your new total value is:${playerValue}<br>
+      total value of dealer is :${dealerValue} <br>
+      enter HIT or STAY
+      `;
+    } else if (input == "STAY" && playerValue < 22) {
+      state = "CALCULATE WINNER";
+      return `Comparing hand Values`;
+    } else {
+      numOfDealerWins += 1;
+      return `BUST
+      ${gameOver()} `;
+    }
+  } else if (state == "CALCULATE WINNER") {
+    while (dealerValue < 17) {
+      dealerCards.push(cardDeck.pop());
+      dealerValue += Number(dealerCards[dealerCards.length - 1].value);
+    }
+    if (dealerValue > 21 || playerValue > dealerValue) {
+      numOfPlayer1Wins += 1
+      return `You win!<br>
+      ${gameOver()}
+      
+`;
+    } else if (playerValue == dealerValue) {
+      return `Draw<br>
+      ${gameOver()}`;
+    } else {
+      numOfDealerWins += 1;
+      return `you lose<br>
+      ${gameOver()}`;
+    }
   }
-
-  let score = 0;
-  for (let i = 0; i < nonAces.length; i++) {
-      if (isPortrait(nonAces[i])) {
-          score += 10;
-      } else {
-          score += Number(nonAces[i].value);
-      }
-  }
-
-  // Treat as aces as 1s
-  score += aces.length;
-
-  if (score >= 21) {
-      return score;
-  }
-
-  // Try tens, convert aces to tens by adding 9
-  // TODO: Optimize this
-  for (let i = 0; i < aces.length; i++) {
-      if (score + 9 <= 21) {
-          score += 9;
-      }
-  }
-
-  return score;
-}
-
-let _state = {
-  value: "WAITING_TO_START",
 };
 
-function render(state) {
-  if (state.value === "WAITING_TO_START") {
-      return "Type START to start";
-  }
-
-  const hasGameEnded = ["WIN", "LOSS", "DRAW"].includes(state.value);
-
-  return `
-  <div>
-      ${hasGameEnded ? "<div>Type START to restart</div><br>" : ""}
-      ${hasGameEnded ? state.value : ""}
-      Player: ${getHandValue(state.player)}
-      <div>${state.player
-        .map(getCard)
-        .map((x) => `<div>${JSON.stringify(x)}</div>`)
-        .join("")}</div>
-      <br>
-      Dealer: ${
-        hasGameEnded
-          ? getHandValue(state.dealer)
-          : getHandValue([state.dealer[0]])
-      }
-      <div>${state.dealer
-        .map(getCard)
-        .map((x, i) =>
-          hasGameEnded
-            ? `<div>${JSON.stringify(x)}</div>`
-            : i === 0
-            ? `<div>${JSON.stringify(x)}</div>`
-            : `<div>(Faced down card)</div>`
-        )
-        .join("")}</div>
-  </div>
-  `;
-}
-
-function nextState(action, prevState) {
-if (
-  action === "START" &&
-  ["WAITING_TO_START", "DRAW", "LOSS", "WIN"].includes(prevState.value)
-) {
-  const deck = shuffle(buildDeck());
-  const player = [deck.pop(), deck.pop()];
-  const dealer = [deck.pop(), deck.pop()];
-  const playerScore = getHandValue(player);
-  const dealerScore = getHandValue(dealer);
-
-  return {
-    ...prevState,
-    value:
-      playerScore === 21
-        ? dealerScore === 21
-          ? "DRAW"
-          : "WIN"
-        : dealerScore === 21
-        ? "LOSS"
-        : "WAITING_FOR_PLAYER",
-    deck,
-    player,
-    dealer,
-  };
-}
-
-if (prevState.value === "WAITING_FOR_PLAYER") {
-  if (action === "HIT") {
-    const newDeck = [...prevState.deck];
-    const newPlayer = [...prevState.player, newDeck.pop()];
-    const playerScore = getHandValue(newPlayer);
-    return {
-      ...prevState,
-      value:
-        playerScore === 21
-          ? "WIN"
-          : playerScore > 21
-          ? "LOSS"
-          : prevState.value,
-      deck: newDeck,
-      player: newPlayer,
-    };
-  } else if (action === "STAND") {
-    const newDeck = [...prevState.deck];
-    let newDealer = [...prevState.dealer];
-    let dealerScore = getHandValue(newDealer);
-    const playerScore = getHandValue(prevState.player);
-
-    // Dealers must hit as long as < 17
-    while (newDeck.length && dealerScore < 17) {
-      newDealer.push(newDeck.pop());
-      dealerScore = getHandValue(newDealer);
-    }
-
-    return {
-      ...prevState,
-      deck: newDeck,
-      dealer: newDealer,
-      value:
-        dealerScore === 21
-          ? "LOSS"
-          : dealerScore > 21
-          ? "WIN"
-          : playerScore > dealerScore
-          ? "WIN"
-          : playerScore < dealerScore
-          ? "LOSS"
-          : "DRAW",
-    };
-  }
-}
-
-return prevState;
-}
-
 var main = function (input) {
-_state = nextState(input, _state);
-console.log(JSON.stringify(_state, null, 4));
-return render(_state);
+  return cardGame(input);
 };
