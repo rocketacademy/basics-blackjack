493d90341e9cd825bd7bdee64d88d6a3f7b865d0
Soh Thiam Hing <daffodilistic@gmail.com>
2021-11-10 21:39:06 +0800

    Basic card dealing and display logic done

--- index.js
+++ index.js
@@ -3,30 +3,98 @@ class App {
   previousState = -1;
   #players = [];
   #currentTurnIndex = 0;
+  #currentDeck = null;
+  #dealerIndex = -1;
 
   constructor() {
-    const deck = new Deck();
-    deck.shuffle();
+    this.initButtons();
+    this.askForName();
+  }
 
-    for (let i = 0; i < 5; i++) {
-      const card = deck.draw();
-      console.log(card);
+  startGame() {
+    const playerName = document.querySelector("#input-player-name").value;
+    if (playerName.length > 0) {
+      document.getElementById("input-player-name").classList.remove("uk-animation-shake");
+      document.getElementById("input-player-name-error").hidden = true;
 
-      this.displayCard(card);
+      this.#players = [new Player(playerName), new Player("Dealer")];
+      this.#dealerIndex = 1;
+
+      this.newRound();
+    } else {
+      document.getElementById("input-player-name").classList.add("uk-animation-shake");
+      document.getElementById("input-player-name-error").hidden = false;
     }
   }
 
-  displayCard(card) {
-    const outputElement = document.querySelector("#output-div");
+  newRound() {
+    document.getElementById("input-hit").disabled = false;
+    document.getElementById("input-stand").disabled = false;
+
+    UIkit.modal("#modal-name").hide();
+
+    this.#currentDeck = new Deck();
+    this.#currentDeck.shuffle();
+
+    this.#players.forEach((player, index) => {
+      player.discardHand();
+      player.addCardToHand(this.#currentDeck.draw());
+      player.addCardToHand(this.#currentDeck.draw());
+    });
+
+    this.#players.forEach((player, index) => {
+      let delay = (index + 1) * 500;
+      player.getHand().forEach((card, cardIndex) => {
+        delay += (cardIndex + 1) * 500;
+        this.delay(delay).then(() => {
+          if (index === this.#dealerIndex) {
+            this.displayCard(card, DisplayArea.Dealer);
+          } else {
+            this.displayCard(card, DisplayArea.Player);
+          }
+        });
+      });
+    });
+  }
+
+  displayCard(card, displayArea) {
+    var outputElement = null;
+    if (displayArea === DisplayArea.Dealer) {
+      outputElement = document.querySelector("#div-dealer-hand");
+    } else {
+      outputElement = document.querySelector("#div-player-hand");
+    }
+
     outputElement.innerHTML +=
-      '<div class="uk-padding-small">' + 
-      '<div class="uk-card uk-card-default">' +
-          '<div class="uk-card-media-top flip-card-front">' +
-            `<img src="${card.getImage()}" class="uk-height-small uk-height-max-small">` + 
-          '</div>'
-          '<div class="uk-card-body flip-card-back"></div>' + 
+        '<div class="uk-padding-small">' +
+          '<div class="uk-card uk-card-default uk-animation-slide-bottom">' +
+            '<div class="uk-card-media-top flip-card-front">' +
+              `<img src="${card.getImage()}" class="uk-height-small uk-height-max-small">` +
+            '</div>' + 
+          // '<div class="uk-card-body flip-card-back"></div>' +
         '</div>'
       '</div>';
+  }
+
+  clearCards(displayArea) {
+    if (displayArea === DisplayArea.Dealer) {
+      document.querySelector("#div-dealer-hand").innerHTML = "";
+    } else {
+      document.querySelector("#div-player-hand").innerHTML = "";
+    }
+  }
+
+  askForName() {
+    UIkit.modal("#modal-name").show();
+  }
+
+  initButtons() {
+    const startGameButton = document.querySelector("#input-start-game");
+    startGameButton.addEventListener("click", this.startGame.bind(this));
+  }
+  
+  delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
   }
 }
 
@@ -86,7 +154,7 @@ class Card {
 class Player {
   #name = "Player Unknown";
   #score = 100;
-  $hand = [];
+  #hand = [];
 
   constructor(name) {
     this.#name = name;
@@ -96,12 +164,28 @@ class Player {
     return this.#name;
   }
 
-  addScore(score) {
-    this.#score += score;
+  getHand() {
+    return this.#hand;
+  }
+
+  addCardToHand(card) {
+    this.#hand.push(card);
+  }
+
+  discardHand() {
+    this.#hand = [];
   }
 
   getScore() {
     return this.#score;
+  }
+
+  addScore(score) {
+    this.#score += score;
+  }
+
+  subtractScore(score) {
+    this.#score -= score;
   }
 }
 
@@ -110,6 +194,11 @@ const GameState = Object.freeze({
   NewRound: 1,
   Playing: 2,
   End: 3
+});
+
+const DisplayArea = Object.freeze({
+  Player: 0,
+  Dealer: 1
 });
 
 const temp = new App();
