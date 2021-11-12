7cae8ad3c751ecd9a5702762461b9360f857ce66
ahshawngoh <ahshawngoh@gmail.com>
2021-11-12 22:28:36 +0800

    Added betting in JS, Add gif in html

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 /*===========================================================================*/
 var PLAYER_TURN = "PLAYER_TURN";
 var DEALER_TURN = "DEALER_TURN";
+var GAME_END = "GAME_END";
+var GAME_START = "GAME_START";
 
 var turns = PLAYER_TURN;
 var shuffledDeck = [];
@@ -17,7 +19,9 @@ var playerObject = {
   value: 0,
 };
 var gotBusted = false;
-var gameStatus = true;
+var gameStatus = GAME_START;
+var currentAmount = 1000;
+var betAmount = 0;
 
 /*===========================================================================*/
 /*========================== ADDITIONAL FUNCTIONS ===========================*/
@@ -116,14 +120,19 @@ var getValue = function (cardOnHand) {
 /*===== GET WINNER BETWEEN PLAYERS AND DEALER =====*/
 var getWinner = function (dealerValue, playerValue) {
   if (playerValue > dealerValue) {
+    currentAmount = currentAmount + betAmount;
     return `Player Win, Dealer Lose  <br>
-            Press 'Start Game' to play again!`;
+    Press 'Start Game' to play again! <br><br>
+    Balance: $${currentAmount}`;
   } else if (playerValue < dealerValue) {
+    currentAmount = currentAmount - betAmount;
     return `Player Lose, Dealer Win <br>
-            Press 'Start Game' to play again!`;
+            Press 'Start Game' to play again!<br><br>
+            Balance: $${currentAmount}`;
   } else {
     return `Draw! <br>
-            Press 'Start Game' to play again!`;
+            Press 'Start Game' to play again!<br><br>
+            Balance: $${currentAmount}`;
   }
 };
 
@@ -133,7 +142,7 @@ var reset = function () {
   shuffledDeck = [];
   turns = PLAYER_TURN;
   gotBusted = false;
-  gameStatus = true;
+  gameStatus = GAME_START;
   shuffledDeck = [];
   dealerObject = {
     name: "Dealer",
@@ -154,6 +163,12 @@ var checkIfBusted = function (playerValue) {
     return `Player Value: ${playerValue}`;
   } else {
     gotBusted = true;
+    gameStatus = GAME_END;
+    currentAmount = currentAmount - betAmount;
+    document.getElementById("hit-button").disabled = true;
+    document.getElementById("stand-button").disabled = true;
+    document.getElementById("start-button").disabled = false;
+    document.getElementById("input-field").disabled = false;
     return `Player Value: ${playerValue} <br> 
             Player Busted! <br> 
             Press 'Start Game' to play again!`;
@@ -173,10 +188,15 @@ var getCardsOnHand = function (currentHand) {
   return myOutputValue;
 };
 
-/*===== CHECK FOR BLACKJACK AT START GAME =====*/
-var checkBlackjack = function (currentValue) {
+/*===== CHECK FOR BLACKJACK AT START GAME (PLAYER) =====*/
+var checkBlackjackPlayer = function (currentValue) {
   if (currentValue == 21) {
-    gameStatus = false;
+    gameStatus = GAME_END;
+    currentAmount += betAmount;
+    document.getElementById("hit-button").disabled = true;
+    document.getElementById("stand-button").disabled = true;
+    document.getElementById("start-button").disabled = false;
+    document.getElementById("input-field").disabled = false;
     return `Dealer Hand: <br> 
             ▮ <br>
             ${dealerObject.hands[1].name} ${dealerObject.hands[1].suit} <br><br>
@@ -185,7 +205,8 @@ var checkBlackjack = function (currentValue) {
             ${playerObject.hands[1].name} ${playerObject.hands[1].suit} <br><br>
             Player got Blackjack! <br>
             Player Win!! <br>
-            Press 'Start Game' to play again!`;
+            Press 'Start Game' to play again!<br><br>
+            Balance: $${currentAmount}`;
   } else {
     return `Dealer Hand: <br> 
             ▮ <br>
@@ -198,14 +219,44 @@ var checkBlackjack = function (currentValue) {
   }
 };
 
+/*===== CHECK FOR BLACKJACK AT START GAME (BANKER) =====*/
+var checkBlackjackDealer = function (currentValue) {
+  if (currentValue == 21) {
+    var cardsOnHand = "";
+    gameStatus = GAME_END;
+    cardsOnHand = getCardsOnHand(playerObject.hands);
+    currentAmount = currentAmount - betAmount;
+    return `Dealer Hand: <br> 
+            ${dealerObject.hands[0].name} ${dealerObject.hands[0].suit}  <br>
+            ${dealerObject.hands[1].name} ${dealerObject.hands[1].suit} <br><br>
+            Player Hand: <br>
+            ${cardsOnHand}<br><br>
+            Banker got Blackjack! <br>
+            Banker Win!! <br>
+            Press 'Start Game' to play again! <br><br>
+            Balance: $${currentAmount}`;
+  }
+};
+
+/*===== CHECK FOR BET AMOUNT =====*/
+var checkBetAmount = function (betAmount) {};
+
 /*===========================================================================*/
 /*============================= MAIN FUNCTIONS ==============================*/
 /*===========================================================================*/
 
-var startGame = function () {
+var startGame = function (input) {
   // Reset all variables for a new game
   reset();
-
+  if (input !== "0" && input !== "") {
+    betAmount = Number(input);
+  } else {
+    document.getElementById("hit-button").disabled = true;
+    document.getElementById("stand-button").disabled = true;
+    document.getElementById("start-button").disabled = false;
+    document.getElementById("input-field").disabled = false;
+    return "Please enter an amount to bet and click 'Start Game'";
+  }
   // Dealer and Player draw 2 cards each
   for (var counter = 0; counter < 2; counter += 1) {
     var playerDrawnCard = shuffledDeck.pop();
@@ -219,23 +270,23 @@ var startGame = function () {
   playerObject.value = getValue(playerObject.hands);
   console.log(playerObject);
 
-  var blackjackOutput = checkBlackjack(playerObject.value);
+  var blackjackOutput = checkBlackjackPlayer(playerObject.value);
 
   return blackjackOutput;
 };
 
 var hitHand = function () {
-  if (gameStatus == true) {
+  if (gameStatus == GAME_START) {
     var bustOutput = "";
     var cardsOnHand = "";
+    // Draw card
+    var playerDrawnCard = shuffledDeck.pop();
+    playerObject.hands.push(playerDrawnCard);
+    // Print hand
+    cardsOnHand = getCardsOnHand(playerObject.hands);
+    playerObject.value = getValue(playerObject.hands);
+    bustOutput = checkIfBusted(playerObject.value);
     if (gotBusted == false) {
-      // Draw card
-      var playerDrawnCard = shuffledDeck.pop();
-      playerObject.hands.push(playerDrawnCard);
-      // Print hand
-      cardsOnHand = getCardsOnHand(playerObject.hands);
-      playerObject.value = getValue(playerObject.hands);
-      bustOutput = checkIfBusted(playerObject.value);
       return `Dealer Hand: <br> 
               ▮ <br>
               ${dealerObject.hands[1].name} ${dealerObject.hands[1].suit} <br><br>
@@ -245,15 +296,16 @@ var hitHand = function () {
               Players Turn, click Hit or Stand!`;
     } else {
       // Print hand
-      cardsOnHand = getCardsOnHand(playerObject.hands);
+      // cardsOnHand = getCardsOnHand(playerObject.hands);
       return `Dealer Hand: <br> 
               ▮ <br>
               ${dealerObject.hands[1].name} ${dealerObject.hands[1].suit} <br><br>
               Player Hand: <br>
               ${cardsOnHand} <br> 
               Player Value: ${playerObject.value} <br>
-              Player Busted! <br>
-              Press 'Start Game' to play again!`;
+              Player Busted! Dealer Wins!!<br>
+              Press 'Start Game' to play again!<br><br>
+              Balance: $${currentAmount}`;
     }
   } else {
     return `Game ended. Click 'Start Game' to play again! `;
@@ -264,26 +316,45 @@ var standHand = function () {
   var playerCardsOnHand = "";
   var dealerCardsOnHand = "";
   var outcome = "";
+  document.getElementById("hit-button").disabled = true;
+  document.getElementById("stand-button").disabled = true;
+  document.getElementById("start-button").disabled = false;
+  document.getElementById("input-field").disabled = false;
 
-  while (dealerObject.value < 17) {
-    var dealerDrawnCard = shuffledDeck.pop();
-    dealerObject.hands.push(dealerDrawnCard);
-    dealerObject.value = getValue(dealerObject.hands);
+  if (dealerObject.value == 21) {
+    var blackjackOutput = checkBlackjackDealer(dealerObject.value);
+    return blackjackOutput;
   }
 
-  // Print Dealer Hands
-  dealerCardsOnHand = getCardsOnHand(dealerObject.hands);
-  // Print Player Hands
-  playerCardsOnHand = getCardsOnHand(playerObject.hands);
+  if (gameStatus == GAME_START) {
+    while (dealerObject.value < 17) {
+      var dealerDrawnCard = shuffledDeck.pop();
+      dealerObject.hands.push(dealerDrawnCard);
+      dealerObject.value = getValue(dealerObject.hands);
+    }
 
-  if (dealerObject.value > 21) {
-    outcome = `Dealer busted! Player Wins <br>
-              Press 'Start Game' to play again!`;
+    // Print Dealer Hands
+    dealerCardsOnHand = getCardsOnHand(dealerObject.hands);
+    // Print Player Hands
+    playerCardsOnHand = getCardsOnHand(playerObject.hands);
+
+    if (dealerObject.value > 21) {
+      currentAmount = currentAmount + betAmount;
+      outcome = `Dealer busted! Player Wins <br>
+                Press 'Start Game' to play again! <br><br>
+                Balance: $${currentAmount}`;
+    } else {
+      outcome = getWinner(dealerObject.value, playerObject.value);
+    }
   } else {
-    outcome = getWinner(dealerObject.value, playerObject.value);
+    document.getElementById("hit-button").disabled = true;
+    document.getElementById("stand-button").disabled = true;
+    document.getElementById("start-button").disabled = false;
+    document.getElementById("input-field").disabled = false;
+    return `Game ended. Click 'Start Game' to play again! `;
   }
-  gameStatus = false;
 
+  gameStatus = GAME_END;
   return `Dealer Hand: <br>
           ${dealerCardsOnHand} Dealer value: ${dealerObject.value} <br><br>
           Player Hand: <br>
