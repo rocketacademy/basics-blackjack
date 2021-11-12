b4ec646c657282dfed314e86be1b42a506a23b42
BeiFangT <teobeifang@gmail.com>
2021-11-13 02:12:50 +0800

    slight revision in code and formatting

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var makeDeck = function () {
     // Store the current suit in a variable
     var currentSuit = suits[suitIndex];
     var currentSymbol = "";
-
+    // Store symbol of suit in a variable
     if (currentSuit == "hearts") {
       currentSymbol = "♥";
     } else if (currentSuit == "diamonds") {
@@ -63,6 +63,7 @@ var makeDeck = function () {
         cardName = "King";
       }
 
+      // If the cardName is 11, 12 or 13, set value of card to 10.
       if (
         currentCardValue == 11 ||
         currentCardValue == 12 ||
@@ -71,7 +72,7 @@ var makeDeck = function () {
         currentCardValue = 10;
       }
 
-      // Create a new card with the current name, suit, and rank
+      // Create a new card with the current name, suit, rank, symbol and value
       var card = {
         name: cardName,
         suit: currentSuit,
@@ -121,12 +122,15 @@ var shuffleCards = function (cardDeck) {
   return cardDeck;
 };
 
+//Calculate value of cards
 var calculateCardsValue = function (cardDeck) {
   var index = 0;
   var cardValue = 0;
   var aceCounter = 0;
 
+  // add up card values in deck
   while (index < cardDeck.length) {
+    //if card is "ace", change card value to 11, and increase ace counter by 1
     if (cardDeck[index].name == "Ace") {
       cardDeck[index].value = 11;
       aceCounter += 1;
@@ -139,6 +143,7 @@ var calculateCardsValue = function (cardDeck) {
 
   index = 0;
 
+  // if card value is more than 21, deduct 10 from card value for every "ace" card. (value of ace becomes 1)
   while (index < aceCounter) {
     if (cardValue > 21) {
       cardValue = cardValue - 10;
@@ -150,27 +155,34 @@ var calculateCardsValue = function (cardDeck) {
   return cardValue;
 };
 
+//determine who wins by comparing player and computer card value
 var whoWins = function (playerValue, computerValue) {
   var winner = "";
   if (
     (playerValue > 21 && computerValue > 21) ||
     playerValue == computerValue
   ) {
+    myImage =
+      '<img src="https://media3.giphy.com/media/9Lycp5ifWTm6I/giphy.gif?cid=790b76119c6a2031b16393028411c3af7e52698183243329&rid=giphy.gif&ct=g"/>';
     winner = "It's a tie.";
   } else if (
     (playerValue > computerValue && playerValue <= 21) ||
     (computerValue > 21 && playerValue <= 21)
   ) {
+    myImage =
+      '<img src="https://media3.giphy.com/media/bBsLmHGPrZKN2/giphy.gif?cid=790b7611a06603e29f2a1d5fa5547278ff7d49aa30137933&rid=giphy.gif"/>';
     winner = "Congratulations! Player wins.";
   } else if (
     (playerValue < computerValue && computerValue <= 21) ||
     (playerValue > 21 && computerValue <= 21)
   ) {
+    myImage = '<img src="https://c.tenor.com/MC7I7H_t4vsAAAAC/sad-cry.gif"/>';
     winner = "Too Bad! Computer wins.";
   }
-  return winner;
+  return winner + myImage;
 };
 
+// display 1 card in hand
 var display1Card = function (cardArray) {
   var index = 0;
   var message = "";
@@ -185,6 +197,7 @@ var display1Card = function (cardArray) {
   return message;
 };
 
+// display all cards in hand
 var displayAllCards = function (cardArray) {
   var index = 0;
   var message = "";
@@ -194,9 +207,11 @@ var displayAllCards = function (cardArray) {
     message = message + `${cardArray[index].name}${cardArray[index].symbol} `;
     index += 1;
   }
+
+  // to let user know if they got blackjack or busted
   if (calculateCardsValue(cardArray) > 21) {
     comment = "Busted!";
-  } else if (calculateCardsValue(cardArray) == 21) {
+  } else if (calculateCardsValue(cardArray) == 21 && cardArray.length == 2) {
     comment = "BlackJack!";
   }
   message = message + ` (${calculateCardsValue(cardArray)}) ` + comment;
@@ -204,53 +219,47 @@ var displayAllCards = function (cardArray) {
   return message;
 };
 
+// hint for user to hit or stand
 var displayHint = function (playerCardArray) {
   var hint = "";
   var playerValue = calculateCardsValue(playerCardArray);
   if (playerValue == 21) {
-    hint = `Hint: You have already got Blackjack! Click "stand" to continue.`;
+    hint = `Hint: You have already got 21! Click "stand" to continue.`;
   } else if (playerValue <= 16) {
     hint = `Hint: You can try your luck by clicking "hit"`;
   } else if (playerValue > 16 && playerValue < 21) {
-    hint = `Hint: You are now very close to Blackjack. Think carefully.`;
+    hint = `Hint: You are now very close to 21. Think carefully.`;
   } else if (playerValue > 21) {
-    hint = 'Hint: You already exceed Blackjack. Click "stand" to continue.';
+    hint = 'Hint: You already exceed 21. Click "stand" to continue.';
   }
   return hint;
 };
 
+//calculate points of bet
 var tabulatePoints = function (playerCards, computerCards) {
-  var initialPoints = playerPoints;
+  var initialPoints = Number(playerPoints);
 
   console.log(playerPoints);
   console.log(initialPoints);
 
   if (
-    whoWins(
-      calculateCardsValue(playerCards),
-      calculateCardsValue(computerCards)
-    ) == "It's a tie."
+    (calculateCardsValue(playerCards) > 21 &&
+      calculateCardsValue(computerCards) > 21) ||
+    calculateCardsValue(playerCards) == calculateCardsValue(computerCards)
   ) {
-    if (
-      calculateCardsValue(playerCards) > 21 &&
-      calculateCardsValue(computerCards) > 21
-    ) {
-      playerPoints = initialPoints - betPerRound;
-    } else {
-      playerPoints = initialPoints;
-    }
+    playerPoints = initialPoints;
   } else if (
-    whoWins(
-      calculateCardsValue(playerCards),
-      calculateCardsValue(computerCards)
-    ) == "Congratulations! Player wins."
+    (calculateCardsValue(playerCards) > calculateCardsValue(computerCards) &&
+      calculateCardsValue(playerCards) <= 21) ||
+    (calculateCardsValue(computerCards) > 21 &&
+      calculateCardsValue(playerCards) <= 21)
   ) {
     playerPoints = initialPoints + betPerRound;
   } else if (
-    whoWins(
-      calculateCardsValue(playerCards),
-      calculateCardsValue(computerCards)
-    ) == "Too Bad! Computer wins."
+    (calculateCardsValue(playerCards) < calculateCardsValue(computerCards) &&
+      calculateCardsValue(computerCards) <= 21) ||
+    (calculateCardsValue(playerCards) > 21 &&
+      calculateCardsValue(computerCards) <= 21)
   ) {
     playerPoints = initialPoints - betPerRound;
   }
@@ -259,12 +268,32 @@ var tabulatePoints = function (playerCards, computerCards) {
   return totalPoints;
 };
 
+var chooseImage = function () {
+  if (myOutputValue.includes("key in a number") == true) {
+    myImage =
+      '<img src ="https://media1.giphy.com/media/S3H8Ke8emWCzu/giphy.gif?cid=790b76113e9acb2578da97bb52538bc509a8c6a280952d44&rid=giphy.gif&ct=g"/>';
+  }
+  return myImage;
+};
+
+var showGameoverText = function () {
+  var gameoverText = "";
+  myImage = '<img src ="https://c.tenor.com/Ho845EEf4v0AAAAC/angry.gif"/>';
+  if (playerPoints == 0) {
+    gameoverText = `Game Over! <br><br> Player runs out of points. <br> Please refresh the page to start a new game. ${myImage}`;
+  } else {
+    gameoverText = `Game Over! <br><br> The deck runs out of cards. <br> Please refresh the page to start a new game. ${myImage}`;
+  }
+  return gameoverText;
+};
+
 // Define Global Variables
+var User_Bet = "get user bet";
 var Deal_Cards = "deal cards";
 var Hit_Or_Stand = "hit or stand";
 var Who_Wins = "who wins";
 var Game_Over = "game over";
-var gameMode = Deal_Cards;
+var gameMode = User_Bet;
 var myOutputValue = "";
 var playerCards = [];
 var computerCards = [];
@@ -272,83 +301,106 @@ var playerCardsValue = 0;
 var computerCardsValue = 0;
 var myImage;
 var playerPoints = 100;
-var betPerRound = 10;
+var betPerRound = 0;
 //create a 52-card Deck
 var myDeck = makeDeck();
 
 var main = function (input) {
   //shuffle deck
   var shuffledDeck = shuffleCards(myDeck);
-  console.log(myDeck);
+
+  // Get user input of bet for the round
+  if (gameMode == User_Bet) {
+    // Validate if user key in "not a number" or "nothing" or "more than existing points."
+    if (playerPoints == 0 || myDeck.length < 4) {
+      gameMode = Game_Over;
+      return showGameoverText();
+    } else if (isNaN(input) == true || input == "" || input > playerPoints) {
+      return (
+        (myOutputValue = `Please key in a number. You have ${playerPoints} points. `) +
+        chooseImage()
+      );
+    } else {
+      betPerRound = Number(input);
+      gameMode = Deal_Cards;
+      myImage =
+        '<img src = "https://media3.giphy.com/media/gD7PbhbmVsw1i/giphy.gif?cid=ecf05e47l9v0recai9l07ef8jhk53x4ngw95tc6cgwmhe6gd&rid=giphy.gif&ct=g"/>';
+      return (myOutputValue = `You have bet ${betPerRound} points for this round. <br> Click "submit" to start the game. Good Luck! ${myImage}`);
+    }
+  }
 
   // when user click "submit" button, to deal 2 cards each to user and computer
   if (gameMode == Deal_Cards) {
     if (myDeck.length < 4) {
       gameMode = Game_Over;
-      return (myOutputValue = `Game Over! <br><br> The deck runs out of cards. <br> Please refresh the page to start a new game.`);
+      return showGameoverText();
     }
-    if (input == "") {
-      playerCards = [];
-      computerCards = [];
-      playerCards.push(shuffledDeck.pop());
-      playerCards.push(shuffledDeck.pop());
-      computerCards.push(shuffledDeck.pop());
-      computerCards.push(shuffledDeck.pop());
+    playerCards = [];
+    computerCards = [];
+    playerCards.push(shuffledDeck.pop());
+    playerCards.push(shuffledDeck.pop());
+    computerCards.push(shuffledDeck.pop());
+    computerCards.push(shuffledDeck.pop());
 
-      playerCardsValue = calculateCardsValue(playerCards);
-      computerCardsValue = calculateCardsValue(computerCards);
+    playerCardsValue = calculateCardsValue(playerCards);
+    computerCardsValue = calculateCardsValue(computerCards);
 
-      gameMode = Hit_Or_Stand;
+    gameMode = Hit_Or_Stand;
 
-      myOutputValue = `Computer Hand: ${display1Card(computerCards)}
+    myOutputValue = `Computer Hand: ${display1Card(computerCards)}
       <br> Player Hand: ${displayAllCards(playerCards)}`;
-      myImage =
-        '<img src="https://c.tenor.com/Je7_88Z0800AAAAC/disney-inside-out.gif"/>';
+    myImage =
+      '<img src="https://c.tenor.com/Je7_88Z0800AAAAC/disney-inside-out.gif"/>';
 
-      return `${myOutputValue} <br><br> Decide if you want to "hit" or "stand". <br><br> ${displayHint(
-        playerCards
-      )} ${myImage}`;
-    } else if (input !== "") {
-      myOutputValue = `Invalid option. Click "submit" to deal cards.`;
-    }
+    return `${myOutputValue} <br><br> Decide if you want to "hit" or "stand". <br><br> ${displayHint(
+      playerCards
+    )} ${myImage}`;
   }
 
   if (gameMode == Hit_Or_Stand) {
     if (input.toLowerCase() != "hit" && input.toLowerCase() != "stand") {
-      return `${myOutputValue} <br><br> Invalid option. Please enter "hit" or "stand".`;
+      return `${myOutputValue} <br><br> Invalid option. Please enter "hit" or "stand".${myImage}`;
     } else if (input.toLowerCase() == "stand") {
       while (computerCardsValue <= 16) {
         if (myDeck.length == 0) {
           gameMode = Game_Over;
-          return (myOutputValue = `Game Over! <br><br> The deck runs out of cards. <br> Please refresh the page to start a new game.`);
+          return showGameoverText();
         } else {
           computerCards.push(shuffledDeck.pop());
           computerCardsValue = calculateCardsValue(computerCards);
         }
       }
-      console.log("computer cards: " + computerCards);
-      gameMode = Who_Wins;
-      myImage =
-        '<img src = "https://media3.giphy.com/media/fEewGteMTVyMM/giphy.gif?cid=790b7611d8af12a9124d7b7b9ddbd5be0e97562e94dc64d7&rid=giphy.gif&ct=g"/>';
-      return `${myOutputValue} <br><br> You choose stand. <br><br> Click "submit" to see who wins. ${myImage}`;
+      gameMode = User_Bet;
+      return (myOutputValue = `${whoWins(
+        playerCardsValue,
+        computerCardsValue
+      )}  <br><br> Computer Hand: ${displayAllCards(
+        computerCards
+      )} <br> Player Hand: ${displayAllCards(
+        playerCards
+      )} <br><br> ${tabulatePoints(
+        playerCards,
+        computerCards
+      )}<br><br> Click "submit" to deal cards again.`);
     } else if (
       (input.toLowerCase() == "hit" && playerCardsValue > 21) ||
       (input.toLowerCase() == "hit" && playerCardsValue == 21)
     ) {
-      return `${myOutputValue} <br><br> Invalid Option. Please enter "stand".`;
+      return `${myOutputValue} <br><br> Invalid Option. Please enter "stand".${myImage}`;
     } else if (input.toLowerCase() == "hit") {
       if (myDeck.length == 0) {
         gameMode = Game_Over;
-        return (myOutputValue = `Game Over! <br><br> The deck runs out of cards. <br> Please refresh the page to start a new game.`);
+        return showGameoverText();
       } else {
         playerCards.push(shuffledDeck.pop());
         playerCardsValue = calculateCardsValue(playerCards);
         myOutputValue = `Computer Hand: ${display1Card(computerCards)} 
       <br> Player Hand: ${displayAllCards(playerCards)}`;
         myImage =
-          '<img src = "https://media1.giphy.com/media/ed0XptTuGv8be/giphy.gif?cid=790b761136d87c9e88ae54897493b08e8662df7f17cf6e30&rid=giphy.gif&ct=g"/>';
+          '<img src = "https://media0.giphy.com/media/tv3AB3kw0hSG4/giphy.gif?cid=ecf05e47eqdtybi4rctxgs1bfaznab48w6nq7p5b46p58229&rid=giphy.gif&ct=g"/>';
       }
 
+      //alternative way to display cards
       // myOutputValue =
       //   myOutputValue.slice(0, -4) +
       //   ` , ${playerCards[playerCards.length - 1].name}${
@@ -361,65 +413,8 @@ var main = function (input) {
     }
   }
 
-  if (gameMode == Who_Wins) {
-    if (input !== "") {
-      return (
-        myOutputValue +
-        `<br><br> Invalid Option. Click "submit" to see who wins.`
-      );
-    }
-    if (
-      whoWins(
-        calculateCardsValue(playerCards),
-        calculateCardsValue(computerCards)
-      ) == "It's a tie."
-    ) {
-      myImage =
-        '<img src="https://media3.giphy.com/media/9Lycp5ifWTm6I/giphy.gif?cid=790b76119c6a2031b16393028411c3af7e52698183243329&rid=giphy.gif&ct=g"/>';
-    } else if (
-      whoWins(
-        calculateCardsValue(playerCards),
-        calculateCardsValue(computerCards)
-      ) == "Congratulations! Player wins."
-    ) {
-      myImage =
-        '<img src="https://media3.giphy.com/media/bBsLmHGPrZKN2/giphy.gif?cid=790b7611a06603e29f2a1d5fa5547278ff7d49aa30137933&rid=giphy.gif"/>';
-    } else if (
-      whoWins(
-        calculateCardsValue(playerCards),
-        calculateCardsValue(computerCards)
-      ) == "Too Bad! Computer wins."
-    ) {
-      myImage = '<img src="https://c.tenor.com/MC7I7H_t4vsAAAAC/sad-cry.gif"/>';
-    }
-
-    myOutputValue = `${whoWins(
-      playerCardsValue,
-      computerCardsValue
-    )} ${myImage} <br><br> Computer Hand: ${displayAllCards(
-      computerCards
-    )} <br> Player Hand: ${displayAllCards(
-      playerCards
-    )} <br><br> ${tabulatePoints(
-      playerCards,
-      computerCards
-    )}<br><br> Click "submit" to deal cards again.`;
-    console.log(myOutputValue);
-  }
-
-  if (playerPoints == 0) {
-    gameMode = Game_Over;
-  } else {
-    gameMode = Deal_Cards;
-  }
-
   if (gameMode == Game_Over) {
-    myImage = '<img src ="https://c.tenor.com/Ho845EEf4v0AAAAC/angry.gif"/>';
-
-    if (playerPoints == 0) {
-      return (myOutputValue = `Game Over! <br><br> Player runs out of points. <br> Please refresh the page to start a new game. ${myImage}`);
-    } else
-      return (myOutputValue = `Game Over! <br><br> The deck runs out of cards. <br> Please refresh the page to start a new game. ${myImage}`);
+    return showGameoverText();
   }
 
   return myOutputValue;
