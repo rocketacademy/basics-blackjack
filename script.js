94649ae2f30a9cfa2d03dde5db889021a0930472
antinglimmy <antinglim@gmail.com>
2021-11-13 12:50:15 +0800

    Changed the values of ace to be 1 or 11

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// There will be only two players. One human and one computer (for the Base solution).
-// The computer will always be the dealer.
-// Each player gets dealt two cards to start.
-// The player goes first, and decides if they want to hit (draw a card) or stand (end their turn).
-// The dealer has to hit if their hand is below 17.
-// Each players' score is the total of their card ranks."Jaacks/Queen/Kings are 10. Aces can be 1 or 11.
-// The player who is closer to, but not above 21 wins the hand.
-
-// Deck is shuffled.
-// User clicks Submit to deal cards.
-// The cards are analysed for game winning conditions, e.g. BlackJack.
-// The cards are displayed to the user.
-// The user decides whether to hit or stand, using the submit button to submit their choice.
-// The user's cards are analysed for winning or losing conditions.
-// The computer decides to hit or stand automatically based on game rules.
-// The game either ends or continues.
+// Blackjack Project for RA
 
 var deck = [];
 var playerCardsArray = [];
@@ -24,20 +9,10 @@ var playerCardsArray = [];
 //     rank: 10,
 //   },
 //   {
-//     name: "Queen",
+//     name: "Ace",
 //     suit: "hearts",
-//     rank: 10,
+//     rank: 11,
 //   },
-//   // {
-//   //   name: "2",
-//   //   suit: "hearts",
-//   //   rank: 2,
-//   // },
-//   // {
-//   //   name: "2",
-//   //   suit: "diamond",
-//   //   rank: 2,
-//   // },
 // ];
 var computerCardsArray = [];
 var playerFinalScore = 0;
@@ -48,6 +23,9 @@ var computerHasBust = "no";
 var secondMessage = "";
 var playerWinCount = 0;
 var computerWinCount = 0;
+var numberOfAce = 0;
+var playerNumberofAce = 0;
+var computerNumberofAce = 0;
 
 var getRandomNumber = function (max) {
   return Math.floor(Math.random() * max);
@@ -111,6 +89,18 @@ var getCards = function (hand, max) {
   }
 };
 
+//Calculate the number of aces in hand
+var aceVariableValues = function (hand) {
+  var counter = 0;
+  numberOfAce = 0;
+  while (counter < hand.length) {
+    if (hand[counter].name == "Ace") {
+      numberOfAce += 1;
+    }
+    counter += 1;
+  }
+  return numberOfAce;
+};
 //Using the rank attribute of the card, add up the final scores for both the player and computer.
 var getScore = function (hand) {
   var getScoreCounter = 0;
@@ -122,36 +112,35 @@ var getScore = function (hand) {
       hand[getScoreCounter].name == "Jack"
     ) {
       currentPlayerFinalScore += 10;
-      //Ace 1 or 10 or 11
-      //***Variable Ace Value */
-    } else if (hand[getScoreCounter].name == "Ace" && hand.length == 2) {
+    } else if (hand[getScoreCounter].name == "Ace") {
       currentPlayerFinalScore += 11;
-    } else if (hand[getScoreCounter].name == "Ace" && hand.length == 3) {
-      currentPlayerFinalScore += 10;
-    } else if (hand[getScoreCounter].name == "Ace" && hand.length > 3) {
-      currentPlayerFinalScore += 1;
     } else {
       currentPlayerFinalScore += hand[getScoreCounter].rank;
     }
     getScoreCounter += 1;
   }
-
   return currentPlayerFinalScore;
 };
 
+var aceScores = function (currentPlayerFinalScore) {
+  if (currentPlayerFinalScore > 21 && numberOfAce > 0) {
+    currentPlayerFinalScore -= 10 * numberOfAce;
+  }
+};
+
+// The cards are analysed for game winning conditions, e.g. BlackJack.
 var blackjack = function (hand, currentPlayerFinalScore) {
   return hand.length == 2 && currentPlayerFinalScore == 21;
 };
 
-var createFirstMessage = function (currentPlayerArray) {
+var displayWholeArray = function (currentPlayerArray) {
   counter = 0;
-  firstMessage = "";
+  var message = "";
   while (counter < currentPlayerArray.length) {
-    firstMessage += `${currentPlayerArray[counter].name} of ${currentPlayerArray[counter].suit} <br>`;
+    message += `${currentPlayerArray[counter].name} of ${currentPlayerArray[counter].suit}・`;
     counter += 1;
   }
-
-  return firstMessage;
+  return message;
 };
 
 var resetGame = function () {
@@ -161,179 +150,163 @@ var resetGame = function () {
   computerHasBust = "no";
 };
 
-var startGame = function () {
-  getCards(playerCardsArray, 2);
-  return playerCardsArray;
-};
-
 var winCount = function () {
   if (playerWinCount >= computerWinCount) {
-    return `🏆 Leaderboard 🏆 : <br> 🧏🏼‍♀️ Player win count: ${playerWinCount}/${
+    return `<br> 🏆 Leaderboard 🏆 : <br> 1. 👩🏻‍💻 You (Win count: ${playerWinCount}/${
       playerWinCount + computerWinCount
-    }<br> 💻 Computer win count: ${computerWinCount}/${
+    })<br> 2. 💻 Computer (Win count: ${computerWinCount}/${
       playerWinCount + computerWinCount
-    } `;
+    }) `;
   } else {
-    return `🏆 Leaderboard 🏆 <br> 🧏🏼‍♀️ Player win count: ${playerWinCount}/${
+    return `<br> 🏆 Leaderboard 🏆 <br> 1. 💻 Computer (Win count: ${computerWinCount}/${
       playerWinCount + computerWinCount
-    }<br> 💻 Computer win count: ${computerWinCount}/${
+    }) <br> 2. 👩🏻‍💻 You (Win count: ${playerWinCount}/${
       playerWinCount + computerWinCount
-    } `;
+    })`;
   }
 };
 
+// The player goes first, and decides if they want to hit (draw a card) or stand (end their turn).
 var hitButtonClicked = function () {
   getCards(playerCardsArray, 1);
   playerFinalScore = getScore(playerCardsArray);
 };
-
 var standButtonClicked = function () {
   playerGameMode = "player has chosen to stand";
   playerFinalScore = getScore(playerCardsArray);
-  return `You have chosen to stand. Your final score is ${playerFinalScore}. <br> It is the computer's turn. <br>Press submit for the computer to draw their cards.`;
-  // return secondMessage;
+  return `You have chosen to stand 🕺🏻 <br> Your final score is ${playerFinalScore}. <br> It is the computer's turn. <br>Press submit for the computer to draw their cards.`;
 };
 
 var main = function (input) {
-  // deckFunction();
-  // getRandomNumber(deck.length);
-
   if (playerGameMode == "start game") {
     playerGameMode = "player initial draw";
-    return `Hi Player! Press submit to draw your first two cards.`;
+    return `Hi Player! Press submit to draw your first two cards. 🚩🚩🚩`;
   }
 
+  // User clicks Submit to deal cards.
   if (playerGameMode == "player initial draw") {
+    // Each player gets dealt two cards to start.
     getCards(playerCardsArray, 2);
     getCards(computerCardsArray, 1);
+
+    //Calculate number of aces and score for player
+    playerNumberofAce = aceVariableValues(playerCardsArray);
     playerGameMode = "player has drawn";
     playerFinalScore = getScore(playerCardsArray);
-    //After player is dealt his 2 cards, show him his draw and ask if he wants to hit or stand
+
+    playerInitialDrawMessage = `Hi Player. <br> You drew: <br>
+       ${displayWholeArray(playerCardsArray)} <br><br> Your current score is
+       ${playerFinalScore}. <br> One of Computer's cards is:  <br> ${displayWholeArray(
+      computerCardsArray
+    )} <br>`;
+
     if (playerFinalScore > 16) {
-      message = `Hi Player. <br> You drew: <br>
-       ${createFirstMessage(playerCardsArray)} Your current score is
-       ${playerFinalScore}. <br> One of Computer's cards is:  <br> ${createFirstMessage(
-        computerCardsArray
-      )} <br> Type hit or stand please.`;
+      playerInitialDrawMessage += `Click hit or stand please.`;
       playerGameMode = "player has drawn";
     } else if (playerFinalScore <= 16) {
-      message = `Hi Player. <br> You drew:<br>
-       ${createFirstMessage(playerCardsArray)}Your current score is
-       ${playerFinalScore}.<br> One of Computer's cards is: <br> ${createFirstMessage(
-        computerCardsArray
-      )} You have drawn below 16. <br> Please enter hit to draw more cards.`;
+      playerInitialDrawMessage +=
+        "You have drawn below 16. <br> Click hit to draw more cards.";
       playerGameMode = "player has drawn";
     }
-    console.log(playerGameMode);
-    console.log(playerFinalScore);
-    return message;
+    return playerInitialDrawMessage;
   }
 
+  //After player has drawn a new card
   if (playerGameMode == "player has drawn") {
-    console.log(playerGameMode);
-    //If hit, draw one more card, add it to his final score, and shoe him his draw and final score
-    // if (input != "hit" || input != "stand") {
-    //   secondMessage = `Please type either hit or stand`;
-    // }
+    playerNumberofAce = aceVariableValues(playerCardsArray);
 
-    // if (input == "hit") {
-    //   var newCard = getCards(playerCardsArray, 1);
-    //   playerFinalScore = getScore(playerCardsArray);
-    //   //If he has bust, change game mode to player bust
+    //Save bust condition
     if (playerFinalScore > 21) {
       playerHasBust = "yes";
-      console.log(playerFinalScore);
     }
 
     return `You drew ${playerCardsArray[playerCardsArray.length - 1].name} of ${
       playerCardsArray[playerCardsArray.length - 1].suit
-    }. <br> Your current hand is: <br> ${createFirstMessage(
+    }. <br> Your current hand is: <br> ${displayWholeArray(
       playerCardsArray
-    )} Your current score is ${playerFinalScore}. Computer drew: <br> ${createFirstMessage(
+    )} Your current score is ${playerFinalScore}. <br> Computer drew: <br> ${displayWholeArray(
       computerCardsArray
-    )} Click hit or stand`;
+    )} <br> Click hit or stand`;
   }
-  //  if (playerGameMode == "player has chosen to stand") {
-  // //   //If stand, show him that he has chosen stand and his final score
-  // //   playerFinalScore = getScore(playerCardsArray);
-  // //   console.log(playerGameMode);
-  // //   console.log(playerFinalScore);
 
-  // //   currentPlayer = "computer";
-  // //   playerGameMode = "computer initial draw";
-  // //   secondMessage = `Your final score is ${playerFinalScore}. <br> It is the computer's turn. <br>Press submit for the computer to draw their cards.`;
-  // // }
-
+  //If player has chosen to stand/click the stand button
   if (playerGameMode == "player has chosen to stand") {
     getCards(computerCardsArray, 1);
+    computerNumberofAce = aceVariableValues(computerNumberofAce);
     computerFinalScore = getScore(computerCardsArray);
 
+    // The dealer has to hit if their hand is below 17.
     if (computerFinalScore < 17) {
       while (computerFinalScore < 17) {
         getCards(computerCardsArray, 1);
+
+        //Calculate number of aces and score for computer
+        computerNumberofAce = aceVariableValues(computerNumberofAce);
         computerFinalScore = getScore(computerCardsArray);
         playerGameMode = "computer has finished hitting";
-        console.log(playerGameMode);
       }
+      //Save bust condition
       if (computerFinalScore > 21) {
         computerHasBust = "yes";
       }
     }
+    //Stand when computer doesnt need to draw more cards
     if (computerFinalScore > 16) {
       playerGameMode = "computer chose to stand";
-      console.log(playerGameMode);
     }
     playerGameMode = "calculate result";
-    console.log(computerFinalScore);
     return `Computer has finished their turn. <br> Computer drew:<br>
-       ${createFirstMessage(computerCardsArray)} Computer's final score is
+       ${displayWholeArray(computerCardsArray)} <br>Computer's final score is
        ${computerFinalScore}. <br> Press submit to calculate the results.`;
   }
 
-  finalMessage = `<br> Player had a hand of: <br> ${createFirstMessage(
+  finalMessage = `<br> You had a hand of: <br> ${displayWholeArray(
     playerCardsArray
-  )} and a final score of ${playerFinalScore}. <br> Computer had a hand of ${createFirstMessage(
+  )} and a final score of ${playerFinalScore}. <br> Computer had a hand of ${displayWholeArray(
     computerCardsArray
   )} and a final score of ${computerFinalScore}.<br> `;
 
+  //Calculate results
   if (playerGameMode == "calculate result") {
     playerGameMode = "start game";
+
+    //Evaluate for blackjack conditions
     if (blackjack(playerCardsArray, playerFinalScore)) {
       resetGame();
       playerWinCount += 1;
-      return `Player has won blackJack. ${finalMessage}${winCount()}`;
+      return `You got blackjack! 🎊 ${finalMessage}${winCount()}<br> <br> Press submit to begin the game again.`;
     }
+    //Evaluate for blackjack conditions
     if (blackjack(computerCardsArray, computerFinalScore)) {
       resetGame();
       computerWinCount += 1;
-      return `Computer has won blackJack. ${finalMessage}${winCount()}`;
+      return `Snap...Computer got blackjack! 😢 ${finalMessage}${winCount()}<br> <br> Press submit to begin the game again.`;
     }
     // Compare both hands and determine a winner.
     if (playerHasBust == "yes" && computerHasBust == "yes") {
       resetGame();
       computerWinCount += 1;
-      return `Player lost. Both the player and computer has bust. <br> ${finalMessage} ${winCount()} <br> Press submit to begin the game again.`;
+      return `You lost! 🥺 Both you and computer has bust. <br> ${finalMessage} ${winCount()} <br> <br> Press submit to begin the game again.`;
     } else if (playerHasBust == "yes" && computerHasBust == "no") {
       resetGame();
       computerWinCount += 1;
-      return `Player has bust and lost. <br> ${finalMessage} ${winCount()}<br> Press submit to begin the game again.`;
+      return `You bust and lost! 🥺 <br> ${finalMessage} ${winCount()}<br> <br> Press submit to begin the game again.`;
     } else if (playerHasBust == "no" && computerHasBust == "yes") {
       resetGame();
       playerWinCount += 1;
-      return `Computer has bust and lost. <br> ${finalMessage}  ${winCount()} Press submit to begin the game again.`;
+      return `Yipee Computer has bust and lost! ✌🏻  <br> ${finalMessage}  ${winCount()} <br> <br> Press submit to begin the game again.`;
     } else if (playerFinalScore > computerFinalScore) {
       resetGame();
       playerWinCount += 1;
-      return `Player has won. <br> ${finalMessage} ${winCount()}<br> Press submit to begin the game again.`;
+      return `Yipee you won! ✌🏻 <br> ${finalMessage} ${winCount()}<br><br>  Press submit to begin the game again.`;
     } else if (playerFinalScore < computerFinalScore) {
       resetGame();
       computerWinCount += 1;
-
-      return `Computer has won. <br> ${finalMessage} ${winCount()}
+      return `Snap...Computer has won! 🥺 <br> ${finalMessage} ${winCount()}
       }<br> Press submit to begin the game again.`;
     } else if (playerFinalScore == computerFinalScore) {
       resetGame();
-      return `It's a tie. <br>${finalMessage} ${winCount()}<br> Press submit to begin the game again.`;
+      return `It's a tie. <br>${finalMessage} ${winCount()}<br><br>  Press submit to begin the game again.`;
     }
   }
 };
