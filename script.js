038a1c270177b9e08a7c6b78f14b281e9257262a
weiyu95 <weiyu95@gmail.com>
2021-12-16 23:24:43 +0800

    able to create multi players

--- script.js
+++ script.js
@@ -1,4 +1,139 @@
+//The main function runs on each player's turn. The sequence of actions in the game might be the following.
+// Create a deck.
+// Deck is shuffled.
+// Multiplayer can play.
+//  a. User to input number of players playing.
+//  b. Generate the number of players according to the input number.
+//  c. Players have attributes: Player Number, Chips.
+// Player clicks Submit to enter game.
+// Players to determine how much chip to bet for that round.
+// Player clicks Draw Hand to receive 2 cards each.
+// The cards are analysed for game winning conditions, e.g. Blackjack.
+// The cards are displayed to the user.
+// Each player is able to choose to Hit, Stand or Split by clicking the respective buttons.
+// The user's cards are analysed for winning or losing conditions.
+// Hide dealer's first card
+// The computer decides to hit or stand automatically based on game rules.
+// The game either ends or continues
+
+// Function to creat a deck of cards
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["♥️", "♦️", "♣️", "♠️"];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+
+// Create a deck of cards
+var deck = makeDeck();
+// Shuffled deck of cards
+var shuffledDeck = shuffleCards(deck);
+// Output message of player card
+var playerOutput = function (cardsArray) {
+  var message = "";
+  for (i = 0; i < cardsArray.length; i += 1) {
+    message += cardsArray[i].name + " of " + cardsArray[i].suit + " ";
+  }
+  return message;
+};
+
+const ENTER_NUM_PLAYER = "enter number of players";
+const ENTER_BET = "enter number of chips to bet";
+let gameMode = ENTER_NUM_PLAYER;
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var myOutputValue = "";
+
+  var playerLimit = ["1", "2", "3", "4"];
+  var numOfPlayersPlaying = [];
+  // Multiplayer can play.
+  if (playerLimit.includes(input)) {
+    for (i = 1; i <= input; i += 1) {
+      var player = {
+        number: i,
+        chips: 100,
+      };
+      numOfPlayersPlaying.push(player);
+    }
+    gameMode = ENTER_BET;
+    return numOfPlayersPlaying;
+  } else {
+    return (myOutputValue =
+      "Please enter the number of players playing (max. 4 players).");
+  }
+  //  a. User to input number of players playing.
+  //  b. Generate the number of players according to the input number.
+  //  c. Players have attributes: Player Number, Chips.
 };
