7705374a66c0a7712f05264017987d8ab68a79d0
Noel Lim <noellimx@gmail.com>
2021-12-19 19:37:48 +0800

    add pre commit hook and refactor Participant class

--- scripts/domain/lounge/participant.js
+++ scripts/domain/lounge/participant.js
@@ -1,50 +1,13 @@
-//PERSON
-
-/**
- * @typedef {Object} Person
- * @property {function() => string} getName Get name of person
- * @property {function() => number} getCredit Get credit of person
- */
-
-/**
- *
- * @returns {Person}
- */
-const newPerson = (name = "nameless homie", startCredit = 100) => {
-  const _name = name;
-  let _credit = startCredit;
-  return {
-    getName: () => _name,
-    getCredit: () => _credit,
-    decreaseCredit: (amt) => (_credit -= amt),
-    increaseCredit: (amt) => (_credit += amt),
-  };
-};
-
-// PARTICIPANT
-
-/**
- *
- * @typedef {Object} Participant
- * @property {function() => Person} getPersonality
- * @property {function() => string} getName
- * @property {function() => Hand[]} getHands
- * @property {function() => number} getCredit
- */
-
-/**
- *
- * @param {Person} person
- * @returns {Participant}
- */
-const newParticipant = (person) => {
-  const _person = person;
-
-  return {
-    getPersonality: () => _person,
-    getName: () => _person.getName(),
-    getCredit: () => _person.getCredit(),
-    decreaseCredit: (amt) => _person.decreaseCredit(amt),
-    increaseCredit: (amt) => _person.increaseCredit(amt),
-  };
-};
+// The participant of a Playing Area
+class Participant {
+  constructor(name = "nameless", credit = 100) {
+    /** @private @const {string} */
+    this._name = name;
+    /** @private @const {number} */
+    this._credit = credit;
+  }
+  getName = () => this._name;
+  getCredit = () => this._credit;
+  decreaseCredit = (amt) => (this._credit -= amt);
+  increaseCredit = (amt) => (this._credit += amt);
+}
