fb144a5f194a2b41cf69e8eb6736110f7a5e2942
consxious <tengkimyen@gmail.com>
2021-12-23 02:03:24 +0800

    Basic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/////////////// Pseudocode ////////////////////
 // First Version: Compare Initial Hands to Determine Winner
 // Aim for a playable game. The essence of blackjack requires:
 // 1. Two players - a player and a dealer (computer).
@@ -8,15 +9,21 @@
 // 4.2 Blackjack win. When either player or dealer draw Blackjack.
 // 4.3 normal win. When neither draw Blackjack, the winner is decided by whomever has the higher hand total.
 
-// Small step at a time
-
 // Global variables
+var GAME_START = "game start";
+var GAME_CARDS_DRAWN = "cards are drawn";
+var GAME_RESULTS_SHOWN = "results are shown";
+var GAME_HIT_OR_STAND = "hit or stand";
+var currentGameMode = GAME_START;
+
+// Each player multiple cards
 var playerCards = [];
-var playerCardsRank = [];
-var playerCardsSum = 0;
-var computerCards = [];
-var computerCardsRank = [];
-var computerCardsSum = 0;
+var dealerCards = [];
+
+// Empty deck of cards
+var shuffledDeck = [];
+
+////// Deck helper functions //////
 
 // A deck of cards
 var makeDeck = function () {
@@ -92,67 +99,256 @@ var shuffleCards = function (cardDeck) {
   return cardDeck;
 };
 
-// create a shuffle deck
-var shuffledDeck = shuffleCards(makeDeck());
+///// Helper functions ///////
 
-// Generate 2 random cards for player and computer
-var randomTwoCards = function () {
-  var randomNumber;
-  var randomCard;
-  var pushValue;
-  var sumValue;
+// Checks for black jack
+var checkForBlackJack = function (cardsOnHand) {
+  // Loop through 2 cards
+  var playerCardOne = cardsOnHand[0];
+  var playerCardTwo = cardsOnHand[1];
+  var isBlackJack = false;
 
-  // Generate 2 numbers then push to empty array
-  // Generate 2 cards for player
-  for (let i = 0; i < 2; i += 1) {
-    randomNumber = randomIndex(shuffledDeck.length);
-    randomCard = shuffledDeck[randomNumber];
-    playerCards.push(randomCard);
+  // 2 possibilities
+  if (
+    (playerCardOne.name == "ace" && playerCardTwo.rank >= 10) ||
+    (playerCardTwo.name == "ace" && playerCardOne.rank >= 10)
+  ) {
+    isBlackJack = true;
   }
 
-  // Generate 2 cards for computer
-  for (let i = 0; i < 2; i += 1) {
-    randomNumber = randomIndex(shuffledDeck.length);
-    randomCard = shuffledDeck[randomNumber];
-    computerCards.push(randomCard);
-  }
-
-  // Extract rank from array to push into another array
-  for (let i = 0; i < 2; i += 1) {
-    pushValue = playerCards[i].rank;
-    playerCardsRank.push(pushValue);
-  }
-
-  for (let i = 0; i < 2; i += 1) {
-    pushValue = computerCards[i].rank;
-    computerCardsRank.push(pushValue);
-  }
-
-  // Sum of 2 cards - error
-  for (let i = 0; i < 2; i += 1) {
-    sumValue = playerCardsRank[i];
-    console.log(sumValue, "sumValue - player value");
-    playerCardsSum = playerCardsSum + sumValue;
-  }
-
-  for (let i = 0; i < 2; i += 1) {
-    sumValue = computerCardsRank[i];
-    console.log(sumValue, "sumValue - computer value");
-    computerCardsSum = computerCardsSum + sumValue;
-  }
+  return isBlackJack;
 };
 
-console.log(playerCards, "playerCards");
-console.log(computerCards, "computerCards");
+// Calculate one player cards value
+var calculateTotalHandValue = function (cardsOnHand) {
+  var totalCardsValue = 0;
+  // Counter for number of aces
+  var aceCounter = 0;
 
-console.log(playerCardsRank, "playerCardsRank");
-console.log(computerCardsRank, "computerCardsRank");
-console.log(playerCardsSum, "playerCardsSum");
-console.log(computerCardsSum, "computerCardsSum");
+  // Loop through cards to add up the ranks
+  var index = 0;
+  while (index < cardsOnHand.length) {
+    var currCard = cardsOnHand[index];
 
-var main = function () {
-  randomTwoCards();
-  var outputValue =
-    "Player Cards: " + playerCards + " Computer Cards: " + computerCards;
-  return outputValue;
+    // King, queen, and jack = 10
+    if (
+      currCard.name == "king" ||
+      currCard.name == "queen" ||
+      currCard.name == "jack"
+    ) {
+      totalCardsValue = totalCardsValue + 10;
+    }
+    // Ace = 11
+    else if (currCard.name == "ace") {
+      totalCardsValue = totalCardsValue + 11;
+      aceCounter = aceCounter + 1;
+      // Other numbered cards are valued by their ranks
+    } else {
+      totalCardsValue = totalCardsValue + currCard.rank;
+    }
+    index = index + 1;
+  }
+
+  // Reset ace counter
+  index = 0;
+  // Loop for aces found, deduct 10 totalCardsValue
+  // when totalCardsValue > 21.
+  while (index < aceCounter) {
+    if (totalCardsValue > 21) {
+      totalCardsValue = totalCardsValue - 10;
+    }
+    index = index + 1;
+  }
+
+  return totalCardsValue;
+};
+
+// Displays each players card value
+var displayPlayerAndDealerCards = function (
+  playerCardsArray,
+  dealerCardsArray
+) {
+  var playerMessage = "Player hand:<br>";
+  var index = 0;
+  while (index < playerCardsArray.length) {
+    playerMessage =
+      playerMessage +
+      "- " +
+      playerCardsArray[index].name +
+      " of " +
+      playerCardsArray[index].suit +
+      "<br>";
+    index = index + 1;
+  }
+
+  index = 0;
+  var dealerMessage = "Dealer hand:<br>";
+  while (index < dealerCardsArray.length) {
+    dealerMessage =
+      dealerMessage +
+      "- " +
+      dealerCardsArray[index].name +
+      " of " +
+      dealerCardsArray[index].suit +
+      "<br>";
+    index = index + 1;
+  }
+
+  return playerMessage + "<br>" + dealerMessage;
+};
+
+// Displays total cards values of each players
+var displayHandTotalValues = function (playerCardsValue, dealerCardsValue) {
+  var totalHandValueMessage =
+    "<br>Player cards value: " +
+    playerCardsValue +
+    "<br>Dealer cards value: " +
+    dealerCardsValue;
+  return totalHandValueMessage;
+};
+
+////// Main function //////////
+var main = function (input) {
+  // Default output msg
+  var outputMessage = "";
+
+  // FIRST CLICK
+  if (currentGameMode == GAME_START) {
+    // create shuffled cards
+    shuffledDeck = shuffleCards(makeDeck());
+
+    // Reset each player cards
+    playerCards = [];
+    dealerCards = [];
+
+    // deal 2 cards to each players
+    playerCards.push(shuffledDeck.pop());
+    playerCards.push(shuffledDeck.pop());
+    dealerCards.push(shuffledDeck.pop());
+    dealerCards.push(shuffledDeck.pop());
+
+    // check
+    console.log("playerCards", playerCards);
+    console.log("dealerCards", dealerCards);
+
+    // update gameMode
+    currentGameMode = GAME_CARDS_DRAWN;
+
+    outputMessage = "Click submit to calculate cards!";
+
+    return outputMessage;
+  }
+
+  // SECOND CLICK
+  if (currentGameMode == GAME_CARDS_DRAWN) {
+    // check for blackjack
+    var playerHasBlackJack = checkForBlackJack(playerCards);
+    var dealerHasBlackJack = checkForBlackJack(dealerCards);
+
+    // check
+    console.log("playerHasBlackJack", playerHasBlackJack);
+    console.log("dealerHasBlackJack", dealerHasBlackJack);
+
+    // Either one has black jack
+    if (playerHasBlackJack == true || dealerHasBlackJack == true) {
+      // Both have black jack
+      if (playerHasBlackJack == true && dealerHasBlackJack == true) {
+        outputMessage =
+          displayPlayerAndDealerCards(playerCards, dealerCards) +
+          "<br>Black Jack Tie!";
+      }
+      // Only player has black jack
+      else if (playerHasBlackJack == true && dealerHasBlackJack == false) {
+        outputMessage =
+          displayPlayerAndDealerCards(playerCards, dealerCards) +
+          "<br>Player wins by Black Jack!";
+      }
+      // Only dealer has black jack
+      else {
+        outputMessage =
+          displayPlayerAndDealerCards(playerCards, dealerCards) +
+          "<br>Dealer wins by Black Jack!";
+      }
+    }
+
+    // Neither one has black jack
+    // Ask for player to input 'hit' or 'stand'
+    else {
+      outputMessage =
+        displayPlayerAndDealerCards(playerCards, dealerCards) +
+        '<br> No one Black Jacks. <br>Please input "hit" or "stand".';
+
+      // update gameMode
+      currentGameMode = GAME_HIT_OR_STAND;
+    }
+
+    return outputMessage;
+  }
+
+  // THIRD CLICK
+  if (currentGameMode == GAME_HIT_OR_STAND) {
+    // Hit
+    if (input == "hit") {
+      playerCards.push(shuffledDeck.pop());
+      outputMessage =
+        displayPlayerAndDealerCards(playerCards, dealerCards) +
+        '<br> You just drew another card. <br>Please input "hit" or "stand".';
+    }
+
+    // Stand
+    else if (input == "stand") {
+      // Calculate hands
+      var playerCardsTotalValue = calculateTotalHandValue(playerCards);
+      var dealerCardsTotalValue = calculateTotalHandValue(dealerCards);
+
+      // Dealer's hit or stand logic
+      while (dealerCardsTotalValue < 17) {
+        dealerCards.push(shuffledDeck.pop());
+        dealerCardsTotalValue = calculateTotalHandValue(dealerCards);
+      }
+
+      // Tied game
+      if (
+        playerCardsTotalValue == dealerCardsTotalValue ||
+        (playerCardsTotalValue > 21 && dealerCardsTotalValue > 21)
+      ) {
+        outputMessage =
+          displayPlayerAndDealerCards(playerCards, dealerCards) +
+          "<br>Its a Tie!" +
+          displayHandTotalValues(playerCardsTotalValue, dealerCardsTotalValue);
+      }
+
+      // Player win
+      else if (
+        (playerCardsTotalValue > dealerCardsTotalValue &&
+          playerCardsTotalValue <= 21) ||
+        (playerCardsTotalValue <= 21 && dealerCardsTotalValue > 21)
+      ) {
+        outputMessage =
+          displayPlayerAndDealerCards(playerCards, dealerCards) +
+          "<br>Player wins!" +
+          displayHandTotalValues(playerCardsTotalValue, dealerCardsTotalValue);
+      }
+
+      // Dealer wins
+      else {
+        outputMessage =
+          displayPlayerAndDealerCards(playerCards, dealerCards) +
+          "<br>Dealer wins!" +
+          displayHandTotalValues(playerCardsTotalValue, dealerCardsTotalValue);
+      }
+
+      // Re-start
+      currentGameMode = GAME_START;
+    }
+
+    // Input validation ('hit' or 'stand')
+    else {
+      outputMessage =
+        'Wrong input. Please input "hit" or "stand" only.<br><br>' +
+        displayPlayerAndDealerCards(playerCards, dealerCards);
+    }
+
+    return outputMessage;
+  }
 };
