82c38ea488bd94f86da712459d0cea5e5f7f3b84
smdewi <smdewi2020@gmail.com>
2021-12-22 22:30:54 +0800

    test button disable and enable

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var main = function (input) {
   if (input == "start") {
+    // Turn on Hit and Stand button
+    document.getElementById("hit-button").disabled = false;
+    document.getElementById("stand-button").disabled = false;
+    // Turn off Start button
+    document.getElementById("start-button").disabled = true;
     return "Start";
   } else if (input == "hit") {
     return "Hit";
   } else if (input == "stand") {
+    // Turn off Hit button and Stand button
+    document.getElementById("hit-button").disabled = true;
+    document.getElementById("stand-button").disabled = true;
+    // Turn on Start button
+    document.getElementById("start-button").disabled = false;
+
     return "Stand";
-  } else if (input == "reset") {
-    return "Reset";
   }
 };
