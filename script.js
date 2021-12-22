749dc77344140f8e2d6e880d7571500656fe7272
smdewi <smdewi2020@gmail.com>
2021-12-22 21:27:41 +0800

    test button function

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (input == "start") {
+    return "Start";
+  } else if (input == "hit") {
+    return "Hit";
+  } else if (input == "stand") {
+    return "Stand";
+  } else if (input == "reset") {
+    return "Reset";
+  }
 };
