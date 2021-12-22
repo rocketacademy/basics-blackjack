356adea70b44158d9f1880ab4678472971d3e711
charlottewee <weecharlotte1@gmail.com>
2021-12-22 12:10:27 +0800

    add check ace

--- script.js
+++ script.js
@@ -48,6 +48,20 @@ var makeDeck = function () {
     suitIndex += 1;
   }
 
+  //change jack, queen, king to rank 10 for blackjack
+  cardDeck[10].rank = 10;
+  cardDeck[11].rank = 10;
+  cardDeck[12].rank = 10;
+  cardDeck[23].rank = 10;
+  cardDeck[24].rank = 10;
+  cardDeck[25].rank = 10;
+  cardDeck[36].rank = 10;
+  cardDeck[37].rank = 10;
+  cardDeck[38].rank = 10;
+  cardDeck[49].rank = 10;
+  cardDeck[50].rank = 10;
+  cardDeck[51].rank = 10;
+
   // Return the completed card deck
   return cardDeck;
 };
@@ -84,12 +98,56 @@ var deck = makeDeck();
 // to communicate that we have shuffled the deck.
 var shuffledDeck = shuffleCards(deck);
 
-//2 players: player vs computer
-//Computer always dealer
-//Player & computer gets 2 cards at first
-//Cards analysed for game winning conditions (blackjack)
+var mode = "Show hand"; //Show hand, Hit or Stand, Compare
+var currentPlayer = "Player"; //switch between Player and Computer
+// Draw 2 cards per player, store into array
+var computerCards = []; //original 2 cards stored here
+var playerCards = []; // original 2 cards stored here
+var outputComputerCard = ""; //original 2 cards drawn
+var outputPlayerCard = ""; //original 2 cards drawn
+var outputPlayerCard1 = " ";
+var outputPlayerCard2 = " "; //newest card drawn
+var outputComputerCard1 = " ";
+var outputComputerCard2 = " "; //newest card drawn
+
+//To check if Ace should be 1 or 11
+var checkAce = function (cardsInHand) {
+  //ace can be 1 or 11
+  var aceValue = 11;
+  var countAce = 0;
+  var cards = [];
+  var index = 0;
+  var points = 0;
+  //store card names in array
+  while (index < cardsInHand.length) {
+    cards[index] = cardsInHand[index].name;
+    if (cards[index] == "ace") {
+      countAce += 1;
+    }
+    points = points + cardsInHand[index].rank;
+    index += 1;
+  }
+  if ((cardsInHand.length > 2 && countAce > 1) || points > 21) {
+    aceValue = 1;
+  }
+
+  console.log(countAce);
+  console.log(points);
+  console.log(cardsInHand);
+  return aceValue;
+};
+
+//Cards analysed for game winning conditions (blackjack), return true or false
 var checkBlackjack = function (drawnCards) {
+  var index = 0;
+  while (index < 2) {
+    if (drawnCards[index].name == "ace") {
+      drawnCards[index].rank = checkAce(playerCards);
+    }
+    index += 1;
+  }
   var totalPoints = drawnCards[0].rank + drawnCards[1].rank;
+
   if (totalPoints == 21) {
     var myOutputValue = true;
   } else {
@@ -97,13 +155,6 @@ var checkBlackjack = function (drawnCards) {
   }
   return myOutputValue;
 };
-
-//Player first --> decide to hit (draw card) or stand (end)
-//Dealer (computer) must hit if hand <17
-//Total score is total of card ranks: J,Q,K = 10, Ace = 1 or 11
-//Closer to or not >21 --> Winner
-
-var outputPlayerCard1 = " ";
 
 var decidePlayerHitOrStand = function (decision, playerCards) {
   var myOutputValue1 = " ";
@@ -116,63 +167,93 @@ var decidePlayerHitOrStand = function (decision, playerCards) {
     console.log(playerCards.length);
     myOutputValue1 = "Player chose hit. Draw a card.";
     console.log("enter after hit");
+    var index = 0;
+    while (index < playerCards.length) {
+      outputPlayerCard2 =
+        "<br>" + playerCards[index].name + " of " + playerCards[index].suit;
+      index += 1;
+    }
   } else if (decision == "stand") {
     //end player's turn and continue with computer's turn
     currentPlayer = "Computer";
     mode = "Show hand";
-    myOutputValue1 = "Player chose stand. It's the computer's turn.";
+
+    myOutputValue1 =
+      "Player chose stand. It's the computer's turn. Click on submit to continue.";
   } else {
     // error, only can choose hit or stand
     myOutputValue1 =
       "Invalid choice. Please only choose 'hit' or 'stand'. Click submit button to try again.";
   }
 
-  var index = 0;
-  while (index < playerCards.length) {
-    var outputPlayerCard2 =
-      "<br>" + playerCards[index].name + " of " + playerCards[index].suit;
-    index += 1;
-  }
   outputPlayerCard1 = outputPlayerCard1 + outputPlayerCard2;
+  outputPlayerCard2 = " "; //reset outputplayercard2: the last drawn card
   return myOutputValue1;
 };
 
-var decideComputerHitOrStand = function (drawnCards) {
-  var totalPoints = drawnCards[0].rank + drawnCards[1].rank;
-  var outputComputerCard = "";
+var decideComputerHitOrStand = function (computerCards) {
+  var totalPoints = computerCards[0].rank + computerCards[1].rank;
+
   var myOutputValue = " ";
   var myOutputValue1 = " ";
   if (totalPoints < 17) {
     //computer is dealer, have to hit (draw another card) if hand is below 17
-    drawnCards.push(shuffledDeck.pop());
+    computerCards.push(shuffledDeck.pop());
+    totalPoints = 0;
+    var index = 0;
+    while (index < computerCards.length) {
+      if (computerCards[index].name == "ace") {
+        computerCards[index].rank = checkAce(computerCards);
+      }
+      totalPoints = totalPoints + computerCards[index].rank;
+      index += 1;
+    }
+    console.log(totalPoints);
+
+    if (totalPoints >= 19) {
+      myOutputValue1 = "Computer chose stand.";
+      mode = "Compare";
+      return myOutputValue1;
+    }
     myOutputValue1 = "Computer's hand is below 17. Computer has to hit.";
   } else if (totalPoints >= 17) {
-    //computer can choose to hit or stand (set default choice for computer?)
-    if (totalPoints >= 20) {
+    //computer can choose to hit or stand (set default choice for computer)
+    if (totalPoints >= 19) {
       myOutputValue1 = "Computer chose stand.";
+      mode = "Compare";
     } else {
-      drawnCards.push(shuffledDeck.pop());
-      myOutputValue1 = "Computer chose hit.";
+      computerCards.push(shuffledDeck.pop());
+      myOutputValue1 = "Computer chose hit. Another card is drawn.";
+      totalPoints = 0;
+      var index = 0;
+      while (index < computerCards.length) {
+        if (computerCards[index].name == "ace") {
+          computerCards[index].rank = checkAce(playerCards);
+        }
+        totalPoints = totalPoints + computerCards[index].rank;
+        index += 1;
+      }
+      console.log(totalPoints);
+      if (totalPoints >= 19) {
+        myOutputValue1 = "Computer chose stand.";
+        mode = "Compare";
+        return myOutputValue1;
+      }
     }
   } else {
     // error, only can choose hit or stand
     myOutputValue1 =
       "Invalid choice. Please only choose 'hit' or 'stand'. Click submit button to try again.";
   }
-
-  //output all cards on hand
-  for (let i = 0; i < drawnCards.length; i++) {
-    outputComputerCard =
-      outputComputerCard +
-      "<br> " +
-      drawnCards[i].name +
-      " of " +
-      drawnCards[i].suit;
+  var index = 0;
+  while (index < computerCards.length) {
+    outputComputerCard2 =
+      "<br>" + computerCards[index].name + " of " + computerCards[index].suit;
+    index += 1;
   }
-
-  myOutputValue =
-    myOutputValue1 + "<br> Computer's drawn cards: " + outputComputerCard;
-  return myOutputValue;
+  outputComputerCard1 = outputComputerCard1 + outputComputerCard2;
+  outputPlayerCard2 = " "; //reset outputcomputercard2: the last drawn card
+  return myOutputValue1;
 };
 
 var comparePoints = function (computerCards, playerCards) {
@@ -182,10 +263,16 @@ var comparePoints = function (computerCards, playerCards) {
   var myOutputValue = " ";
   //check total points of player's hand --> need to convert rank to number?
   for (let i = 0; i < playerCards.length; i++) {
+    if (playerCards[i].name == "ace") {
+      playerCards[i].rank = checkAce(playerCards);
+    }
     playerPoints = playerPoints + playerCards[i].rank;
   }
   //check total points of computer's hand
   for (let i = 0; i < computerCards.length; i++) {
+    if (computerCards[i].name == "ace") {
+      computerCards[i].rank = checkAce(computerCards);
+    }
     computerPoints = computerPoints + computerCards[i].rank;
   }
 
@@ -193,8 +280,8 @@ var comparePoints = function (computerCards, playerCards) {
     (computerPoints > playerPoints && computerPoints <= 21) ||
     playerPoints > 21
   ) {
-    if (playerPoints > 21) {
-      myOutputValue1 = "Player's hand BURST! Computer wins!";
+    if (playerPoints > 21 && computerPoints <= 21) {
+      myOutputValue1 = "Player BUSTS! Computer wins!";
     } else {
       myOutputValue1 = "Computer wins!";
     }
@@ -202,8 +289,8 @@ var comparePoints = function (computerCards, playerCards) {
     (playerPoints > computerPoints && playerPoints <= 21) ||
     computerPoints > 21
   ) {
-    if (computerPoints > 21) {
-      myOutputValue1 = "Copmuter's hand BURST! Player wins!";
+    if (computerPoints > 21 && playerPoints <= 21) {
+      myOutputValue1 = "Computer BUSTS! Player wins!";
     } else {
       myOutputValue1 = "Player wins!";
     }
@@ -221,14 +308,22 @@ var comparePoints = function (computerCards, playerCards) {
   return myOutputValue;
 };
 
-var mode = "Show hand"; //Show hand, Hit or Stand, Compare
-var currentPlayer = "Player"; //switch between Player and Computer
-// Draw 2 cards per player, store into array
-var computerCards = [];
-var playerCards = [];
-var outputComputerCard = "";
-var outputPlayerCard = "";
+var resetGame = function () {
+  mode = "Show hand"; //Show hand, Hit or Stand, Compare
+  currentPlayer = "Player"; //switch between Player and Computer
+  // Draw 2 cards per player, store into array
+  computerCards = [];
+  playerCards = [];
+  outputComputerCard = "";
+  outputPlayerCard = "";
+  outputPlayerCard1 = " ";
+  outputPlayerCard2 = " ";
+  outputComputerCard1 = " ";
+};
 
+//2 players: player vs computer
+//Computer always dealer
+//Player & computer gets 2 cards at first
 var main = function (input) {
   //Player starts first
   if (currentPlayer == "Player") {
@@ -252,6 +347,7 @@ var main = function (input) {
           " of " +
           playerCards[i].suit;
       }
+
       console.log(computerCards);
       console.log(playerCards);
       console.log(outputPlayerCard);
@@ -291,40 +387,48 @@ var main = function (input) {
         outputPlayerCard +
         "<br>" +
         outputPlayerCard1;
-
+      console.log(outputPlayerCard1);
+      console.log(outputPlayerCard2);
       //will switch to computer's turn if player chooses "stand"
     }
   } else if (currentPlayer == "Computer") {
     if (mode == "Show hand") {
-      for (let i = 0; i < 2; i++) {
-        //const element = array[i];
-        outputComputerCard =
-          outputComputerCard +
-          "<br> " +
-          computerCards[i].name +
-          " of " +
-          computerCards[i].suit;
-      }
       myOutputValue =
         "Computer's hand: " +
         outputComputerCard +
-        "<br> Computer to decide whether to Hit or Stand";
+        "<br> Computer to decide whether to Hit or Stand. Click 'Submit' to continue.";
       // need to switch mode here...
       mode = "Hit or Stand";
     } else if (mode == "Hit or Stand") {
       console.log("Computer entered here");
       //Computer decision for hit or stand defnined by standard logic (automatic, not by user input)
-      myOutputValue = decideComputerHitOrStand(computerCards);
+      myOutputValue1 = decideComputerHitOrStand(computerCards);
       console.log(outputComputerCard);
-      mode = "Compare";
-    } else if (mode == "Compare") {
-      myOutputValue = comparePoints(computerCards, playerCards);
-      console.log(computerCards);
-      console.log(playerCardspwd);
+
+      myOutputValue =
+        myOutputValue1 +
+        "<br> Computer's drawn cards: " +
+        outputComputerCard +
+        "<br>" +
+        outputComputerCard1;
+      console.log(outputComputerCard1);
+      //add condition to change mode only when computer ends turn
+      //mode = "Compare"
+      if (mode == "Compare") {
+        console.log(computerCards);
+        console.log(playerCards);
+        //Total score is total of card ranks: J,Q,K = 10, Ace = 1 or 11
+        //Closer to or not >21 --> Winner
+        myOutputValue = comparePoints(computerCards, playerCards);
+
+        resetGame();
+      }
     }
   }
 
   console.log(mode);
+  console.log(outputComputerCard);
+  console.log(outputComputerCard1);
 
   return myOutputValue;
 };
