31407bf3422c4acb110a77d705c53399164e3040
kaizer1989 <xingwei.poh@gmail.com>
2021-12-23 04:49:19 +0800

    test

--- script.js
+++ script.js
@@ -1,4 +1,320 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+// +++ Deck Code Begins +++
+
+var createDeck = function () {
+  // deck array
+  var deck = [];
+  // for 'while loop' to create suits for cards
+  var suits = ["Diamonds ♦️", "Clubs ♣️", "Hearts ♥️", "Spades ♠️"];
+  var indexSuits = 0;
+  while (indexSuits < suits.length) {
+    var currSuit = suits[indexSuits];
+    // 13 ranks... ace to king - rank to define "card positions"
+    var indexRanks = 1;
+    while (indexRanks <= 13) {
+      var cardName = indexRanks;
+      // define card value - differentiate from rank: 'ace' = 1 / 11, 'jack' & 'queen' & 'king' = 10
+      if (cardName == 1) {
+        cardName = "Ace";
+        // define ace value as 11 all the way. if handValue > 10, -11 to total value
+        // vs. coding a function to redefine the value for ace
+      }
+      if (cardName == 11) {
+        cardName = "Jack";
+      }
+      if (cardName == 12) {
+        cardName = "Queen";
+      }
+      if (cardName == 13) {
+        cardName = "King";
+      }
+      var card = {
+        name: cardName,
+        suit: currSuit,
+        rank: indexRanks,
+      };
+      deck.push(card);
+      indexRanks = indexRanks + 1;
+    }
+    indexSuits = indexSuits + 1;
+  }
+  return deck;
 };
+
+// Function that generates a random number, used by shuffle deck function
+var getRandomIndex = function (size) {
+  return Math.floor(Math.random() * size);
+};
+
+// Function that shuffles a deck, used by createNewDeck function
+var shuffleDeck = function (cards) {
+  var index = 0;
+  while (index < cards.length) {
+    var randomIndex = getRandomIndex(cards.length);
+    var currentItem = cards[index];
+    var randomItem = cards[randomIndex];
+    cards[index] = randomItem;
+    cards[randomIndex] = currentItem;
+    index = index + 1;
+  }
+  return cards;
+};
+
+// Function that creates and shuffles a deck
+var createNewDeck = function () {
+  var newDeck = createDeck();
+  var shuffledDeck = shuffleDeck(newDeck);
+  return shuffledDeck;
+};
+// --- Deck Code Ends ---
+
+// +++ Game Functions Begins +++
+
+// Function For total
+var calculateTotalHandValue = function (cardHand) {
+  var totalHandValue = 0;
+  var handIndex = 0;
+  console.log("went into calculation");
+  while (handIndex < cardHand.length) {
+    var currentCard = cardHand[handIndex];
+    console.log("currentCard", currentCard);
+    if (
+      currentCard.name == "Jack" ||
+      currentCard.name == "King" ||
+      currentCard.name == "Queen"
+    ) {
+      totalHandValue = totalHandValue + 10;
+    } else {
+      totalHandValue = totalHandValue + currentCard.rank;
+    }
+    if (cardHand.length == 2 && currentCard.name == "Ace") {
+      totalHandValue = totalHandValue + 10;
+    }
+    handIndex = handIndex + 1;
+  }
+  console.log("totalHandValue", totalHandValue);
+  return totalHandValue;
+};
+
+// Function that displays the total hand values of the player and the dealer in a message
+var displayHandTotalValues = function (playerHandValue, dealerHandValue) {
+  var totalHandValueMessage =
+    "<br>Player total hand value: " +
+    playerHandValue +
+    "<br>Dealer total hand value: " +
+    dealerHandValue;
+  return totalHandValueMessage;
+};
+
+// *** Global Game Variables ***
+var playerHand = [];
+var computerHand = [];
+var currentGameMode = "waiting to start";
+
+// +++ Minimum Game Function +++
+
+// waiting to start
+
+var main = function (input) {
+  var myOutputValue = "";
+  var newShuffledDeck = createNewDeck();
+  var playerHandTotalValue = calculateTotalHandValue(playerHand);
+  var computerHandTotalValue = calculateTotalHandValue(computerHand);
+
+  console.log("player hand value total ==> ", playerHandTotalValue);
+  console.log("Computer hand value total ==> ", computerHandTotalValue);
+
+  var displayIndex = 0;
+  var displayTotalHandValue = function (playerHand, computerHand) {
+    var playerMessage = "Player's Hand is: <br>";
+    while (displayIndex < playerHand.length) {
+      playerMessage =
+        playerMessage +
+        playerHand[displayIndex].name +
+        " of " +
+        playerHand[displayIndex].suit +
+        "<br>";
+      displayIndex++;
+    }
+    playerHandTotalValue = calculateTotalHandValue(playerHand);
+    var totalPlayerValueMessage = "";
+    if (playerHandTotalValue < 17) {
+      totalPlayerValueMessage = `Your total cards value is: ${playerHandTotalValue}<br><br>
+      <b>Your total card value is less than 17, please input hit to continue.</b>`;
+    } else if (playerHandTotalValue > 21) {
+      totalPlayerValueMessage = `
+      Your total cards value is: ${playerHandTotalValue}<br><br><b>You are bust! Your total card value is more than 21. Please input stand to wait for Computer.</b>`;
+    } else {
+      totalPlayerValueMessage = `Your total cards value is:  ${playerHandTotalValue}`;
+    }
+
+    displayIndex = 0;
+    var computerMessage = "Computer's Hand is: <br>";
+    while (displayIndex < computerHand.length) {
+      computerMessage =
+        computerMessage +
+        computerHand[displayIndex].name +
+        " of " +
+        computerHand[displayIndex].suit +
+        "<br>";
+      displayIndex++;
+    }
+    computerHandTotalValue = calculateTotalHandValue(computerHand);
+    var totalComputerValueMessage =
+      "Computer total cards value is: " + computerHandTotalValue;
+
+    return (
+      playerMessage +
+      "<br>" +
+      totalPlayerValueMessage +
+      "<br><br>" +
+      computerMessage +
+      "<br>" +
+      totalComputerValueMessage
+    );
+  };
+
+  if (currentGameMode == "waiting to start") {
+    playerHand = [];
+    computerHand = [];
+    playerHand.push(newShuffledDeck.pop());
+    playerHand.push(newShuffledDeck.pop());
+    computerHand.push(newShuffledDeck.pop());
+    computerHand.push(newShuffledDeck.pop());
+
+    currentGameMode = "gameCardsDrawn";
+    console.log("current game mode===>", currentGameMode);
+
+    myOutputValue = "<b>Click on the submit button to start Blackjack!</b>";
+    return myOutputValue;
+  }
+
+  if (currentGameMode == "gameCardsDrawn") {
+    console.log("game mode changed to", currentGameMode);
+
+    currentGameMode = "player hit or stand";
+
+    return `${displayTotalHandValue(
+      playerHand,
+      computerHand
+    )} <br><br> <b>This is your starting hand. If you have less than 17, input hit to continue. If you have 17 or more, input stand if you wish to stop drawing.</b>`;
+  }
+  if (currentGameMode == "player hit or stand" && input == "hit") {
+    playerHand.push(newShuffledDeck.pop());
+    playerHandTotalValue = calculateTotalHandValue(playerHand);
+    computerHandTotalValue = calculateTotalHandValue(computerHand);
+
+    myOutputValue = `${displayTotalHandValue(
+      playerHand,
+      computerHand
+    )} <br><br><b> Please input hit or stand to continue</b>`;
+    return myOutputValue;
+  }
+  if (
+    currentGameMode == "player hit or stand" &&
+    input == "stand" &&
+    playerHandTotalValue >= 17
+  ) {
+    console.log("currrent game mode: stand", currentGameMode);
+
+    while (computerHandTotalValue < 17) {
+      computerHand.push(newShuffledDeck.pop());
+      console.log(`computer hit`);
+      computerHandTotalValue = calculateTotalHandValue(computerHand);
+    }
+
+    myOutputValue = `${displayTotalHandValue(
+      playerHand,
+      computerHand
+    )}<br><br><b> Click on Submit to see the results!</b>`;
+    currentGameMode = "result of game";
+    return myOutputValue;
+  }
+  if (currentGameMode == "player hit or stand" && playerHandTotalValue > 21) {
+    console.log("currrent game mode: stand", currentGameMode);
+    myOutputValue = `<b>Stand</b><br><br>${displayTotalHandValue(
+      playerHand,
+      computerHand
+    )}`;
+    return myOutputValue;
+  }
+
+  if (currentGameMode == "player hit or stand") {
+    console.log("currrent game mode: stand", currentGameMode);
+    myOutputValue = `<b>Game only accepts Hit or Stand function</b><br><br>${displayTotalHandValue(
+      playerHand,
+      computerHand
+    )}`;
+    return myOutputValue;
+  }
+
+  if (currentGameMode == "result of game") {
+    //both bust
+    if (computerHandTotalValue > 21 && playerHandTotalValue > 21) {
+      myOutputValue = `<b>Both you and the computer are bust! Click on submit to play again!</b><br><br>${displayTotalHandValue(
+        playerHand,
+        computerHand
+      )}`;
+      currentGameMode = "waiting to start";
+      newShuffledDeck = createNewDeck();
+      return myOutputValue;
+    }
+    // player bust
+    if (playerHandTotalValue > 21) {
+      myOutputValue = `<b>You are bust! Click on submit to play again!</b><br><br>${displayTotalHandValue(
+        playerHand,
+        computerHand
+      )}`;
+      currentGameMode = "waiting to start";
+      newShuffledDeck = createNewDeck();
+      return myOutputValue;
+    }
+    // computer tie
+    if (computerHandTotalValue == playerHandTotalValue) {
+      myOutputValue = `<b>Both you and computer tied! Click on submit to play again!</b><br><br>${displayTotalHandValue(
+        playerHand,
+        computerHand
+      )}`;
+      currentGameMode = "waiting to start";
+      newShuffledDeck = createNewDeck();
+      return myOutputValue;
+    }
+    // player wins
+    if (computerHandTotalValue < playerHandTotalValue) {
+      myOutputValue = `<b>You Won Computer! Click on submit to play again!</b><br><br>${displayTotalHandValue(
+        playerHand,
+        computerHand
+      )}`;
+      currentGameMode = "waiting to start";
+      newShuffledDeck = createNewDeck();
+      return myOutputValue;
+    }
+    // computer wins
+    if (computerHandTotalValue > playerHandTotalValue) {
+      myOutputValue = `<b>You lost to Computer! Click on submit to play again!</b><br><br>${displayTotalHandValue(
+        playerHand,
+        computerHand
+      )}`;
+      currentGameMode = "waiting to start";
+      newShuffledDeck = createNewDeck();
+      return myOutputValue;
+    }
+  }
+
+  // Compare Blackjack Function
+  // Total Up Function
+  // if (currentGameMode == Game_Cards_Drawn) {
+
+  // }
+
+  // Win
+  // Lose
+  // Tie
+};
+
+// Before Play clicks connect
+
+// Global value = shuffle deck
+// on submit, give player 2 cards
+// give computer 2 cards
+// game value = compare player hand & computerhand
+// output who wins
