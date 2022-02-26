ddd1b3cf6f822dbd8c09b3f0359671598899a2ac
babiesq <malikibinali@gmail.com>
2022-02-26 19:55:01 +0800

    new deck with for loop

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+var makeDeck = function () {
+  var cardDeck = [];
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  for (suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
+    var currentSuit = suits[suitIndex];
+    for (rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
+      var cardName = rankCounter;
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+      cardDeck.push(card);
+    }
+  }
+  return cardDeck;
+};
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
