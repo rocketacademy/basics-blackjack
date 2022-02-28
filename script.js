cf9391eb530fd42904099c9000a072ddafbe88db
zabec <denis.imsirovic@gmail.com>
2022-02-28 19:57:52 +0100

    basic html, media

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// let toggle = () => {
+//   let element = document.getElementById("button");
+//   let hidden = element.getAttribute("hidden");
+
+//   if (hidden) {
+//     element.removeAttribute("hidden");
+//   } else {
+//     element.setAttribute("hidden", "hidden");
+//   }
+// };
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var card = `<div ><img class="card" src="./svg-cards/2_of_hearts.svg" /></div>`;
+  return card;
 };
