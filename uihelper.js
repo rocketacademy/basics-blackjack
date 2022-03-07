acd353a51af0b164706ad108238b2b2aaa20bbbf
Floatingtales <58957425+floatingtales@users.noreply.github.com>
2022-03-07 21:12:34 +0700

    Hidden some buttons, and have minor changes on the UI for display purposes

--- uihelper.js
+++ uihelper.js
@@ -2,6 +2,33 @@ var deal = document.querySelector("#deal-button");
 var dealerHand = document.querySelector("#dealer-hand");
 var dealerInfo = document.querySelector("#dealer-info");
 
+var p1Hand = doocument.querySelector("#p1-hand");
+var p1Info = document.querySelector("#p1-info");
+var p1Hit = document.querySelector("#p1-hit");
+var p1Stand = document.querySelector("#p1-stand");
+var p1Double = document.querySelector("#p1-double");
+var p1BetArea = document.querySelector("#p1-bet-area");
+var p1BetInput = document.querySelector("#p1-bet");
+var p1ChipIn = document.querySelector("#p1-chip-in");
+
+var p2Hand = doocument.querySelector("#p2-hand");
+var p2Info = document.querySelector("#p2-info");
+var p2Hit = document.querySelector("#p2-hit");
+var p2Stand = document.querySelector("#p2-stand");
+var p2Double = document.querySelector("#p2-double");
+var p2BetArea = document.querySelector("#p2-bet-area");
+var p2BetInput = document.querySelector("#p2-bet");
+var p2ChipIn = document.querySelector("#p2-chip-in");
+
+var p3Hand = doocument.querySelector("#p3-hand");
+var p3Info = document.querySelector("#p3-info");
+var p3Hit = document.querySelector("#p3-hit");
+var p3Stand = document.querySelector("#p3-stand");
+var p3Double = document.querySelector("#p3-double");
+var p1BetArea = document.querySelector("#p3-bet-area");
+var p1BetInput = document.querySelector("#p3-bet");
+var p3ChipIn = document.querySelector("#p3-chip-in");
+
 deal.addEventListener("click", function () {
   var card1 = playDeck.pop();
   var card2 = playDeck.pop();
@@ -14,25 +41,4 @@ deal.addEventListener("click", function () {
 
   dealerHand.innerHTML = card1.svg + card2.svg;
   handArray.length = 0;
-});
-
-var button = document.querySelector("#reset-button");
-
-var hit = document.querySelector("#hit-button");
-
-var output = document.querySelector("#output-div");
-
-hit.addEventListener("click", function () {
-  output.innerHTML = "hit button clicked.";
-});
-
-button.addEventListener("click", function () {
-  var result = main();
-
-  // Display result in output element
-
-  output.innerHTML = result;
-
-  // Reset input value
-  input.value = "";
 });
