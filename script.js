6480d1bbd926fc71b998b10755a8d2a822fead91
starstormfall <photon_dazzles_02@icloud.com>
2022-06-08 13:40:17 +0800

    added game rules and description

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+/* Game Rules
+
+1. There will be only two players. One human and one computer (for the Base solution).
+2. The computer will always be the dealer.
+3. Each player gets dealt two cards to start.
+4. The player goes first, and decides if they want to hit (draw a card) or stand (end their turn).
+5. The dealer has to hit if their hand is below 17.
+6. Each players' score is the total of their card ranks. Jacks/Queen/Kings are 10. 
+7. Aces can be 1 or 11.
+8. The player who is closer to, but not above 21 wins the hand.
+
+*/
+
+/* Gameplay Description
+
+1. Deck is shuffled.
+2. User clicks Submit to deal cards.
+3. The cards are analysed for game winning conditions, e.g. Blackjack.
+4. The cards are displayed to the user.
+5. The user decides whether to hit or stand, using the submit button to submit their choice.
+6. The user's cards are analysed for winning or losing conditions.
+7. The computer decides to hit or stand automatically based on game rules.
+8. The game either ends or continues.
+
+*/
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
