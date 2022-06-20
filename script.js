239a9c081b43efa2bb41adf92acc50cf2732e0fe
sRanay <jsong2506@gmail.com>
2022-06-20 22:04:19 +0800

    Started Blackjack

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+// Blackjack Project
+
+// Global Variable
+var cardDeck;
+var player
+
+// Function makeDeck
+// Purpose: To generate the card deck
+var makeDeck = function () {
+  // Initialise an empty deck array
+  cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = 'ace';
+      } else if (cardName == 11) {
+        cardName = 'jack';
+      } else if (cardName == 12) {
+        cardName = 'queen';
+      } else if (cardName == 13) {
+        cardName = 'king';
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+};
+
+// Generate a Card Deck
+makeDeck();
+
+// Function getRandomCard
+// Purpose: Returns a random card from the deck
+var getRandomCard = function() {
+  return cardDeck[Math.floor(Math.random()*cardDeck.length)];
+};
+
 var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
