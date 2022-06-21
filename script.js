acf7858dcf9e2f72f5e7d35850be9b3d86a7f598
sherwintieonwj <sherwintieon@gmail.com>
2022-06-21 22:55:27 +0800

    modified template code helper functions to suit context of blackjack

--- script.js
+++ script.js
@@ -1,4 +1,135 @@
+// Helper functions
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+
+// Modify card deck creation for blackjack
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    // Blackjack - need separate counter since max is rank 10 but 13 cards still needed
+    var cardCounter = 1;
+    while (cardCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var rankCounter = cardCounter;
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      // Blackjack - all picture cards are now rank 10
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+        rankCounter = 10;
+      } else if (cardName == 12) {
+        cardName = "queen";
+        rankCounter = 10;
+      } else if (cardName == 13) {
+        cardName = "king";
+        rankCounter = 10;
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment cardCounter to iterate over the next rank
+      cardCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+
+// High Card
+// Initialise the card deck representation as an array of objects
+// Modify template code to generate deck with function call
+var deck = makeDeck();
+
+// Shuffle the deck and save it in a new variable shuffledDeck
+// to communicate that we have shuffled the deck.
+var shuffledDeck = shuffleCards(deck);
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  // Draw 2 cards from the top of the deck
+  var computerCard = shuffledDeck.pop();
+  var playerCard = shuffledDeck.pop();
+
+  // Construct an output string to communicate which cards were drawn
+  var myOutputValue =
+    "Computer had " +
+    computerCard.name +
+    " of " +
+    computerCard.suit +
+    ". Player had " +
+    playerCard.name +
+    " of " +
+    playerCard.suit +
+    ". ";
+
+  // Compare computer and player cards by rank attribute
+  // If computer card rank is greater than player card rank, computer wins
+  if (computerCard.rank > playerCard.rank) {
+    // Add conditional-dependent text to the output string
+    myOutputValue = myOutputValue + "Computer wins.";
+    // Else if computer card rank is less than player card rank, player wins
+  } else if (computerCard.rank < playerCard.rank) {
+    myOutputValue = myOutputValue + "Player wins!";
+    // Otherwise (i.e. ranks are equal), it's a tie
+  } else {
+    myOutputValue = myOutputValue + "It's a tie.";
+  }
+
+  // Return the fully-constructed output string
+  return myOutputValue;
+};
+
+// Template
+var main = function (input) {
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
