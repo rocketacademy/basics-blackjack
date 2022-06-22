d09b618b2a31a6f7d5273608787b05955e59d431
sherwintieonwj <sherwintieon@gmail.com>
2022-06-23 01:02:24 +0800

    completed base

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// Helper functions
+/*
+The main function runs on each player's turn. The sequence of actions in the game might be the following.
+Deck is shuffled.
+User clicks Submit to deal cards.
+The cards are analysed for game winning conditions, e.g. Blackjack.
+The cards are displayed to the user.
+The user decides whether to hit or stand, using the submit button to submit their choice.
+The user's cards are analysed for winning or losing conditions.
+The computer decides to hit or stand automatically based on game rules.
+The game either ends or continues.
+
+Note that for the main function to perform different logic on user input, for example when a player decides to hit or stand, we may wish to consider using a new game mode.
+*/
+
+// Helper functions from template
 // Get a random index ranging from 0 (inclusive) to max (exclusive).
 var getRandomIndex = function (max) {
   return Math.floor(Math.random() * max);
@@ -94,6 +108,81 @@ var deck = makeDeck();
 // to communicate that we have shuffled the deck.
 var shuffledDeck = shuffleCards(deck);
 
+// Global variables
+// Winning value, minimum value to hit, player's decision, game end, list to store player and dealer hand
+var BLACKJACK = 21;
+var dealerHitMin = 16;
+var playerDecision = false;
+var endGame = false;
+var playerHand = [];
+var dealerHand = [];
+
+// Helper function 1 - deal cards to hand (push to list)
+var dealCard = function (hand) {
+  var cardDealt = shuffledDeck.pop();
+  hand.push(cardDealt);
+};
+
+// Helper function 2 - check hand value (variable ace values to consider here)
+var checkHandValue = function (hand) {
+  var numOfAce = 0;
+  var handValue = 0;
+  var cardInHandCounter = 0;
+  while (cardInHandCounter < hand.length) {
+    var currentCard = hand[cardInHandCounter];
+    // Let ace be 11 by default so easier to win
+    if (currentCard.rank == 11) {
+      numOfAce += 1;
+      handValue += 11;
+    }
+    handValue += currentCard.rank;
+    cardInHandCounter += 1;
+  }
+
+  // If > 21 but have ace, change ace value to 1
+  if (handValue > BLACKJACK && numOfAce > 0) {
+    var aceCardCounter = 0;
+    while (aceCardCounter < numOfAce) {
+      handValue -= 10;
+      // If < 21 at first, obtain original sum by exiting loop and go back up
+      if (handValue <= BLACKJACK) {
+        break;
+      }
+      aceCardCounter += 1;
+    }
+  }
+  return handValue;
+};
+
+// Helper function 3 - check hand for blackjack (compare initial hand to determine winner), return bool
+var checkBlackjack = function (hand) {
+  return hand.length == 2 && checkHandValue(hand) == BLACKJACK;
+};
+
+// Helper function 4 - display hand
+var displayHand = function (hand) {
+  var cardsInHand = "";
+  var currentCardIndex = 0;
+
+  while (currentCardIndex < hand.length) {
+    cardsInHand += `${cardsInHand}, ${hand[currentCardIndex].name} of ${hand[currentCardIndex].suit}`;
+    currentCardIndex += 1;
+  }
+  return cardsInHand;
+};
+
+// Helper function 5 - display output message
+var displayOutputMessage = function () {
+  return `Player hand: ${displayHand(
+    playerHand
+  )}. Total Value: ${checkHandValue(playerHand)} <br>
+          Dealer hand: ${displayHand(
+            dealerHand
+          )}. Total Value: ${checkHandValue(dealerHand)}`;
+};
+
+/* 
+// High Card
 var main = function (input) {
   // Draw 2 cards from the top of the deck
   var computerCard = shuffledDeck.pop();
@@ -128,8 +217,90 @@ var main = function (input) {
   return myOutputValue;
 };
 
+*/
+
 // Template
 var main = function (input) {
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  // Check if game ended already
+  if (endGame) {
+    return ` ~ Game End ~ <br> 
+    Refresh to play again!`;
+  }
+
+  // Deal Hands if hand empty (initial) - length is 0
+  if (playerHand.length == 0) {
+    // Call Helper function 1, 2 times for cards
+    dealCard(playerHand);
+    dealCard(dealerHand);
+    dealCard(playerHand);
+    dealCard(dealerHand);
+
+    // Check blackjack - call Helper function 3
+    if (checkBlackjack(dealerHand)) {
+      endGame = true;
+      return `${displayOutputMessage()} <br> 
+    Dealer wins by black jack!`;
+    }
+
+    if (checkBlackjack(playerHand)) {
+      endGame = true;
+      return `${displayOutputMessage()} <br>
+    Player wins by black jack!`;
+    }
+    return `${displayOutputMessage()} <br>
+    Please enter "hit" or "stand" and then click Submit`;
+  }
+
+  // Player's action - hit or stand (2nd version)
+  if (!playerDecision) {
+    // Input validation
+    if (input !== "hit" && input !== "stand") {
+      return `Please input "hit" or "stand" only.`;
+    }
+
+    // Hit - deal card to player - call Helper function 1
+    if (input == "hit") {
+      dealCard(playerHand);
+      // Check if > 21 - call Helper function 2
+      if (checkHandValue(playerHand) > BLACKJACK) {
+        endGame = true;
+        return `${displayOutputMessage()} <br>
+        Player has busted and lost. Refresh to play again!`;
+      }
+    }
+
+    // Stand - change player decision
+    if (input == "stand") {
+      playerDecision = true;
+    }
+  }
+
+  // Dealer's action - hit or stand (3rd version)
+  // Dealer hit if <= min - call Helper function 1, else stand
+  var dealerHandValue = checkHandValue(dealerHand);
+  if (dealerHandValue <= dealerHitMin) {
+    dealCard(dealerHand);
+    // Update after new dealing
+    dealerHandValue = checkHandValue(dealerHand);
+    // Check if > 21 - call Helper function 2
+    if (dealerHandValue > BLACKJACK) {
+      endGame = true;
+      return `${displayOutputMessage()} <br>
+        Dealer has busted and lost. Refresh to play again!`;
+    }
+  }
+
+  // Stand - check winner, end game
+  if (playerDecision && dealerHandValue > dealerHitMin) {
+    playerDecision = true;
+    if (checkHandValue(playerHand) > dealerHandValue) {
+      return `${displayOutputMessage()} <br>
+      Player wins! Refresh to play again!`;
+    }
+    return `${displayOutputMessage()} <br>
+      Dealer wins! Refresh to play again!`;
+  }
+  return `${displayOutputMessage()} <br>
+  Player's decision to stand: ${playerDecision} <br>
+  If player has not yet decided, please enter "hit" or "stand" and then click Submit. Click Submit to check Dealer's move.`;
 };
