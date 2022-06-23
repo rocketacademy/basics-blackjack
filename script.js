4c62880a03e7354fe10184c3c554e3e0525ac524
Nawu <naroashburn@gmail.com>
2022-06-23 22:12:11 +0800

    demonstrating on how to commit

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
+
+//first we make a deck case
+var cardDeck = [];
+
+//then we need a suits array
+var suits = [heart, diamond, spade, clover];
+var suitsIndex;
+
+//we loop over the suits array
+while (suitsIndex < suits.length) {
+  var currentSuit = suits[suitIndex];
+}
