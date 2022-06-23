52b7d977e1be6bdacf6b3fd0a5473ee75350aac0
Nawu <naroashburn@gmail.com>
2022-06-23 23:53:08 +0800

    makeDeck function done

--- script.js
+++ script.js
@@ -3,14 +3,45 @@ var main = function (input) {
   return myOutputValue;
 };
 
-//first we make a deck case
-var cardDeck = [];
+// MAKE A CARD DECK
+var makeDeck = function () {
+  //first we make a deck case
+  var cardDeck = [];
 
-//then we need a suits array
-var suits = [heart, diamond, spade, clover];
-var suitsIndex;
+  //then we need a suits array
+  var suits = [heart, diamond, spade, clover];
+  var suitsIndex;
 
-//we loop over the suits array
-while (suitsIndex < suits.length) {
-  var currentSuit = suits[suitIndex];
-}
+  //we loop over the suits array
+  while (suitsIndex < suits.length) {
+    var currentSuit = suits[suitIndex];
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      //create a card with name, suit and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+      //Add the card to the deck
+      cardDeck.push(Card);
+
+      //increase rank
+      rankCounter += 1;
+    }
+
+    suitIndex += 1;
+  }
+
+  return cardDeck;
+};
