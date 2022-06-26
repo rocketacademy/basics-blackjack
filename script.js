c0d1cf712cf394ef7f777b8426b5637986c948b7
Nawu <naroashburn@gmail.com>
2022-06-27 01:22:13 +0800

    why does it push 3 cards instead of 1 card each time

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-//ver 1:
+//ver 2: player hit or stand
 var DRAW_MODE = "DRAW MODE";
 var RESULT_MODE = "RESULT MODE";
+var PLAYER_CHOICE_MODE = "PLAYER CHOICE MODE";
 
-var GAME_MODE = DRAW_MODE;
+//player start with choice mode to decide if hit or stand
+// draw mode is when player decides to hit
+// result mode is when player decides to stand
+var gameMode = PLAYER_CHOICE_MODE;
+
+//ok we should have a list for player hand and CPU hand
+var playerHand = [];
+var computerHand = [];
 
 var makeDeck = function () {
   // Initialise an empty deck array
@@ -82,13 +90,18 @@ var addTotalValue = function (card1, card2) {
   return totalValue;
 };
 
+var addTotalValue2 = function () {
+  var totalValue = playerHand;
+  return totalValue;
+};
+
 //function for checking who wins.... hold on we need to add them all
 
 var checkBustOrBlackjack = function (deckValue1, deckValue2) {
   var endResult;
   // Compare computer and player cards by rank attribute
   if (deckValue1 == 21 && deckValue2 != 21) {
-    //GAME_MODE = RESULT_MODE;
+    //gameMode = RESULT_MODE;
     endResult = `Player wins! 21 means blackjack!`;
   } else if (deckValue1 != 21 && deckValue2 == 21) {
     endResult = `Player loses! Computer got 21 which means blackjack!`;
@@ -116,14 +129,53 @@ var checkWhoWinLose = function (deckValue1, deckValue2) {
   return endResult2;
 };
 
+var resetGame = function () {
+  gameMode = PLAYER_CHOICE_MODE;
+  playerHand = [];
+  computerHand = [];
+};
+
+var listAllCards = function (cards) {
+  var resultString = String();
+  //console.log(cards);
+  for (var i = 0; i < cards.length; i += 1) {
+    var currentCard = cards[i];
+    //console.log(currentCard);
+    resultString += `<br><br>${currentCard.name} of ${currentCard.suit}`;
+    //console.log("results String:" + resultString);
+  }
+  return resultString;
+};
+
 var main = function (input) {
-  //Let's draw 2 cards
+  var myOutputValue;
+  //Let's draw 2 cards and push them into each others hand
   var computerCard1 = shuffledDeck.pop();
-  var playerCard1 = shuffledDeck.pop();
   var computerCard2 = shuffledDeck.pop();
+  computerHand.push(computerCard1, computerCard2);
+
+  var playerCard1 = shuffledDeck.pop();
   var playerCard2 = shuffledDeck.pop();
+  playerHand.push(playerCard1, playerCard2);
+  //var computerCard3;
+  var playerCard3;
   //console.log(playerCard);
-  console.log(deck);
+  //console.log(deck);
+
+  //game mode please
+  //if ((gameMode = PLAYER_CHOICE_MODE)) {
+  //if (input != "h" || input != "s") {
+  //return `retry h or s`;
+  if (input == "h") {
+    //ok draw more cards! draw mode...
+    //gameMode = DRAW_MODE;
+    var playerCard3 = shuffledDeck.pop();
+    playerHand.push(playerCard3);
+    console.log("Player Hand now has these cards after hit" + playerHand.rank);
+  } else if (input == "s") {
+    gameMode = RESULT_MODE;
+  }
+  //}
 
   var totalvaluePlayer = addTotalValue(playerCard1, playerCard2);
   var totalvalueComputer = addTotalValue(computerCard1, computerCard2);
@@ -134,7 +186,14 @@ var main = function (input) {
   );
   console.log(resultAnnouncement);
 
-  return `Player Card: ${playerCard1.name} of ${playerCard1.suit}, ${playerCard2.name} of ${playerCard2.suit} <br><br> CPU card: ${computerCard1.name} of ${computerCard1.suit}, ${computerCard2.name} of ${computerCard2.suit} 
-  <br><br> Player deck Value is ${totalvaluePlayer} and CPU total value is ${totalvalueComputer}
-  <br><br> ${resultAnnouncement}`;
+  //resetGame();
+
+  myOutputValue = `Player Hand has ${listAllCards(
+    playerHand
+  )}<br><br> Player deck Value is ${totalvaluePlayer} and CPU total value is ${totalvalueComputer}
+  <br><br> Player, would you like to hit or stand? type h for hit and s for stand.`;
+
+  //<br><br> ${resultAnnouncement}`;
+
+  return myOutputValue;
 };
