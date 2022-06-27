0d1bd55a002055fd762df0038d676b33620ce650
sRanay <jsong2506@gmail.com>
2022-06-27 19:42:34 +0800

    Add tabs to the function

--- buttonselector.js
+++ buttonselector.js
@@ -3,31 +3,31 @@ var hitButton = document.querySelector("#hit-button");
 var submitButton = document.querySelector("#submit-button");
       
 standButton.addEventListener("click", function () {
-// Set result to input value
-var input = document.querySelector("#stand-button");
-var result = main(input.value);
+    // Set result to input value
+    var input = document.querySelector("#stand-button");
+    var result = main(input.value);
 
-// Display result in output element
-var output = document.querySelector("#output-div");
-output.innerHTML = result;
+    // Display result in output element
+    var output = document.querySelector("#output-div");
+    output.innerHTML = result;
 });
 
 hitButton.addEventListener("click", function () {
-// Set result to input value
-var input = document.querySelector("#hit-button");
-var result = main(input.value);
+    // Set result to input value
+    var input = document.querySelector("#hit-button");
+    var result = main(input.value);
 
-// Display result in output element
-var output = document.querySelector("#output-div");
-output.innerHTML = result;
+    // Display result in output element
+    var output = document.querySelector("#output-div");
+    output.innerHTML = result;
 });
       
 submitButton.addEventListener("click", function () {
-// Set result to input value
-var input = document.querySelector("#input-field");
-var result = main(input.value);
+    // Set result to input value
+    var input = document.querySelector("#input-field");
+    var result = main(input.value);
 
-// Display result in output element
-var output = document.querySelector("#output-div");
-output.innerHTML = result;
+    // Display result in output element
+    var output = document.querySelector("#output-div");
+    output.innerHTML = result;
 });
