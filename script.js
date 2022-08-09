6036643936ad89927ab3f95041362121ab4ac79c
lazyhazydaze <juruissic@gmail.com>
2022-08-09 14:32:16 +0800

    V1 Compare Initial Hands to Determine Winner. Completion time: ~2h

--- script.js
+++ script.js
@@ -1,4 +1,201 @@
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "Ace";
+      } else if (cardName == 11) {
+        cardName = "Jack";
+      } else if (cardName == 12) {
+        cardName = "Queen";
+      } else if (cardName == 13) {
+        cardName = "King";
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+
+var deck = makeDeck();
+
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+
+// Shuffle the deck and save it in a new variable shuffledDeck
+var shuffledDeck = shuffleCards(deck);
+
+// ==================== V1 - Compare initial hands to determine winner ====================
+// two players - one player and one computer
+// a deck of cards
+// a starting hand of 2 cards for each player
+// comparing both hands and determining a winner
+
+// Create global variables to store each player's cards
+var playerHands = [];
+var dealerHands = [];
+
+// Create a helper function to draw 2 cards for each player
+var drawCards = function (input) {
+  // Input is a numerical value to determine number of cards drawn by player
+  var cardsDrawn = []; // ??? QUESTION: what's the diff between setting this as global variable vs within a function?
+  var i = 0;
+  while (i < input) {
+    cardsDrawn.push(shuffledDeck.pop());
+    i += 1;
+  }
+  return cardsDrawn;
+};
+
+// Derive a method to calculate the hand values
+// In blackjack, ace can be 1 or 11, jack/queen/king are 10.
+// Ace will have a value of 11 unless that would give the player or dealer a score in excess of 21
+var calcCardValue = function (input) {
+  //input is an array of card objects with name, suit and rank
+  var value = Number(0);
+  var i = 0;
+  while (i < input.length) {
+    // for jack, queen, king, value = 10
+    if (input[i].rank == 11 || input[i].rank == 12 || input[i].rank == 13) {
+      value += 10;
+
+      // for ace, value = 11 or 1 if adding 11 gives an excess of 21
+    } else if (input[i].rank == 1) {
+      if (value + 11 <= 21) {
+        value += 11;
+      } else {
+        value += 1;
+      }
+
+      // for all other cards, value is as is.
+    } else {
+      value += input[i].rank;
+    }
+    i += 1;
+  }
+  return value;
+};
+
+// Create a helper function with winning conditions
+var compareHands = function (player, dealer) {
+  // Inputs are 2 numerical values
+  var outputValue = "";
+  if (player == dealer) {
+    if (player == Number(21)) {
+      outputValue = "Tie. Both drew Blackjack.";
+    } else {
+      outputValue = "Tie.";
+    }
+  } else if (player == Number(21)) {
+    outputValue = "Player wins by Blackjack!";
+  } else if (dealer == Number(21)) {
+    outputValue = "Dealer wins by Blackjack!";
+  } else if (player > dealer && player != Number(21)) {
+    outputValue = "Player wins";
+  } else if (player < dealer && dealer != Number(21)) {
+    outputValue = "Dealer wins";
+  }
+  return outputValue;
+};
+
+// Create a function to reveal the cards drawn by each player
+var revealCards = function (input) {
+  // Input is an object with card arrays
+  var cardsInHand = "";
+  var i = 0;
+  while (i < input.length) {
+    cardsInHand += `${input[i].name} of ${input[i].suit} <br>`;
+    i += 1;
+  }
+  return cardsInHand;
+};
+
+// // [REDUNDANT FOR V1] Create a helper function to declare if player has blackjack.
+// var haveBlackJack = function (input) {
+//   var message = "";
+//   // input is an array of card objects
+//   if (calcCardValue(input) == Number(21)) {
+//     message = "wins by Blackjack 👑";
+//   }
+// };
+
+var gameMode = "deal cards";
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var outputMsg = "";
+  if (gameMode == "deal cards") {
+    gameMode = "compare cards";
+    playerHands = drawCards(2);
+    dealerHands = drawCards(2);
+    outputMsg = `<i> The dealer deals 2 cards to each player... </i><br><br> Click 'Submit' for the winner.`;
+  } else if (gameMode == "compare cards") {
+    gameMode = "deal cards";
+    // Store the value of each player's hands into a variable
+    var playerValue = calcCardValue(playerHands);
+    var dealerValue = calcCardValue(dealerHands);
+    outputMsg = `Player:<br>${revealCards(
+      playerHands
+    )}<br> Dealer:<br> ${revealCards(dealerHands)} <br> <b>${compareHands(
+      playerValue,
+      dealerValue
+    )}</b>`;
+  }
+  return outputMsg;
 };
