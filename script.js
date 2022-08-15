18c069c145f593027c2ef22cf7329bb93853e822
joleneho <jolenehzn96@gmail.com>
2022-08-15 16:13:24 +0800

    v4 added aces counter

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var dealerHand = [];
 
 //Declare variable to hold deck of cards
 var gameDeck = "empty at the start";
-
+// ======================GAME FUNCTIONS===================
 //====== MAKE DECK FUNCTION ======
 var makeDeck = function () {
   var cardDeck = [];
@@ -23,7 +23,7 @@ var makeDeck = function () {
   // loop 1
   // rank 1-10
   var suitIndex = 0;
-  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  var suits = ["♥ ", "◆ ", "♣️ ", "♠️ "];
   while (suitIndex < suits.length) {
     var currentSuit = suits[suitIndex];
     //loop 2
@@ -37,13 +37,10 @@ var makeDeck = function () {
         cardName = "Ace";
       } else if (cardName == 11) {
         cardName = "Jack";
-        rankCounter = "10";
       } else if (cardName == 12) {
         cardName = "Queen";
-        rankCounter = "10";
       } else if (cardName == 13) {
         cardName = "King";
-        rankCounter = "10";
       }
 
       //Create a new card with the current name, suit and rank
@@ -61,6 +58,10 @@ var makeDeck = function () {
   return cardDeck;
 };
 
+//Deal card function
+var dealCardToHand = function (hand) {
+  hand.push(deck.pop());
+};
 // Get a random index ranging from 0 (inclusive) to max (exclusive).
 var getRandomIndex = function (max) {
   return Math.floor(Math.random() * max);
@@ -96,21 +97,93 @@ var createNewDeck = function () {
 };
 
 //Check whether Blackjack is true function
-var checkForBlackjack = function () {
+var checkForBlackjack = function (handArray) {
+  var playerCardOne = handArray[0];
+  var playerCardTwo = handArray[1];
   var isBlackjack = false;
   if (
-    (playerHand[0]["name"] == "ace" && playerHand[1]["rank"] == 10) ||
-    (playerHand[1]["name"] == "ace" && playerHand[0]["rank"] == 10) ||
-    (dealerHand[0]["name"] == "ace" && dealerHand[1]["rank"] == 10) ||
-    (dealerHand[1]["name"] == "ace" && dealerHand[0]["rank"] == 10)
+    (playerCardOne.name == "Ace" && playerCardTwo == 10) ||
+    (playerCardTwo.name == "Ace" && playerCardOne == 10)
   ) {
     isBlackjack = true;
   }
   return isBlackjack;
 };
 
+// Calculate Total Hand Value Function
+var calculateTotalHandValue = function (handArray) {
+  var totalHandValue = 0;
+  var aceCounter = 0;
+  //loop through player or dealer hand and add up values
+  var index = 0;
+  while (index < handArray.length) {
+    var currentCard = handArray[index];
+
+    //For Jack, Queen, King, change value to 0
+    if (
+      currentCard.name == "Jack" ||
+      currentCard.name == "Queen" ||
+      currentCard.name == "King"
+    ) {
+      totalHandValue = totalHandValue + 10;
+    } else if (currentCard.name == "Ace") {
+      totalHandValue = totalHandValue + 11;
+      aceCounter = aceCounter + 1;
+    } else {
+      totalHandValue = totalHandValue + currentCard.rank;
+    }
+    index = index + 1;
+  }
+
+  index = 0;
+  while (index < aceCounter) {
+    if (totalHandValue > 21) {
+      totalHandValue = totalHandValue - 10;
+    }
+    index = index + 1;
+  }
+  return totalHandValue;
+};
+
+//Function that displays player and dealer hands totals in a message
+var displayHandsTotals = function (playerHand, dealerHand) {
+  var displayPlayerPoints =
+    "Player's Totals: " + calculateTotalHandValue(playerHand);
+  var displayDealerPoints =
+    "Karen's Totals: " + calculateTotalHandValue(dealerHand);
+  return displayPlayerPoints + "<br>" + displayDealerPoints;
+};
+
+//Function that displays playes and dealer hands in a message
+var displayPlayerAndDealerHands = function (playerHandArray, dealerHandArray) {
+  //player
+  var playerMessage = "Player's Hand: ";
+  var handIndex = 0;
+
+  while (handIndex < playerHandArray.length) {
+    var currentCard = playerHandArray[handIndex];
+    playerMessage = playerMessage + currentCard.name + currentCard.suit;
+    handIndex = handIndex + 1;
+  }
+  //banker
+  var dealerMessage = "Karen's Totals: ";
+  var handIndex = 0;
+  while (handIndex < dealerHandArray.length) {
+    var currentCard = dealerHandArray[handIndex];
+    dealerMessage = dealerMessage + currentCard.name + currentCard.suit;
+    handIndex = handIndex + 1;
+  }
+  return playerMessage + "<br>" + dealerMessage;
+};
+
+//Version 4
+//Determine whether Aces should have 1 or 11 in a hand
+// If totalhandvalue < 10, ace = 11
+// If total handvalue < 21, ace = 1
+
 //====== MAIN FUNCTION =====
-var main = function () {
+var main = function (input) {
+  var outputMessage = "";
   //First click
   if (currentMode == WELCOME_MODE) {
     console.log("Check current mode: " + currentMode);
@@ -133,7 +206,7 @@ var main = function () {
     currentMode = GAME_CARDS_DRAWN;
 
     outputMessage =
-      "Karen deals all the cards on the table, with a disgruntled look on her face. Click the 'submit' button to see the results.";
+      "Karen deals all the cards on the table, with a disgruntled look on her face. Click the 'submit' button again to see the results.";
     return outputMessage;
   }
   if (currentMode == GAME_CARDS_DRAWN) {
@@ -154,40 +227,114 @@ var main = function () {
     console.log("Does Dealer have Blackjack? ==>", dealerHasBlackjack);
 
     // Both BJ = Tie
-    var outputMessage = "";
 
     if (playerHasBlackjack && dealerHasBlackjack) {
       outputMessage =
-        "It's a double Blackjack for both Karen and you. Too bad, try again, loser.";
+        "Karen: It's a double Blackjack, which means nobody won. Too bad, try again, loser.";
       // Player BJ = Player Wins
     } else if (playerHasBlackjack && dealerHasBlackjack == false) {
       outputMessage =
-        "Oh, you had a Blackjack? Let me check with the manager to see if your pesky ass cheated.";
+        "Karen: Oh, you had a Blackjack? Let me check with the manager to see if your pesky ass cheated.";
       // Dealer BJ = Player Loses
     } else if (dealerHasBlackjack && playerHasBlackjack == false) {
-      outputMessage = "Karen wins a Blackjack. TRY AGAIN NEXT TIME LOSER!";
+      outputMessage = "Karen: I win the Blackjack. TRY AGAIN NEXT TIME LOSER!";
     } else {
       //No BJ = Proceed
-      // Calculate total hand value
-      playerHandValue =
+      //Tabulate total points
+      /*displayPlayerPoints =
         Number(playerHand[0]["rank"]) + Number(playerHand[1]["rank"]);
-      dealerHandValue =
+      displayBankerPoints =
         Number(dealerHand[0]["rank"]) + Number(dealerHand[1]["rank"]);
+        */
 
-      //Tie
-      if (playerHandValue == dealerHandValue) {
-        outputMessage = "It's a tie. Try again peasant.";
-        //Player Win
-      } else if (playerHandValue > dealerHandValue) {
-        outputMessage =
-          "Oh, you won? Let me check with the manager, I think you obviously cheated.";
-        //Player Lose
-      } else {
-        outputMessage =
-          "I knew you were going to lose, but I didn't expect it to be THIISSS pathetic. Try harder loser.";
-      }
-      myOutputValue = `Player hand: ${playerHand[0]["name"]} of ${playerHand[0]["suit"]}, ${playerHand[1]["name"]} of ${playerHand[1]["suit"]}. <br> Karen's hand: ${dealerHand[0]["name"]} of ${dealerHand[0]["suit"]}, ${dealerHand[1]["name"]} of ${dealerHand[1]["suit"]}. <br> ${outputMessage}.`;
+      // Change to hit or stand mode
+      currentMode = HIT_OR_STAND_MODE;
+      console.log("Check current mode: " + currentMode);
+      outputMessage = `${displayPlayerAndDealerHands(
+        playerHand,
+        dealerHand
+      )} <br> ${displayHandsTotals(
+        playerHand,
+        dealerHand
+      )}<br><br>It's your move, key in 'hit' or 'stand' in the input box and click the submit button to proceed.`;
     }
+    return outputMessage;
   }
-  return myOutputValue;
+  // Hit or stand mode
+  if (currentMode == HIT_OR_STAND_MODE) {
+    // Hit
+    if (input == !"hit" && input == !"stand") {
+      outputMessage =
+        "Karen: Do you even understand English? Key in 'hit' or 'stand' ONLY. Try better. <br><br>" +
+        displayPlayerAndDealerHands(playerHand, dealerHand) +
+        "<br>" +
+        displayHandsTotals(playerHand, dealerHand);
+    } else if (input == "hit") {
+      playerHand.push(gameDeck.pop());
+      console.log("Check player hand ==>");
+      console.log(playerHand);
+      console.log(calculateTotalHandValue(playerHand));
+      if (calculateTotalHandValue(playerHand) > 21) {
+        outputMessage = `You have busted. <br> Karen smiles gleefully and mocks you. <br><br> ${displayPlayerAndDealerHands(
+          playerHand,
+          dealerHand
+        )} <br> ${displayHandsTotals(
+          playerHand,
+          dealerHand
+        )} <br><br> Try Again?`;
+      } else {
+        outputMessage = `You have chosen to hit. <br><br> ${displayPlayerAndDealerHands(
+          playerHand,
+          dealerHand
+        )} <br> ${displayHandsTotals(
+          playerHand,
+          dealerHand
+        )} <br> Do you want to hit or stand?`;
+      }
+      return outputMessage;
+    }
+    // Stand
+  } else if (input == "stand")
+    outputMessage = `You have chosen to stand. <br><br> ${displayPlayerAndDealerHands(
+      playerHand,
+      dealerHand
+    )} <br> ${displayHandsTotals(playerHand, dealerHand)}`;
+  calculateTotalHandValue(dealerHand);
+  calculateTotalHandValue(playerHand);
+
+  while (calculateTotalHandValue(dealerHand) < 17) {
+    //dealer to hit
+    dealerHand.push(gameDeck.pop());
+    calculateTotalHandValue(dealerHand);
+  }
+  if (calculateTotalHandValue(dealerHand) > 21) {
+    outputMessage =
+      "Karen Busted! <br> Karen: Oh, you won? Let me check with the manager, I think you obviously cheated<br>Refresh to play again.<br><br>" +
+      displayPlayerAndDealerHands(playerHand, dealerHand) +
+      "<br>" +
+      displayHandsTotals(playerHand, dealerHand);
+  } else if (
+    calculateTotalHandValue(playerHand) > calculateTotalHandValue(dealerHand)
+  ) {
+    outputMessage =
+      "Karen: Oh, you won? Let me check with the manager, I think you obviously cheated<br>Refresh to play again.<br><br>" +
+      displayPlayerAndDealerHands(playerHand, dealerHand) +
+      "<br>" +
+      displayHandsTotals(playerHand, dealerHand);
+  } else if (
+    calculateTotalHandValue(playerHand) == calculateTotalHandValue(dealerHand)
+  ) {
+    outputMessage =
+      "Karen: It's a tie. Try again peasant.<br>Refresh to play again.<br><br>" +
+      displayPlayerAndDealerHands(playerHand, dealerHand) +
+      "<br>" +
+      displayHandsTotals(playerHand, dealerHand);
+  } else {
+    outputMessage =
+      "Karen: I knew you were going to lose, but I didn't expect it to be THIISSS pathetic. Try harder loser.<br><br>" +
+      displayPlayerAndDealerHands(playerHand, dealerHand) +
+      "<br>" +
+      displayHandsTotals(playerHand, dealerHand);
+  }
+  return outputMessage;
 };
