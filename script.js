909d7ed4c846ff2a77b5d87659c9e518afff3b8d
Yabing <sherry.liuyabing@gmail.com>
2022-09-17 21:47:48 +0800

    Implement multi-player mode

--- script.js
+++ script.js
@@ -1,60 +1,123 @@
+// Default settings
 var gameMode = "new round";
 var gameDeck = shuffleDeck(makeSixDecks());
-var playersArray = ["Player", "Dealer"];
+var numberOfPlayers = 1;
+var playersArray = [];
 var dealerCards = [];
 var playerCards = [];
+var allPlayersCards = [];
 var result = "";
 var winner = "";
 var dealerHand = 0;
 var playerHand = 0;
+var allPlayersHand = [];
+var playerCounter = 0;
+var roundScoreBoard = {};
 
-var main = function (input) {
-  return playGame(input);
+var main = function (hitOrStand, playersArray) {
+  return playGame(hitOrStand, playersArray);
 };
 
-function playGame(input) {
-  if (gameMode == "new round") {
-    // Reset last round's cards and hand, if any
-    dealerHand = 0;
-    playerHand = 0;
-    dealerCards = [];
-    playerCards = [];
-    gameMode = "draw cards";
-    return "Drawing cards...";
-  } else if (gameMode == "draw cards") {
-    dealerCards.push(gameDeck.pop());
-    dealerCards.push(gameDeck.pop());
-    playerCards.push(gameDeck.pop());
-    playerCards.push(gameDeck.pop());
-    playerHand = sumCardsValue(playerCards);
-    dealerHand = sumCardsValue(dealerCards);
+// Generate the list of Players
+function generatePlayersArray(numberOfPlayers) {
+  var playersArray = [];
+  var counter = 0;
+  while (counter < numberOfPlayers) {
+    playersArray.push(`Player ${counter + 1}`);
+    counter += 1;
+  }
+  roundScoreBoard = generateRoundScoreBoard(playersArray);
+  printRoundScoreBoard(roundScoreBoard);
+  return playersArray;
+}
 
+function playGame(hitOrStand, playersArray) {
+  // Reset last round's cards and hand, if any
+  if (gameMode == "new round") {
+    dealerHand = 0;
+    dealerCards = [];
+    allPlayersHand = [];
+    allPlayersCards = [];
+    playerCounter = 0;
+    gameMode = "deal cards";
+    roundScoreBoard = generateRoundScoreBoard(playersArray);
+    printOutput("Dealing cards... Click 'next' to check your hands.");
+  } else if (gameMode == "deal cards") {
+    // Dealer draws cards
+    dealerCards.push(gameDeck.pop());
+    dealerCards.push(gameDeck.pop());
+    dealerHand = sumCardsValue(dealerCards);
     console.log(
-      `Dealer drew ${dealerCards[0].suit} ${dealerCards[0].name} ${dealerCards[1].suit} ${dealerCards[1].name}`
+      `Dealer drew ${dealerCards[0].suit} ${dealerCards[0].name} ${dealerCards[1].suit} ${dealerCards[1].name}. Dealer's hand: ${dealerHand}.`
     );
-    console.log(
-      `Player drew ${playerCards[0].suit} ${playerCards[0].name} ${playerCards[1].suit} ${playerCards[1].name}`
-    );
+    // Players take turn to draw cards
+    for (
+      let playerCounter = 0;
+      playerCounter < playersArray.length;
+      playerCounter += 1
+    ) {
+      // Reset before a player draws
+      playerCards = [];
+      playerHand = 0;
+      // Each player's cards are stored as an array of objects
+      playerCards.push(gameDeck.pop());
+      playerCards.push(gameDeck.pop());
+      // All players' cards are stored as an array of arrays
+      // E.g. [[player1-card1, player1-card2],[player2-card1, player2-card2]]
+      allPlayersCards.push(playerCards);
+      playerHand = sumCardsValue(playerCards);
+      allPlayersHand.push(playerHand);
+      console.log(
+        `${playersArray[playerCounter]} got ${playerCards[0].suit} ${playerCards[0].name} ${playerCards[1].suit} ${playerCards[1].name}`
+      );
+    }
+    console.log(`All players' hand: ${allPlayersHand}`);
     gameMode = "hit or stand";
     hitButton.disabled = false;
     standButton.disabled = false;
-    return `One of Dealer's card is ${dealerCards[0].suit} ${dealerCards[0].name}. <br>Player drew ${playerCards[0].suit} ${playerCards[0].name} and ${playerCards[1].suit} ${playerCards[1].name}; Player's score is ${playerHand}. <br>Player - hit or stand?`;
-  } else if (gameMode == "hit or stand") {
-    if (input != "hit" && input != "stand") {
-      return "Please select either 'hit' or 'stand'.";
-    } else if (input == "hit") {
+    printOutput(
+      `One of Dealer's cards is ${dealerCards[0].suit} ${dealerCards[0].name}. <br>Here's everyone's hand so far: ${allPlayersHand}. <br>Player 1 - select hit or stand.`
+    );
+  }
+
+  // Players take turns to hit or stand
+  else if (gameMode == "hit or stand") {
+    if (hitOrStand != "hit" && hitOrStand != "stand") {
+      printOutput("Please select either 'hit' or 'stand'.");
+    } else if (hitOrStand == "hit") {
       var hitCard = gameDeck.pop();
-      playerCards.push(hitCard);
-      playerHand = sumCardsValue(playerCards);
-      console.log(`Player drew ${hitCard.suit} ${hitCard.name}.`);
-      return `Player drew ${hitCard.suit} ${hitCard.name}. Player's score is ${playerHand}. <br>Player - hit or stand?`;
-    } else if (input == "stand") {
-      hitButton.disabled = true;
-      standButton.disabled = true;
-      gameMode = "Dealer plays";
-      return "Player chose to stand. Dealer's turn.";
+      allPlayersCards[playerCounter].push(hitCard);
+      allPlayersHand[playerCounter] = sumCardsValue(
+        allPlayersCards[playerCounter]
+      );
+      console.log(
+        `${playersArray[playerCounter]} drew ${hitCard.suit} ${hitCard.name}.`
+      );
+      printOutput(
+        `${playersArray[playerCounter]} drew ${hitCard.suit} ${hitCard.name}. ${playersArray[playerCounter]}'s hand is ${allPlayersHand[playerCounter]}. <br>${playersArray[playerCounter]} - hit or stand?`
+      );
+    } else if (hitOrStand == "stand") {
+      playerCounter += 1;
+      if (playerCounter < playersArray.length) {
+        printOutput(
+          `${playersArray[playerCounter - 1]} chose to stand. ${
+            playersArray[playerCounter]
+          }'s turn - your current hand is ${allPlayersHand[playerCounter]}.`
+        );
+      } else {
+        hitButton.disabled = true;
+        standButton.disabled = true;
+        gameMode = "Dealer plays";
+        printOutput(
+          `${
+            playersArray[playerCounter - 1]
+          } chose to stand. Click 'next' to let Dealer have their turn.`
+        );
+      }
     }
-  } else if (gameMode == "Dealer plays") {
+  }
+  // Dealer hit or stand
+  else if (gameMode == "Dealer plays") {
     dealerHand = sumCardsValue(dealerCards);
     while (dealerHand < 17) {
       var hitCard = gameDeck.pop();
@@ -65,54 +128,59 @@ function playGame(input) {
       );
     }
     gameMode = "check results";
-    return `Dealer's turn ended. Click next to tally results.`;
-  } else if (gameMode == "check results") {
+    printOutput(`Dealer's turn ended. Click 'next' to tally results.`);
+  }
+
+  // Tally results
+  else if (gameMode == "check results") {
     // Check for Blackjack
-    if (checkBlackjack(dealerCards) || checkBlackjack(playerCards)) {
-      if (checkBlackjack(dealerCards) && checkBlackjack(playerCards)) {
-        result = "double blackjack";
-        winner = "nobody wins";
-      } else if (checkBlackjack(dealerCards)) {
-        result = "blackjack";
-        winner = "Dealer";
-      } else {
-        result = "blackjack";
-        winner = "Player";
+    for (
+      let playerCounter = 0;
+      playerCounter < playersArray.length;
+      playerCounter += 1
+    ) {
+      if (checkBlackjack(allPlayersCards[playerCounter])) {
+        roundScoreBoard[playersArray[playerCounter]] = "Blackjack";
       }
     }
+    if (checkBlackjack(dealerCards)) {
+      roundScoreBoard["Dealer"] = "Blackjack";
+    }
+
     // Check for bust
-    else if (dealerHand > 21 || playerHand > 21) {
-      if (dealerHand > 21 && playerHand > 21) {
-        result = "double bust";
-        winner = "nobody wins";
-      } else if (dealerHand > 21 && playerHand <= 21) {
-        result = "bust";
-        winner = "Player";
-      } else if (playerHand > 21 && dealerHand <= 21) {
-        result = "bust";
-        winner = "Dealer";
+    for (
+      let playerCounter = 0;
+      playerCounter < playersArray.length;
+      playerCounter += 1
+    ) {
+      if (allPlayersHand[playerCounter] > 21) {
+        roundScoreBoard[playersArray[playerCounter]] = "Bust";
       }
     }
-    // If nobody has Blackjack and nobody bust
-    else {
-      if (dealerHand == playerHand) {
-        result = "tie";
-        winner = "nobody wins";
-      } else if (dealerHand > playerHand) {
-        result = "win";
-        winner = "Dealer";
-      } else {
-        result = "win";
-        winner = "Player";
+    if (dealerHand > 21) {
+      roundScoreBoard["Dealer"] = "Bust";
+    }
+
+    // Tally rest of the scores
+    for (
+      let playerCounter = 0;
+      playerCounter < playersArray.length;
+      playerCounter += 1
+    ) {
+      if (roundScoreBoard[playersArray[playerCounter]] == "") {
+        roundScoreBoard[playersArray[playerCounter]] =
+          allPlayersHand[playerCounter];
       }
     }
-    console.log(`dealer ${dealerHand} player ${playerHand}`);
-    console.log(`${result}, ${winner}`);
+    if (roundScoreBoard["Dealer"] == "") {
+      roundScoreBoard["Dealer"] = dealerHand;
+    }
+    printRoundScoreBoard(roundScoreBoard);
+    printOutput(
+      "This round's results are out! Click 'next' to start another round."
+    );
+    // Reset for new round
     gameMode = "new round";
-    if (gameDeck.length < 60) {
-      gameDeck = shuffleDeck(makeSixDecks());
-    }
-    return `The Dealer's hand is ${dealerHand} and the Player's hand is ${playerHand}. We have a ${result}! The winner is... ${winner}!`;
   }
 }
 
@@ -196,4 +264,51 @@ function sumCardsValue(cards) {
     }
   }
   return sum;
+}
+
+function generateRoundScoreBoard(playersArray) {
+  var counter = 0;
+  var roundScoreBoard = {};
+  while (counter < playersArray.length) {
+    roundScoreBoard[playersArray[counter]] = "";
+    counter += 1;
+  }
+  roundScoreBoard["Dealer"] = "";
+  return roundScoreBoard;
+}
+
+function printRoundScoreBoard(scoreBoard) {
+  var scoreBoardBox = document.querySelector("#round-results");
+  var output = "";
+  for (var entry in scoreBoard) {
+    output += `${entry}: ${scoreBoard[entry]}<br>`;
+  }
+  scoreBoardBox.innerHTML = output;
+}
+
+function generateLeaderboard(numberOfPlayers) {
+  var counter = 0;
+  var leaderBoard = {};
+  while (counter < numberOfPlayers) {
+    leaderBoard[`Player ${counter + 1}`] = 0;
+    counter += 1;
+  }
+  leaderBoard["Dealer"] = 0;
+  return leaderBoard;
+}
+
+// Update the leaderboard player by player
+function updateLeaderboard(winningPlayer) {
+  leaderBoard[winningPlayer] += 1;
+  var scoreSpace = document.querySelector("#scores");
+  var output = "";
+  for (var entry in leaderBoard) {
+    output += `${entry}: ${leaderBoard[entry]}<br>`;
+  }
+  scoreSpace.innerHTML = output;
+}
+
+function printOutput(output) {
+  var outputBox = document.querySelector("#output-div");
+  outputBox.innerHTML = output;
 }
