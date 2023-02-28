8bfa7f371376ce5459d3fa61236e33eb61bec61d
wong834 <weiyin1116@gmail.com>
2023-02-28 15:21:22 +0800

    added function to make card deck

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
+var makeDeck = function () {
+  //empty deck array
+  var cardDeck = [];
+  //initialise array with 4 suits
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+
+  //loop over suits array
+  for (var suitIndex = 0; suitIndex < suits.length; suitIndex++) {
+    //store current suit in variable
+    var currentSuit = suits[suitIndex];
+
+    //loop from 1 to 13 to create all cards for a given suit
+    for (var rankCounter = 1; rankCounter <= 13; rankCounter++) {
+      //default card name is same as its rank
+      var cardName = rankCounter;
+
+      //if rank is 1, 11, 12, 13
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      //create a new card with name, suit and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+    }
+  }
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
