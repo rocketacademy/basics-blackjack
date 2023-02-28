e8e1cf85119d8809d77c635457fa79fe3ca6471f
Alayne1 <alaynelxr@gmail.com>
2023-02-28 20:44:35 +0800

    create and shuffle deck, draw two cards for each player and display them

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Rocket Academy Coding Basics: Blackjack
+
+Sequential things to do
+
+1. Create deck of cards using the loop function. This will be a helper function
+2. Shuffle deck of cards using the function. This will be a helper function.
+3. Draw two cards for player
+4. Draw two cards for dealer
+5. Sum up the two cards
+6. Use if else if to handle the conditionals of game
+7. Change mode so that player can input action to hit or to stand
+8. Draw additional card if hit, else move on to dealer
+9. Dealer must hit if hand is below 17, else they will stand.
+10. Sum up cards for player
+11. Sum up cards for dealer
+12. Use if else to handle conditionals, report outcome
+13. Rerun game
+
+## Break it down
+
+Attempt the project in parts to make sure each section works before adding more to it.
+
+### Commit 1 Goal
+
+- Create deck
+- Shuffle deck
+- Draw two cards for each player and display them in output
+
+### Commit 2 Goal
+
+- Sum up the card value for each player
+- Display the sums as output
+- Compare the cards and display one out of 3 outcomes: tie, blackjack win or highest hand win
+
+### Commit 3 Goal
+
+- Player can choose to hit or stand
+- Draw card for player
+- Add card to sum
+-
--- script.js
+++ script.js
@@ -1,4 +1,108 @@
+// Blackjack Project
+
+/* RULES */
+
+/* There will be two players. 
+One human and one computer (for the Base solution).
+The computer will always be the dealer.
+Each player gets dealt two cards to start.
+The player goes first, and decides if they want to hit (draw a card) or stand (end their turn).
+The dealer has to hit if their hand is below 17.
+Each players' score is the total of their card ranks. Jacks/Queen/Kings are 10. Aces can be 1 or 11.
+The player who is closer to, but not above 21 wins the hand.
+*/
+
+// Helper function to create a deck of cards using a loop
+
+var makeDeck = function () {
+  var cardDeck = [];
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    var currentSuit = suits[suitIndex];
+    var rankCounter = 1;
+    // rankCounter starts from 1, otherwise it would start from 0
+
+    while (rankCounter <= 13) {
+      var cardName = rankCounter;
+      // set cardName as rankCounter. i.e. 2 of hearts will have 2 as name and 2 as rank
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      cardDeck.push(card);
+
+      rankCounter += 1;
+    }
+
+    suitIndex += 1;
+  }
+
+  return cardDeck;
+};
+
+// Helper function to get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+// the length of card deck will be used as "max". Hence 42 cards is the total/max number of cards in the deck
+
+// Helper function to shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    var randomIndex = getRandomIndex(cardDeck.length);
+    var randomCard = cardDeck[randomIndex];
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    currentIndex = currentIndex + 1;
+  }
+  return cardDeck;
+};
+
+// Helper function to draw cards
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var deck = makeDeck();
+  var shuffledDeck = shuffleCards(deck);
+  var dealerArr = [];
+  var playerArr = [];
+  var dealerHandArr = [];
+  var playerHandArr = [];
+  var dealerCard1 = shuffledDeck.pop();
+  dealerArr.push(dealerCard1);
+  dealerHandArr.push(dealerCard1.rank);
+  var dealerCard2 = shuffledDeck.pop();
+  dealerArr.push(dealerCard2);
+  dealerHandArr.push(dealerCard2.rank);
+  var playerCard1 = shuffledDeck.pop();
+  playerArr.push(playerCard1);
+  playerHandArr.push(playerCard1.rank);
+  var playerCard2 = shuffledDeck.pop();
+  playerArr.push(playerCard2);
+  playerHandArr.push(playerCard2.rank);
+  // console.log(dealerArr, "this is the dealer's cards");
+  // console.log(playerArr, "this is the player's cards");
+  // console.log(dealerHandArr, "dealer hand array");
+  // console.log(playerHandArr, "player hand arr");
+  var myOutputValue = `This is the dealer's cards:${dealerHandArr}. This is the player's cards. ${playerHandArr}`;
+
   return myOutputValue;
 };
+
+// ideas: use a for loop to draw cards multiple times and push them into an array?
