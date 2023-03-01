b54ca3ad23edb2296008b26c93dc08f0838d8b71
wong834 <weiyin1116@gmail.com>
2023-03-01 16:48:29 +0800

    added function to shuffle cards

--- script.js
+++ script.js
@@ -31,8 +31,34 @@ var makeDeck = function () {
         suit: currentSuit,
         rank: rankCounter,
       };
+
+      //push card into deck
+      cardDeck.push(card);
     }
   }
+  //return completed card deck
+  return cardDeck;
+};
+
+var getRandomIndex = function (cardsNum) {
+  return Math.floor(Math.random() * cardsNum);
+};
+
+var shuffleCards = function (cardDeck) {
+  //loop over the card deck array once
+  for (var currentIndex = 0; currentIndex < cardDeck.length; currentIndex++) {
+    //select random index 0-51
+    var randomIndex = getRandomIndex(cardDeck.length);
+    //select random card according to chosen index
+    var randomCard = cardDeck[randomIndex];
+    //select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    //swap positions of both cards
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+  }
+  //return shuffled deck
+  return cardDeck;
 };
 
 var main = function (input) {
