b607213c6cc6f4738dc8089eb9d2a40d10095c67
wong834 <weiyin1116@gmail.com>
2023-03-02 17:44:58 +0800

    added a couple of functions - count cards sum, make cardsList

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var drawCards = "GAMEMODE drawCards";
+var hitOrStand = "GAMEMODE hitOrStand";
+var gameMode = drawCards;
+var deck;
+var shuffledDeck;
+var playerCards = [];
+var compCards = [];
+
 var makeDeck = function () {
   //empty deck array
   var cardDeck = [];
@@ -10,19 +18,23 @@ var makeDeck = function () {
     var currentSuit = suits[suitIndex];
 
     //loop from 1 to 13 to create all cards for a given suit
-    for (var rankCounter = 1; rankCounter <= 13; rankCounter++) {
+    for (var counter = 1; counter <= 13; counter++) {
       //default card name is same as its rank
+      var rankCounter = counter;
       var cardName = rankCounter;
 
       //if rank is 1, 11, 12, 13
       if (cardName == 1) {
-        cardName = "ace";
+        cardName = "ace of";
       } else if (cardName == 11) {
-        cardName = "jack";
+        rankCounter = 10;
+        cardName = "jack of";
       } else if (cardName == 12) {
-        cardName = "queen";
+        rankCounter = 10;
+        cardName = "queen of";
       } else if (cardName == 13) {
-        cardName = "king";
+        rankCounter = 10;
+        cardName = "king of";
       }
 
       //create a new card with name, suit and rank
@@ -61,7 +73,65 @@ var shuffleCards = function (cardDeck) {
   return cardDeck;
 };
 
+var draw2Cards = function (shuffledDeck) {
+  var card1 = shuffledDeck.pop();
+  var card2 = shuffledDeck.pop();
+  var array = [];
+  array.push(card1, card2);
+  return array;
+};
+
+var listOfCards = function (cards) {
+  var cardsList = "";
+  for (var cardsCounter = 0; cardsCounter < cards.length; cardsCounter++) {
+    cardsList +=
+      cards[cardsCounter].name + " " + cards[cardsCounter].suit + "<br>";
+  }
+  return cardsList;
+};
+
+var sumCounter = function (cards) {
+  var sum = 0;
+  var aceNum = 0;
+  for (var counter = 0; counter < cards.length; counter++) {
+    var currentCard = cards[counter];
+    if (currentCard.rank == 1) {
+      aceNum += 1;
+      sum += 1;
+    }
+    sum += currentCard.rank;
+  }
+
+  if (sum > 21 && aceNum > 0) {
+    for (var aceCounter = 0; aceCounter < aceNum; aceCounter++) {
+      sum -= 10;
+      if (sum <= 21) {
+        break;
+      }
+    }
+  }
+  return sum;
+};
+
 var main = function (input) {
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  if (gameMode == drawCards) {
+    deck = makeDeck();
+    shuffledDeck = shuffleCards(deck);
+    playerCards = draw2Cards(shuffledDeck);
+    compCards = draw2Cards(shuffledDeck);
+    gameMode = hitOrStand;
+    return `your cards are: <br><br> ${listOfCards(
+      playerCards
+    )} <br><br> Enter 'hit' or 'stand'!`;
+  }
+
+  if (gameMode == hitOrStand) {
+    if (input != "hit" && input != "stand") {
+      return `Please ONLY input 'hit' or 'stand'!😡`;
+    }
+    if (input == "hit") {
+      cardExtra = shuffledDeck.pop();
+      playerCards.push(cardExtra);
+    }
+  }
 };
