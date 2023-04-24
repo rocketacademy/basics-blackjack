f9ad8b32ba88aa0cd9ad2fb2a33a88686beae9b1
quppiehy <sunhuiyi@gmail.com>
2023-04-24 19:35:47 +0800

    Added functions

--- ui.js
+++ ui.js
@@ -2,9 +2,9 @@ var button = document.querySelector("#deal-button");
 button.addEventListener("click", function () {
   // Set result to input value
   var result = main("deal");
-
   // Display result in output element
   var output = document.querySelector("#output-div");
+
   output.innerHTML = result;
 });
 
