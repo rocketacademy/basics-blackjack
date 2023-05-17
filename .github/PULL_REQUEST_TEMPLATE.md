9e78658ac8d4f91b2b93cea2dea166eb6a074959
Snowlyst <tclam.snow@gmail.com>
2023-05-18 03:13:19 +0800

    submitting 1st working version

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -3,17 +3,18 @@ Please fill out the survey before submitting the pull request. Thanks!
 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 
 How many hours did you spend on this assignment?
-
+6
 Please fill in one error and/or error message you received while working on this assignment.
-
+logic errors when i was trying to convert the aces to a smaller version
 What part of the assignment did you spend the most time on?
-
+trying to randomize 2 aces to see if one ace value would be reduced to 1
 Comfort Level (1-5):
-
+5
 Completeness Level (1-5):
-
+4
 What did you think of this deliverable?
-
+no comments
 Is there anything in this code that you feel pleased about?
-
+nothing much in general
 What's one aspect of your code you would like specific, elaborate feedback on?
+whichever you think is a disaster
--- script.js
+++ script.js
@@ -92,52 +92,127 @@ var shuffleCards = function (cardDeck) {
 /* things to do
 1) edit the card editor to add in points for blackjack score counting (D)
 3) plan for states
-  a) "initialize" should make it such that when you click submit. draws 2 cards, shows player the results and total score.
-  b) "playerMidgame" should asks you to hit or stand based off the results. plays a card if hit, shows the results and total score.
-   ace will be lowered to score 10 if a 3rd card is hit. check for bust, if bust and in the presence of ace, reduce ace to 1.
+  a) "initialize" should make it such that when you click submit. draws 2 cards, shows player the results and total score. tells you to hit or stand.
+  b) "playerMidgame" should have 2 inputs, hit or stand. plays a card if hit, shows the results and total score.
+   ace will be lowered to score 10 if a card is hit. check for bust, if bust and in the presence of ace, reduce ace to 1.
    if stand, move game phase to "computerMidgame"
   c) "computerMidgame" should basically make the cpu hit until the cpu is either bigger than player, or busts. then moves to "result" phase  
-  d) "result" phase shows who has won. it also resets the deck, the hand and all required stuff on submit, moves it back to "initialize".
+  d) "result" phase shows who has won. moves it to cleaning phase
+  e) "cleaning" phase will resets the deck, the hand and all required stuff on submit, moves it back to "initialize"
 */
+
 let deck = shuffleCards(makeDeck());
 let gameState = "initialize";
 let playerHand = [];
 let computerHand = [];
 let playerScore = 0;
 let computerScore = 0;
+let playerBustStatus = false;
+let computerBustStatus = false;
+let playerWonTimes = 0;
+let computerWonTimes = 0;
 //main game
-function main(input) {
-  if ((gameState = "initialize")) {
+function main(gameInput) {
+  let input = gameInput.toLowerCase();
+  if (gameState === "initialize") {
     if (input !== "") {
       return `Hello Player! Please do not type in anything and just press submit to start a game of Blackjack!`;
+    } else {
+      dealInitialHands();
+      console.log(playerHand, computerHand);
+      if (checkInitialHands(playerHand, computerHand) === "double blackjack") {
+        gameState = "result";
+        return "It is a double blackjack! It is a draw!";
+      } else if (
+        checkInitialHands(playerHand, computerHand) === "player blackjack"
+      ) {
+        gameState = "result";
+        return "You Win! You have a blackjack!";
+      } else if (
+        checkInitialHands(playerHand, computerHand) === "computer blackjack"
+      ) {
+        gameState = "result";
+        return "You Lose! Computer has a blackjack! ";
+      } else if (
+        checkInitialHands(playerHand, computerHand === "game continue")
+      ) {
+        // gameState = "playerHitOrStand";
+        checkForAces(playerHand);
+        playerScore = calculatePlayerScore(playerHand);
+        computerScore = calculateComputerScore(computerHand);
+        gameState = "playerMidgame";
+        console.log(playerScore);
+        console.log(computerScore);
+        return outputMessage();
+      }
     }
-  }
-  //when i click submit. player & computer draws 2 cards, clear off any winning conditions, then i will return the 2 cards of the player, asking for a hit or stand.
-  dealInitialHands();
-  if (checkInitialHands(playerHand, computerHand) === "double blackjack") {
-    gameState = "result";
-    return "It is a double blackjack! It is a draw!";
-  } else if (
-    checkInitialHands(playerHand, computerHand) === "player blackjack"
-  ) {
-    gameState = "result";
-    return "You Win! You have a blackjack!";
-  } else if (
-    checkInitialHands(playerHand, computerHand) === "computer blackjack"
-  ) {
-    gameState = "result";
-    return "You Lose! Computer has a blackjack! ";
-  } else if (checkInitialHands(playerHand, computerHand === "game continue")) {
-    // gameState = "playerHitOrStand";
-    playerScore = calculatePlayerScore(playerHand);
-    computerScore = calculateComputerScore(computerHand);
-    console.log(playerHand, computerHand);
-    console.log(playerScore);
-    console.log(computerScore);
-    return `The Cards that you have drew are as follows..${callCards()}<br>The total sum is currently ${playerScore}.<br> Please decide if you would like to Hit or Stand!`;
+  } else if (gameState === "playerMidgame") {
+    if (input !== "h" && input !== "s") {
+      return `Please input only hit or stand!<br>${outputMessage()}`;
+    } else if (input === "h") {
+      playerHand.push(deck.pop());
+      checkForAces(playerHand);
+      playerScore = calculatePlayerScore(playerHand);
+      console.log(playerScore, playerHand);
+      if (playerScore > 21) {
+        gameState = "computerMidgame";
+        playerBustStatus = true;
+        return `You drew a ${playerHand[playerHand.length - 1].name} of ${
+          playerHand[playerHand.length - 1].suit
+        }! You BUSTED at💣${playerScore}💣!<br><br> The Computer will now take it's turn!<br> ${outputMessageForCPU()} Click Submit to proceed! `;
+      }
+      return `${outputMessage()}`;
+    } else if (input === "s") {
+      gameState = "computerMidgame";
+      return `You have chosen to stand at ${playerScore} points!<br><br> The Computer will now take it's turn!<br> ${outputMessageForCPU()}`;
+    }
+  } else if (gameState === "computerMidgame") {
+    console.log(computerScore, computerHand);
+    if (computerScore > 21) {
+      gameState = "result";
+      computerBustStatus = true;
+      return `The Computer drew ${
+        computerHand[computerHand.length - 1].name
+      } of ${
+        computerHand[computerHand.length - 1].suit
+      }! The Computer BUSTED at 💣${computerScore}💣!<br><br> Click Submit to proceed!`;
+    } else if (computerScore < 17) {
+      computerHand.push(deck.pop());
+      checkForAces(computerHand);
+      computerScore = calculateComputerScore(computerHand);
+      return outputMessageForCPU();
+    } else if (computerScore > 16 && computerScore < 22) {
+      gameState = "result";
+      return `The Computer has chosen to stand at ${computerScore}! Click Submit to Proceed!`;
+    } else {
+      return "ERROR: LOGIC WRONG PLEASE CHECK";
+    }
+  } else if (gameState === "result") {
+    gameState = "scoreboard";
+    if (playerBustStatus && computerBustStatus) {
+      return `Both players have 💣BUSTED💣! It is a draw!<br>You scored ${playerScore}!<br> The Computer scored ${computerScore}!<br>Click Submit to head to the scoreboard!`;
+    } else if (!playerBustStatus && computerBustStatus) {
+      playerWonTimes += 1;
+      return `You won!<br> You scored ${playerScore}!<br>The Computer scored 💣💣${computerScore}💣💣!<br>Click Submit to head to the scoreboard!`;
+    } else if (playerBustStatus && !computerBustStatus) {
+      computerWonTimes += 1;
+      return `The Computer won!<br> You scored 💣💣${playerScore}💣💣!<br>The Computer scored ${computerScore}!<br>Click Submit to head to the scoreboard!`;
+    } else if (playerScore === computerScore) {
+      return `Both players have scored ${playerScore}!<br> It is a draw!<br> Click Submit to head to the scoreboard!`;
+    } else if (playerScore > computerScore) {
+      playerWonTimes += 1;
+      return `You won!<br> You scored ${playerScore}!<br>The Computer scored ${computerScore}! Click Submit to head to the scoreboard!`;
+    } else if (playerScore < computerScore) {
+      computerWonTimes += 1;
+      return `The Computer won!<br> You scored ${playerScore}!<br>The Computer scored ${computerScore}! Click Submit to head to the scoreboard! `;
+    } else {
+      return "THE RESULT LOGIC WENT WRONG";
+    }
+  } else if (gameState === "scoreboard") {
+    refreshEverything();
+    return `Scoreboard- You : Computer <br> ${playerWonTimes} : ${computerWonTimes}<br> Click Submit to start a new round!`;
   }
 }
-
 // dealing initial hands small function.
 function dealInitialHands() {
   for (let i = 0; i < 2; i++) {
@@ -170,17 +245,31 @@ function checkInitialHands(playerHand, computerHand) {
 }
 // function to reset the whole game
 function refreshEverything() {
+  gameState = "initialize";
   deck = [];
   deck = shuffleCards(makeDeck());
   playerHand = [];
   computerHand = [];
+  playerScore = 0;
+  computerScore = 0;
+  playerBustStatus = false;
+  computerBustStatus = false;
 }
 
-//function to display all cards depending on current size of hand
+//function to display all players cards depending on current size of hand
 function callCards() {
   let message = "";
   for (i = 0; i < playerHand.length; i += 1) {
     message += `<br>${playerHand[i].name} of ${playerHand[i].suit} [${playerHand[i].score}]`;
+  }
+  return message;
+}
+
+// function to display all cpu cards depending on current size of hand
+function callCPUCards() {
+  let message = "";
+  for (i = 0; i < computerHand.length; i += 1) {
+    message += `<br>${computerHand[i].name} of ${computerHand[i].suit} [${computerHand[i].score}]`;
   }
   return message;
 }
@@ -201,4 +290,26 @@ function calculateComputerScore(computerHand) {
     score += computerHand[i].score;
   }
   return score;
+}
+//function to check for aces, and reduce ONLY one ace each time if there are multiple aces.
+function checkForAces(playerHand) {
+  let temporaryPlayerScore = calculatePlayerScore(playerHand);
+  if (temporaryPlayerScore > 21) {
+    for (let i = 0; i < playerHand.length; i += 1) {
+      if (playerHand[i].name === "Ace") {
+        playerHand[i].name = "🂡 Ace";
+        playerHand[i].score = 1;
+        break;
+      }
+    }
+  }
+}
+//output message for player turn
+function outputMessage() {
+  return `The Cards that you drew are as follows..${callCards()}<br>The total sum is currently ${playerScore}.<br> Please decide if you would like to Hit or Stand! Submit h to hit and submit s to stand!`;
+}
+
+//output message for computer turn
+function outputMessageForCPU() {
+  return `The Cards that the CPU drew are as follows.. ${callCPUCards()}<br>The total sum is currently ${computerScore}. <br> Please click on submit to proceed!`;
 }
