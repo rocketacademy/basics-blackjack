49bf05907ed3fcbbc2aad1daff404f882f246d06
heahminan <zerocustom_84@yahoo.com.sg>
2023-06-07 17:40:30 +0800

    updated deck attribute from rank to score

--- script.js
+++ script.js
@@ -2,295 +2,314 @@ var deck = [
   {
     name: "ace",
     suit: "hearts",
-    rank: 1,
+    score: 1,
   },
   {
     name: "2",
     suit: "hearts",
-    rank: 2,
+    score: 2,
   },
   {
     name: "3",
     suit: "hearts",
-    rank: 3,
+    score: 3,
   },
   {
     name: "4",
     suit: "hearts",
-    rank: 4,
+    score: 4,
   },
   {
     name: "5",
     suit: "hearts",
-    rank: 5,
+    score: 5,
   },
   {
     name: "6",
     suit: "hearts",
-    rank: 6,
+    score: 6,
   },
   {
     name: "7",
     suit: "hearts",
-    rank: 7,
+    score: 7,
   },
   {
     name: "8",
     suit: "hearts",
-    rank: 8,
+    score: 8,
   },
   {
     name: "9",
     suit: "hearts",
-    rank: 9,
+    score: 9,
   },
   {
     name: "10",
     suit: "hearts",
-    rank: 10,
+    score: 10,
   },
   {
     name: "jack",
     suit: "hearts",
-    rank: 11,
+    score: 10,
   },
   {
     name: "queen",
     suit: "hearts",
-    rank: 12,
+    score: 10,
   },
   {
     name: "king",
     suit: "hearts",
-    rank: 13,
+    score: 10,
   },
   {
     name: "ace",
     suit: "diamonds",
-    rank: 1,
+    score: 1,
   },
   {
     name: "2",
     suit: "diamonds",
-    rank: 2,
+    score: 2,
   },
   {
     name: "3",
     suit: "diamonds",
-    rank: 3,
+    score: 3,
   },
   {
     name: "4",
     suit: "diamonds",
-    rank: 4,
+    score: 4,
   },
   {
     name: "5",
     suit: "diamonds",
-    rank: 5,
+    score: 5,
   },
   {
     name: "6",
     suit: "diamonds",
-    rank: 6,
+    score: 6,
   },
   {
     name: "7",
     suit: "diamonds",
-    rank: 7,
+    score: 7,
   },
   {
     name: "8",
     suit: "diamonds",
-    rank: 8,
+    score: 8,
   },
   {
     name: "9",
     suit: "diamonds",
-    rank: 9,
+    score: 9,
   },
   {
     name: "10",
     suit: "diamonds",
-    rank: 10,
+    score: 10,
   },
   {
     name: "jack",
     suit: "diamonds",
-    rank: 11,
+    score: 10,
   },
   {
     name: "queen",
     suit: "diamonds",
-    rank: 12,
+    score: 10,
   },
   {
     name: "king",
     suit: "diamonds",
-    rank: 13,
+    score: 10,
   },
   {
     name: "ace",
     suit: "clubs",
-    rank: 1,
+    score: 1,
   },
   {
     name: "2",
     suit: "clubs",
-    rank: 2,
+    score: 2,
   },
   {
     name: "3",
     suit: "clubs",
-    rank: 3,
+    score: 3,
   },
   {
     name: "4",
     suit: "clubs",
-    rank: 4,
+    score: 4,
   },
   {
     name: "5",
     suit: "clubs",
-    rank: 5,
+    score: 5,
   },
   {
     name: "6",
     suit: "clubs",
-    rank: 6,
+    score: 6,
   },
   {
     name: "7",
     suit: "clubs",
-    rank: 7,
+    score: 7,
   },
   {
     name: "8",
     suit: "clubs",
-    rank: 8,
+    score: 8,
   },
   {
     name: "9",
     suit: "clubs",
-    rank: 9,
+    score: 9,
   },
   {
     name: "10",
     suit: "clubs",
-    rank: 10,
+    score: 10,
   },
   {
     name: "jack",
     suit: "clubs",
-    rank: 11,
+    score: 10,
   },
   {
     name: "queen",
     suit: "clubs",
-    rank: 12,
+    score: 10,
   },
   {
     name: "king",
     suit: "clubs",
-    rank: 13,
+    score: 10,
   },
   {
     name: "ace",
     suit: "spades",
-    rank: 1,
+    score: 1,
   },
   {
     name: "2",
     suit: "spades",
-    rank: 2,
+    score: 2,
   },
   {
     name: "3",
     suit: "spades",
-    rank: 3,
+    score: 3,
   },
   {
     name: "4",
     suit: "spades",
-    rank: 4,
+    score: 4,
   },
   {
     name: "5",
     suit: "spades",
-    rank: 5,
+    score: 5,
   },
   {
     name: "6",
     suit: "spades",
-    rank: 6,
+    score: 6,
   },
   {
     name: "7",
     suit: "spades",
-    rank: 7,
+    score: 7,
   },
   {
     name: "8",
     suit: "spades",
-    rank: 8,
+    score: 8,
   },
   {
     name: "9",
     suit: "spades",
-    rank: 9,
+    score: 9,
   },
   {
     name: "10",
     suit: "spades",
-    rank: 10,
+    score: 10,
   },
   {
     name: "jack",
     suit: "spades",
-    rank: 11,
+    score: 10,
   },
   {
     name: "queen",
     suit: "spades",
-    rank: 12,
+    score: 10,
   },
   {
     name: "king",
     suit: "spades",
-    rank: 13,
+    score: 10,
   },
 ];
 var shuffledDeck;
+var gameBustScore = 21;
 var gameMode = "shuffled cards";
-var computerCardDeck = [];
 var numberOfCardsToDraw;
+var computerCardDeck = [];
+var computerCurrentCardScore;
 var playerCardDeck = [];
+var playerCurrentCardScore;
+
+// cards 2 through 10 are scored using their face value
+// jack, queen and king are scored 10
+// ace can be scored 1 or 11
 
 var main = function (input) {
   if (gameMode == "shuffled cards") {
     shuffledDeck = shuffleCards(deck);
-    gameMode = "compare results";
+    gameMode = "compare card score";
     console.log(shuffledDeck);
   }
 
-  if (gameMode == "compare results") {
+  if (gameMode == "compare card score") {
     // assign 2 cards to computer
     numberOfCardsToDraw = 2;
     for (let i = 0; i < numberOfCardsToDraw; i++) {
       var currentCard = shuffledDeck.pop();
       computerCardDeck.push(currentCard);
     }
-    console.log(computerCardDeck);
+    // console.log(computerCardDeck);
+    // assign 2 cards to player
     for (let i = 0; i < numberOfCardsToDraw; i++) {
       var currentCard = shuffledDeck.pop();
       playerCardDeck.push(currentCard);
     }
     console.log(playerCardDeck);
-    // assign 2 cards to player
+
+    // let's go into each array and add the score
   }
   var myOutputValue = 1;
   return myOutputValue;
 };
+
+// 20230607 decided not to use this function
+// var assignTwoCards = function () {
+//   numberOfCardsToDraw = 2;
+//   for (let i = 0; i < numberOfCardsToDraw; i++) {
+//     var currentCard = shuffledDeck.pop();
+//     // computerCardDeck.push(currentCard);
+//     return currentCard;
+//   }
+// };
 
 // Shuffle the elements in the cardDeck array
 var shuffleCards = function (cardDeck) {
