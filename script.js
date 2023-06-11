38935b8c4d662715cac59cfa2733d12030507dc6
junqi122 <junqi122@hotmail.com>
2023-06-11 21:25:47 +0800

    added player and dealer hit or stand

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var stage = "deal";
 var userCards = [];
 var computerCards = [];
 
+var myOutputValue = "";
+var userDrawValue = "You drew: <br>";
+var computerDrawValue = "Computer drew: <br>";
+
 var getRandomIndex = function (max) {
   return Math.floor(Math.random() * max);
 };
@@ -47,6 +51,10 @@ var makeDeck = function () {
       // By default, the card name is the same as rankCounter
       var cardName = rankCounter;
 
+      if (cardName > 10) {
+        var cardValue = 10;
+      } else cardValue = cardName;
+
       // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
       if (cardName == 1) {
         cardName = "ace";
@@ -74,6 +82,7 @@ var makeDeck = function () {
         suit: currentSuit,
         rank: rankCounter,
         emoji: suitEmoji,
+        value: cardValue,
       };
 
       // Add the new card to the deck
@@ -92,20 +101,117 @@ var makeDeck = function () {
 };
 var shuffledDeck = shuffleCards(makeDeck());
 
-var dealInitial = function () {
-  var i = 0;
-  while (i < 2) {
-    var userCard = shuffledDeck.pop();
-    var computerCard = shuffledDeck.pop();
-    computerCards.push(computerCard);
-    userCards.push(userCard);
-    i++;
+var dealCards = function (cardDeck, drawValue) {
+  var dealtCard = shuffledDeck.pop();
+  cardDeck.push(dealtCard);
+  var currIndex = cardDeck.length - 1;
+  drawValue =
+    drawValue +
+    ` ${cardDeck[currIndex].name} ${cardDeck[currIndex].emoji} <br>`;
+  return drawValue;
+};
+
+var blackJack = function (inputCards) {
+  if (inputCards[0].value == 10 && inputCards[1].rank == 1) {
+    return true;
+  }
+  if (inputCards[1].value == 10 && inputCards[0].rank == 1) {
+    return true;
   }
 };
 
+var sumCards = function (inputCards) {
+  var sum = 0;
+  for (i = 0; i < inputCards.length; i += 1) {
+    sum = sum + inputCards[i].value;
+  }
+  return sum;
+};
+
+var computerPlay = function () {
+  while (sumCards(computerCards) < 17) {
+    computerDrawValue = dealCards(computerCards, computerDrawValue);
+  }
+};
+
+var winLose = function () {};
+
 var main = function (input) {
   if (stage == "deal") {
-    dealInitial();
-    return `You drew: <br> ${userCards[0].name} ${userCards[0].emoji} <br> ${userCards[1].name} ${userCards[1].emoji} <br><br> Input 'hit' or 'stand' to continue.`;
+    for (i = 0; i < 2; i += 1) {
+      userDrawValue = dealCards(userCards, userDrawValue);
+      computerDrawValue = dealCards(computerCards, computerDrawValue);
+    }
+
+    if (blackJack(userCards) && blackJack(computerCards)) {
+      myOutputValue =
+        userDrawValue + computerDrawValue`<br>2 BLACJACKS!! You tied!`;
+    } else if (blackJack(userCards)) {
+      myOutputValue = userDrawValue + `<br>BLACJACK!! You win!`;
+    } else if (blackJack(computerCards)) {
+      myOutputValue = userDrawValue + `<br>COMPUTER BLACKJACK. You lose ):`;
+    } else {
+      stage = "draw";
+      myOutputValue = userDrawValue + `<br>Input 'hit' or 'stand' to continue.`;
+    }
+    return myOutputValue;
+  }
+
+  if (stage == "draw") {
+    input = input.trim().toLowerCase();
+    if (input == "hit") {
+      userDrawValue = dealCards(userCards, userDrawValue);
+
+      if (sumCards(userCards) > 21) {
+        computerPlay();
+        if (sumCards(computerCards) > 21) {
+          myOutputValue =
+            userDrawValue +
+            "<br>" +
+            computerDrawValue +
+            "<br>" +
+            `BOTH BUST! It's a tie`;
+        } else {
+          myOutputValue =
+            userDrawValue +
+            "<br>" +
+            computerDrawValue +
+            "<br>" +
+            `BUST! You lose ):`;
+        }
+      } else {
+        myOutputValue =
+          userDrawValue + "<br>" + `Input 'hit' or 'stand' to continue.`;
+      }
+    } else if (input == "stand") {
+      computerPlay();
+      if (sumCards(computerCards) > 21) {
+        myOutputValue =
+          userDrawValue +
+          "<br>" +
+          computerDrawValue +
+          "<br>" +
+          `Computer BUST! You win!`;
+      } else if (sumCards(computerCards) > sumCards(userCards)) {
+        myOutputValue =
+          userDrawValue +
+          "<br>" +
+          computerDrawValue +
+          "<br>" +
+          `You: ${sumCards(userCards)} <br> Computer:${sumCards(
+            computerCards
+          )}. <br> Computer wins!`;
+      } else {
+        myOutputValue =
+          userDrawValue +
+          "<br>" +
+          computerDrawValue +
+          "<br>" +
+          `You: ${sumCards(userCards)} <br> Computer:${sumCards(
+            computerCards
+          )}. <br> You win!`;
+      }
+    } else return `Input 'hit' or 'stand' to continue.`;
+    return myOutputValue;
   }
 };
