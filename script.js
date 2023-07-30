129a7ea2dd0afe7e5638761f96d7b58b1c562a38
Mannushka <zaimar1910@gmail.com>
2023-07-30 20:30:35 +0800

    version 1: Dealer and player get 2 cards per hand

--- script.js
+++ script.js
@@ -7,6 +7,11 @@
 // The player who is closer to, but not above 21 wins the hand.
 
 //Clobal variables
+
+var GAME_START = "start the game";
+var HIT_OR_STAND = "hit or stand after the first hand is dealt";
+var SHOW_SCORE = "show the total hands and declare the winner";
+var gameState = GAME_START;
 var HEARTS = "♥️";
 var DIAMONDS = "♦️";
 var SPADES = "♠️";
@@ -15,19 +20,23 @@ var ACE = "ace";
 var JACK = "jack";
 var QUEEN = "queen";
 var KING = "king";
+var deck = []; //array for the card deck
+console.log(deck);
+var playerHand = [];
+var dealerHand = [];
 
 //1. create a card deck (helper function)
 var createDeck = function () {
-  var deck = []; //array for the card deck
-  console.log(deck);
   // 4 suits
   var suits = [HEARTS, DIAMONDS, SPADES, CLUBS];
   //create a loop so for each suit there's 13 cards of different rank
   var suitsCounter = 0;
   while (suitsCounter < 4) {
+    console.log("suitsCounter", suitsCounter);
     var currentSuit = suits[suitsCounter];
     var rankCounter = 1;
     while (rankCounter <= 13) {
+      console.log("rank counter", rankCounter);
       var cardName = rankCounter;
       if (rankCounter === 1) {
         cardName = ACE;
@@ -48,7 +57,6 @@ var createDeck = function () {
       };
       console.log("card", card);
       deck.push(card);
-
       rankCounter = rankCounter + 1;
     }
 
@@ -66,11 +74,14 @@ var getRandomIndex = function (max) {
 var shuffleCards = function (deck) {
   //we need to swap the current card with a random card
   var currentIndex = 0;
-  var randomIndex = getRandomIndex(deck.length);
-  console.log("random index", randomIndex);
   while (currentIndex < deck.length) {
+    console.log("currentIndex", currentIndex);
+    var randomIndex = getRandomIndex(deck.length);
+    console.log("random index", randomIndex);
     var currentCard = deck[currentIndex];
     var randomCard = deck[randomIndex];
+    console.log(currentCard);
+    console.log(randomCard);
     deck[currentIndex] = randomCard;
     deck[randomIndex] = currentCard;
     console.log("random card", randomCard);
@@ -79,9 +90,49 @@ var shuffleCards = function (deck) {
   }
   return deck;
 };
+//Game mode = GAME_START
+//5. Deal the cards:
+//Player gets 2 cards
+//Computer gets 2 cards
 
 var main = function (input) {
-  var shuffledDeck = shuffleCards(createDeck());
-  var randomCard = shuffledDeck[getRandomIndex(shuffledDeck.length)];
-  return randomCard.name + " of " + randomCard.suit;
+  var myOutputMessage = "";
+  if (gameState === GAME_START) {
+    var cardDeck = shuffleCards(createDeck());
+    index = 0;
+    while (index < 2) {
+      playerHand.push(cardDeck.pop());
+      console.log("player hand: ", playerHand);
+      dealerHand.push(cardDeck.pop());
+      console.log("dealer hand: ", dealerHand);
+      index = index + 1;
+    }
+    myOutputMessage =
+      "You got a " +
+      playerHand[0].name +
+      " of " +
+      playerHand[0].suit +
+      " and a " +
+      playerHand[1].name +
+      " of " +
+      playerHand[0].suit +
+      "." +
+      "<br>" +
+      "One card in Dealer's hand is a " +
+      dealerHand[0].name +
+      " of " +
+      dealerHand[0].suit +
+      "." +
+      "<br>" +
+      "Now key in 'hit' if you'd like one more card or 'stand' if you think it's enough.";
+  }
+  return myOutputMessage;
 };
+
+////4. draw a random card (helper function)
+// var dealCards = function () {
+// var shuffledDeck = shuffleCards(createDeck());
+// var randomCard = shuffledDeck[getRandomIndex(shuffledDeck.length)];
+// var cardInHand = randomCard.name + " of " + randomCard.suit;
+// console.log("Card drawn: ", cardInHand);
+// return cardInHand;
