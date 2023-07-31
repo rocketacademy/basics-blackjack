5dacf1c910b03b1447892b302831ba746de6230d
arwinzgreato <arwindersingh88@gmail.com>
2023-07-31 10:43:56 +0800

    Initialised game to display intro message and deal first two cards

--- script.js
+++ script.js
@@ -1,4 +1,114 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+///Helper Functions
+//Make a deck of cards
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 2;
+    while (rankCounter <= 14) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+      var cardScore = rankCounter;
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 14) {
+        cardName = "ace";
+        cardScore = 11;
+      } else if (cardName == 11) {
+        cardName = "jack";
+        cardScore = 10;
+      } else if (cardName == 12) {
+        cardName = "queen";
+        cardScore = 10;
+      } else if (cardName == 13) {
+        cardName = "king";
+        cardScore = 10;
+      }
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+        score: cardScore,
+      };
+      // Add the new card to the deck
+      cardDeck.push(card);
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+  // Return the completed card deck
+  return cardDeck;
 };
+//Shuffle a deck of cards
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+//Generate a random number ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+//Deal first two cards to player and dealer
+var dealFirstTwoCards = function () {
+  playerCards.push(shuffledDeck.pop());
+  dealerCards.push(shuffledDeck.pop());
+  playerCards.push(shuffledDeck.pop());
+  dealerCards.push(shuffledDeck.pop());
+};
+
+///Global Variables
+var INTRO_MESSAGE = "INTRO_MESSAGE";
+var DEAL_CARDS = "DEAL_CARDS";
+var CHECK_FOR_BLACKJACK = "CHECK_FOR_BLACKJACK";
+var playerCards = [];
+var dealerCards = [];
+var gamemode = INTRO_MESSAGE;
+var shuffledDeck = shuffleCards(makeDeck());
+
+var main = function (input) {
+  // Display intro message to ask Player to click Submit button to deal cards
+  if (gamemode === INTRO_MESSAGE) {
+    console.log(`Current Game Mode: ${gamemode}`);
+    outputMessage = `Welcome! Click the Submit button below to deal the cards! Good Luck!`;
+    gamemode = DEAL_CARDS;
+    return outputMessage;
+  }
+  //Deal cards for Player and Dealer
+  if (gamemode === DEAL_CARDS) {
+    console.log(`Current Game Mode: ${gamemode}`);
+    dealFirstTwoCards();
+    console.log(playerCards);
+  }
+};
+// Check for blackjack, if anyone has blackjack, display winner and restart game
+// Display cards to user
+// The user decides whether to hit or stand, using the submit button to submit their choice.
+// The user's cards are analysed for winning or losing conditions.
+// The computer decides to hit or stand automatically based on game rules.
+// The game either ends or continues.
