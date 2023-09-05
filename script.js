00f2e7006b8d7f3460e3c6f0b95052615e32a6c9
ianlauyin <ianlauyin@gmail.com>
2023-09-05 21:18:22 +0800

    cal Value Completed

--- script.js
+++ script.js
@@ -1,56 +1,124 @@
+//3hours
+
+//6 people at most
+//Add user/computer before play
+//After hitting to play button
+//computer will automaticlly hit/stand.(no button can be pressed)
+//After each round you can choose quit/play again.
+
+//Game button flow (Completed)
+//Play button, add player , add computer, delete player
+//bet hit, stand
+//play again, quit
+
+//Logic/Function Needed
+//player name validation(Complete)
+//Add player(Complete)
+//Delete Player(Complete)
+//hit (complete)
+//stand (complete)
+//Coins select(complete)
+//Check if the user over 21 or not
+
+//Comparing result
+//End Game Cal
+//hit/stand for computer awaits
+// setTimeout(() => {
+//   output.innerHTML = result;
+// }, 5000);
+
 var deck = [];
-//player contains name,coins,stand-check and array of cards.
-var player = [];
+//player contains name,coins,stand-check,value and array of cards.
+//player[0] is the dealer(computer)
+var player = [
+  {
+    stand: false,
+    card: [],
+    value: 0,
+  },
+];
 var playerRound = 0;
-var gameOn = false;
 
-var main = function (input) {
-  createDeck();
-  shuffleDeck();
-
-  submit.style.visibility = "hidden";
-  gameOn = true;
-  return `Game On`;
-};
-
-var addPlayer = function (input) {
-  let name = input;
+var addPlayer = function (playerName) {
+  if (player.length == 6) {
+    return `At most 6 player`;
+  }
   let playerInfo = {
-    name: name,
+    name: playerName,
     coin: 100,
     stand: false,
     card: [],
+    value: 0,
   };
   player.push(playerInfo);
-  return `${name} added`;
+  return `${playerName} added`;
 };
 
-var addComputer = function () {
-  let nameTag = 1;
-  let name = `Computer ${nameTag}`;
-  for (let i = 1; i < player.length; i++) {
-    if (player[i].name == name) {
-      nameTag += 1;
-      name = `Computer ${nameTag}`;
+var playerDelete = function (deletePlayer) {
+  for (let i = 0; i < player.length; i++) {
+    if (deletePlayer == player[i].name) {
+      player.splice(i, 1);
+      return `${deletePlayer} deleted`;
     }
   }
-  addPlayer(name);
-  return `${name} Added`;
+  return `Player name ${deletePlayer} cannot be found`;
+};
+
+var main = function () {
+  if (player.length == 1) {
+    return `Please Add player first`;
+  }
+  createDeck();
+  shuffleDeck();
+
+  for (let i = 0; i < player.length; i++, playerRound += 1) {
+    dealCard();
+    dealCard();
+  }
+  playerRound = 1;
+
+  calValue();
+  return `Game On`;
+};
+
+var bet = function (input) {
+  if (player[playerRound].coin < input) {
+    return `you do not have enough coins`;
+  }
+  return `you bet ${input}`;
 };
 
 var hit = function () {
-  let cardDealt = dealCard();
-  player[playerRound].card.push(cardDealt);
+  dealCard();
+  calValue();
   return `Card Dealed`;
 };
 
 var stand = function () {
   player[0].stand = true;
+  playerRound += 1;
   return `Stand`;
 };
 
+var quit = function () {};
+
+var calValue = function () {
+  for (let i = 0; i < player[playerRound].card.length; i++) {
+    if (player[playerRound].card[i].rank >= 11) {
+      player[playerRound].value += 10;
+    } else {
+      player[playerRound].value += player[playerRound].card[i].rank;
+    }
+  }
+  console.log(player[playerRound].value);
+};
+
+var compareValue = function () {};
+
 var dealCard = function () {
-  return deck.pop();
+  cardDealt = deck.pop();
+  player[playerRound].card.push(cardDealt);
+  return;
 };
 
 var createDeck = function () {
