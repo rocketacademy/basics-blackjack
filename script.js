36443eef7f6454a8242b08178b4078bc5377e429
zwjryan <zwjryan@gmail.com>
2023-09-12 21:39:01 +0800

    updates updated for dealer to hit or stand automatically added function to reset game state fixed javascript Uncaught TypeError: Cannot read properties of undefined

--- script.js
+++ script.js
@@ -7,13 +7,14 @@
 // The cards are displayed to the user. - done
 // The user decides whether to hit or stand, using the submit button to submit their choice. - done
 // The user's cards are analysed for winning or losing conditions. - done
-// The computer decides to hit or stand automatically based on game rules.
-// The game either ends or continues.
+// The computer decides to hit or stand automatically based on game rules. - done
+// The game either ends or continues. - done
 
 // declare game state
 var gameState1 = "game start";
 var gameState2 = "cards dealt";
 var gameState3 = "hit or stand";
+var gameState4 = "game reset";
 var currentGameState = gameState1;
 
 // store player's and dealer's hand
@@ -21,7 +22,7 @@ var playerHand = [];
 var dealerHand = [];
 
 // initialize deck
-var deckConstruct = "";
+var deckConstruct = [];
 
 // a deck of cards
 var deckBuilder = function () {
@@ -93,20 +94,30 @@ var checkForBlackjack = function (card) {
   var cardIndexOne = card[0];
   var cardIndexTwo = card[1];
   var blackjack = false;
+  // check if cardIndexOne and cardIndexTwo are defined and have the 'name' and 'value' properties
   if (
-    (cardIndexOne.name == "ace" && cardIndexTwo.value >= 10) ||
-    (cardIndexOne.value >= 10 && cardIndexTwo.name == "ace")
+    cardIndexOne &&
+    cardIndexTwo &&
+    cardIndexOne.name &&
+    cardIndexOne.value &&
+    cardIndexTwo.name &&
+    cardIndexTwo.value
   ) {
-    blackjack = true;
+    if (
+      (cardIndexOne.name == "ace" && cardIndexTwo.value >= 10) ||
+      (cardIndexOne.value >= 10 && cardIndexTwo.name == "ace")
+    ) {
+      blackjack = true;
+    }
   }
   return blackjack;
 };
 
 // win by natural blackjack (21 points on first two cards)
 var blackjackWin = function () {
+  var outputMessage = "";
   var playerBlackjack = checkForBlackjack(playerHand);
   var dealerBlackjack = checkForBlackjack(dealerHand);
-  var outputMessage = "";
   if (playerBlackjack == true || dealerBlackjack == true) {
     if (playerBlackjack == true && dealerBlackjack == true) {
       outputMessage = `${showHand(playerHand, dealerHand)}blackjack draw<br>`;
@@ -133,14 +144,21 @@ var blackjackWin = function () {
 // sum up individual's hand
 var checkHandTotalValue = function (hand) {
   var handCounter = 0;
+  var aceCounter = 0;
   for (i = 0; i < hand.length; i += 1) {
     var j = hand[i];
     if (j.name == "jack" || j.name == "queen" || j.name == "king") {
       handCounter += 10;
     } else if (j.name == "ace") {
       handCounter += 11;
+      aceCounter += 1;
     } else {
       handCounter += j.value;
+    }
+  }
+  for (k = 0; k < aceCounter; k += 1) {
+    if (handCounter > 21) {
+      handCounter += -10;
     }
   }
   return handCounter;
@@ -149,6 +167,7 @@ var checkHandTotalValue = function (hand) {
 // win by hand value without exceeding 21 points
 var normalWin = function (input) {
   var outputMessage = "";
+  deckConstruct = shuffled();
   if (input == "z") {
     playerHand.push(deckConstruct.pop());
     outputMessage = `${showHand(
@@ -158,22 +177,35 @@ var normalWin = function (input) {
   } else if (input == "x") {
     var playerHandTotalValue = checkHandTotalValue(playerHand);
     var dealerHandTotalValue = checkHandTotalValue(dealerHand);
-    if (playerHandTotalValue == dealerHandTotalValue) {
+
+    while (dealerHandTotalValue < 17) {
+      dealerHand.push(deckConstruct.pop());
+      dealerHandTotalValue = checkHandTotalValue(dealerHand);
+    }
+
+    if (
+      playerHandTotalValue == dealerHandTotalValue ||
+      (playerHandTotalValue > 21 && dealerHandTotalValue > 21)
+    ) {
       outputMessage = `${showHand(
         playerHand,
         dealerHand
       )}it's a tie<br> ${showHandValue(
         playerHandTotalValue,
         dealerHandTotalValue
-      )}`;
-    } else if (playerHandTotalValue < dealerHandTotalValue) {
+      )}<br>click "submit" to reset game`;
+    } else if (
+      (playerHandTotalValue < dealerHandTotalValue &&
+        dealerHandTotalValue <= 21) ||
+      (playerHandTotalValue > 21 && dealerHandTotalValue <= 21)
+    ) {
       outputMessage = `${showHand(
         playerHand,
         dealerHand
       )}dealer wins<br> ${showHandValue(
         playerHandTotalValue,
         dealerHandTotalValue
-      )}`;
+      )}<br>click "submit" to reset game`;
     } else {
       outputMessage = `${showHand(
         playerHand,
@@ -181,8 +213,9 @@ var normalWin = function (input) {
       )}player wins<br> ${showHandValue(
         playerHandTotalValue,
         dealerHandTotalValue
-      )}`;
+      )}<br>click "submit" to reset game`;
     }
+    resetGame();
   } else {
     outputMessage = `input: "z" - hit or "x" - stand<br><br>${showHand(
       playerHand,
@@ -211,11 +244,19 @@ var showHandValue = function (player, dealer) {
   return returnOutput;
 };
 
+// reset game state
+var resetGame = function () {
+  currentGameState = gameState1;
+  playerHand = [];
+  dealerHand = [];
+  deckConstruct = [];
+};
+
 var main = function (input) {
   var myOutputValue = "";
   if (currentGameState == gameState1) {
     console.log(`gameState: 1`);
-    myOutputValue = dealCards();
+    outputMessage = dealCards();
     currentGameState = gameState2;
     console.log(`gameState: 2`);
   }
@@ -224,14 +265,14 @@ var main = function (input) {
     console.log(checkHandTotalValue(playerHand));
     console.log(checkForBlackjack(dealerHand));
     console.log(checkHandTotalValue(dealerHand));
-    myOutputValue += blackjackWin();
-    console.log(blackjackWin());
+    outputMessage = blackjackWin();
     currentGameState = gameState3;
+    console.log(blackjackWin());
     console.log(`gameState: 3`);
+    return outputMessage;
   }
   if (currentGameState == gameState3) {
-    myOutputValue += normalWin(input);
-    console.log(normalWin(input));
+    return normalWin(input);
   }
   return myOutputValue;
 };
