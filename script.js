788b1bc3cdf6b0722a35939c61102ca63afeb299
jasperteo <jaspertzj@outlook.sg>
2023-10-21 17:17:52 +0800

    added function to construct and shuffle deck

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
+//Global Variables
+const deck = [];
+
+function main(input, myOutputValue) {
+  initDeck();
+  console.log(deck);
   return myOutputValue;
-};
+}
+
+function initDeck() {
+  const nameConstruct = [
+    "Ace",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King",
+  ];
+  const suits = ["Diamonds", "Clubs", "Hearts", "Spades"];
+  const indexName = Object.entries({ ...nameConstruct });
+  for (const [index, name] of indexName) {
+    for (const suit of suits) {
+      const card = { name: name, suit: suit, rank: Number(index) + 1 };
+      deck.push(card);
+    }
+  }
+}
+
+function shuffleDeck(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i + 1);
+    let holdIndex = deck[i];
+    deck[i] = deck[j];
+    deck[j] = holdIndex;
+  }
+}
+//Fisher-Yates Shuffle, Durstenfeld variation
+// function shuffle(array) {
+//   var m = array.length,
+//     t,
+//     i;
+//   // While there remain elements to shuffle…
+//   while (m) {
+//     // Pick a remaining element…
+//     i = Math.floor(Math.random() * m--);
+//     // And swap it with the current element.
+//     t = array[m];
+//     array[m] = array[i];
+//     array[i] = t;
+//   }
+//   return array;
+// }
