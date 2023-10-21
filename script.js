aca1e005a3438bfd26895af788c3c66a60db6fb1
jasperteo <jaspertzj@outlook.sg>
2023-10-21 18:28:09 +0800

    fixed bug in shuffle algo

--- script.js
+++ script.js
@@ -3,40 +3,44 @@ const deck = [];
 
 function main(input, myOutputValue) {
   initDeck();
-  console.log(deck);
+  shuffleDeck();
+  myOutputValue = printDeck();
   return myOutputValue;
 }
 
 function initDeck() {
-  const nameConstruct = [
-    "Ace",
-    "Two",
-    "Three",
-    "Four",
-    "Five",
-    "Six",
-    "Seven",
-    "Eight",
-    "Nine",
-    "Ten",
-    "Jack",
-    "Queen",
-    "King",
-  ];
-  const suits = ["Diamonds", "Clubs", "Hearts", "Spades"];
-  const indexName = Object.entries({ ...nameConstruct });
-  for (const [index, name] of indexName) {
-    for (const suit of suits) {
-      const card = { name: name, suit: suit, rank: Number(index) + 1 };
-      deck.push(card);
+  if (deck.length <= 0) {
+    const nameConstruct = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King",
+    ];
+    const suits = ["Diamonds", "Clubs", "Hearts", "Spades"];
+    const indexName = Object.entries({ ...nameConstruct });
+    for (const [index, name] of indexName) {
+      for (const suit of suits) {
+        const card = { Name: name, Suit: suit, Rank: Number(index) + 1 };
+        deck.push(card);
+      }
     }
   }
 }
 
-function shuffleDeck(deck) {
-  for (let i = deck.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i + 1);
-    let holdIndex = deck[i];
+function shuffleDeck() {
+  let i, j, holdIndex;
+  for (i = deck.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * i);
+    holdIndex = deck[i];
     deck[i] = deck[j];
     deck[j] = holdIndex;
   }
@@ -57,3 +61,11 @@ function shuffleDeck(deck) {
 //   }
 //   return array;
 // }
+
+function printDeck() {
+  let output = "";
+  for (let i = 0; i < deck.length; i++) {
+    output += `${deck[i].Name} of ${deck[i].Suit}<br>`;
+  }
+  return output;
+}
