4dfd4fbe4f102cf692fefd8024ab9abb992efda3
jasperteo <jaspertzj@outlook.sg>
2023-10-29 01:48:08 +0800

    fixed the arrangements to make logic flow more clear

--- script.js
+++ script.js
@@ -39,54 +39,7 @@ function main(input, myOutputValue) {
   return myOutputValue;
 }
 
-function playSplitRound(input) {
-  if (handCounter < 2) {
-    switch (input) {
-      case "H":
-        hit(playersHands[handCounter]);
-        if (calculateScore(playersHands[handCounter + 1]) < 21) {
-          gameMessage = `Current Hand: ${handCounter}<br>${displayHand(
-            playersHands[handCounter]
-          )}<br><br>${displayHand(playersHands[0])}<br><br>${displayHand(
-            playersHands[1]
-          )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
-          break;
-        }
-      case "S":
-        handCounter++;
-        if (handCounter === 2) {
-          dealerAI();
-          gameMessage =
-            `${displayHand(playersHands[0])}` +
-            `<br><br>${gameEvaluation(
-              playersHands[0],
-              dealerHand
-            )} with this hand<br><br>` +
-            `${displayHand(playersHands[1])}` +
-            `<br><br>${gameEvaluation(
-              playersHands[1],
-              dealerHand
-            )} with this hand<br><br>${displayHand(dealerHand)}` +
-            `<br><br>Press submit to play another round`;
-          gameState = endingRound;
-          break;
-        }
-        gameMessage = `Current Hand: ${handCounter + 1}<br>${displayHand(
-          playersHands[handCounter]
-        )}<br><br>${displayHand(playersHands[0])}<br><br>${displayHand(
-          playersHands[1]
-        )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
-        break;
-      default:
-        gameMessage = `Invalid input!<br><br>${displayHand(
-          playersHands[handCounter]
-        )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
-    }
-  } else {
-  }
-}
-
-//Print array
+//Print any hand
 function print(hand) {
   let output = "";
   for (const card of hand) {
@@ -94,77 +47,29 @@ function print(hand) {
   }
   return output;
 }
-
+//Prints first card of dealer face up
 dealerFaceUp = () => `${dealerHand[0].Name} of ${dealerHand[0].Suit}<br>`;
-
-//Draw a card
-const hit = (hand) => hand.push(deck.pop());
-
-//Start round
-function initRound() {
-  for (let i = 0; i < 2; i++) {
-    hit(playersHands);
-    hit(dealerHand);
+//Helper function for hard values
+function hardScore(hand) {
+  let totalPoints = 0;
+  for (const card of hand) {
+    let point = card.Rank >= 10 ? 10 : card.Rank;
+    totalPoints += point;
   }
-  if (
-    calculateScore(playersHands) === 21 ||
-    calculateScore(dealerHand) === 21
-  ) {
-    gameMessage =
-      `${displayHand(playersHands)}<br><br>${displayHand(dealerHand)}` +
-      `<br><br>${gameEvaluation(playersHands, dealerHand)}` +
-      `<br>${bjEvaluation(playersHands, dealerHand)}`;
-    gameState = endingRound;
-  } else if (playersHands[0].Rank === playersHands[1].Rank) {
-    gameMessage = `${displayHand(
-      playersHands
-    )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>You have a pair of ${
-      playersHands[0].Name
-    }s! Type "Y" to Split, "H" to Hit and "S" to Stand`;
-    gameState = playingRound;
-  } else {
-    gameMessage = `${displayHand(
-      playersHands
-    )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
-    gameState = playingRound;
-  }
+  return totalPoints;
 }
-
-//Game flow for Hit and Stand
-function playNormalRound(input) {
-  if (playersHands[0].Rank === playersHands[1].Rank && input === "Y") {
-    playersHands = playersHands.map((card) => [card]);
-    hit(playersHands[0]);
-    hit(playersHands[1]);
-    gameMessage = `${displayHand(playersHands[0])}<br><br>${displayHand(
-      playersHands[1]
-    )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand for first hand`;
-    gameState = playingSplitRound;
-  } else {
-    switch (input) {
-      case "H":
-        hit(playersHands);
-        if (calculateScore(playersHands) < 21) {
-          gameMessage = `${displayHand(
-            playersHands
-          )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
-          break;
-        }
-      case "S":
-        dealerAI();
-        gameMessage =
-          `${displayHand(playersHands)}<br><br>${displayHand(dealerHand)}` +
-          `<br><br>${gameEvaluation(playersHands, dealerHand)}` +
-          `<br><br>Press submit to play another round`;
-        gameState = endingRound;
-        break;
-      default:
-        gameMessage = `Invalid input!<br><br>${displayHand(
-          playersHands
-        )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
-    }
-  }
-}
+//Calculate points of current hand
+const calculateScore = (hand) =>
+  !hand.some((card) => card.Rank === 1)
+    ? hardScore(hand)
+    : hardScore(hand) > 11
+    ? hardScore(hand)
+    : hardScore(hand) + 10;
+//Display hand and score
+const displayHand = (hand) =>
+  hand == dealerHand
+    ? `Dealer's Hand:<br>${print(hand)}Score: ${calculateScore(hand)}`
+    : `Hand:<br>${print(hand)}Score: ${calculateScore(hand)}`;
 
 //Evaluation if draw
 const drawEvaluation = (individualPlayerHand, dealerHand) =>
@@ -179,10 +84,10 @@ const winEvaluation = (individualPlayerHand, dealerHand) =>
 //Evaluate game
 const gameEvaluation = (individualPlayerHand, dealerHand) =>
   drawEvaluation(individualPlayerHand, dealerHand)
-    ? "You tied!"
+    ? "You tied"
     : winEvaluation(individualPlayerHand, dealerHand)
-    ? "You won!"
-    : "You lost!";
+    ? "You won"
+    : "You lost";
 //Evaluate Blackjack
 const bjEvaluation = (individualPlayerHand, dealerHand) =>
   drawEvaluation(individualPlayerHand, dealerHand)
@@ -191,11 +96,137 @@ const bjEvaluation = (individualPlayerHand, dealerHand) =>
     ? "Player has Blackjack"
     : "Dealer has Blackjack";
 
-//Fisher-Yates Shuffle, Durstenfeld variation
-function shuffleDeck() {
-  for (let i = deck.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [deck[i], deck[j]] = [deck[j], deck[i]];
+//Start round
+function initRound() {
+  for (let i = 0; i < 2; i++) {
+    hit(playersHands);
+    hit(dealerHand);
+  }
+  if (
+    calculateScore(playersHands) === 21 ||
+    calculateScore(dealerHand) === 21
+  ) {
+    gameMessage =
+      `Your ${displayHand(playersHands)}<br><br>${displayHand(dealerHand)}` +
+      `<br><br>${gameEvaluation(playersHands, dealerHand)}` +
+      `<br>${bjEvaluation(playersHands, dealerHand)}`;
+    gameState = endingRound;
+  } else if (playersHands[0].Rank === playersHands[1].Rank) {
+    gameMessage = `Your ${displayHand(
+      playersHands
+    )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>You have a pair of ${
+      playersHands[0].Name
+    }s! Type "Y" to Split, "H" to Hit and "S" to Stand`;
+    gameState = playingRound;
+  } else {
+    gameMessage = `Your ${displayHand(
+      playersHands
+    )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
+    gameState = playingRound;
+  }
+}
+
+//Game flow for Hit and Stand and Split
+function playNormalRound(input) {
+  if (playersHands[0].Rank === playersHands[1].Rank && input === "Y") {
+    playersHands = playersHands.map((card) => [card]);
+    hit(playersHands[0]);
+    hit(playersHands[1]);
+    gameMessage = `First ${displayHand(
+      playersHands[0]
+    )}<br><br>Second ${displayHand(
+      playersHands[1]
+    )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand for first hand`;
+    gameState = playingSplitRound;
+  } else {
+    switch (input) {
+      case "H":
+        hit(playersHands);
+        if (calculateScore(playersHands) < 21) {
+          gameMessage = `Your ${displayHand(
+            playersHands
+          )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
+          break;
+        }
+      case "S":
+        dealerAI();
+        gameMessage =
+          `Your ${displayHand(playersHands)}<br><br>${displayHand(
+            dealerHand
+          )}` +
+          `<br><br>${gameEvaluation(playersHands, dealerHand)}` +
+          `<br><br>Press submit to play another round`;
+        gameState = endingRound;
+        break;
+      default:
+        gameMessage = `Invalid input!<br><br>Your ${displayHand(
+          playersHands
+        )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
+    }
+  }
+}
+
+//Game flow when player has 2 hands
+function playSplitRound(input) {
+  switch (input) {
+    case "H":
+      hit(playersHands[handCounter]);
+      if (calculateScore(playersHands[handCounter]) < 21) {
+        gameMessage = `Curent ${displayHand(
+          playersHands[handCounter]
+        )}<br><br>First ${displayHand(
+          playersHands[0]
+        )}<br><br>Second ${displayHand(
+          playersHands[1]
+        )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
+        break;
+      }
+    case "S":
+      handCounter++;
+      if (handCounter === 2) {
+        dealerAI();
+        gameMessage =
+          `First ${displayHand(playersHands[0])}` +
+          `<br><br>${gameEvaluation(
+            playersHands[0],
+            dealerHand
+          )} with first hand<br><br>` +
+          `Second ${displayHand(playersHands[1])}` +
+          `<br><br>${gameEvaluation(
+            playersHands[1],
+            dealerHand
+          )} with second hand<br><br>${displayHand(dealerHand)}` +
+          `<br><br>Press submit to play another round`;
+        gameState = endingRound;
+        break;
+      } else {
+        gameMessage = `Curent ${displayHand(
+          playersHands[handCounter]
+        )}<br><br>First ${displayHand(
+          playersHands[0]
+        )}<br><br>Second ${displayHand(
+          playersHands[1]
+        )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
+      }
+      break;
+    default:
+      gameMessage = `Invalid input!<br><br>Curent ${displayHand(
+        playersHands[handCounter]
+      )}<br><br>First ${displayHand(
+        playersHands[0]
+      )}<br><br>Second ${displayHand(
+        playersHands[1]
+      )}<br><br>Dealer's Face up:<br>${dealerFaceUp()}<br>Type "H" to Hit and "S" to Stand`;
+  }
+}
+
+//Draw a card
+const hit = (hand) => hand.push(deck.pop());
+
+//Dealer hits soft 17, H17 rules
+function dealerAI() {
+  while (calculateScore(dealerHand) < 17 || hardScore(dealerHand) + 10 <= 17) {
+    hit(dealerHand);
   }
 }
 
@@ -208,34 +239,11 @@ function resetRound() {
   shuffleDeck();
 }
 
-//Calculate points of current hand
-const calculateScore = (hand) =>
-  !hand.some((card) => card.Rank === 1)
-    ? hardScore(hand)
-    : hardScore(hand) > 11
-    ? hardScore(hand)
-    : hardScore(hand) + 10;
-
-//Helper function for hard values
-function hardScore(hand) {
-  let totalPoints = 0;
-  for (const card of hand) {
-    let point = card.Rank >= 10 ? 10 : card.Rank;
-    totalPoints += point;
-  }
-  return totalPoints;
-}
-
-//Display hand and score
-const displayHand = (hand) =>
-  hand == dealerHand
-    ? `Dealer's Hand:<br>${print(hand)}Score: ${calculateScore(hand)}`
-    : `Player's Hand:<br>${print(hand)}Score: ${calculateScore(hand)}`;
-
-//Dealer hits soft 17, H17 rules
-function dealerAI() {
-  while (calculateScore(dealerHand) < 17 || hardScore(dealerHand) + 10 <= 17) {
-    hit(dealerHand);
+//Fisher-Yates Shuffle, Durstenfeld variation
+function shuffleDeck() {
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
   }
 }
 
