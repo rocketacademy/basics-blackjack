4efae2dc1cbda608bfd8c280a77d5e888d31f5cc
ryx19 <ruiyangchia.xry@gmail.com>
2023-12-11 18:23:48 +0800

    ver 1, deck creation, random number, shuffle deck, basic draw 2 cards logic. Next ver to work on deck creation to be enhanced to take in values of picture cards = 10 and Ace = 1 || 11

--- script.js
+++ script.js
@@ -1,4 +1,128 @@
+/*
+Gameplay Description
+The main function runs on each player's turn. The sequence of actions in the game might be the following.
+Deck is shuffled.
+User clicks Submit to deal cards.
+The cards are analysed for game winning conditions, e.g. Blackjack.
+The cards are displayed to the user.
+The user decides whether to hit or stand, using the submit button to submit their choice.
+The user's cards are analysed for winning or losing conditions.
+The computer decides to hit or stand automatically based on game rules.
+The game either ends or continues.
+*/
+
+// GLOBAL STATES
+var gameState = "drawCards";
+var gameStatePlayerHit = "playerHit";
+var gameStateComputerHit = "computerHit";
+var playerCards = [];
+var computerCards = [];
+
+// HELPER FUNCTIONS
+
+// Function that makes the deck
+var makeDeck = function () {
+  // Initialise an empty deck array
+  var cardDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  var suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    var rankCounter = 1;
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "Ace";
+      } else if (cardName == 11) {
+        cardName = "Jack";
+      } else if (cardName == 12) {
+        cardName = "Queen";
+      } else if (cardName == 13) {
+        cardName = "King";
+      }
+
+      // Create a new card with the current name, suit, and rank
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+
+// Function that shuffles the deck
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+var getRandomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+// Shuffle the elements in the cardDeck array
+var shuffleCards = function (cardDeck) {
+  // Loop over the card deck array once
+  var currentIndex = 0;
+  while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
+    var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
+    var randomCard = cardDeck[randomIndex];
+    // Select the card that corresponds to currentIndex
+    var currentCard = cardDeck[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cardDeck[currentIndex] = randomCard;
+    cardDeck[randomIndex] = currentCard;
+    // Increment currentIndex
+    currentIndex = currentIndex + 1;
+  }
+  // Return the shuffled deck
+  return cardDeck;
+};
+
+var deck = makeDeck();
+console.log(`Make deck`);
+
+// MAIN FUNCTION
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  console.log(`Shuffle deck`);
+  var shuffledDeck = shuffleCards(deck);
+
+  var myOutputValue = "";
+
+  // Start game to draw the cards
+  if (gameState == "drawCards") {
+    while (playerCards < 2) {
+      playerCards.push(shuffledDeck.pop());
+      computerCards.push(shuffledDeck.pop());
+      playerCards.push(shuffledDeck.pop());
+      computerCards.push(shuffledDeck.pop());
+
+      console.log(`Deal player and computer cards`);
+      myOutputValue = `Player hand: ${playerCards[0].name} of ${playerCards[0].suit}, ${playerCards[1].name} of ${playerCards[1].suit} <br> Computer hand: ${computerCards[0].name} of ${computerCards[0].suit}, ${computerCards[1].name} of ${computerCards[1].suit} `;
+    }
+  }
+
   return myOutputValue;
 };
