f57091370747bb5bcc5892f58fd4648b8cc003e8
zhdzd <zahidabdullah18@gmail.com>
2023-12-12 21:20:38 +0800

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+/*
+Base
+Gameplay Description
+The main function runs on each player's turn. The sequence of actions in the game might be the following.
+Deck is shuffled.
+User clicks Submit to deal cards.
+The cards are analysed for game winning conditions, e.g. Blackjack.
+The cards are displayed to the user.
+The user decides whether to hit or stand, using the submit button to submit their choice.
+The user's cards are analysed for winning or losing conditions.
+The computer decides to hit or stand automatically based on game rules.
+The game either ends or continues.
+*/
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
